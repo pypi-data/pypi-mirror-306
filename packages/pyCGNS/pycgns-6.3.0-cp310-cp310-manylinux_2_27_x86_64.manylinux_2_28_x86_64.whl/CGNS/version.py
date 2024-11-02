584e83108e945a4edcf4b140bbba31774da88858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:19 +0000

    Add package pyCGNS/6.3.0/pycgns-6.3.0-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pyCGNS/pycgns-6.3.0-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/CGNS/version.py
@@ -0,0 +1,8 @@
+
+version = "6.3.0"
+__version__ = version
+full_version = version
+
+git_revision = "e5d7fc971dfea5aa5733572dda86f983098c46cb"
+release = 'dev' not in version and '+' not in version
+short_version = version.split("+")[0]
