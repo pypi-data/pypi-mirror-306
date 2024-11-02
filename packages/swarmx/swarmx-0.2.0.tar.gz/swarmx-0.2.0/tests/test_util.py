c803a6bdfbcbbb2b7a8b9cc931aa0d7d33f1f36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:04:45 +0000

    Add package swarmx/0.2.0/swarmx-0.2.0.tar.gz

---
+++ packages/swarmx/swarmx-0.2.0.tar.gz/swarmx-0.2.0/tests/test_util.py
@@ -0,0 +1,60 @@
+import pytest
+from pydantic import ValidationError
+
+from swarmx import ChatCompletionMessageToolCall, Function, Tool, handle_tool_calls
+
+
+def test_basic_tool_call():
+    def test_tool(arg1, arg2):
+        return f"Tool called with {arg1} and {arg2}"
+
+    response = handle_tool_calls(
+        [
+            ChatCompletionMessageToolCall(
+                id="mock_tc_id",
+                type="function",
+                function=Function(
+                    name="test_tool",
+                    arguments='{"arg1": "value1", "arg2": "value2"}',
+                ),
+            )
+        ],
+        [Tool(test_tool)],
+        {},
+    )
+    assert len(response.messages) == 1
+    assert response.messages[0]["content"] == "Tool called with value1 and value2"
+
+    invalid_function = Function(
+        name="test_tool",
+        arguments="{}",
+    )
+    with pytest.raises(ValidationError):
+        response = handle_tool_calls(
+            [
+                ChatCompletionMessageToolCall(
+                    id="mock_tc_id",
+                    type="function",
+                    function=invalid_function,
+                )
+            ],
+            [Tool(test_tool)],
+            {},
+        )
+
+
+def test_function_to_openai_tool():
+    def print_account_details(context_variables: dict):
+        user_id = context_variables.get("user_id", None)
+        name = context_variables.get("name", None)
+        return f"Account Details: {name} {user_id}"
+
+    t = Tool(print_account_details)
+    assert t.json() == {
+        "function": {
+            "description": "",
+            "name": "print_account_details",
+            "parameters": {"properties": {}, "type": "object"},
+        },
+        "type": "function",
+    }
