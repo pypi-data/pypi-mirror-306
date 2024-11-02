14ee2264d7b82aca1dce8ebf7fd998b8cc1cf61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 14:03:31 +0000

    Add package agentp/0.0.1/agentp-0.0.1.tar.gz

---
+++ packages/agentp/agentp-0.0.1.tar.gz/agentp-0.0.1/agentp/__init__.py
@@ -0,0 +1,6 @@
+from .Project import Project 
+from .Static import Paths, Scripts, Models, TAB, MSG
+from .Genotypes import BGEN
+from .GWAS import GWAS
+from .GREX import GREX
+from .TWAS import SummaryTWAS, GenotypeTWAS
