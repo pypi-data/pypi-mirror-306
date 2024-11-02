a1e8b569abcf9639aafe404281ee854d4047428d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:35 +0000

    Add package zcc-helper/3.2.2/zcc-helper-3.2.2.tar.gz

---
+++ packages/zcc-helper/zcc-helper-3.2.2.tar.gz/zcc-helper-3.2.2/setup.py
@@ -0,0 +1,26 @@
+"""A setuptools based setup module."""
+
+import pathlib
+from setuptools import setup, find_packages
+
+from zcc.constants import NAME, VERSION
+
+
+# The text of the README file
+README = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description='ZIMI ZCC helper module',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Mark Hannon',
+    author_email='mark.hannon@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[''],
+)
