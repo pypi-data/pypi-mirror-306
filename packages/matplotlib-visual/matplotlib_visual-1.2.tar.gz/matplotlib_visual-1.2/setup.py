0a09f6491e475b994f5e363f8de9c6d0f770f1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:03:38 +0000

    Add package matplotlib-visual/1.2/matplotlib_visual-1.2.tar.gz

---
+++ packages/matplotlib-visual/matplotlib_visual-1.2.tar.gz/matplotlib_visual-1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="matplotlib-visual",
+    version="v1.2",
+    packages=find_packages(),
+    description="",
+    author="mcafin31",
+    author_email="johnmecafin31@gmail.com",
+    license="GPL 3.0",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
