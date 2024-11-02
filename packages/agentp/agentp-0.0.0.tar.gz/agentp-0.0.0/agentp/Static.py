4407e6d39da5576694bede9fa86f4fcd70c3f3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 14:04:08 +0000

    Add package agentp/0.0.0/agentp-0.0.0.tar.gz

---
+++ packages/agentp/agentp-0.0.0.tar.gz/agentp-0.0.0/agentp/Static.py
@@ -0,0 +1,86 @@
+'''
+File containing static paths and variables. 
+
+- Nhung, Sept 2024
+'''
+
+import os
+
+class Paths: 
+
+    ## directory that this software/repo is in
+    ## not necessarily the same as user's current path 
+    MAIN = os.path.dirname(os.path.abspath(__file__)) 
+
+    SOFTWARE = f'{MAIN}/software'
+    MODELS = f'{MAIN}/models' 
+
+class Scripts: 
+
+    PLINK2 = f'{Paths.SOFTWARE}/plink2'
+    BGENIX = f'{Paths.SOFTWARE}/bgen/build/apps/bgenix'
+    GWAS = f'{Paths.SOFTWARE}/regenie/regenie'
+
+    SUMMARY_TWAS = f'{Paths.SOFTWARE}/MetaXcan/software/SPrediXcan.py'
+    GENOTYPE_TWAS = f'{Paths.SOFTWARE}/MetaXcan/software/PrediXcanAssociation.py'
+    GREX_PREDICTION = f'{Paths.SOFTWARE}/MetaXcan/software/Predict.py'
+
+    FUS_LDSC = f'{Paths.SOFTWARE}/ldsc/munge_sumstats.py'
+    FUS_TWAS = f'{Paths.SOFTWARE}/fusion_twas/FUSION.assoc_test.R'
+
+class Models: 
+
+    rlabel = ['dlpfc', 'ant-cingulate', 'amygdala', 'hippocampus', \
+              'caudate', 'putamen', 'nuc-accumbens', 'cerebellar-hemi']
+
+    tissue = ['Frontal_Cortex_BA9', 'Anterior_cingulate_cortex_BA24', \
+              'Amygdala', 'Hippocampus', 'Caudate_basal_ganglia', \
+              'Putamen_basal_ganglia', 'Nucleus_accumbens_basal_ganglia', \
+              'Cerebellar_Hemisphere']
+
+    REGION = {r:t for r,t in zip(rlabel, tissue)}
+
+    method = ['PDX', 'FUS', 'UTM', 'JTI']
+    folder = ['PrediXcan', 'FUSION', 'UTMOST_dzhou', 'JTI']
+    prefix = ['en_Brain', 'GTExv8.EUR.Brain', 'UTMOST_Brain', 'JTI_Brain']
+
+    FOLD_MAP = {m:f for m,f in zip(method, folder)}
+    PREF_MAP = {m:p for m,p in zip(method, prefix)} 
+    
+    def WEIGHT_DIR(method):
+        if method == 'FUS': 
+            return f'{Paths.MODELS}/FUSION/WEIGHTS'
+        else: 
+            fold = Models.FOLD_MAP[method] 
+            return f'{Paths.MODELS}/{fold}/weights'
+
+    def WEIGHT(method, region): 
+        fold = Models.FOLD_MAP[method] 
+        pref = Models.PREF_MAP[method]
+        rmod = Models.REGION[region]
+        if method == 'FUS': 
+            return f'{Paths.MODELS}/FUSION/WEIGHTS/{pref}_{rmod}.pos'
+        else: 
+            return f'{Paths.MODELS}/{fold}/weights/{pref}_{rmod}.db'
+
+    def COVARIANCE(method, region): 
+        fold = Models.FOLD_MAP[method] 
+        pref = Models.PREF_MAP[method]
+        rmod = Models.REGION[region]
+        return f'{Paths.MODELS}/{fold}/covariance/{pref}_{rmod}.txt.gz'
+
+    def SNPS(method): 
+        if method == 'FUS':
+            return f'{Paths.MODELS}/FUSION/snps'
+        else: 
+            return f'{Paths.MODELS}/snps_PDX_UTM_JTI'
+
+    def FUS_SCORES(region):
+        return f'{Paths.MODELS}/FUSION/scores/{region}'
+
+    FUS_LDREF = f'{Paths.MODELS}/FUSION/LDREF/1000G.EUR.'
+
+## string elements for logging 
+TAB = ' ' * 4
+MSG = '\n> '
+
---
+++ packages/agentp/agentp-0.0.0.tar.gz/agentp-0.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages 
+
+VERSION = '0.0.0'
+DESCRIPTION = 'Associating Gene Expression to Neuroimaging Traits Pipeline (AGENT-P)'
+REQ_PACKAGES = ['bgen_reader', 'h5py', 'numpy', 'pandas', 'patsy', \
+                'bitarray', 'scipy', 'statsmodels', 'pyliftover']
+
+setup(
+    name='agentp', 
+    version=VERSION, 
+    author='Nhung Hoang', 
+    author_email='nhung.hoang@vanderbilt.edu', 
+    url='https://github.com/nhunghoang/neurogen',
+    description=DESCRIPTION, 
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3', 
+        ],
+    packages=find_packages(),
+    install_requires=[
+        'bgen_reader>=4.0.8',
+        'h5py>=3.7.0',
+        'numpy>=1.23.5',
+        'pandas>=1.5.3',
+        'patsy>=0.5.6',
+        'bitarray>=3.0.0',
+        'scipy>=1.10.1',
+        'statsmodels>=0.14.1',
+        'pyliftover>=0.4.1', 
+        ], 
+) 
