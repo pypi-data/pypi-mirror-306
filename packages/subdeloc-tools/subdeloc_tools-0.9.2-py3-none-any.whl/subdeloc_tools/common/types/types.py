937b4f9522ff1d82cabd7da14655b4112885a1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:34 +0000

    Add package subdeloc-tools/0.9.2/subdeloc_tools-0.9.2-py3-none-any.whl

---
+++ packages/subdeloc-tools/subdeloc_tools-0.9.2-py3-none-any.whl/subdeloc_tools/common/types/types.py
@@ -0,0 +1,19 @@
+from typing import List, Dict, TypedDict
+
+# FFMPEG
+StreamsVar = List[dict]
+
+# Pair
+class IntervalVar(TypedDict):
+	start: int
+	end: int
+	text: str
+	nl: int
+
+class MatchesVar(TypedDict):
+	start: int
+	end: int
+	original: List[IntervalVar]
+	reference: List[IntervalVar]
+
+SubtitleSetVar = List[IntervalVar]
