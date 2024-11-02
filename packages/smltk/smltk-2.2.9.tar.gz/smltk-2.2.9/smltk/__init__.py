40b242cd57e5bf0a9a6e205c17ea6803a568cbe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:36 +0000

    Add package smltk/2.2.9/smltk-2.2.9.tar.gz

---
+++ packages/smltk/smltk-2.2.9.tar.gz/smltk-2.2.9/smltk/__init__.py
@@ -0,0 +1,20 @@
+"""Simple Machine Learning Tool Kit package
+
+This package contains the modules to simplify your code for your machine learning processes.
+
+It is part of the educational repositories (https://github.com/pandle/materials)
+to learn how to write stardard code and common uses of the TDD.
+
+Package contents two classes to manage data preprocessing, testing and reporting.
+
+    >>> import smltk
+    >>> help(smltk)
+    >>> import smltk.metrics as Metrics
+    >>> help(Metrics)
+
+# license MIT
+# support https://github.com/bilardi/smltk/issues
+"""
+
+__version__ = "2.2.9"
+__author__ = "Alessandra Bilardi"
