9e0efb33be9f40d3dea5d6d3870a9a85e5b8563a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:03:58 +0000

    Add package tanimachi/0.0.6/tanimachi-0.0.6-py3-none-any.whl

---
+++ packages/tanimachi/tanimachi-0.0.6-py3-none-any.whl/tanimachi/__init__.py
@@ -0,0 +1,11 @@
+from .schemas import Categories, Fingerprints, Groups, Har  # noqa: F401
+from .wappalyzer import (  # noqa: F401
+    Wappalyzer,
+    analyze_css,
+    analyze_dom,
+    analyze_headers,
+    analyze_html,
+    analyze_meta,
+    analyze_scripts,
+    analyze_url,
+)
---
+++ packages/tanimachi/tanimachi-0.0.6-py3-none-any.whl/tanimachi/schemas/__init__.py
@@ -0,0 +1,10 @@
+from .har import Entry, Har, Log, Page  # noqa: F401
+from .wappalyzer import (  # noqa: F401
+    Categories,
+    Category,
+    Fingerprint,
+    Fingerprints,
+    Group,
+    Groups,
+    Pattern,
+)
