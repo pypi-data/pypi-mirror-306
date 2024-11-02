535cfe2be750fbf8391219f4be11361e9daecfad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:03:55 +0000

    Add package mtmai/0.3.1349/mtmai-0.3.1349.tar.gz

---
+++ packages/mtmai/mtmai-0.3.1349.tar.gz/mtmai-0.3.1349/mtmai/api/thread.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+
+from mtmai.core.logging import get_logger
+from mtmai.crud import curd_chat
+from mtmai.deps import OptionalUserDep
+
+router = APIRouter()
+logger = get_logger()
+
+
+@router.get("/thread/{thread_id}/element/{element_id}")
+async def get_thread_element(
+    thread_id: str,
+    element_id: str,
+    user: OptionalUserDep,
+):
+    """Get a specific thread element."""
+    if not user:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    ok = await curd_chat.is_thread_author(user.id, thread_id)
+    if not ok:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    item = await curd_chat.get_chat_element(thread_id, element_id)
+    if not item:
+        raise HTTPException(status_code=404, detail="Element not found")
+    return JSONResponse(content=item)
