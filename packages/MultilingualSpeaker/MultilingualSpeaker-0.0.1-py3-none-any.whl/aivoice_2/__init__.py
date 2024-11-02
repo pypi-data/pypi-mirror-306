892e74d6e27c21b18364d1d4005b7d52b9642225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 16:03:39 +0000

    Add package MultilingualSpeaker/0.0.1/MultilingualSpeaker-0.0.1-py3-none-any.whl

---
+++ packages/MultilingualSpeaker/MultilingualSpeaker-0.0.1-py3-none-any.whl/aivoice_2/__init__.py
@@ -0,0 +1,22 @@
+from aivoice_2.ks_903_voice.KS_903_Vesion_VOICE import MultilingualSpeaker
+
+
+"""
+aivoice_2/
+ ├──voic.py                      # 指定の自作モジュールを実行するPYTHONファイル　実行させるには次の構成図と環境が必要です。
+ ├──requirements.txt             # 他の公開されている他人のライブラリーが必要な場合には「pip install requirements.txt」もしくは　「requirements.txt」の中身の内容について記載されているライブラリーが必要不可欠です。
+ └── ks_903_voice/
+        ├── __init__.py             # パッケージとして認識させるための空ファイル
+        ├── KS_903_Vesion_VOICE.py  # MultilingualSpeakerクラスが定義されているファイル
+        └── other_module.py          # その他のモジュール（必要に応じて追加）
+
+
+
+# aivoice_2
+# voic.py                      # 指定の自作モジュールを実行するPYTHONファイル　実行させるには次の構成図と環境が必要です。
+# requirements.txt             # 他の公開されている他人のライブラリーが必要な場合には「pip install requirements.txt」もしくは　「requirements.txt」の中身の内容について記載されているライブラリーが必要不可欠です。
+# └── ks_903_voice/
+#     ├── __init__.py          # パッケージとして認識させるための空ファイル
+#     C other_module.py      # その他のモジュール（必要に応じて追加）
+
+"""
