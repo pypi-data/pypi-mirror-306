96ab017cb86036adeefe3658d68b84a4cc1a0924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:03:42 +0000

    Add package i3ctools/0.0.0/i3ctools-0.0.0.tar.gz

---
+++ packages/i3ctools/i3ctools-0.0.0.tar.gz/i3ctools-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='i3ctools',
+    version='0.0.0',
+    description='Coming Soon',
+    long_description='',
+    url='https://www.binho.io',
+    author='Binho LLC',
+    author_email='support@binho.io',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
