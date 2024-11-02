e39ade2f4c24240e2621a98db3a86dac1323bfde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:08:50 +0000

    Add package mat3ra-made/2024.11.1.post0/mat3ra_made-2024.11.1.post0-py3-none-any.whl

---
+++ packages/mat3ra-made/mat3ra_made-2024.11.1.post0-py3-none-any.whl/mat3ra/made/tools/build/interface/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class StrainModes(str, Enum):
+    strain = "strain"
+    von_mises_strain = "von_mises_strain"
+    mean_abs_strain = "mean_abs_strain"
+
+
+class SupercellTypes(str, Enum):
+    hexagonal = "hexagonal"
+    orthogonal = "orthogonal"
