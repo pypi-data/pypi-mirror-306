41c57e41b7827bba337010cf46d6e2dc9d276a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:03:37 +0000

    Add package apachelabs/2.0.0/apachelabs-2.0.0-py3-none-any.whl

---
+++ packages/apachelabs/apachelabs-2.0.0-py3-none-any.whl/apachelabs/config.py
@@ -0,0 +1,7 @@
+# apachelabs/config.py
+
+MODEL_AGENT_MAP = {
+    "apachelm-v3": "ag:332133fc:20241102:apachelm-v3:de8afc4d",
+    "apachelm-v4": "ag:332133fc:20241102:apachelm-v4:18ea79ed",
+    "apachelm-v3.5": "ag:332133fc:20241102:apachelm-v3-5:1722c5ec"
+}
