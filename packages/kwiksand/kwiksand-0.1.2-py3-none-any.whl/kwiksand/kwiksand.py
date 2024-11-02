37cc255bdd39531b51aaee85314441686fbad546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:08:14 +0000

    Add package kwiksand/0.1.2/kwiksand-0.1.2-py3-none-any.whl

---
+++ packages/kwiksand/kwiksand-0.1.2-py3-none-any.whl/kwiksand/kwiksand.py
@@ -0,0 +1,22 @@
+import time
+import shutil
+
+def kwik(text, delay):
+    global txt
+    txt = []
+    if len(text) > shutil.get_terminal_size().columns:
+        print("ERR: Text too long!")
+    else:
+        txt.clear()
+        for l in text:
+            txt.append(l)
+        for i in range(len(txt)):
+            print(*txt[0:i], sep="", end='\r')
+            time.sleep(delay)
+        print(*txt[0:len(txt)], sep="")
+
+def kwiklist():
+    print(txt)
+
+def kwiklist_str():
+    print(*txt, sep="")
