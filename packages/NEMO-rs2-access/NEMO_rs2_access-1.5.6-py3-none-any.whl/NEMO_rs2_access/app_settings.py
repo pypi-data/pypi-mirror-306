6d87ad8ac0a4ed8be7f9ddc5abba2c88f18c1a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:08:14 +0000

    Add package NEMO-rs2-access/1.5.6/NEMO_rs2_access-1.5.6-py3-none-any.whl

---
+++ packages/NEMO-rs2-access/NEMO_rs2_access-1.5.6-py3-none-any.whl/NEMO_rs2_access/app_settings.py
@@ -0,0 +1,12 @@
+DEFAULT = {
+	"cardholder_sync": False,
+	"user_sync_field": "CardNumber",
+	"nemo_user_sync_field": "badge_number",
+	# 601 is access granted, 607 Access Granted Door Unlocked
+	"relevant_event_types" : [601, 607],
+	# we will store the date to restart as the last processed event minus this buffer
+	# this allows us to make sure we are not missing events between calls to all the separate readers
+	"buffer_in_seconds": 7,
+}
+
+RS2_ACCESS = {}
