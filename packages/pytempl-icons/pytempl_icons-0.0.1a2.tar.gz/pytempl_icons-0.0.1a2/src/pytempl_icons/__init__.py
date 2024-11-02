1913954232fa9e039edc06006cb9d0a70958c82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 14:03:47 +0000

    Add package pytempl-icons/0.0.1a2/pytempl_icons-0.0.1a2.tar.gz

---
+++ packages/pytempl-icons/pytempl_icons-0.0.1a2.tar.gz/pytempl_icons-0.0.1a2/src/pytempl_icons/__init__.py
@@ -0,0 +1,32 @@
+from .abstract import (
+    CheckIcon,
+    CirclePlusIcon,
+    CrossIcon,
+    DotFilledIcon,
+    ExclamationTriangleIcon,
+    HamburgerMenuIcon,
+    PlusIcon,
+    RefreshCWIcon,
+)
+from .arrows import ArrowLeftIcon, ArrowRightIcon, ChevronDownIcon
+from .objects import BanknoteIcon, EyeIcon, EyeOffIcon, PencilIcon, TrashIcon
+
+__version__ = "0.0.1a2"
+__all__ = [
+    "ArrowRightIcon",
+    "ArrowLeftIcon",
+    "BanknoteIcon",
+    "CheckIcon",
+    "ChevronDownIcon",
+    "CirclePlusIcon",
+    "CrossIcon",
+    "DotFilledIcon",
+    "ExclamationTriangleIcon",
+    "EyeIcon",
+    "EyeOffIcon",
+    "HamburgerMenuIcon",
+    "PencilIcon",
+    "PlusIcon",
+    "RefreshCWIcon",
+    "TrashIcon",
+]
