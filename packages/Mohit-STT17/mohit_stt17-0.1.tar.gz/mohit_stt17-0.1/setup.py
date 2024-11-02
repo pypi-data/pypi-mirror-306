ecc3e2d756328c27503fff5b13422ca30c5664ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:22 +0000

    Add package Mohit-STT17/0.1/mohit_stt17-0.1.tar.gz

---
+++ packages/Mohit-STT17/mohit_stt17-0.1.tar.gz/mohit_stt17-0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+
+long_description = ""
+if os.path.exists("README.md"):
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
+setup(
+    name='Mohit_STT17',
+    version='0.1',
+    author='Mohit Kumar',
+    author_email='megamohit2006@gmail.com',
+    description='This is a speech-to-text package created by Mohit Kumar',
+    long_description=long_description,  # Optional
+    long_description_content_type="text/markdown",  # Optional, if using Markdown for README
+    packages=find_packages(),
+    install_requires=[
+        'selenium',
+        'webdriver_manager'
+    ],
+    include_package_data=True,
+    package_data={
+        'Mohit_SpeechToText': ['index.html', 'script.js', 'style.css', 'input.txt'],  # Include all necessary files
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
