a449cfb398d22215437961f176e745c6575e2e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 05:03:24 +0000

    Add package econ-datareader/1.0.5/econ-datareader-1.0.5.tar.gz

---
+++ packages/econ-datareader/econ-datareader-1.0.5.tar.gz/econ-datareader-1.0.5/econdatareader/__init__.py
@@ -0,0 +1,5 @@
+
+from econdatareader.version import version as __version__
+from econdatareader.fred import FredDownloader
+from econdatareader.finance import FinanceDownloader
+from econdatareader.bok import BokDownloader
