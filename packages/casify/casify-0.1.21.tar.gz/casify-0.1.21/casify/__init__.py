e5134a4be4906813b6c1b7b03ad97265b148018c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:07:56 +0000

    Add package casify/0.1.21/casify-0.1.21.tar.gz

---
+++ packages/casify/casify-0.1.21.tar.gz/casify-0.1.21/casify/__init__.py
@@ -0,0 +1,20 @@
+from .equations import solve, løs, Solve, Løs
+from .functions import function, Function, funksjon, Funksjon
+from .algebra import expand, factor, utvid, Utvid, faktoriser, Faktoriser
+
+__all__ = [
+    "solve",
+    "løs",
+    "Solve",
+    "Løs",
+    "function",
+    "Function",
+    "funksjon",
+    "Funksjon",
+    "expand",
+    "factor",
+    "utvid",
+    "Utvid",
+    "faktoriser",
+    "Faktoriser",
+]
