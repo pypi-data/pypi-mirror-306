b2d9a0003aea05ad67d55a44555f8c64c1edaf48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:25 +0000

    Add package holisticai/1.0.7/holisticai-1.0.7.tar.gz

---
+++ packages/holisticai/holisticai-1.0.7.tar.gz/holisticai-1.0.7/docs/source/getting_started/index.rst
@@ -0,0 +1,67 @@
+===============
+Getting Started
+===============
+
+This section will guide you through the installation of the `holisticai` open-source library and the first steps to get you started.
+
+.. grid:: 3 3 3 3
+   :gutter: 4
+   :padding: 2 2 0 0
+   :class-container: sd-text-center
+
+   .. grid-item-card:: Install
+         :link: install.html
+         :shadow: md
+
+         Installation and customizations, workaround and troubleshooting.
+
+   .. grid-item-card:: Quickstart
+         :link: quickstart.ipynb
+         :shadow: md
+
+         Quickstart guide to get you started with the library.
+
+   .. grid-item-card:: Datasets
+         :link: datasets.html
+         :shadow: md
+           
+         Datasets and artifacts available in the library.
+
+   .. grid-item-card:: Bias
+         :link: bias/index.html
+         :shadow: md
+
+         Bias metrics and mitigation techniques.
+
+   .. grid-item-card:: Explainability
+         :link: explainability/index.html
+         :shadow: md
+            
+         Explainability metrics and techniques.          
+
+   .. grid-item-card:: Security
+         :link: security/index.html
+         :shadow: md
+
+         Security metrics and mitigation techniques.
+
+   .. grid-item-card:: Robustness
+         :link: robustness/index.html
+         :shadow: md
+            
+         Robustness metrics and attackers.          
+        
+
+.. If you update this toctree, also update the manual toctree in the
+.. main index.rst.template
+
+.. toctree::
+   :hidden:
+
+   install
+   quickstart.ipynb
+   datasets
+   bias/index
+   explainability/index
+   security/index
+   robustness/index
---
+++ packages/holisticai/holisticai-1.0.7.tar.gz/holisticai-1.0.7/src/holisticai/datasets/_dataloaders.py
@@ -0,0 +1,120 @@
+# Base Imports
+from os import environ, makedirs
+from os.path import expanduser, join
+
+# sklearn imports
+from sklearn.datasets import fetch_openml
+
+
+def get_data_home(data_home=None):
+    """
+    Return the path of the holisticai data directory.
+    By default the data directory is set to a folder named 'holisticai_data' in the
+    user home folder.
+    Alternatively, it can be set by the 'HOLISTICAI_DATA' environment
+    variable or programmatically by giving an explicit folder path. The '~'
+    symbol is expanded to the user home folder.
+    If the folder does not already exist, it is automatically created.
+    Parameters
+    ----------
+    data_home : str, default=None
+        The path to holisticai data directory. If `None`, the default path
+        is `~/holisticai_data`.
+    Returns
+    -------
+    data_home: str
+        The path to holisticai data directory.
+    """
+    if data_home is None:
+        data_home = environ.get("HOLISTIC_AI_DATA", join("~", "holisticai_data"))
+    data_home = expanduser(data_home)
+    makedirs(data_home, exist_ok=True)
+    return data_home
+
+
+def load_openml(name, version, data_home=None, return_X_y=False, as_frame=True):
+    """
+    Generic function to load datasets from OpenML.
+
+    Parameters
+    ----------
+    name : str
+        Name of the dataset on OpenML.
+    version : int
+        Version of the dataset.
+    data_home : str, optional
+        The directory to which the data is downloaded. If None, we download
+        to the default data home directory.
+    return_X_y : bool, default=False
+        If True, returns ``(data, target)`` instead of a Bunch object.
+    as_frame : bool, default=True
+        If True, the data is a pandas DataFrame including columns with
+        appropriate dtypes (numeric).
+
+    Returns
+    -------
+    data : sklearn.utils.Bunch or tuple
+        Dataset object or (data, target) tuple if return_X_y is True.
+    """
+    if data_home is None:
+        data_home = get_data_home()
+
+    return fetch_openml(
+        name=name,
+        version=version,
+        data_home=data_home,
+        return_X_y=return_X_y,
+        as_frame=as_frame,
+    )
+
+
+def load_student(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("UCI-student-performance-mat", 1, data_home, return_X_y, as_frame)
+
+
+def load_adult(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("adult", 2, data_home, return_X_y, as_frame)
+
+
+def load_law_school(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("law-school-admission-bianry", 1, data_home, return_X_y, as_frame)
+
+
+def load_last_fm(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("LastFM_dataset", 1, data_home, return_X_y, as_frame)
+
+
+def load_us_crime(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("us_crime", 1, data_home, return_X_y, as_frame)
+
+
+def load_heart(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("heart-failure", 1, data_home, return_X_y, as_frame)
+
+
+def load_german_credit(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("German-Credit-Risk-with-Target", 1, data_home, return_X_y, as_frame)
+
+
+def load_census_kdd(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("Census-Income-KDD", 3, data_home, return_X_y, as_frame)
+
+
+def load_bank_marketing(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("bank-marketing", 9, data_home, return_X_y, as_frame)
+
+
+def load_compass(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("compass", 3, data_home, return_X_y, as_frame)
+
+
+def load_diabetes(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("Diabetes-130-Hospitals_(Fairlearn)", 2, data_home, return_X_y, as_frame)
+
+
+def load_acsincome(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("ACSIncome", 3, data_home, return_X_y, as_frame)
+
+
+def load_acspublic(data_home=None, return_X_y=False, as_frame=True):
+    return load_openml("ACSPublicCoverage", 2, data_home, return_X_y, as_frame)
