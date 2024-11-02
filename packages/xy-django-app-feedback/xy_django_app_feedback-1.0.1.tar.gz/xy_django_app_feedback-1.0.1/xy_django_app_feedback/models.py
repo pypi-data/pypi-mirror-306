a4ece04313f34afbc079052a953a1b9f950a3cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:03:33 +0000

    Add package xy-django-app-feedback/1.0.1/xy_django_app_feedback-1.0.1.tar.gz

---
+++ packages/xy-django-app-feedback/xy_django_app_feedback-1.0.1.tar.gz/xy_django_app_feedback-1.0.1/xy_django_app_feedback/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from .abstracts import MAFeedback
+
+
+class MFeedback(MAFeedback):
+    images = models.ManyToManyField(
+        "xy_django_app_resource.MImage",
+        verbose_name=_("图片"),
+        related_name="%(app_label)s_%(class)s_images",
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = _("反馈")
+        verbose_name_plural = _("反馈")
+        app_label = "xy_django_app_feedback"
