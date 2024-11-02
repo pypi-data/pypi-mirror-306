b7683ecd2fe4f13a04786ea031ed70c8c0cc5eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 14:03:48 +0000

    Add package blue-objects/5.162.1/blue_objects-5.162.1.tar.gz

---
+++ packages/blue-objects/blue_objects-5.162.1.tar.gz/blue_objects-5.162.1/blue_objects/.abcli/metadata.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+function abcli_metadata() {
+    local task=${1:-help}
+
+    if [ "$task" == "help" ]; then
+        abcli_metadata_get "$@"
+        abcli_metadata_post "$@"
+        return
+    fi
+
+    local function_name="abcli_metadata_$task"
+    if [[ $(type -t $function_name) == "function" ]]; then
+        $function_name "${@:2}"
+        return
+    fi
+
+    abcli_log_error "-@metadata: $task: command not found."
+    return 1
+}
+
+abcli_source_caller_suffix_path /metadata
