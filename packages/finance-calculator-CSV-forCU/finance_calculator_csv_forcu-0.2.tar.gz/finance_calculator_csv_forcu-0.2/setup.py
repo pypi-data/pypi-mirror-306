7d9f628d879cb789d3ff1b111c3ec43cb97fb065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:07:00 +0000

    Add package finance-calculator-CSV-forCU/0.2/finance_calculator_csv_forcu-0.2.tar.gz

---
+++ packages/finance-calculator-CSV-forCU/finance_calculator_csv_forcu-0.2.tar.gz/finance_calculator_csv_forcu-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+def read_readme():
+    with open('README.md', 'r', encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='finance-calculator_CSV_forCU',
+    version='0.2',
+    description='Пакет для анализа доходов и расходов',
+    author='Ruslan',
+    author_email='r.khuseyinov@edu.centraluniversity.ru',
+    packages=find_packages(),  # Находит все пакеты в проекте
+    install_requires=[
+        'pandas',  # Зависимости вашего проекта
+    ],
+    entry_points={
+        'console_scripts': [
+            'finance-calculator=finance_calculator.main:main',  # Команда для запуска из командной строки
+        ],
+    },
+    python_requires='>=3.6',  # Минимальная версия Python
+    long_description=read_readme(),
+    long_description_content_type='text/markdown',
+)
