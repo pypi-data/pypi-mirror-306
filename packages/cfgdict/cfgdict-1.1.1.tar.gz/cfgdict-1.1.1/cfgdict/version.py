b50f5b70fb8d04354d9daa178ade60d5e666ef7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:04:45 +0000

    Add package cfgdict/1.1.1/cfgdict-1.1.1.tar.gz

---
+++ packages/cfgdict/cfgdict-1.1.1.tar.gz/cfgdict-1.1.1/cfgdict/version.py
@@ -0,0 +1,6 @@
+__title__ = "cfgdict"
+__description__ = "A lightweight dict-like config library with validation support"
+__url__ ='http://github.com/gseismic/cfgdict',
+__version__ = "1.1.1"
+__author__ = "Liu Shengli"
+__license__ = "MIT"
