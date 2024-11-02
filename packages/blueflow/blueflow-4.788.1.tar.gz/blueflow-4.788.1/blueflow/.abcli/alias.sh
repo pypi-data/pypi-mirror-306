64939307b3b86c496051f1abf0606ec45c6db205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:03:35 +0000

    Add package blueflow/4.788.1/blueflow-4.788.1.tar.gz

---
+++ packages/blueflow/blueflow-4.788.1.tar.gz/blueflow-4.788.1/blueflow/.abcli/alias.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+alias today="date +%Y-%m-%d"
+
+alias @batch=abcli_aws_batch
+
+alias @docker=abcli_docker
+
+alias @huggingface=blueflow_huggingface
+
+alias @notebook=abcli_notebooks
+alias @notebooks=abcli_notebooks
+
+alias @mv="abcli_scripts move"
+
+alias @sagemaker=abcli_sagemaker
+
+alias @scripts=abcli_scripts
+alias @source="abcli_scripts source"
+
+alias workflow="blueflow_workflow"
+
+# meta scripts
+alias roofserver="abcli_meta_scripts roofserver"
+alias sagesemseg="abcli_meta_scripts sagesemseg"
