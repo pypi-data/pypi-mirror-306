4e05cb923bd675390e52f1bc3ed2326cf3f76877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:18 +0000

    Add package rapid-router/6.7.1/rapid-router-6.7.1.tar.gz

---
+++ packages/rapid-router/rapid-router-6.7.1.tar.gz/rapid-router-6.7.1/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import re
+import sys
+
+from setuptools import find_packages, setup
+
+with open("game/__init__.py", "r") as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
+    ).group(1)
+
+try:
+    from semantic_release import setup_hook
+
+    setup_hook(sys.argv)
+except ImportError:
+    pass
+
+setup(
+    name="rapid-router",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "django==3.2.25",
+        "django-csp==3.7",
+        "django-reverse-js==0.1.7",
+        "django-pipeline==2.0.8",
+        "djangorestframework==3.13.1",
+        "more-itertools==8.7.0",
+        "pyhamcrest==2.0.2",
+        "libsass==0.23.0",
+        "cfl-common",
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: " "Python :: 3.12",
+        "Framework :: Django",
+    ],
+    version=version,
+    zip_safe=False,
+)
