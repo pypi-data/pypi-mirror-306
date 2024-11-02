9a250ea5a7e64d6aa39f5df01660b1624463265e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:32 +0000

    Add package cocotbext-waves/0.1.8/cocotbext_waves-0.1.8-py3-none-any.whl

---
+++ packages/cocotbext-waves/cocotbext_waves-0.1.8-py3-none-any.whl/cocotbext/waves/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : __init__.py
+# License           : MIT license <Check LICENSE>
+# Author            : Anderson I. da Silva (aignacio) <anderson@aignacio.com>
+# Date              : 25.10.2024
+# Last Modified Date: 25.10.2024
+from .waves import waveform
