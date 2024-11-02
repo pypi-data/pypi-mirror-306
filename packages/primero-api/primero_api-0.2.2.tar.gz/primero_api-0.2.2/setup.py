904866562cf950f423ebfe0b08c8b236040d4009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:03:40 +0000

    Add package primero-api/0.2.2/primero_api-0.2.2.tar.gz

---
+++ packages/primero-api/primero_api-0.2.2.tar.gz/primero_api-0.2.2/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2.2'
+
+setup(
+    name='primero-api',
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        'requests',
+        'requests-cache',
+        'requests-ratelimiter',
+        'pandas',
+        'python-slugify',
+    ],
+    # development dependencies
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-cov',
+            'pytest-mock',
+            'flake8',
+            'black',
+            'isort',
+            'mypy',
+            'sphinx',
+            'sphinx_rtd_theme',
+            'requests_mock'
+        ]
+    },
+    author='merlos',
+    author_email='merlos@users.github.com',
+    description='A simple client for consuming data from Primero API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/unicef/magasin-primero-paquet',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
