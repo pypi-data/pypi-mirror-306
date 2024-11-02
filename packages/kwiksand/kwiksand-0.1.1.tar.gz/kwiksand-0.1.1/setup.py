0e14aaacef2d5ad96c697e6c53ec6e9e8552c34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:07:58 +0000

    Add package kwiksand/0.1.1/kwiksand-0.1.1.tar.gz

---
+++ packages/kwiksand/kwiksand-0.1.1.tar.gz/kwiksand-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='kwiksand',
+    packages=find_packages(),
+    version='0.1.1',
+    description='a thing to write text charachter by character',
+    author='HyperSource01',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',    
+)
