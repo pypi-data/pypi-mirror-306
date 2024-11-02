b9de9028bf6b95ef4a1a807095c641346bb52b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:07:18 +0000

    Add package shirin/0.0.5/shirin-0.0.5-py3-none-any.whl

---
+++ packages/shirin/shirin-0.0.5-py3-none-any.whl/shirin/__init__.py
@@ -0,0 +1,4 @@
+from ._util_codes import create_folder_path, save_to_txt, read_from_txt, read_img, save_img, delete_folder
+
+# Optionally define __all__ to restrict imports when using *
+__all__ = ['create_folder_path', 'save_to_txt', 'read_from_txt', 'read_img', 'save_img', 'delete_folder']
