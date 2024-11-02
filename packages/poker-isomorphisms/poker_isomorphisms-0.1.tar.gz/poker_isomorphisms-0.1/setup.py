266b99bcb52a5d73ae5007acda1aff9c4b70220d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:35 +0000

    Add package poker-isomorphisms/0.1/poker_isomorphisms-0.1.tar.gz

---
+++ packages/poker-isomorphisms/poker_isomorphisms-0.1.tar.gz/poker_isomorphisms-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+setup(
+    name='poker_isomorphisms',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    long_description=description,
+    long_description_content_type='text/markdown'
+)
