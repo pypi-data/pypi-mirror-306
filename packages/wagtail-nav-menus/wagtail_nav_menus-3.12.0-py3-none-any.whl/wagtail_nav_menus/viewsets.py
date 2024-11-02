ae69fdb7239721dd077094120a3f26ca86df2a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:08:56 +0000

    Add package wagtail-nav-menus/3.12.0/wagtail_nav_menus-3.12.0-py3-none-any.whl

---
+++ packages/wagtail-nav-menus/wagtail_nav_menus-3.12.0-py3-none-any.whl/wagtail_nav_menus/viewsets.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets
+from wagtail.models import Site
+from wagtail_nav_menus.models import NavMenu
+from wagtail_nav_menus.serializers import NavMenuSerializer
+
+
+class NavMenuViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = NavMenu.objects.all()
+    serializer_class = NavMenuSerializer
+
+    def get_queryset(self):
+        site_hostname = self.request.GET.get("site", None)
+        site = Site.find_for_request(self.request)
+        if site_hostname:
+            try:
+                site = Site.objects.get(hostname=site_hostname)
+            except Site.DoesNotExist:
+                pass
+        qs = super().get_queryset()
+        if site:
+            qs = qs.filter(site=site)
+        return qs
