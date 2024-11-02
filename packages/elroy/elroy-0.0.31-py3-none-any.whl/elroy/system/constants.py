f89dc87397d123f83d3f4c4c00c3572cbbd45195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:32 +0000

    Add package elroy/0.0.31/elroy-0.0.31-py3-none-any.whl

---
+++ packages/elroy/elroy-0.0.31-py3-none-any.whl/elroy/system/constants.py
@@ -0,0 +1,26 @@
+INNER_THOUGHT_TAG = "inner_thought_monologue"
+
+UNKNOWN = "Unknown"
+MEMORY_TITLE_EXAMPLES = """
+Examples of good and bad memory titles are below. Note, the BETTER examples, some titles have been split into two.:
+
+BAD:
+- [User Name]'s project progress and personal goals: 'Personal goals' is too vague, and the title describes two different topics.
+
+BETTER:
+- [User Name]'s project on building a treehouse: More specific, and describes a single topic.
+- [User Name]'s goal to be more thoughtful in conversation: Describes a specific goal.
+
+BAD:
+- [User Name]'s weekend plans: 'Weekend plans' is too vague, and dates must be referenced in ISO 8601 format.
+
+BETTER:
+- [User Name]'s plan to attend a concert on 2022-02-11: More specific, and includes a specific date.
+
+BAD:
+- [User Name]'s preferred name and well being: Two different topics, and 'well being' is too vague.
+
+BETTER:
+- [User Name]'s preferred name: Describes a specific topic.
+- [User Name]'s feeling of rejuvenation after rest: Describes a specific topic.
+"""
