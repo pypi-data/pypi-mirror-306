b2520aead0a501d85fb598920b43d4c6c17be84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:24 +0000

    Add package vizdata/0.32/vizdata-0.32.tar.gz

---
+++ packages/vizdata/vizdata-0.32.tar.gz/vizdata-0.32/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="vizdata",
+    version="0.32",
+    description="Simple Exploratory Data Analysis tool.",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/darkhan-ai/vizdata",
+    license="MIT",
+    packages=["vizdata"],
+    install_requires=[
+        "pandas",
+    ],
+    zip_safe=False,
+)
