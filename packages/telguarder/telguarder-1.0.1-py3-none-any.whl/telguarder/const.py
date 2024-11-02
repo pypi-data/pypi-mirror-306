a9a1e55b0bc96c8b5aae7e1bceaea4c8d7e25220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 14:03:53 +0000

    Add package telguarder/1.0.1/telguarder-1.0.1-py3-none-any.whl

---
+++ packages/telguarder/telguarder-1.0.1-py3-none-any.whl/telguarder/const.py
@@ -0,0 +1,6 @@
+"""telguarder constants."""
+
+TELGUARDER_API_URL = "https://tgedgeapi.telguarder.com"
+LOOKUP_URI = "/v2/Search/NumberLookup/Mobile"
+SERVICE_KEY = "6D04E359-F659-4A55-9622-36E31FC4889A"
+DEFAULT_REQUEST_TIMEOUT = 10
