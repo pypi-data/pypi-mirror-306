73b72fa8fea18807a764ea9a8c1b2282aa2f815a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:04:03 +0000

    Add package pg-common/1.1.8/pg_common-1.1.8-py3-none-any.whl

---
+++ packages/pg-common/pg_common-1.1.8-py3-none-any.whl/pg_common/conf.py
@@ -0,0 +1,14 @@
+__all__ = ["RuntimeException", "GLOBAL_DEBUG"]
+__auth__ = "baozilaji@gmail.com"
+
+
+GLOBAL_DEBUG = True
+
+
+class RuntimeException(Exception):
+    """
+      全局运行时异常
+    """
+    def __init__(self, name: str, msg: str):
+        self.name = name
+        self.msg = msg
