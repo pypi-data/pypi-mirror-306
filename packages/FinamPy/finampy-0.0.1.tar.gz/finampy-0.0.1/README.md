a044e8385ada88fd55b5d7615d59f010dee8cda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:30 +0000

    Add package FinamPy/0.0.1/finampy-0.0.1.tar.gz

---
+++ packages/FinamPy/finampy-0.0.1.tar.gz/finampy-0.0.1/README.md
@@ -0,0 +1,1 @@
+FinamPy - auto-trading lib.
---
+++ packages/FinamPy/finampy-0.0.1.tar.gz/finampy-0.0.1/src/Config.py
@@ -0,0 +1,7 @@
+# Чтобы получить торговый токен:
+# 1. Открыть счет в "Финаме" https://open.finam.ru/registration
+# 2. Зарегистрироваться в сервисе Comon https://www.comon.ru/
+# 3. В личном кабинете Comon получить торговый токен https://www.comon.ru/my/trade-api/tokens
+
+client_ids = ('<883549RHC4W>',)  # Торговые счета
+access_token = '<CAEQ5aLdAxoYuq740AuXZM/UMXYqWeNBys9u+Wa2+/8x>'  # Торговый токен
