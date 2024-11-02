51861c0228167f9f6330bc204509cf46f645a79d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:03:43 +0000

    Add package ATCForm/0.1.9.1/atcform-0.1.9.1.tar.gz

---
+++ packages/ATCForm/atcform-0.1.9.1.tar.gz/atcform-0.1.9.1/src/ATCForm/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+from .AUTO_FORM import *
+from .AUTO_TASK import *
+from .utils import *
+__all__ = [
+    'AuTotask_JSJ',
+    'AuTotask_TX',
+    'ATThreadPool',
+    'download_driver'
+]
---
+++ packages/ATCForm/atcform-0.1.9.1.tar.gz/atcform-0.1.9.1/src/ATCForm/examlpe.py
@@ -0,0 +1,53 @@
+import ATCForm as AT
+import time
+#最简单使用方法 复杂使用方法将在后续加入文档
+
+
+# Args:
+#     url (str): 收集表地址
+#     data_path (str): 数据地址
+#     start_time (str): 开始时间 格式为202405121200 年月日时分(注意补零).
+#     is_enable (bool, optional): 是否提前输入需要选择内容 Defaults to True.
+#     SELECT_LIST (list, optional): 需要选择的内容. Defaults to [] 如["1","2"].
+
+
+#金
+task1 = AT.AuTotask_JSJ(url="https://...", #会自动解析选项稍后会在终端要求输入
+                            data_path="./data.txt",
+                            start_time="202405141734",
+                            # is_enable=False,
+                            # SELECT_LIST=["a"]
+                            )
+
+    
+task2 = AT.AuTotask_JSJ(url="https://...",
+                            data_path="./data.txt",
+                            start_time="202405141200",
+                            is_enable=False,
+                            SELECT_LIST=["拍摄照片视频"]
+                            )
+    
+task3 = AT.AuTotask_JSJ(url="https://....",
+                            data_path="./data.txt",
+                            start_time="202405141230",
+                            is_enable=False,
+                            SELECT_LIST=[["11111111"],["1dsad"]]
+                            )
+
+
+#腾
+task3=AT.AuTotask_TX(url="https://......",
+                           data_path="./data.txt",
+                            start_time="202405141230",
+                            is_enable=False,
+                            SELECT_LIST=[["11111111"],["1dsad"]]
+                           )
+
+
+#下载驱动
+AT.download_driver()
+
+
+#多线程
+tasks=[task1,task2,task3]
+AT.ATThreadPool(tasks).start()
