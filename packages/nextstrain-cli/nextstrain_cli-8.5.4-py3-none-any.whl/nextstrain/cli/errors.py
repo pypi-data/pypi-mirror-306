25e33bf8e7f399b5a9322299e5fafebb9703156f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:03:39 +0000

    Add package nextstrain-cli/8.5.4/nextstrain_cli-8.5.4-py3-none-any.whl

---
+++ packages/nextstrain-cli/nextstrain_cli-8.5.4-py3-none-any.whl/nextstrain/cli/errors.py
@@ -0,0 +1,26 @@
+"""
+Exception classes for internal use.
+"""
+from textwrap import dedent
+
+
+class NextstrainCliError(Exception):
+    """Exception base class for all custom :mod:`nextstrain.cli` exceptions."""
+    pass
+
+class InternalError(NextstrainCliError):
+    pass
+
+class UserError(NextstrainCliError):
+    def __init__(self, message, *args, **kwargs):
+        # Remove leading newlines, trailing whitespace, and then indentation
+        # to better support nicely-formatted """multi-line strings""".
+        formatted_message = dedent(message.lstrip("\n").rstrip()).format(*args, **kwargs)
+
+        super().__init__("Error: " + formatted_message)
+
+class UsageError(UserError):
+    """
+    Prints brief command usage before the error message.
+    """
+    pass
