7f90ed01edaadf2f945b9a427189a5db295a7dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:03:35 +0000

    Add package hms-utils/1.4.45/hms_utils-1.4.45.tar.gz

---
+++ packages/hms-utils/hms_utils-1.4.45.tar.gz/hms_utils-1.4.45/src/hms_utils/portal/portal_utils.py
@@ -0,0 +1,32 @@
+from typing import List, Optional, Union
+from dcicutils.portal_utils import Portal as PortalFromUtils
+from dcicutils.ff_utils import delete_metadata, purge_metadata
+from hms_utils.type_utils import is_uuid
+
+
+class Portal(PortalFromUtils):
+
+    def delete_metadata(self, object_id: str) -> Optional[dict]:
+        if self.key:
+            return delete_metadata(obj_id=object_id, key=self.key)
+        return None
+
+    def purge_metadata(self, object_id: str) -> Optional[dict]:
+        if self.key:
+            return purge_metadata(obj_id=object_id, key=self.key)
+        return None
+
+    def reindex_metadata(self, uuids: Union[List[str], str], raise_exception: bool = False) -> bool:
+        if isinstance(uuids, str) and is_uuid(uuids):
+            uuids = [uuids]
+        elif isinstance(uuids, list):
+            if not (uuids := [uuid for uuid in uuids if is_uuid(uuid)]):
+                return False
+        else:
+            return False
+        try:
+            return self.post("/queue_indexing", json={"uuids": uuids}).status_code == 200
+        except Exception as e:
+            if raise_exception is True:
+                raise e
+            return False
---
+++ packages/hms-utils/hms_utils-1.4.45.tar.gz/hms_utils-1.4.45/src/hms_utils/type_utils.py
@@ -0,0 +1,74 @@
+import re
+from typing import Any, List, Optional, Tuple, Union
+
+primitive_type = (int, float, str, bool)
+
+
+def is_primitive_type(value: Any) -> bool:
+    return isinstance(value, primitive_type)
+
+
+def is_integer(value: str) -> bool:
+    return to_integer(value) is not None
+
+
+def to_integer(value: str) -> Optional[int]:
+    if isinstance(value, str) and (value := value.strip()):
+        try:
+            return int(value)
+        except Exception:
+            pass
+    return None
+
+
+def is_float(value: str) -> bool:
+    return to_float(value) is not None
+
+
+def to_float(value: str) -> Optional[int]:
+    if isinstance(value, str) and (value := value.strip()):
+        try:
+            return float(value)
+        except Exception:
+            pass
+    return None
+
+
+def any_of_bool(*booleans) -> bool:
+    for boolean in booleans:
+        if boolean is True:
+            return True
+    return False
+
+
+def at_most_one_of_bool(*booleans) -> bool:
+    ntrue = 0
+    for boolean in booleans:
+        if boolean is True:
+            if ntrue > 0:
+                return False
+            ntrue += 1
+    return True
+
+
+def to_bool(value: str) -> bool:
+    return value if isinstance(value, bool) else ((value.strip().lower() == "true")
+                                                  if isinstance(value, str) else False)
+
+
+def to_non_empty_string_list(value: Union[List[str], Tuple[str, ...], str], strip: bool = True) -> List[str]:
+    strings = []
+    if isinstance(value, (list, tuple)):
+        for item in value:
+            if isinstance(item, str) and ((strip is False) or (item := item.strip())):
+                strings.append(item)
+    elif isinstance(value, str) and ((strip is False) or (value := value.strip())):
+        strings = [value]
+    return strings
+
+
+_UUID_PATTERN = re.compile(r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
+
+
+def is_uuid(value: str) -> bool:
+    return _UUID_PATTERN.match(value) if isinstance(value, str) else False
