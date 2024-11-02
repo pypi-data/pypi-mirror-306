89d20ddc3521765f57b304789af4e7c63ced4129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:08:52 +0000

    Add package fusion-bench/0.2.3/fusion_bench-0.2.3-py3-none-any.whl

---
+++ packages/fusion-bench/fusion_bench-0.2.3-py3-none-any.whl/fusion_bench/method/linear/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa F401
+from .linear_interpolation import LinearInterpolationAlgorithm
+from .simple_average_for_llama import SimpleAverageForLlama
+from .task_arithmetic_for_llama import TaskArithmeticForLlama
+from .expo import ExPOAlgorithm
