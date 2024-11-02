c483a4dfb4a08426a99c5405a9201e3da6b3f57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:03:49 +0000

    Add package electrox/1.24.0/electrox-1.24.0.tar.gz

---
+++ packages/electrox/electrox-1.24.0.tar.gz/electrox-1.24.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="electrox",
+    version="1.24.0",
+    packages=find_packages(),
+    install_requires=[
+        'pygame',  # Make sure to install pygame for the game
+    ],
+    entry_points={
+        'console_scripts': [
+            'electrox = electrox.main:main',  # Specify main function within main.py
+        ],
+    },
+    author="Hussain Luai",
+    author_email="hxolotl15@gmail.com",
+    description="Electrox is a Python framework for 2D game development with Gen Z and Gen Alpha vibes for some reason... nvm enjoy electrox.",
+)
