3457221ec3547762841b4dadd79a8fa467c91cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 05:03:15 +0000

    Add package dependency-container/0.2.0/dependency_container-0.2.0-py3-none-any.whl

---
+++ packages/dependency-container/dependency_container-0.2.0-py3-none-any.whl/dependency_container/__init__.py
@@ -0,0 +1,10 @@
+"""FastApi Delayed Dependency Injection package.
+
+Support delayed dependency injection in FastApi to enable app constructor pattern.
+"""
+
+from dependency_container.container import DependencyContainer
+from dependency_container.routing import InjectableRouter
+
+__all__: list[str] = ["DependencyContainer", "InjectableRouter"]
+__version__ = "0.2.0"
