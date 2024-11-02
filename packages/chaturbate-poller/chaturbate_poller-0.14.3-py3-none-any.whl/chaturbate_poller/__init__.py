dffd830d251bf90a3a0e7490cd6f759c13758b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 05:03:33 +0000

    Add package chaturbate-poller/0.14.3/chaturbate_poller-0.14.3-py3-none-any.whl

---
+++ packages/chaturbate-poller/chaturbate_poller-0.14.3-py3-none-any.whl/chaturbate_poller/__init__.py
@@ -0,0 +1,17 @@
+"""chaturbate_poller package."""
+
+# Read version from installed package
+from importlib.metadata import version
+
+__version__ = version("chaturbate_poller")
+__author__ = "MountainGod2"
+__author_email__ = "admin@reid.ca"
+__maintainer__ = "MountainGod2"
+__maintainer_email__ = "admin@reid.ca"
+__license__ = "MIT"
+__url__ = "https://github.com/MountainGod2/chaturbate_poller"
+__description__ = "Poller for the Chaturbate events API."
+
+
+__all__ = [""]
+"""List[str]: The package exports."""
