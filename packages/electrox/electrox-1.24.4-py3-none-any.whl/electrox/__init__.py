17d2a0e8276f692ee011caa10d3eebbba1185377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 15:04:38 +0000

    Add package electrox/1.24.4/electrox-1.24.4-py3-none-any.whl

---
+++ packages/electrox/electrox-1.24.4-py3-none-any.whl/electrox/__init__.py
@@ -0,0 +1,18 @@
+import pygame
+from .electrox import ElectroxGame  # Assuming your ElectroxGame class is in electrox.py
+
+def main():
+    """
+    Starts and runs the Electrox game.
+    """
+    # Initialize Pygame
+    pygame.init()
+
+    # Create an instance of ElectroxGame
+    game = ElectroxGame()
+
+    # Run the game loop
+    game.run_game()
+
+if __name__ == "__main__":
+    main()
