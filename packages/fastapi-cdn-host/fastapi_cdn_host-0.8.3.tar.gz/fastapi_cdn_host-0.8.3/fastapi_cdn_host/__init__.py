57b7d497e2461ecd802834f436057c2e204ae7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 15:03:45 +0000

    Add package fastapi-cdn-host/0.8.3/fastapi_cdn_host-0.8.3.tar.gz

---
+++ packages/fastapi-cdn-host/fastapi_cdn_host-0.8.3.tar.gz/fastapi_cdn_host-0.8.3/fastapi_cdn_host/__init__.py
@@ -0,0 +1,22 @@
+from .client import (
+    AssetUrl,
+    CdnHostEnum,
+    CdnHostItem,
+    monkey_patch_for_docs_ui,
+    patch_docs,
+)
+from .utils import today_lock, weekday_lock
+
+monkey_patch = monkey_patch_for_docs_ui
+__version__ = "0.8.3"
+__all__ = (
+    "__version__",
+    "AssetUrl",
+    "CdnHostEnum",
+    "CdnHostItem",
+    "monkey_patch",
+    "monkey_patch_for_docs_ui",
+    "patch_docs",
+    "today_lock",
+    "weekday_lock",
+)
