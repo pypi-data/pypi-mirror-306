f5edbabcdb83078a60110a0788c16c74b2db6f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:03:15 +0000

    Add package DevilPy/0.6/devilpy-0.6.tar.gz

---
+++ packages/DevilPy/devilpy-0.6.tar.gz/devilpy-0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+
+setup(
+	version='0.6',
+	name='DevilPy',
+	author='DevilPy',
+	description='A powerful encryption libraries in Python created by the NasrPy',
+	packages=find_packages(),
+	classifiers=[
+	'Programming Language :: Python :: 3'
+
+	]
+
+)
+
+
+
+
+
