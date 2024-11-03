1addd880f603c24448447351a4a1dc9a4e303abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:05:21 +0000

    Add package NasrPy/0.2/NasrPy-0.2.tar.gz

---
+++ packages/NasrPy/NasrPy-0.2.tar.gz/NasrPy-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+
+setup(
+	version='0.2',
+	name='NasrPy',
+	author='NasrPy',
+	description='This is the best encryption library for Nasr',
+	packages=find_packages(),
+	classifiers=[
+	'Programming Language :: Python :: 3'
+
+	]
+
+)
+
+
+
+
+
