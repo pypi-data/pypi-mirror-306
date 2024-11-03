09d4620fd6e80ec84887c60dfd8285e65b10f67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:04:07 +0000

    Add package pythonwebtools/0.1.7/pythonwebtools-0.1.7-py3-none-any.whl

---
+++ packages/pythonwebtools/pythonwebtools-0.1.7-py3-none-any.whl/pythonwebtools/services/CryptService.py
@@ -0,0 +1,36 @@
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+
+
+class CryptService:
+    def __init__(self):
+        self.key = bytes.fromhex('cc2b77c79470aea8b5beaee3a544f90b5af1ecf1dcaacb2d21ee2d49eafc874e')
+        self.backend = default_backend()
+
+    def encrypt(self, plaintext: str) -> str:
+        iv = os.urandom(16)
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=self.backend)
+        encryptor = cipher.encryptor()
+
+        padder = padding.PKCS7(128).padder()
+        padded_data = padder.update(plaintext.encode()) + padder.finalize()
+
+        encrypted = encryptor.update(padded_data) + encryptor.finalize()
+        return (iv + encrypted).hex()
+
+    def decrypt(self, encrypted_text: str) -> str:
+        encrypted_data = bytes.fromhex(encrypted_text)
+        iv = encrypted_data[:16]
+        encrypted_message = encrypted_data[16:]
+
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=self.backend)
+        decryptor = cipher.decryptor()
+
+        decrypted_padded = decryptor.update(encrypted_message) + decryptor.finalize()
+
+        unpadder = padding.PKCS7(128).unpadder()
+        decrypted = unpadder.update(decrypted_padded) + unpadder.finalize()
+
+        return decrypted.decode()
