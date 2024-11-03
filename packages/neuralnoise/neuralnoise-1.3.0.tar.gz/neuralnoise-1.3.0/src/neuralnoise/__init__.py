40cda505119bc236dc64b1723097cfeb4be78553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:03:58 +0000

    Add package neuralnoise/1.3.0/neuralnoise-1.3.0.tar.gz

---
+++ packages/neuralnoise/neuralnoise-1.3.0.tar.gz/neuralnoise-1.3.0/src/neuralnoise/__init__.py
@@ -0,0 +1,4 @@
+from neuralnoise.extract import extract_content, aextract_content
+from neuralnoise.studio import create_podcast_episode
+
+__all__ = ["create_podcast_episode", "extract_content", "aextract_content"]
