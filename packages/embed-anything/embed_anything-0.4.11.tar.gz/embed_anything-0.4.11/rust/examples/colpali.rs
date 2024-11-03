1e29f5aab55d6c37ff7f0f3be270142ab65badd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:03:43 +0000

    Add package embed-anything/0.4.11/embed_anything-0.4.11.tar.gz

---
+++ packages/embed-anything/embed_anything-0.4.11.tar.gz/embed_anything-0.4.11/rust/examples/colpali.rs
@@ -0,0 +1,14 @@
+use embed_anything::embeddings::local::colpali::ColPaliEmbedder;
+
+fn main() -> Result<(), anyhow::Error> {
+    let colpali_model = ColPaliEmbedder::new("vidore/colpali-v1.2-merged", None)?;
+    let file_path = "test_files/attention.pdf";
+    let batch_size = 1;
+    let embed_data = colpali_model.embed_file(file_path, batch_size)?;
+    println!("{:?}", embed_data.len());
+
+    let prompt = "What is attention?";
+    let query_embeddings = colpali_model.embed_query(prompt)?;
+    println!("{:?}", query_embeddings.len());
+    Ok(())
+}
---
+++ packages/embed-anything/embed_anything-0.4.11.tar.gz/embed_anything-0.4.11/rust/src/embeddings/local/mod.rs
@@ -0,0 +1,7 @@
+pub mod bert;
+pub mod clip;
+pub mod colpali;
+pub mod jina;
+pub mod model_info;
+pub mod pooling;
+pub mod text_embedding;
