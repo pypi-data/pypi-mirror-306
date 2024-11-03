3a104739e96eeabad91877f2bd16d1f63d64776c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 15:03:50 +0000

    Add package anu/0.1.0/anu-0.1.0.tar.gz

---
+++ packages/anu/anu-0.1.0.tar.gz/anu-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="anu",
+    version="0.1.0",
+    description="Anu is a collection of libraries for building AI applications",
+    # long_description=open("README.md").read(),
+    # long_description_content_type="text/markdown",
+    # author="Your Name",
+    # author_email="your.email@example.com",
+    # url="https://github.com/yourusername/your-repo",
+    # packages=find_packages(),
+    # install_requires=[
+    #     "required-package1",
+    #     "required-package2",
+    # ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
