9ab4e411232ad48ddb32062ddb3c9022f8116bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 15:03:47 +0000

    Add package django-double-entry/1.0.6/django_double_entry-1.0.6-py2.py3-none-any.whl

---
+++ packages/django-double-entry/django_double_entry-1.0.6-py2.py3-none-any.whl/accounting/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoSimpleLedgerConfig(AppConfig):
+    name = "accounting"
+    verbose_name = "django_simple_ledger"
+    default_auto_field = "django.db.models.BigAutoField"
