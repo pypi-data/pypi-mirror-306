ae53dbacee293712fd119fcbd56ece487bb923a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:03:52 +0000

    Add package ano-code/0.1.14/ano_code-0.1.14-py3-none-any.whl

---
+++ packages/ano-code/ano_code-0.1.14-py3-none-any.whl/auto_code/cli.py
@@ -0,0 +1,38 @@
+
+import click
+from ai_assistant.llm_cli import openai_client
+from ai_assistant.prompt_llm import AIAssistant
+from ai_assistant.consts import COMMANDS
+from file_processing import file_handling
+from rich.console import Console
+from rich.theme import Theme
+
+custom_theme = Theme({"success": "green", "error": "bold red", "fun": "purple"})
+
+
+console = Console(theme=custom_theme)
+
+
+def prompt(code: str):
+    assistant = AIAssistant(openai_client)
+    result = assistant.run_assistant(code, COMMANDS["w_doc"])
+    return result
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.argument('directory', type=click.Path(exists=True, file_okay=False, dir_okay=True))
+def write_doc(directory):
+    source_code = file_handling.process_directory(directory)
+    response = prompt(source_code)
+    file_handling.create_markdown_file("./documentation", response)
+
+
+
+cli.add_command(write_doc)
+
+
+if __name__ == "__main__":
+    cli()
