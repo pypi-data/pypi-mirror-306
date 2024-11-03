7ea61201e3c00b36682ce8429a0eb4dafa1c30b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:03:52 +0000

    Add package keyalias/1.0.3/keyalias-1.0.3.tar.gz

---
+++ packages/keyalias/keyalias-1.0.3.tar.gz/keyalias-1.0.3/src/keyalias/tests/__init__.py
@@ -0,0 +1,13 @@
+import unittest
+
+__all__ = [
+    "test",
+]
+
+
+def test():
+    loader = unittest.TestLoader()
+    tests = loader.discover(start_dir="keyalias.tests")
+    runner = unittest.TextTestRunner()
+    result = runner.run(tests)
+    return result
