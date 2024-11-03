0d924b292588270f2675f1c24388a3b349f63e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:04:13 +0000

    Add package djreport/0.0.2/djreport-0.0.2-py3-none-any.whl

---
+++ packages/djreport/djreport-0.0.2-py3-none-any.whl/djreport/static/scripts/djreport.js
@@ -0,0 +1,55 @@
+class Report {
+  constructor(license, reportName, reportFile, reportData, outputFormat) {
+    this.license = license;
+    this.reportName = reportName;
+    this.reportFile = reportFile;
+    this.reportData = reportData;
+    this.outputFormat = outputFormat;
+
+    this._report = null;
+  }
+
+  get report() {
+    if (!this._report) {
+      // set license
+      Stimulsoft.Base.StiLicense.Key = this.license;
+      // initialize report
+      this._report = new Stimulsoft.Report.StiReport();
+      // load report file
+      this._report.loadFile(this.reportFile);
+      this._report.reportAlias = this.reportName;
+      // load and bind report data
+      const dataSet = new Stimulsoft.System.Data.DataSet('data');
+      dataSet.readJson(this.reportData);
+      this._report.dictionary.databases.clear();
+      this._report.regData('data', 'data', dataSet);
+    }
+    return this._report;
+  }
+
+  exportToPDF() {
+    this.report.exportDocumentAsync(content => {
+      Stimulsoft.System.StiObject.saveAs(
+        content, `${this.report.reportAlias}.pdf`, 'application/pdf'
+      );
+    }, Stimulsoft.Report.StiExportFormat.Pdf);
+  }
+
+  exportToHTML() {
+    this.report.exportDocumentAsync(content => {
+      Stimulsoft.System.StiObject.saveAs(
+        content, `${this.report.reportAlias}.html`, 'text/html;charset=utf-8'
+      );
+    }, Stimulsoft.Report.StiExportFormat.Html);
+  }
+
+  render() {
+    this.report.renderAsync(() => {
+      if (this.outputFormat === 'html') {
+        this.exportToHTML();
+      } else if (this.outputFormat === 'pdf') {
+        this.exportToPDF();
+      }
+    })
+  }
+}
