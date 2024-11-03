ed45cd9f9e327ea50e31e8c20ccb1b997ffd9b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:03:46 +0000

    Add package yt-dlp-types/0.0.9/yt_dlp_types-0.0.9.tar.gz

---
+++ packages/yt-dlp-types/yt_dlp_types-0.0.9.tar.gz/yt_dlp_types-0.0.9/yt_dlp-stubs/utils/_utils.pyi
@@ -0,0 +1,62 @@
+import urllib.request
+from collections.abc import Callable
+from inspect import Traceback
+from typing import Any, Literal, TypeVar
+
+__all__ = ('ExtractorError', 'HEADRequest', 'YoutubeDLError', 'int_or_none', 'parse_iso8601',
+           'sanitize_filename', 'try_get', 'unified_timestamp')
+
+_T = TypeVar('_T')
+_U = TypeVar('_U')
+
+
+def try_get(x: _T, getter: Callable[[_T], _U], type_: type[_U] = ...) -> _U | None:
+    ...
+
+
+def parse_iso8601(s: str, delimiter: str) -> int | None:
+    ...
+
+
+def int_or_none(x: Any) -> int | None:
+    ...
+
+
+class HEADRequest(urllib.request.Request):
+    def get_method(self) -> Literal['HEAD']:
+        ...
+
+
+class NO_DEFAULT:  # noqa: N801
+    ...
+
+
+def sanitize_filename(s: str, restricted: bool = ..., is_id: bool | NO_DEFAULT = ...) -> str:
+    ...
+
+
+class YoutubeDLError(Exception):
+    def __init__(self, msg: str | None = ...) -> None:
+        ...
+
+
+class ExtractorError(YoutubeDLError):
+    def __init__(self,
+                 msg: str,
+                 tb: Traceback | None = ...,
+                 expected: bool = ...,
+                 cause: BaseException | None = ...,
+                 video_id: str | None = ...,
+                 ie: str | None = ...) -> None:
+        ...
+
+    def format_traceback(self) -> str:
+        ...
+
+    @property
+    def __msg(self) -> str:
+        ...
+
+
+def unified_timestamp(date_str: str, day_first: bool = ...) -> int | float | None:
+    ...
