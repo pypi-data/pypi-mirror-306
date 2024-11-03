3aea8185eb82a7a26d553bcd7925dbeb49b203ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:03:17 +0000

    Add package scope/0.4.7/scope-0.4.7.tar.gz

---
+++ packages/scope/scope-0.4.7.tar.gz/scope-0.4.7/viewer/__main__.py
@@ -0,0 +1,27 @@
+import os
+import pathlib
+import sys
+
+import uvicorn
+
+package = str(pathlib.Path(__file__).parent)
+sys.path.insert(0, package)
+from config import config
+
+
+def main():
+
+  # Uvicorn watches cwd for changes for reload.
+  os.chdir(package)
+
+  uvicorn.run(
+      'server:app',
+      host='0.0.0.0',
+      port=config.port,
+      reload=config.debug,
+      workers=None if config.debug else config.workers,
+  )
+
+
+if __name__ == '__main__':
+  main()
