910c07d6deaf9430b4b4a4f753d1676896b1694c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:11:55 +0000

    Add package kubewhisper/1.0.2/kubewhisper-1.0.2.tar.gz

---
+++ packages/kubewhisper/kubewhisper-1.0.2.tar.gz/kubewhisper-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kubewhisper",
+    version="1.0.2",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    entry_points={
+        'console_scripts': [
+            'kubewhisper=cli.main:main',
+        ],
+    },
+    author="Branko Petric",
+    author_email="bane@brankopetric.com",
+    description="KubeWhisper - Generate kubectl commands from natural language queries",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/petricbranko/kubewhisper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
