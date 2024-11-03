e19a2c7b9858212039d14a74da5064836673bfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:04:06 +0000

    Add package pythonwebtools/0.1.5/pythonwebtools-0.1.5.tar.gz

---
+++ packages/pythonwebtools/pythonwebtools-0.1.5.tar.gz/pythonwebtools-0.1.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythonwebtools',
+    version='0.1.5',
+    description='A utility library for Python that provides common validation and cryptographic functions for web development.',
+    long_description=open('docs/README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Eros Gabriel Vieira',
+    author_email='apenaclaskrr@gmail.com',
+    url='https://github.com/erosnoxx/pythonwebtools',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'email-validator',   # For email validation
+        'python-dotenv',     # For loading environment variables from .env files
+        'cryptography',      # For cryptographic operations
+        'argon2-cffi'       # For secure password hashing
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Security :: Cryptography',
+    ],
+    python_requires='>=3.6',
+)
