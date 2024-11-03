2b4640a691f57d9cee9f8b9b321b10f5c0432f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:04:09 +0000

    Add package django-replyify-oauth2/0.3.0/django_replyify_oauth2-0.3.0-py3-none-any.whl

---
+++ packages/django-replyify-oauth2/django_replyify_oauth2-0.3.0-py3-none-any.whl/replyify_oauth2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE', which is part of this source code package.
+#
+default_app_config = 'replyify_oauth2.apps.ReplyifyOAuth2Config'
+
+__version__ = '0.3.0'
