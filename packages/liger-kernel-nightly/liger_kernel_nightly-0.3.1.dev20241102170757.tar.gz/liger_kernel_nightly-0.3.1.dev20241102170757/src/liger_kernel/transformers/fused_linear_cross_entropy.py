ac0305ce5c544642a3385f78c5c26af4211c5170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:04:19 +0000

    Add package liger-kernel-nightly/0.3.1.dev20241102170757/liger_kernel_nightly-0.3.1.dev20241102170757.tar.gz

---
+++ packages/liger-kernel-nightly/liger_kernel_nightly-0.3.1.dev20241102170757.tar.gz/liger_kernel_nightly-0.3.1.dev20241102170757/src/liger_kernel/transformers/fused_linear_cross_entropy.py
@@ -0,0 +1,21 @@
+from torch.nn import CrossEntropyLoss
+
+from liger_kernel.ops.fused_linear_cross_entropy import (
+    LigerFusedLinearCrossEntropyFunction,
+)
+
+
+class LigerFusedLinearCrossEntropyLoss(CrossEntropyLoss):
+    def __init__(self, *args, **kwargs):
+        super(LigerFusedLinearCrossEntropyLoss, self).__init__(*args, **kwargs)
+
+    def forward(self, lin_weight, _input, target, bias=None):
+        return LigerFusedLinearCrossEntropyFunction.apply(
+            _input,
+            lin_weight,
+            target,
+            bias,
+            self.ignore_index,
+            self.label_smoothing,
+            self.reduction,
+        )
