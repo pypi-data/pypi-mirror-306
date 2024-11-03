8c5534e4b4398ca85b8cd5d9f29fd12a6a83fa5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:03:32 +0000

    Add package nimc/1.2.2/nimc-1.2.2.tar.gz

---
+++ packages/nimc/nimc-1.2.2.tar.gz/nimc-1.2.2/nimc/defoldExtension.py
@@ -0,0 +1,86 @@
+
+
+
+DEFOLD_CPP_EXTENSION = 
+cpp_code = '''
+// {{extension}}.cpp
+// Extension lib defines
+#define LIB_NAME "{{extension}}"
+#define MODULE_NAME "{{extension}}"
+#include <dmsdk/sdk.h>
+
+extern "C" {
+    #include "{{extension}}.h"
+}
+
+{{nim_cpp_implemenation}}
+
+static void LuaInit(lua_State* L)
+{
+    int top = lua_gettop(L);
+
+    // Register lua names
+    luaL_register(L, MODULE_NAME, Module_methods);
+
+    lua_pop(L, 1);
+    assert(top == lua_gettop(L));
+}
+
+static dmExtension::Result AppInitializeMyExtension(dmExtension::AppParams* params)
+{
+    dmLogInfo("AppInitializeMyExtension");
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result InitializeMyExtension(dmExtension::Params* params)
+{
+    // Init Lua
+    LuaInit(params->m_L);
+    NimMain();
+    dmLogInfo("Registered %s Extension", MODULE_NAME);
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result AppFinalizeMyExtension(dmExtension::AppParams* params)
+{
+    dmLogInfo("AppFinalizeMyExtension");
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result FinalizeMyExtension(dmExtension::Params* params)
+{
+    dmLogInfo("FinalizeMyExtension");
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result OnUpdateMyExtension(dmExtension::Params* params)
+{
+    dmLogInfo("OnUpdateMyExtension");
+    return dmExtension::RESULT_OK;
+}
+
+static void OnEventMyExtension(dmExtension::Params* params, const dmExtension::Event* event)
+{
+    switch(event->m_Event)
+    {
+        case dmExtension::EVENT_ID_ACTIVATEAPP:
+            dmLogInfo("OnEventMyExtension - EVENT_ID_ACTIVATEAPP");
+            break;
+        case dmExtension::EVENT_ID_DEACTIVATEAPP:
+            dmLogInfo("OnEventMyExtension - EVENT_ID_DEACTIVATEAPP");
+            break;
+        case dmExtension::EVENT_ID_ICONIFYAPP:
+            dmLogInfo("OnEventMyExtension - EVENT_ID_ICONIFYAPP");
+            break;
+        case dmExtension::EVENT_ID_DEICONIFYAPP:
+            dmLogInfo("OnEventMyExtension - EVENT_ID_DEICONIFYAPP");
+            break;
+        default:
+            dmLogWarning("OnEventMyExtension - Unknown event id");
+            break;
+    }
+}
+
+
+DM_DECLARE_EXTENSION({{extension}}, LIB_NAME, AppInitializeMyExtension, AppFinalizeMyExtension, InitializeMyExtension, OnUpdateMyExtension, OnEventMyExtension, FinalizeMyExtension)
+'''
