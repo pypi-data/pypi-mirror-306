394403a8986ab05176efa22ee270adac83af45d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:12:54 +0000

    Add package psychsem/0.3/psychsem-0.3.tar.gz

---
+++ packages/psychsem/psychsem-0.3.tar.gz/psychsem-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+name='psychsem',
+version='0.3',
+description='StromBreaker',
+author='Psych', 
+packages=['psychsem'],
+install_requires=[
+    #empty 
+],
+)
