a19af8200455fc6b641cb678ec874549cc233dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:03:59 +0000

    Add package calibrator/0.0.12/calibrator-0.0.12-py3-none-any.whl

---
+++ packages/calibrator/calibrator-0.0.12-py3-none-any.whl/calibrator/metrics/classwiseece.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ClasswiseECE(nn.Module):
+    '''
+    Compute Classwise ECE
+    '''
+    def __init__(self, n_bins=15):
+        super(ClasswiseECE, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+
+    def forward(self, logits=None, labels=None, softmaxes=None):
+        '''
+        args:
+            logits: torch.Tensor
+                The logits to calibrate, the output of the model before softmax layer
+            labels: torch.Tensor
+                The labels of the test data
+            softmaxes: torch.Tensor
+                The softmaxes of the test data, if None, compute the softmaxes from logits, if both softmaxes provided, use softmaxes
+
+        Returns:
+            sce: float
+                The Classwise ECE value
+        '''
+        if softmaxes is None:
+            softmaxes = F.softmax(logits, dim=1)
+        confidences, predictions = torch.max(softmaxes, 1)
+        num_classes = int((torch.max(labels) + 1).item())
+        per_class_sce = None
+
+        for i in range(num_classes):
+            class_confidences = softmaxes[:, i]
+            class_sce = torch.zeros(1, device=labels.device)
+            labels_in_class = labels.eq(i) # one-hot vector of all positions where the label belongs to the class i
+
+            for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+                in_bin = class_confidences.gt(bin_lower.item()) * class_confidences.le(bin_upper.item())
+                prop_in_bin = in_bin.float().mean()
+                if prop_in_bin.item() > 0:
+                    accuracy_in_bin = labels_in_class[in_bin].float().mean()
+                    avg_confidence_in_bin = class_confidences[in_bin].float().mean()
+                    class_sce += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+
+            if (i == 0):
+                per_class_sce = class_sce
+            else:
+                per_class_sce = torch.cat((per_class_sce, class_sce), dim=0)
+
+        sce = torch.mean(per_class_sce)
+        return sce.item()
