9d0d1228ab396e697e95cb9e80cc47f3d37f4131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:11:48 +0000

    Add package bpf-asm/0.1.4/bpf_asm-0.1.4-cp38-cp38-win32.whl

---
+++ packages/bpf-asm/bpf_asm-0.1.4-cp38-cp38-win32.whl/bpf_asm/__init__.py
@@ -0,0 +1,25 @@
+# pybpf_asm - Python BPF Assembler
+# Copyright (C) 2022  Segev Finer
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Python BPF Assembler.
+
+Based on the BPF assembler in Linux sources.
+"""
+
+from ._bpf_asm import *
+
+
+__version__ = "0.1.4"
