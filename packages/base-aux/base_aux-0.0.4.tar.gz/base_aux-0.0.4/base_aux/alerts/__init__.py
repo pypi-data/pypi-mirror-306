ac2a480c40dac3480f6063996cfc83ee865193bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:12:55 +0000

    Add package base-aux/0.0.4/base_aux-0.0.4.tar.gz

---
+++ packages/base-aux/base_aux-0.0.4.tar.gz/base_aux-0.0.4/base_aux/alerts/__init__.py
@@ -0,0 +1,20 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorerct
+#   from .main import EXACT_OBJECTS     # CORERCT
+
+
+# =====================================================================================================================
+from .base import AlertBase
+from .smtp import SmtpAddress, SmtpServers, AlertSmtp
+from .telegram import RecipientTgID, AlertTelegram
+
+
+# =====================================================================================================================
+# FIXME: move to main!
+class AlertSelect:
+    SMTP_DEF = AlertSmtp
+    TELEGRAM_DEF = AlertTelegram
+
+
+# =====================================================================================================================
