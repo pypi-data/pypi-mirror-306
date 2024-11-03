8789915cfaf388a209d058ae80ac2d74037567b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:05:43 +0000

    Add package mimodulo-he/0.1/mimodulo_he-0.1.tar.gz

---
+++ packages/mimodulo-he/mimodulo_he-0.1.tar.gz/mimodulo_he-0.1/setup.py
@@ -0,0 +1,17 @@
+# mimodulo/setup.py
+from setuptools import setup, find_packages
+setup(
+name='mimodulo_he',
+version='0.1',
+packages=find_packages(),
+description='Un módulo simple para saludar',
+author='Hector Madera',
+author_email='tuemail@ejemplo.com',
+url='https://tuurl.com', # Cambia esto por tu URL si es necesario
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
