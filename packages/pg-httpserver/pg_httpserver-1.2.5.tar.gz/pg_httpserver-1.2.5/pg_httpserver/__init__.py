40964697ea28aab9be832df06863a393b2710e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:12:04 +0000

    Add package pg-httpserver/1.2.5/pg_httpserver-1.2.5.tar.gz

---
+++ packages/pg-httpserver/pg_httpserver-1.2.5.tar.gz/pg_httpserver-1.2.5/pg_httpserver/__init__.py
@@ -0,0 +1,6 @@
+from pg_httpserver.fapi import run, app, CODE_VERSION
+from pg_httpserver.define import ENV_HANDLER_DIR, httpserver_init, \
+    ENV_NEEDS_BODY_MIDDLEWARE, GameException, GameErrorCode, Container, RequestMap, RequestHeader, RequestData, \
+    ResponseMap, ResponseData, ResponseHeader, FieldContainer
+from pg_httpserver.manager import GameConfigManager, GamePropertyManager
+VERSION = "1.2.5"
