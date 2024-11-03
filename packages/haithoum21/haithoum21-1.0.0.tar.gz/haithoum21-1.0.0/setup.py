88846370da556d568d0a1914c74bd7ca66a70cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:03:34 +0000

    Add package haithoum21/1.0.0/haithoum21-1.0.0.tar.gz

---
+++ packages/haithoum21/haithoum21-1.0.0.tar.gz/haithoum21-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='haithoum21',
+   version='1.0.0',
+   author='Haitham',
+   author_email='haithambadereddine.dz@gmail.com',
+   packages=['rocket'],
+   url='http://pypi.python.org/pypi/rocket/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['rocket']
+)
