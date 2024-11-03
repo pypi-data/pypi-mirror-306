e24ecf556c8dd65abc3219a37849d91ab63bf19a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:03:38 +0000

    Add package moderatorpy/0.1.1/moderatorpy-0.1.1.tar.gz

---
+++ packages/moderatorpy/moderatorpy-0.1.1.tar.gz/moderatorpy-0.1.1/examples/async_limiter.py
@@ -0,0 +1,32 @@
+import asyncio
+
+from httpx import AsyncClient
+from moderatorpy.async_ import AsyncLimiterTransport, RedisBucket
+
+DEFAULT_BASE_URL = "https://public-api.birdeye.so"
+DEFAULT_API_KEY = "5c85c95570c74f75bb5c9f0fd2772927"
+
+
+async def main():
+    transport = AsyncLimiterTransport(
+        per_second=10,
+        per_minute=100,
+        bucket_class=RedisBucket,
+        bucket_kwargs={"redis_url": "redis://localhost:6379/0"},
+    )
+    async with AsyncClient(base_url=DEFAULT_BASE_URL, transport=transport) as client:
+        response = await client.get(
+            "/defi/tokenlist",
+            headers={"x-chain": "solana", "x-api-key": DEFAULT_API_KEY},
+            params={
+                "limit": 1,
+                "sort_by": "v24hChangePercent",
+                "offset": 0,
+            },
+        )
+        response_json = response.json()
+        print(response_json.get("data", {}).get("total", 0))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ packages/moderatorpy/moderatorpy-0.1.1.tar.gz/moderatorpy-0.1.1/src/moderatorpy/__init__.py
@@ -0,0 +1,4 @@
+from moderatorpy.http_limiter import LimiterTransport
+from moderatorpy.redis_bucket import RedisBucket
+
+__all__ = ["LimiterTransport", "RedisBucket"]
