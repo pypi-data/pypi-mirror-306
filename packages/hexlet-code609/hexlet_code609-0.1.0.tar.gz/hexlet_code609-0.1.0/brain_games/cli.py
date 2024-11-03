09caae01e14d34ed0e10a4de379a872f5506fe45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:11:31 +0000

    Add package hexlet-code609/0.1.0/hexlet_code609-0.1.0.tar.gz

---
+++ packages/hexlet-code609/hexlet_code609-0.1.0.tar.gz/hexlet_code609-0.1.0/brain_games/cli.py
@@ -0,0 +1,8 @@
+# brain_games/cli.py
+import prompt
+
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
