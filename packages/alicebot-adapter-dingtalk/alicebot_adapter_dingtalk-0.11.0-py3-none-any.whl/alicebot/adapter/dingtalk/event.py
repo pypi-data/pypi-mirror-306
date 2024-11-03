1f5f89b36fc410681db5f557ea560eaa9ec7b89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 15:03:54 +0000

    Add package alicebot-adapter-dingtalk/0.11.0/alicebot_adapter_dingtalk-0.11.0-py3-none-any.whl

---
+++ packages/alicebot-adapter-dingtalk/alicebot_adapter_dingtalk-0.11.0-py3-none-any.whl/alicebot/adapter/dingtalk/event.py
@@ -0,0 +1,99 @@
+"""DingTalk 适配器事件。"""
+
+import time
+from typing import TYPE_CHECKING, Any, Literal, Optional, Union
+from typing_extensions import override
+
+from pydantic import BaseModel, Field
+
+from alicebot.event import MessageEvent
+
+from .exceptions import WebhookExpiredError
+from .message import DingTalkMessage
+
+if TYPE_CHECKING:
+    from . import DingTalkAdapter
+
+__all__ = ["UserInfo", "Text", "DingTalkEvent"]
+
+
+class UserInfo(BaseModel):
+    """用户信息"""
+
+    dingtalkId: str
+    staffId: Optional[str] = None
+
+
+class Text(BaseModel):
+    """文本消息"""
+
+    content: str
+
+
+class DingTalkEvent(MessageEvent["DingTalkAdapter"]):
+    """DingTalk 事件基类"""
+
+    type: Optional[str] = Field(alias="msgtype")
+
+    msgtype: str
+    msgId: str
+    createAt: str
+    conversationType: Literal["1", "2"]
+    conversationId: str
+    conversationTitle: Optional[str] = None
+    senderId: str
+    senderNick: str
+    senderCorpId: Optional[str] = None
+    sessionWebhook: str
+    sessionWebhookExpiredTime: int
+    isAdmin: Optional[bool] = None
+    chatbotCorpId: Optional[str] = None
+    isInAtList: Optional[bool] = None
+    senderStaffId: Optional[str] = None
+    chatbotUserId: str
+    atUsers: list[UserInfo]
+    text: Text
+
+    response_msg: Union[None, str, dict[str, Any], DingTalkMessage] = None
+    response_at: Union[None, dict[str, Any], DingTalkMessage] = None
+
+    @property
+    def message(self) -> DingTalkMessage:
+        """返回 message 字段。"""
+        return DingTalkMessage.text(self.text.content)
+
+    @override
+    def get_sender_id(self) -> str:
+        return self.senderId
+
+    @override
+    def get_plain_text(self) -> str:
+        return self.message.get_plain_text()
+
+    @override
+    async def reply(
+        self,
+        message: Union[str, dict[str, Any], DingTalkMessage],
+        at: Union[None, dict[str, Any], DingTalkMessage] = None,
+    ) -> dict[str, Any]:
+        """回复消息。
+
+        Args:
+            message: 回复消息的内容，可以是 `str`, `Dict` 或 `DingTalkMessage`。
+            at: 回复消息时 At 的对象，必须时 at 类型的 `DingTalkMessage`，或者符合标准的 `Dict`。
+
+        Returns:
+            调用 Webhook 地址后钉钉服务器的响应。
+
+        Raises:
+            WebhookExpiredError: 当前事件的 Webhook 地址已经过期。
+            ...: 同 `DingTalkAdapter.send()` 方法。
+        """
+        if self.sessionWebhookExpiredTime > time.time() * 1000:
+            return await self.adapter.send(
+                webhook=self.sessionWebhook,
+                conversation_type=self.conversationType,
+                msg=message,
+                at=at,
+            )
+        raise WebhookExpiredError
---
+++ packages/alicebot-adapter-dingtalk/alicebot_adapter_dingtalk-0.11.0-py3-none-any.whl/alicebot/adapter/dingtalk/message.py
@@ -0,0 +1,134 @@
+"""DingTalk 适配器消息。"""
+
+from typing import Any, Optional
+from typing_extensions import override
+
+from pydantic import model_serializer
+
+from alicebot.message import MessageSegment
+
+__all__ = ["DingTalkMessage"]
+
+
+class DingTalkMessage(MessageSegment):  # type: ignore
+    """DingTalk 消息"""
+
+    @override
+    @classmethod
+    def get_message_class(cls) -> None:  # type: ignore
+        return None
+
+    @override
+    def __str__(self) -> str:
+        if self.type == "text":
+            return self.data["content"]
+        return super().__str__()
+
+    @model_serializer
+    def ser_model(self) -> dict[str, Any]:
+        """返回符合钉钉消息标准的消息字段字典。
+
+        Returns:
+            符合钉钉消息标准的消息字段字典。
+        """
+        if self.type == "raw":
+            return self.data
+        return {self.type: self.data}
+
+    def get_plain_text(self) -> str:
+        """获取消息中的纯文本部分。
+
+        Returns:
+            消息中的纯文本部分。
+        """
+        if self.type == "text":
+            return self.data["content"]
+        return ""
+
+    @classmethod
+    def raw(cls, data: dict[str, Any]) -> "DingTalkMessage":
+        """DingTalk 原始消息"""
+        return cls(type="raw", data=data)
+
+    @classmethod
+    def text(cls, content: str) -> "DingTalkMessage":
+        """DingTalk text 消息"""
+        return cls(type="text", data={"content": content})
+
+    @classmethod
+    def link(
+        cls, text: str, title: str, message_url: str, pic_url: Optional[str] = None
+    ) -> "DingTalkMessage":
+        """DingTalk link 消息"""
+        return cls(
+            type="link",
+            data={
+                "text": text,
+                "title": title,
+                "messageUrl": message_url,
+                "picUrl": pic_url,
+            },
+        )
+
+    @classmethod
+    def markdown(cls, title: str, text: str) -> "DingTalkMessage":
+        """DingTalk markdown 消息"""
+        return cls(type="markdown", data={"title": title, "text": text})
+
+    @classmethod
+    def action_card_single_btn(
+        cls,
+        title: str,
+        text: str,
+        single_title: str,
+        single_url: str,
+        btn_orientation: str = "0",
+    ) -> "DingTalkMessage":
+        """DingTalk 整体跳转 actionCard 消息"""
+        return cls(
+            type="actionCard",
+            data={
+                "title": title,
+                "text": text,
+                "singleTitle": single_title,
+                "singleURL": single_url,
+                "btnOrientation": btn_orientation,
+            },
+        )
+
+    @classmethod
+    def action_card_multi_btns(
+        cls, title: str, text: str, btns: list[Any], btn_orientation: str = "0"
+    ) -> "DingTalkMessage":
+        """DingTalk 独立跳转 actionCard 消息"""
+        return cls(
+            type="actionCard",
+            data={
+                "title": title,
+                "text": text,
+                "btns": btns,
+                "btnOrientation": btn_orientation,
+            },
+        )
+
+    @classmethod
+    def feed_card(cls, links: list[Any]) -> "DingTalkMessage":
+        """DingTalk feedCard 消息"""
+        return cls(type="feedCard", data={"links": links})
+
+    @classmethod
+    def at(
+        cls,
+        at_mobiles: Optional[list[str]] = None,
+        at_user_ids: Optional[list[str]] = None,
+        is_at_all: bool = False,
+    ) -> "DingTalkMessage":
+        """DingTalk At 信息"""
+        return cls(
+            type="at",
+            data={
+                "atMobiles": at_mobiles,
+                "atUserIds": at_user_ids,
+                "isAtAll": is_at_all,
+            },
+        )
