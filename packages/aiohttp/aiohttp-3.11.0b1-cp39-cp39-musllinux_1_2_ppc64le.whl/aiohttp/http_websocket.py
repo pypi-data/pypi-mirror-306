6b7fc6e63a339615b39e6332acc3c5361181dce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:04:21 +0000

    Add package aiohttp/3.11.0b1/aiohttp-3.11.0b1-cp39-cp39-musllinux_1_2_ppc64le.whl

---
+++ packages/aiohttp/aiohttp-3.11.0b1-cp39-cp39-musllinux_1_2_ppc64le.whl/aiohttp/http_websocket.py
@@ -0,0 +1,30 @@
+"""WebSocket protocol versions 13 and 8."""
+
+from ._websocket.helpers import WS_KEY, ws_ext_gen, ws_ext_parse
+from ._websocket.models import (
+    WS_CLOSED_MESSAGE,
+    WS_CLOSING_MESSAGE,
+    WebSocketError,
+    WSCloseCode,
+    WSHandshakeError,
+    WSMessage,
+    WSMsgType,
+)
+from ._websocket.reader import WebSocketReader
+from ._websocket.writer import WebSocketWriter
+
+__all__ = (
+    "WS_CLOSED_MESSAGE",
+    "WS_CLOSING_MESSAGE",
+    "WS_KEY",
+    "WebSocketReader",
+    "WebSocketWriter",
+    "WSMessage",
+    "WebSocketError",
+    "WSMsgType",
+    "WSCloseCode",
+    "ws_ext_gen",
+    "ws_ext_parse",
+    "WSHandshakeError",
+    "WSMessage",
+)
