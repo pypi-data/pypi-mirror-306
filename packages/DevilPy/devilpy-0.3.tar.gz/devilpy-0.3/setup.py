7716cfc11d715678de89dcccf4616e8317260707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:04:24 +0000

    Add package DevilPy/0.3/devilpy-0.3.tar.gz

---
+++ packages/DevilPy/devilpy-0.3.tar.gz/devilpy-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+
+setup(
+	version='0.3',
+	name='DevilPy',
+	author='DevilPy',
+	description='This is the best encryption library for Nasr',
+	packages=find_packages(),
+	classifiers=[
+	'Programming Language :: Python :: 3'
+
+	]
+
+)
+
+
+
+
+
