d691806b055d2dad7f627031e536b6b592ecfef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:04:21 +0000

    Add package PDF-Repair/1.0/pdf_repair-1.0.tar.gz

---
+++ packages/PDF-Repair/pdf_repair-1.0.tar.gz/pdf_repair-1.0/README.md
@@ -0,0 +1,29 @@
+# PDFRepair-PyPDF
+
+PDFRepair Tool using PyPDF.
+
+## Installation
+
+```bash
+python3 setup.py install
+```
+
+### Tested Python Version
+
+| Version        | Work? |
+| -------------- | ----- |
+| Python 3.11.7  | ✅    |
+| Python 3.10.14 | ✅    |
+
+## Usage
+
+```bash
+usage: pdf-repair [-h] [--compress] FILEPATH
+
+positional arguments:
+  FILEPATH        The PDF file or directory to repair.
+
+options:
+  -h, --help      show this help message and exit
+  --compress, -c  Compress the PDF.
+```
---
+++ packages/PDF-Repair/pdf_repair-1.0.tar.gz/pdf_repair-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as file:
+  long_description = file.read()
+
+setup(
+    name="PDF-Repair",
+    version="1.0",
+    install_requires=[
+        "pypdf",
+        "tqdm",
+        "argparse",
+    ],
+    scripts=["repair.py"],
+    author="obtuse-triangle",
+    author_email="me@obtuse.kr",
+    description="PDFRepair Tool using PyPDF",
+    entry_points={
+        "console_scripts": [
+            "pdf-repair=repair:main"
+        ]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/obtuse-triangle/PDFRepair-PyPDF"
+)
