563575515a0d1ba5cc8a2258ccf8617be1ffdcad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:05:40 +0000

    Add package CodeShakespeare/0.1.0/codeshakespeare-0.1.0.tar.gz

---
+++ packages/CodeShakespeare/codeshakespeare-0.1.0.tar.gz/codeshakespeare-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CodeShakespeare",
+    version="0.1.0",
+    description="A package for generating Shakespearean-style quotes and comments",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if any
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
