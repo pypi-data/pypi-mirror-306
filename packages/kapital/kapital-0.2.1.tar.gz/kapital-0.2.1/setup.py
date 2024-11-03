cff465f263af1e4902f7f29e8ccc10bc1ad65116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 15:04:01 +0000

    Add package kapital/0.2.1/kapital-0.2.1.tar.gz

---
+++ packages/kapital/kapital-0.2.1.tar.gz/kapital-0.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="kapital",
+    version="0.2.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Zaman Kazimov",
+    author_email="kazimovzaman2@gmail.com",
+    maintainer="Fuad Huseynov",
+    maintainer_email="fuadhuseynov@gmail.com",
+)
