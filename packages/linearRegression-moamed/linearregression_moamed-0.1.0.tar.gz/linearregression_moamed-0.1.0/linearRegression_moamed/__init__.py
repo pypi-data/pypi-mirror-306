f6f62b00ae933f7833df9e653712a6f1362eb890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 15:03:51 +0000

    Add package linearRegression-moamed/0.1.0/linearregression_moamed-0.1.0.tar.gz

---
+++ packages/linearRegression-moamed/linearregression_moamed-0.1.0.tar.gz/linearregression_moamed-0.1.0/linearRegression_moamed/__init__.py
@@ -0,0 +1,1 @@
+import linearRgression_moamed
---
+++ packages/linearRegression-moamed/linearregression_moamed-0.1.0.tar.gz/linearregression_moamed-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='linearRegression_moamed',
+    version='0.1.0',
+    author='Boufafa MOhamed',
+    author_email='boufafa.moamed@gmail.com',
+    description='A package for my own regression linear.',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
