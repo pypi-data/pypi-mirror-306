309ca9ef99b7e362e4f0b4300ce4b2d975ec4a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 15:03:49 +0000

    Add package abcmeta/2.2.1/abcmeta-2.2.1.tar.gz

---
+++ packages/abcmeta/abcmeta-2.2.1.tar.gz/abcmeta-2.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="abcmeta",
+    version="2.2.1",
+    author="Morteza NourelahiAlamdari",
+    author_email="m@0t1.me",
+    description="Python meta class and abstract method library with restrictions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="AGPLv3",
+    url="https://github.com/mortymacs/abcmeta",
+    project_urls={"Bug Tracker": "https://github.com/mortymacs/abcmeta/issues"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.9",
+)
