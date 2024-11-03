c48b0afb1d5476588a50e9fd7f8a887cc3b1715c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:11:32 +0000

    Add package patchright/0.0.1/patchright-0.0.1.tar.gz

---
+++ packages/patchright/patchright-0.0.1.tar.gz/patchright-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='patchright',
+    version='0.0.1',
+    description='',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
