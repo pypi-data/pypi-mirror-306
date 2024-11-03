bfa8c63038af12b96f9b8ba17fd6fcd6626886d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:12:52 +0000

    Add package river-rrcf/0.1.1/river_rrcf-0.1.1-py3-none-any.whl

---
+++ packages/river-rrcf/river_rrcf-0.1.1-py3-none-any.whl/river_rrcf/__init__.py
@@ -0,0 +1,4 @@
+from .__version__ import __version__
+from .rrcf import RobustRandomCutForest
+
+__all__ = ["__version__", "RobustRandomCutForest"]
---
+++ packages/river-rrcf/river_rrcf-0.1.1-py3-none-any.whl/river_rrcf/_vendor/rrcf/__init__.py
@@ -0,0 +1,5 @@
+from .rrcf import Branch, Leaf, RCTree
+from .shingle import shingle
+
+__version__ = "0.4.4"
+__all__ = ["Branch", "Leaf", "RCTree", "shingle"]
