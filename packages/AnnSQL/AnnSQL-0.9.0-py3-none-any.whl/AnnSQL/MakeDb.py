f4dd34c0073092783475e4dec96082fa63e08c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:04:27 +0000

    Add package AnnSQL/0.9.0/AnnSQL-0.9.0-py3-none-any.whl

---
+++ packages/AnnSQL/AnnSQL-0.9.0-py3-none-any.whl/AnnSQL/MakeDb.py
@@ -0,0 +1,39 @@
+from .BuildDb import BuildDb
+import scanpy as sc
+import pandas as pd
+import duckdb
+import os 
+
+class MakeDb:
+	def __init__(self, adata=None, db_name=None, db_path="db/", create_all_indexes=False, create_basic_indexes=False, convenience_view=True, layers=["X", "obs", "var", "var_names", "obsm", "varm", "obsp", "uns"]):
+		self.adata = adata
+		self.db_name = db_name
+		self.db_path = db_path
+		self.layers = layers
+		self.create_all_indexes = create_all_indexes
+		self.create_basic_indexes = create_basic_indexes
+		self.convenience_view = convenience_view
+		self.validate_params()
+		self.build_db()
+
+	def validate_params(self):
+		if self.db_name is None:
+			raise ValueError('db_name is required and must be a string')
+		if self.db_path is None:
+			raise ValueError('db_path is required and must be a valid system path')
+		if self.adata is not None:
+			if not isinstance(self.adata, sc.AnnData):
+				raise ValueError('adata must be a scanpy AnnData object')
+
+	def create_db(self):
+		if os.path.exists(self.db_path+self.db_name+'.asql'):
+			raise ValueError('The database'+ self.db_path+self.db_name+'  exists already.')
+		else:
+			if not os.path.exists(self.db_path):
+				os.makedirs(self.db_path)
+			self.conn = duckdb.connect(self.db_path+self.db_name+'.asql')
+
+	def build_db(self):
+		self.create_db()
+		BuildDb(adata=self.adata, conn=self.conn, create_all_indexes=self.create_all_indexes, create_basic_indexes=self.create_basic_indexes, convenience_view=self.convenience_view, layers=self.layers)
+		self.conn.close()
