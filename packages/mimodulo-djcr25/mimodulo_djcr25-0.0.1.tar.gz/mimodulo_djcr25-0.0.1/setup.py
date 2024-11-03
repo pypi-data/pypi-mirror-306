475c7b1fa2407138e84db11e5709b3582fbff143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:05:13 +0000

    Add package mimodulo-djcr25/0.0.1/mimodulo_djcr25-0.0.1.tar.gz

---
+++ packages/mimodulo-djcr25/mimodulo_djcr25-0.0.1.tar.gz/mimodulo_djcr25-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup , find_packages
+
+setup(
+    name="mimodulo_djcr25",
+    version="0.0.1",
+    packages = find_packages(),
+    description="modulo de saludos a tu nombre",
+    author="David chaparro",
+    author_email="chaparrodavid91@icloud.com",
+    url='https://tuurl.com', # Cambia esto por tu URL si es necesario
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
+
