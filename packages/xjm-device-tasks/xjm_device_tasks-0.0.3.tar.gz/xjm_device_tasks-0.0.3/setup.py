e9f538067355384b326c3412f9a5f7bd8ca8ff42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:04:20 +0000

    Add package xjm-device-tasks/0.0.3/xjm_device_tasks-0.0.3.tar.gz

---
+++ packages/xjm-device-tasks/xjm_device_tasks-0.0.3.tar.gz/xjm_device_tasks-0.0.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+# 获取 setup.py 所在目录的绝对路径
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# 使用绝对路径读取 requirements.txt
+with open(os.path.join(here, 'requirements.txt')) as f:
+    required = f.read().splitlines()
+
+setup(
+    name="xjm_device_tasks",
+    version="0.0.3",
+    author="23233",
+    author_email="",
+    description="Android device management and automation tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=[
+        'test*',  # 排除所有以test开头的包
+        '*.test*',  # 排除所有子包中以test开头的包
+        '*.test.*',  # 排除test包下的所有模块
+        'tests',  # 排除tests目录
+        '*.tests.*',  # 排除tests目录下的所有模块
+    ]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    install_requires=required,
+)
---
+++ packages/xjm-device-tasks/xjm_device_tasks-0.0.3.tar.gz/xjm_device_tasks-0.0.3/xjm_publish/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xjm_publish",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A private package for task execution",
+    author="XJM",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Private :: Do Not Upload",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
