643c3e539992dd2b8974ee0d7a8b9b9104411d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:04:08 +0000

    Add package meteor-maps/0.2.6/meteor_maps-0.2.6.tar.gz

---
+++ packages/meteor-maps/meteor_maps-0.2.6.tar.gz/meteor_maps-0.2.6/test/conftest.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import pytest
+
+from meteor.testing import check_test_file_exists
+
+
+@pytest.fixture(scope="session")
+def data_dir() -> Path:
+    return Path(__file__).parent.resolve() / "data"
+
+
+@pytest.fixture(scope="session")
+def testing_cif_file(data_dir: Path) -> Path:
+    return data_dir / "8a6g-chromophore-removed.cif"
+
+
+@pytest.fixture(scope="session")
+def testing_pdb_file(data_dir: Path) -> Path:
+    return data_dir / "8a6g-chromophore-removed.pdb"
+
+
+@pytest.fixture(scope="session")
+def testing_mtz_file(data_dir: Path) -> Path:
+    path = data_dir / "scaled-test-data.mtz"
+    check_test_file_exists(path)
+    return path
