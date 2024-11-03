809c5960ca4288ec98102bbe29c2ca123e78898b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:11:37 +0000

    Add package distribution-probability-np/0.3/distribution_probability_np-0.3.tar.gz

---
+++ packages/distribution-probability-np/distribution_probability_np-0.3.tar.gz/distribution_probability_np-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='distribution_probability_np',
+      version='0.3',
+      description='Gaussian and Binomial distributions',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[
+          
+      ]
+)
