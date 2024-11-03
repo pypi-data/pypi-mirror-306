951f546dd12fa12472c65b46e2c4c86436e3edcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:04:06 +0000

    Add package stollen/0.2.14/stollen-0.2.14-py3-none-any.whl

---
+++ packages/stollen/stollen-0.2.14-py3-none-any.whl/stollen/requests/types.py
@@ -0,0 +1,22 @@
+from typing import Any, Optional, Union
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class StollenRequest(BaseModel):
+    url: str
+    http_method: str
+    response_data_key: list[str]
+    headers: dict[str, Any] = Field(default_factory=dict)
+    query: dict[str, Union[str, int, float]] = Field(default_factory=dict)
+    body: Optional[Any] = None
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+class StollenResponse(BaseModel):
+    status_code: int
+    headers: dict[str, Any] = Field(default_factory=dict)
+    body: Optional[Any] = None
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
