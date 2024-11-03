37181a5a463c0d33a363acb62a91e99073d7c8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:03:26 +0000

    Add package DevDec/0.2/devdec-0.2.tar.gz

---
+++ packages/DevDec/devdec-0.2.tar.gz/devdec-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+
+setup(
+	version='0.2',
+	name='DevDec',
+	author='Devil',
+	description='This is the best encryption library for Devil',
+	packages=find_packages(),
+	classifiers=[
+	'Programming Language :: Python :: 3'
+
+	]
+
+)
+
+
+
+
+
