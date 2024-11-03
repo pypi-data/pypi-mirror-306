9cda04cf875c8210e65409c0c07c384edda4d881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:04:11 +0000

    Add package XMZSDK/0.2.0/xmzsdk-0.2.0.tar.gz

---
+++ packages/XMZSDK/xmzsdk-0.2.0.tar.gz/xmzsdk-0.2.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+setup(
+    name="XMZSDK",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[
+        # 这里添加你的包依赖，例如：
+        "opentelemetry-api",
+        "opentelemetry-sdk",
+        "opentelemetry-exporter-otlp",
+        "requests",
+        "huaweicloudsdkcore",
+        "huaweicloudsdknlp",
+        "esdk-obs-python",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="小米粥",
+    author_email="mzapi@x.mizhoubaobei.top",
+    url="https://github.com/xiaomizhoubaobei/XMZAPI",  # 你的项目URL
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)
