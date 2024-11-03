b851c09c87210e841081e61c9d883204782b4574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:12:50 +0000

    Add package TaiwanBus/0.0.1/taiwanbus-0.0.1.tar.gz

---
+++ packages/TaiwanBus/taiwanbus-0.0.1.tar.gz/taiwanbus-0.0.1/tests/test_taiwanbus.py
@@ -0,0 +1,13 @@
+import unittest
+import taiwanbus
+
+
+class TestYouBike(unittest.TestCase):
+
+    def test_youbike(self):
+        data = youbike.getdata()
+        self.assertIsInstance(data, list, "getdata() should return a list")
+
+
+if __name__ == '__main__':
+    unittest.main()
