1dc42a7143333c0a06756393dfb196633402872f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 15:04:08 +0000

    Add package gpt-researcher/0.10.3/gpt_researcher-0.10.3-py3-none-any.whl

---
+++ packages/gpt-researcher/gpt_researcher-0.10.3-py3-none-any.whl/gpt_researcher/skills/__init__.py
@@ -0,0 +1,11 @@
+from .context_manager import ContextManager
+from .researcher import ResearchConductor
+from .writer import ReportGenerator
+from .browser import BrowserManager
+
+__all__ = [
+    'ResearchConductor',
+    'ReportGenerator',
+    'ContextManager',
+    'BrowserManager'
+]
