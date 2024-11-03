9117899d5ade4e38641a2f3c38e2ff0beaff8281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:03:58 +0000

    Add package pytrilogyt/0.0.10/pytrilogyt-0.0.10.tar.gz

---
+++ packages/pytrilogyt/pytrilogyt-0.0.10.tar.gz/pytrilogyt-0.0.10/trilogyt/__init__.py
@@ -0,0 +1,7 @@
+from trilogyt.core import enrich_environment
+from trilogyt.dbt.generate import generate_model as generate_dbt_model
+from trilogyt.dbt.config import DBTConfig
+
+__version__ = "0.0.10"
+
+__all__ = ["enrich_environment", "generate_dbt_model", "DBTConfig"]
---
+++ packages/pytrilogyt/pytrilogyt-0.0.10.tar.gz/pytrilogyt-0.0.10/trilogyt/constants.py
@@ -0,0 +1,10 @@
+from logging import getLogger, Logger
+
+TRILOGY_NAMESPACE = "_trilogyt"
+
+OPTIMIZATION_NAMESPACE = "optimization"
+
+OPTIMIZATION_FILE = "_internal_cached_intermediates"
+
+
+logger: Logger = getLogger("trilogyt")
