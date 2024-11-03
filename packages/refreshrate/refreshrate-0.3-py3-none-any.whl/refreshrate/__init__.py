08feb7b179e2112b45eb0c59645d743af18f27f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:12:51 +0000

    Add package refreshrate/0.3/refreshrate-0.3-py3-none-any.whl

---
+++ packages/refreshrate/refreshrate-0.3-py3-none-any.whl/refreshrate/__init__.py
@@ -0,0 +1,47 @@
+import ctypes
+import wmi
+
+class DEVMODEW(ctypes.Structure):
+    _fields_ = [
+        ("dmDeviceName", ctypes.c_wchar * 32),
+        ("dmSpecVersion", ctypes.c_uint16),
+        ("dmDriverVersion", ctypes.c_uint16),
+        ("dmSize", ctypes.c_uint32),
+        ("dmDriverExtra", ctypes.c_uint16),
+        ("dmFields", ctypes.c_uint32),
+        ("dmPosition", ctypes.c_int32 * 2),
+        ("dmDisplayOrientation", ctypes.c_uint32),
+        ("dmDisplayFixedOutput", ctypes.c_uint32),
+        ("dmColor", ctypes.c_short),
+        ("dmDuplex", ctypes.c_short),
+        ("dmYResolution", ctypes.c_short),
+        ("dmTTOption", ctypes.c_short),
+        ("dmCollate", ctypes.c_short),
+        ("dmFormName", ctypes.c_wchar * 32),
+        ("dmLogPixels", ctypes.c_uint16),
+        ("dmBitsPerPel", ctypes.c_uint32),
+        ("dmPelsWidth", ctypes.c_uint32),
+        ("dmPelsHeight", ctypes.c_uint32),
+        ("dmDisplayFlags", ctypes.c_uint32),
+        ("dmDisplayFrequency", ctypes.c_uint32),
+        ("dmICMMethod", ctypes.c_uint32),
+        ("dmICMIntent", ctypes.c_uint32),
+        ("dmMediaType", ctypes.c_uint32),
+        ("dmDitherType", ctypes.c_uint32),
+        ("dmReserved1", ctypes.c_uint32),
+        ("dmReserved2", ctypes.c_uint32),
+        ("dmPanningWidth", ctypes.c_uint32),
+        ("dmPanningHeight", ctypes.c_uint32)
+    ]
+
+def get():
+    wmi_client = wmi.WMI()
+    try:
+        for monitor in wmi_client.Win32_VideoController():
+            refreshrate = monitor.MaxRefreshRate
+            if refreshrate is not None:
+                return refreshrate
+        print("MaxRefreshRate not found for any video controller.")
+    except Exception as e:
+        print(f"Failed to retrieve monitor refresh rate: {e}")
+    return None
