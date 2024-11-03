24ed8ec99c70c4d3e24bcd8aaba8e1f5bfc00cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:05:37 +0000

    Add package Ascii-Art-TNH/0.0.1/ascii_art_tnh-0.0.1.tar.gz

---
+++ packages/Ascii-Art-TNH/ascii_art_tnh-0.0.1.tar.gz/ascii_art_tnh-0.0.1/README.md
@@ -0,0 +1,28 @@
+# Python Package Exercise
+
+[![Run Moo Test](https://github.com/software-students-fall2024/3-python-package-tnh-tnh-needs-help/actions/workflows/test_one.yaml/badge.svg)](https://github.com/software-students-fall2024/3-python-package-tnh-tnh-needs-help/actions/workflows/test_one.yaml)
+[![Run input_parse_test](https://github.com/software-students-fall2024/3-python-package-tnh-tnh-needs-help/actions/workflows/test_two.yaml/badge.svg)](https://github.com/software-students-fall2024/3-python-package-tnh-tnh-needs-help/actions/workflows/test_two.yaml)
+[![Run wrong_test](https://github.com/software-students-fall2024/3-python-package-tnh-tnh-needs-help/actions/workflows/test_three.yaml/badge.svg)](https://github.com/software-students-fall2024/3-python-package-tnh-tnh-needs-help/actions/workflows/test_three.yaml)
+[![Run get_noise_test](https://github.com/software-students-fall2024/3-python-package-tnh-tnh-needs-help/actions/workflows/test_four.yaml/badge.svg)](https://github.com/software-students-fall2024/3-python-package-tnh-tnh-needs-help/actions/workflows/test_four.yaml)
+
+An exercise to create a Python package, build it, test it, distribute it, and use it. See [instructions](./instructions.md) for details.
+
+## How this package was created
+
+1. [Install pipenv]...
+
+## How to install and use this package
+...
+
+## How to run unit tests
+Test are included within the 'test' directory. To run these test...
+1. Install pytest in a virtual enviorment
+2. Run the test from the main project directory: 'python3 -m pytest'.
+
+## Pro tip
+
+...
+
+## Continuous integration
+
+This project uses a continuous integration workflow that builds and runs unit tests automatically with every _push_ of the code to GitHub.
---
+++ packages/Ascii-Art-TNH/ascii_art_tnh-0.0.1.tar.gz/ascii_art_tnh-0.0.1/src/ascii_art_TNH/ascii_art.py
@@ -0,0 +1,105 @@
+import src.ascii_art_TNH.noises as noises
+import src.ascii_art_TNH.gallery as gallery
+from .noises import animal_noises #testing this
+import random
+
+def ascii_art(user_input):
+  # Type of the input-make sure string
+  # Strip trailing spaces
+  animal_array = parse_input(user_input)
+  if random.random() < 0.05:
+    print(print_art("cow"))
+  if len(animal_array) == 0:
+    wrong_input("nothing", 0)
+    return
+  for animal in animal_array:
+    # If animal is in the gallery
+    if animal in gallery.animals:
+      print_art(animal)
+    elif animal in noises.animal_noises:
+      print(print_art(get_noise(animal)))
+    else:
+      # If animal is not in the gallery
+      wrong_input(animal, len(animal_array))
+
+def parse_input(user_input):
+  inputs = []
+  inputs = user_input.split(" ")
+  clean_input = []
+  for input in inputs:
+    if input != "" and input.isalpha():
+      clean_input.append(input)
+  return clean_input
+
+def print_art(animal):
+  #match animal with art
+  #calls print_noise or wrong_input
+  return(gallery.animals.get(animal))
+
+
+def get_noise(noise):
+   #match animal with noise
+
+  # return(noises.get(noise))
+  return animal_noises.get(noise)
+
+def wrong_input(animals, length):
+  #if multiple animals were inputed, print flowers
+  #if one animal, print flower
+  # reference: https://www.asciiart.eu/plants/flowers
+  flowers = r"""
+              ,,,                      ,,,
+        {{{}}    ,,,             {{{}}    ,,,
+      ,,, ~Y~    {{{}},,,      ,,, ~Y~    {{{}},,,
+    {{}}} |/,,,  ~Y~{{}}}    {{}}} |/,,,  ~Y~{{}}}
+      ~Y~ \|{{}}}/\|/ ~Y~  ,,, ~Y~ \|{{}}}/\|/ ~Y~  ,,,
+      \|/ \|/~Y~  \|,,,|/ {{}}}\|/ \|/~Y~  \|,,,|/ {{}}}
+      \|/ \|/\|/  \{{{}}/  ~Y~ \|/ \|/\|/  \{{{}}/  ~Y~
+      \|/\\|/\|/ \\|~Y~//  \|/ \|/\\|/\|/ \\|~Y~//  \|/
+      \|//\|/\|/,\\|/|/|// \|/ \|//\|/\|/,\\|/|/|// \|/
+  jgs^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  """
+  flower = r""" 
+                .-.'  '.-.
+            .-(   \  /   )-.
+          /   '..oOOo..'   \
+  ,       \.--.oOOOOOOo.--./
+  |\  ,   (   :oOOOOOOo:   )
+  _\.\/|   /'--'oOOOOOOo'--'\
+  '-.. ;/| \   .''oOOo''.   /
+  .--`'. :/|'-(   /  \   )-'
+  '--. `. / //'-'.__.'-;
+    `'-,_';//      ,  /|
+          '((       |\/./_
+            \\  . |\; ..-'
+            \\ |\: .'`--.
+              \\, .' .--'
+              ))'_,-'`
+        jgs  //-'
+            // 
+            //
+          |/
+    """
+  vase = r"""
+          *
+      *  *r*  *
+    * *a* ^Y^ *i* *
+  *m*^Y^*^\^*^Y^*s*
+  ^Y^*\*e*/*l*/*^Y^
+  *\*t*|Y^\^Y|*l*/*
+  *s*|Y^\\^/^//^Y|*a*
+  ^Y^\\_^\\\//^_//^Y^
+  ^\_^\_\_\//_/_/^_/^
+  ^^\_^\_\\/_/^_/^^
+    ^^\_ \// _/^^
+        \_\_/
+          /|\
+        /\\/\
+  """
+  if(length>1):
+    return flowers
+  elif (length==0):
+    return vase
+  else:
+    return flower
+  
