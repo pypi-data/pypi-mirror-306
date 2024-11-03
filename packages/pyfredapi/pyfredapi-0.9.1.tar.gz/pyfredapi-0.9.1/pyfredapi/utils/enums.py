10cc87c5cb407f363a2a0b56f461a3316e25f34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:04:31 +0000

    Add package pyfredapi/0.9.1/pyfredapi-0.9.1.tar.gz

---
+++ packages/pyfredapi/pyfredapi-0.9.1.tar.gz/pyfredapi-0.9.1/pyfredapi/utils/enums.py
@@ -0,0 +1,10 @@
+"""The enum module contains enum definitions."""
+
+from enum import Enum
+
+
+class ReturnFormat(str, Enum):
+    """Defines how to format the data returned from the FRED endpoint."""
+
+    pandas = "pandas"
+    json = "json"
