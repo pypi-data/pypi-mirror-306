64ed302b3f60face478e13f14bc5c5d8fb8cb75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:03:17 +0000

    Add package analoguesplit/0.1.4/analoguesplit-0.1.4-py3-none-any.whl

---
+++ packages/analoguesplit/analoguesplit-0.1.4-py3-none-any.whl/analoguesplit/__init__.py
@@ -0,0 +1,15 @@
+####################################################
+# ANALOGUE SPLIT                                   #
+# ~ Chemically biased parametric data splitting    #
+# -------------------------------------------------#
+####################################################
+
+from .analoguesplit import (
+    analogue_split,
+    calculate_fp,
+    calculate_simmat,
+    convert_smiles_to_mol,
+    plot_evaluation_results,
+    tanimoto_similarity,
+    train_and_evaluate_models,
+)
