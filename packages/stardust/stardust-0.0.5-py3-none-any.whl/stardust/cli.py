57d96f012e4b629b50478800a3ba21d9b89e91ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:12:52 +0000

    Add package stardust/0.0.5/stardust-0.0.5-py3-none-any.whl

---
+++ packages/stardust/stardust-0.0.5-py3-none-any.whl/stardust/cli.py
@@ -0,0 +1,40 @@
+import argparse
+import sys
+
+import uvicorn
+
+from stardust.stardust import Stardust
+
+from stardust.file_handler import handle
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--file",
+        metavar="F",
+        type=str,
+        default="app.py",
+        help="Python file or directory with at least one async function that returns a string, object or Response.",
+    )
+    parser.add_argument(
+        "--port",
+        metavar="P",
+        type=int,
+        default=8000,
+        help="Port number where the framework will listen for incoming requests.",
+    )
+    parser.add_argument(
+        "--debug",
+        metavar="D",
+        type=bool,
+        default=False,
+        help="Enable debug option.",
+    )
+
+    args = parser.parse_args()
+    fun = handle(args.file)
+    app = Stardust(fun=fun, port=args.port, debug=args.debug).build()
+    uvicorn.run(
+        app, host="0.0.0.0", port=args.port, access_log=False, log_level="error"
+    )
