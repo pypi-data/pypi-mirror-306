8bce2d87263149e427f37e591031ac890b34d5b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 15:04:09 +0000

    Add package pdfsc/2.0.8/pdfsc-2.0.8.tar.gz

---
+++ packages/pdfsc/pdfsc-2.0.8.tar.gz/pdfsc-2.0.8/ecommerce/configuration/ApplicationConfig.java
@@ -0,0 +1,34 @@
+package com.kiatkoding.ecommerce.configuration;
+
+import com.kiatkoding.ecommerce.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final CustomUserDetailsService detailsService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            HttpSecurity httpSecurity,
+            BCryptPasswordEncoder bCryptPasswordEncoder
+    ) throws Exception {
+        AuthenticationManagerBuilder builder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(detailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
+        return builder.build();
+    }
+
+}
