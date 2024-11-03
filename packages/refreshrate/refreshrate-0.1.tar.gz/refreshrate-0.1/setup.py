9f252e0376c37a74da9a165911dec202a41d7da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 03:12:50 +0000

    Add package refreshrate/0.1/refreshrate-0.1.tar.gz

---
+++ packages/refreshrate/refreshrate-0.1.tar.gz/refreshrate-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="refreshrate",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'wmi'
+    ],
+    author="Ayaan",
+    author_email="iamayaanalee@gmail.com",
+    description="A python package that displays your monitor refresh rate.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ruskydev/refreshrate",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
