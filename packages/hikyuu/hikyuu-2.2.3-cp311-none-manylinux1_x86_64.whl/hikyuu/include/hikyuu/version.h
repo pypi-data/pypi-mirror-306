66d3d6bdefbe9a1ab0e19a819ea11140ee877da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 14:04:10 +0000

    Add package hikyuu/2.2.3/hikyuu-2.2.3-cp311-none-manylinux1_x86_64.whl

---
+++ packages/hikyuu/hikyuu-2.2.3-cp311-none-manylinux1_x86_64.whl/hikyuu/include/hikyuu/version.h
@@ -0,0 +1,24 @@
+/*
+ * version.h
+ *
+ *  Copyright (c) 2020, hikyuu.org
+ *
+ *  Created on: 2020-4-19
+ *      Author: fasiondog
+ */
+
+#pragma once
+#ifndef HKU_VERSION_H
+#define HKU_VERSION_H
+
+// clang-format off
+#define HKU_VERSION "2.2.3"
+#define HKU_VERSION_MAJOR 2
+#define HKU_VERSION_MINOR 2
+#define HKU_VERSION_ALTER 3
+#define HKU_VERSION_BUILD 202411030250
+#define HKU_VERSION_MODE "RELEASE"
+#define HKU_VERSION_GIT "2.2.3 release.39fd4b4c (RELEASE)"
+// clang-format on
+
+#endif /* HKU_VERSION_H */
