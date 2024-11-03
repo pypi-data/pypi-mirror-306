124b8712e0bbdabb72f7855e08435519ac56fe14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 15:03:55 +0000

    Add package ponderosa/0.2.0/ponderosa-0.2.0.tar.gz

---
+++ packages/ponderosa/ponderosa-0.2.0.tar.gz/ponderosa-0.2.0/ponderosa/__init__.py
@@ -0,0 +1,16 @@
+from .ponderosa import (ArgAdderFunc,
+                     ArgGroup,
+                     ArgParser,
+                     CmdTree,
+                     NamespaceFunc,
+                     SubCmd,
+                     arggroup)
+
+__version__ = '0.2.0'
+__all__ = ['ArgAdderFunc',
+           'ArgGroup',
+           'ArgParser',
+           'CmdTree',
+           'NamespaceFunc',
+           'SubCmd',
+           'arggroup']
