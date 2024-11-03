cbc91d6630c862d886270e9f0be3f976b7307a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:04:13 +0000

    Add package known/0.0.24/known-0.0.24.tar.gz

---
+++ packages/known/known-0.0.24.tar.gz/known-0.0.24/known/nbs/__init__.py
@@ -0,0 +1,8 @@
+
+__doc__=f""" 
+-------------------------------------------------------------
+Flask-based notebook server 
+-------------------------------------------------------------
+python -m pip install Flask waitress nbconvert
+python -m known.nbs --base=/home
+"""
