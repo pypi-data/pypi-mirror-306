5f0c222dd310647de01c8d87793a4295cb63935e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 15:03:45 +0000

    Add package psychsem/0.6/psychsem-0.6.tar.gz

---
+++ packages/psychsem/psychsem-0.6.tar.gz/psychsem-0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+name='psychsem',
+version='0.6',
+description='ALL WM, All DS',
+author='Psych', 
+packages=['psychsem'],
+install_requires=[
+    #empty 
+],
+)
