16d26d21e5f7116c942a5c68858b151ab1af5fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 05:05:06 +0000

    Add package pcu/0.1.3/pcu-0.1.3.tar.gz

---
+++ packages/pcu/pcu-0.1.3.tar.gz/pcu-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pcu',
+    version='0.1.3',
+    description='Comprehensive suite for competitive programming.',
+    author='Jerry Ma',
+    author_email='jmnospam@mail.com',
+    url='https://github.com/jma127/pcu',
+    license='BSD-new',
+
+    packages=find_packages(),
+    scripts=['scripts/pcu'],
+    include_package_data=True,
+
+    install_requires=[
+        'colorama>=0.4,<1',
+        'filelock>=3,<4',
+        'inflection>=0.5,<1',
+        'pyyaml>=6,<7',
+    ],
+
+    zip_safe=False,
+)
