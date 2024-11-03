661cbc16bd08aec3fe6b6d1b86c6ec6283a974ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:05:43 +0000

    Add package xTerm/1.6.0/xTerm-1.6.0-py3-none-any.whl

---
+++ packages/xTerm/xTerm-1.6.0-py3-none-any.whl/xTerm/__init__.py
@@ -0,0 +1,8 @@
+from .Alpha import Maths, FontManager
+from .Terminal import Table
+
+__all__ = ["Maths", "FontManager", "Table"]
+__version__ = "1.6.0"
+__author__ = "Mmdrza"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024, Mmdrza.Com"
