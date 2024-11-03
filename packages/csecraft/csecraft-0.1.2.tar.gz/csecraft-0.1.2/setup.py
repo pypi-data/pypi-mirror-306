4d6ae1866233e977694698a2ceb173000d8b91e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-03 04:04:35 +0000

    Add package csecraft/0.1.2/csecraft-0.1.2.tar.gz

---
+++ packages/csecraft/csecraft-0.1.2.tar.gz/csecraft-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+library_description = ""
+with open('README.md', 'r', encoding="utf-8") as f:
+    library_description = f.read()
+
+setup(
+    name='csecraft',
+    version='0.1.2',
+    description='A Python library for easily interacting with the Google Custom Search API',
+    long_description=library_description,
+    long_description_content_type='text/markdown',
+    author='Benard K. Wachira (@benkimz)',
+    maintainer='@benkimz',
+    url='https://github.com/benkimz/csecraft',
+    license='MIT',
+    keywords=['google', 'cse', 'api', 'search', 'custom search'],
+    platforms=['any'],
+    packages=find_packages(),
+    install_requires=[
+        # Dependencies
+        'requests',
+        'pydantic',
+        'typing'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
