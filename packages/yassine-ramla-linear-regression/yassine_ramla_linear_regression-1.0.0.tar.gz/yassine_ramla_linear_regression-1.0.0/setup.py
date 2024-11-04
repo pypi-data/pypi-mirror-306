2e157056f084306edeaaf1da1288609401c970c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:12:02 +0000

    Add package yassine-ramla-linear-regression/1.0.0/yassine_ramla_linear_regression-1.0.0.tar.gz

---
+++ packages/yassine-ramla-linear-regression/yassine_ramla_linear_regression-1.0.0.tar.gz/yassine_ramla_linear_regression-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='yassine_ramla_linear_regression',
+   version='1.0.0',
+   author='yassine ramla',
+   author_email='y.ramla@esi-sba.dz',
+   packages=['yassine_ramla_linear_regression'],
+   url='http://pypi.python.org/pypi/yassine_ramla_linear_regression/',
+   license='LICENSE.txt',
+   description='An awesome package for linear regression',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['numpy']
+)
