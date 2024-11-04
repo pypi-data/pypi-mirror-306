21f6c4eeb7df802b87a0efef09c3e39301ca8929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:29 +0000

    Add package nonebot-plugin-nailongremove/2.2.1/nonebot_plugin_nailongremove-2.2.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-nailongremove/nonebot_plugin_nailongremove-2.2.1-py3-none-any.whl/nonebot_plugin_nailongremove/model/target_detection.py
@@ -0,0 +1,96 @@
+from typing import TYPE_CHECKING
+from typing_extensions import override
+
+import numpy as np
+import onnxruntime
+
+from ..config import config
+from .update import GitHubLatestReleaseModelUpdater, ModelInfo, UpdaterGroup
+from .yolox_utils import demo_postprocess, multiclass_nms, preprocess, vis
+
+if TYPE_CHECKING:
+    from . import CheckResult
+
+model_filename_sfx = f"_{config.nailong_model1_type}.onnx"
+
+
+class ModelUpdater(GitHubLatestReleaseModelUpdater):
+    @override
+    def get_info(self) -> ModelInfo[None]:
+        info = super().get_info()
+        info.filename = f"nailong{model_filename_sfx}"
+        return info
+
+
+OWNER = "nkxingxh"
+REPO = "NailongDetection"
+
+model_path, labels_path = UpdaterGroup(
+    ModelUpdater(
+        OWNER,
+        REPO,
+        lambda x: x.endswith(model_filename_sfx),
+    ),
+    GitHubLatestReleaseModelUpdater(
+        OWNER,
+        REPO,
+        lambda x: x == "labels.txt",
+    ),
+).get()
+labels = labels_path.read_text("u8").splitlines()
+
+session = onnxruntime.InferenceSession(
+    model_path,
+    providers=(
+        [
+            "TensorrtExecutionProvider",
+            "CUDAExecutionProvider",
+            "CPUExecutionProvider",
+        ]
+        if config.nailong_onnx_try_to_use_gpu
+        else ["CPUExecutionProvider"]
+    ),
+)
+input_shape = config.nailong_model1_yolox_size
+
+
+def check_image(image: np.ndarray) -> "CheckResult":
+    img, ratio = preprocess(image, input_shape)
+    ort_inputs = {session.get_inputs()[0].name: img[None, :, :, :]}
+    output = session.run(None, ort_inputs)
+    predictions = demo_postprocess(output[0], input_shape)[0]
+
+    boxes = predictions[:, :4]
+    scores = predictions[:, 4:5] * predictions[:, 5:]
+
+    boxes_xyxy = np.ones_like(boxes)
+    boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2] / 2.0
+    boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3] / 2.0
+    boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2] / 2.0
+    boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3] / 2.0
+    boxes_xyxy /= ratio
+    dets = multiclass_nms(boxes_xyxy, scores, nms_thr=0.45, score_thr=0.1)
+    if dets is None:
+        return False
+
+    final_boxes, final_scores, final_cls_inds = (
+        dets[:, :4],  # type: ignore
+        dets[:, 4],  # type: ignore
+        dets[:, 5],  # type: ignore
+    )
+    has = any(
+        True
+        for c, s in zip(final_cls_inds, final_scores)
+        if labels[int(c)] == "nailong" and s >= config.nailong_model1_score
+    )
+    if has:
+        image = vis(
+            image,
+            final_boxes,
+            final_scores,
+            final_cls_inds,
+            conf=0.3,
+            class_names=labels,
+        )
+        return True, image
+    return False
