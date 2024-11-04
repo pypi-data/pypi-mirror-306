fe90c2a4e3d7530646f15e02b14c41d4ea0e2a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:09 +0000

    Add package leonardo-floresgonzalez-calculator-5436/0.1.0/leonardo_floresgonzalez_calculator_5436-0.1.0.tar.gz

---
+++ packages/leonardo-floresgonzalez-calculator-5436/leonardo_floresgonzalez_calculator_5436-0.1.0.tar.gz/leonardo_floresgonzalez_calculator_5436-0.1.0/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name = "leonardo_floresgonzalez_calculator_5436",
+    version = "0.1.0",
+    author="Leonardo Flores Gonzalez",
+    author_email = "leof7812@gmail.com",
+    description="An advanced calculator package",
+    long_description="I am building this calculator package for CS-122 class",
+    long_description_content_type="text/markdown",
+    url="https://github.com/leo7812/my_calculator",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    python_requires='>=3.12'
+)
