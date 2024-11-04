135c8d13e5b65e3c8f5f2239ec61136537f7094e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:11:47 +0000

    Add package aiovban/0.1.0/aiovban-0.1.0.tar.gz

---
+++ packages/aiovban/aiovban-0.1.0.tar.gz/aiovban-0.1.0/src/aiovban/asyncio/device.py
@@ -0,0 +1,78 @@
+from dataclasses import dataclass, field
+from typing import Any
+
+from .streams import VBANIncomingStream, VBANTextStream, VBANCommandStream, VBANStream, VBANOutgoingStream
+from ..enums import BackPressureStrategy
+from ..packet import VBANPacket
+from ..packet.body.service import DeviceType, Features
+from ..packet.headers.service import PingFunctions, ServiceType, VBANServiceHeader
+
+
+@dataclass
+class VBANDevice:
+    address: str
+    port: int = 6980
+    default_stream_size: int = 200
+
+    _client: Any = None
+    _streams: dict = field(default_factory=dict)
+
+    async def handle_packet(self, address, packet: VBANPacket):
+        stream: VBANStream = self._streams.get(packet.header.streamname)
+        from ..packet.headers.service import VBANServiceHeader
+        if stream and isinstance(stream, VBANIncomingStream):
+            await stream.handle_packet(packet)
+        elif packet.header.streamname == 'VBAN Service' and isinstance(packet.header, VBANServiceHeader):
+            if packet.header.service == ServiceType.Identification and packet.header.function == PingFunctions.Request:
+                print(f"Received ping request from {address}")
+                await self.send_ping_response(address)
+
+        else:
+            print(f"Received packet for unregistered stream {packet.header.streamname} from {address}")
+            print(packet.header)
+
+    async def send_ping_response(self, address):
+        from ..packet.body.service import Ping
+        response_body = Ping(
+                deviceType=DeviceType.Receptor,
+                features=Features.Audio | Features.Text,
+                deviceName='Python VBAN Client',
+                version="1.0"
+            )
+        packet = VBANPacket(
+            header=VBANServiceHeader(
+                streamname='VBAN Service',
+                service=ServiceType.Identification,
+                function=PingFunctions.Response
+            ),
+            body=response_body.pack()
+        )
+        print(f"Sending ping response {response_body}")
+        out_stream = VBANOutgoingStream(name="VBAN Service", _client=self._client)
+        await out_stream.connect(address, self.port)
+        await out_stream.send_packet(packet)
+
+    def receive_stream(self, stream_name: str, back_pressure_strategy=BackPressureStrategy.DROP):
+        stream = VBANIncomingStream(stream_name, queue_size=self.default_stream_size, _back_pressure_strategy=back_pressure_strategy)
+        self._streams[stream_name] = stream
+        return stream
+
+    def text_stream(self, stream_name: str):
+        stream = VBANTextStream(stream_name, _client=self._client)
+        self._streams[stream_name] = stream
+        return stream
+
+    async def command_stream(self, update_interval: int, stream_name: str, back_pressure_strategy=BackPressureStrategy.DROP):
+        stream = VBANCommandStream(
+            name = stream_name,
+            queue_size=20,
+            update_interval=update_interval,
+            _back_pressure_strategy=back_pressure_strategy,
+            _client=self._client
+        )
+
+        await stream.connect(self.address, self.port)
+
+        self._streams[stream_name] = stream
+        self._streams['Voicemeeter-RTP'] = stream # Responses come to this stream
+        return stream
---
+++ packages/aiovban/aiovban-0.1.0.tar.gz/aiovban-0.1.0/src/aiovban/asyncio/protocol.py
@@ -0,0 +1,54 @@
+import asyncio
+from asyncio import Future
+from dataclasses import dataclass, field
+from typing import Any
+
+from . import AsyncVBANClient
+from ..packet import VBANPacket
+
+
+@dataclass
+class VBANBaseProtocol(asyncio.DatagramProtocol):
+    client: AsyncVBANClient
+
+    done: Future = asyncio.get_event_loop().create_future()
+
+    def connection_made(self, transport):
+        pass
+
+    def datagram_received(self, data, addr):
+        pass
+
+    def error_received(self, exc):
+        self.done.set_exception(exc)
+
+    def connection_lost(self, exc):
+        self.done.set_result(None)
+
+
+@dataclass
+class VBANListenerProtocol(VBANBaseProtocol):
+    loop: asyncio.BaseEventLoop = asyncio.get_running_loop()
+
+    def connection_made(self, transport):
+        print(f"Connection made to {transport}")
+
+    def datagram_received(self, data, addr):
+        packet = VBANPacket.unpack(data)
+        asyncio.create_task(self.client.process_packet(addr[0], packet))
+
+@dataclass
+class VBANSenderProtocol(VBANBaseProtocol):
+    _transport: Any = field(default=None, init=False)
+
+    def connection_made(self, transport):
+        self._transport = transport
+        print(f"Connection made to {transport.get_extra_info('peername')}")
+
+    def send_packet(self, data: VBANPacket, addr):
+        if self._transport:
+            self._transport.sendto(data.pack(), addr)
+
+    def connection_lost(self, exc):
+        self._transport = None
+        super().connection_lost(exc)
