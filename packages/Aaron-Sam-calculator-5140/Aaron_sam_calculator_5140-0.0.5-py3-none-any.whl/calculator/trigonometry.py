93b21c320622c1b03a4f4cb6f1bcd1fc260e2995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:13 +0000

    Add package Aaron-Sam-calculator-5140/0.0.5/Aaron_sam_calculator_5140-0.0.5-py3-none-any.whl

---
+++ packages/Aaron-Sam-calculator-5140/Aaron_sam_calculator_5140-0.0.5-py3-none-any.whl/calculator/trigonometry.py
@@ -0,0 +1,10 @@
+import math
+
+def sin(angle):
+    return math.sin(angle)
+
+def cos(angle):
+    return math.cos(angle)
+
+def tan(angle):
+    return math.tan(angle)
---
+++ packages/Aaron-Sam-calculator-5140/Aaron_sam_calculator_5140-0.0.5-py3-none-any.whl/tests/test_calculator.py
@@ -0,0 +1,8 @@
+from calculator import add, complex_add, ln, sqrt, cos, mean
+
+print("Addition: ", add(8, 2))
+print("Complex Addition: ", complex_add(1+4j, 4+3j))
+print("Natural log: ", ln(1))
+print("Square root: ", sqrt(9))
+print("Cosine: ", cos(0))
+print("Mean: ", mean([1, 2, 3, 4, 5]))
