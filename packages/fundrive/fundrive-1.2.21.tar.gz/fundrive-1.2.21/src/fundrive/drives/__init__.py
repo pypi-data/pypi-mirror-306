ba37431488c13c479a3c6f8ed2bf8b974948b44c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 16:06:11 +0000

    Add package fundrive/1.2.21/fundrive-1.2.21.tar.gz

---
+++ packages/fundrive/fundrive-1.2.21.tar.gz/fundrive-1.2.21/src/fundrive/drives/__init__.py
@@ -0,0 +1,9 @@
+from .tsinghua import TSingHuaDrive
+from .tsinghua import download as download_tsinghua
+from .tianchi import TianChiDrive
+from .oss import OSSDrive
+from .opendatalab import OpenDataLabDrive
+from .lanzou import LanZouDrive, LanZouSnapshot
+from .wenshushu import WSSDrive
+from .alipan import AlipanDrive
+from .os import OSDrive
