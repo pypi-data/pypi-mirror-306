6f423e51d1c94d5839ff56159669c39b201e3c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:04:37 +0000

    Add package jk_systools/0.2024.11.3/jk_systools-0.2024.11.3.tar.gz

---
+++ packages/jk_systools/jk_systools-0.2024.11.3.tar.gz/jk_systools-0.2024.11.3/jk_systools/__init__.py
@@ -0,0 +1,14 @@
+﻿
+
+__version__ = "0.2024.11.3"
+
+
+
+from .IExitCodes import IExitCodes
+from .AppRuntime import AppRuntime
+from .CLIRunCtx import CLIRunCtx
+from .AbstractCLICmd import AbstractCLICmd
+from .AbstractMultiCmdCLIApp import AbstractMultiCmdCLIApp
+
+
+
