e83a42ddf5ed657862dea4698d259d4c13cbd3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:54 +0000

    Add package fortune-package/0.1.3/fortune_package-0.1.3.tar.gz

---
+++ packages/fortune-package/fortune_package-0.1.3.tar.gz/fortune_package-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fortune_package',
+    version='0.1.3',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'python-dotenv',
+    ],
+    entry_points={
+        'console_scripts': [
+            'fortune=fortune_package.fortune:main',
+        ],
+    },
+    author='Your Name',
+    author_email='your_email@example.com',
+    description='A fun package that provides fortunes and quotes',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/fortune_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
