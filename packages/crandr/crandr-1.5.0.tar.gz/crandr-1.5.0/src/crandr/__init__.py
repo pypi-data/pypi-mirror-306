3567477af7fd15df86fe4aabbc096a6653fd57b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:11:26 +0000

    Add package crandr/1.5.0/crandr-1.5.0.tar.gz

---
+++ packages/crandr/crandr-1.5.0.tar.gz/crandr-1.5.0/src/crandr/__init__.py
@@ -0,0 +1,6 @@
+"""
+This is a unifying wrapper around xrandr and swaymsg to control your monitors/outputs,
+mainly intended for setups where only one or two monitors are enabled at the same time.
+"""
+
+from .version import __version__
