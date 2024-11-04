f2e1a22d44a556c8de759f4d0c9917507d9e86f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:31 +0000

    Add package textware/0.0.5/textware-0.0.5-py3-none-any.whl

---
+++ packages/textware/textware-0.0.5-py3-none-any.whl/textware/pipe/distr/association.py
@@ -0,0 +1,48 @@
+from typing import Iterator, TypeAlias
+
+from textware.core.batching.batchmaker import BatchMaker
+from textware.core.counting.nested_counter import NestedCounter
+from textware.core.transprocessing.base_processor import Transcessor
+
+DocStream: TypeAlias = Iterator[str]
+
+
+def distr_bay(
+    docs: DocStream,
+    batchsize: int = 5
+) -> NestedCounter:
+    """Build the bayesian distribution
+
+    Parameters
+    ----------
+    docs : DocStream
+        _description_
+    batchsize : int, optional
+        Size of the LearningWindow, by default 5
+
+    Examples
+    --------
+    >>> cnt = distr_bay(('Hello world ni hao servus!', 'Lorem ipsum ni hao'))
+    >>> cnt['ni']['hao']
+    2
+    >>> cnt['world']['ni']
+    1
+    >>> dict(cnt['world']['ipsum'])
+    {}
+    >>> cnt['world'].get('42', 0)
+    0
+    >>> cnt['hello'].get('hao')
+    1
+
+    Returns
+    -------
+    NestedCounter
+        _description_
+    """
+
+    cnt = NestedCounter()
+    doctokens = Transcessor.get_words_from_docs(docs)
+    for batch in BatchMaker.get_batches(doctokens, batchsize=batchsize):
+        keys = [(prio, batch[-1]) for prio in batch[0:-1]]
+        cnt.update_from(keys)
+    return cnt
