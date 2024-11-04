834802bf87f4d4162c2836b1b79707fac7b92fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:11:27 +0000

    Add package route-360/0.7.11/route_360-0.7.11.tar.gz

---
+++ packages/route-360/route_360-0.7.11.tar.gz/route_360-0.7.11/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+build_requires = [
+    'pandas',
+    'datasets',
+    'sentence-transformers',
+    'setfit==1.0.2',
+    'onnx==1.14.0',
+    'tqdm',
+    'scikit-learn',
+    'huggingface_hub',
+    'accelerate',
+    'ollama',
+    'matplotlib',
+    'chardet',
+    'openai==1.37.1',
+    'anthropic',
+    'onnxconverter-common',
+    'faiss-cpu',
+]
+
+route_requires = [
+    'numpy==1.24.4',
+    'tokenizers==0.19.1',
+    'onnxruntime==1.15.1',
+    'joblib==1.4.2',
+]
+
+setup(
+    name='route-360',
+    version='0.7.11',
+    description='Low latency, High Accuracy, Custom Query routers for Humans and Agents',
+    packages=find_packages('src'),  # Automatically find packages in src
+    package_dir={'': 'src'},        # Set src as the root for packages
+    install_requires=[],
+    extras_require={
+        'build': build_requires,
+        'route': route_requires,
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',         # Optional: specify minimum Python version
+)
+
