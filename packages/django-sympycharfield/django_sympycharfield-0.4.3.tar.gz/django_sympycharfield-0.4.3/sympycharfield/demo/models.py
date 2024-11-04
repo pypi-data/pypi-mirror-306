504626b70857f666bf98559c4ac67d77f3a7d8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:16 +0000

    Add package django-sympycharfield/0.4.3/django_sympycharfield-0.4.3.tar.gz

---
+++ packages/django-sympycharfield/django_sympycharfield-0.4.3.tar.gz/django_sympycharfield-0.4.3/sympycharfield/demo/models.py
@@ -0,0 +1,8 @@
+from django.db                    import models
+from sympycharfield.models        import SympyCharField
+
+class SympyCharFieldTest(models.Model):
+    formula = SympyCharField(max_length=255, blank=True, null=True)
+    
+    def __str__(self):
+        return str(self.formula)
