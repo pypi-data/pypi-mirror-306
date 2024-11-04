e6d27be9c1c5d6ce0c6e8fc8efb5eeecac1618be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:18 +0000

    Add package qxmt/0.3.4/qxmt-0.3.4.tar.gz

---
+++ packages/qxmt/qxmt-0.3.4.tar.gz/qxmt-0.3.4/qxmt/feature_maps/pennylane/__init__.py
@@ -0,0 +1,10 @@
+from qxmt.feature_maps.pennylane.ising import XXFeatureMap, YYFeatureMap, ZZFeatureMap
+from qxmt.feature_maps.pennylane.rotation import HRotationFeatureMap, RotationFeatureMap
+
+__all__ = [
+    "XXFeatureMap",
+    "YYFeatureMap",
+    "ZZFeatureMap",
+    "HRotationFeatureMap",
+    "RotationFeatureMap",
+]
