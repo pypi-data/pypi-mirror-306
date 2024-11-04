0723454aef1704047efd5d33553e0f6351b778b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:04:51 +0000

    Add package plurally/0.2.46/plurally-0.2.46-py3-none-any.whl

---
+++ packages/plurally/plurally-0.2.46-py3-none-any.whl/plurally/models/triggers.py
@@ -0,0 +1,7 @@
+from .form import Form  # noqa: F401
+from .google_auth import GMailNewEmail
+from .meta import InstagramNewDm  # noqa: F401
+from .source.email_imap import NewEmail  # noqa: F401
+from .source.schedule import Schedule  # noqa: F401
+
+__all__ = ["NewEmail", "Schedule", "InstagramNewDm", "GMailNewEmail", "Form"]
