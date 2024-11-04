ae15e42a4e8a6f3e5637ad08629290803950b890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:11:51 +0000

    Add package pydipole/0.0.1/pydipole-0.0.1-py3-none-any.whl

---
+++ packages/pydipole/pydipole-0.0.1-py3-none-any.whl/pydipole/__init__.py
@@ -0,0 +1,30 @@
+# PYDIPOLE: molecular density partition schemes based on HORTON package.
+# Copyright (C) 2023-2024 The PYDIPOLE Development Team
+#
+# This file is part of PYDIPOLE
+#
+# PYDIPOLE is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# PYDIPOLE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+# --
+"""Table of atomic dipole polarizability."""
+
+
+from .periodic import *
+from .utils import *
+
+try:
+    from ._version import __version__, __version_tuple__
+except ImportError:
+    __version__ = "0.0.0a-dev"
+    __version_tuple__ = (0, 0, 0, "a-dev")
