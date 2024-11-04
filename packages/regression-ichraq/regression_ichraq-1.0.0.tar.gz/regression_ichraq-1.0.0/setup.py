d4a58d733bab93bd8300d655711a7d09ed843611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:12:03 +0000

    Add package regression-ichraq/1.0.0/regression_ichraq-1.0.0.tar.gz

---
+++ packages/regression-ichraq/regression_ichraq-1.0.0.tar.gz/regression_ichraq-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='regression_ichraq',
+   version='1.0.0',
+   author='ichraq',
+   author_email='zi.amieur@esi-sba.dz',
+   packages=['regression_ichraq'],
+   url='http://pypi.python.org/pypi/regression_ichraq/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['regression_ichraq']
+)
