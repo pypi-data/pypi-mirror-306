7fcb7ee3dddf899c92b43690cf48b37004809162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:11:54 +0000

    Add package timethis/0.1.2/timethis-0.1.2-py3-none-any.whl

---
+++ packages/timethis/timethis-0.1.2-py3-none-any.whl/timethis.py
@@ -0,0 +1,28 @@
+from contextlib import contextmanager
+import threading
+from time import perf_counter as _timer
+
+_tld = threading.local()
+
+
+def _push_time():
+    if not hasattr(_tld, 'time_stack'):
+        _tld.time_stack = []
+    _tld.time_stack.append(_timer())
+
+
+def _pop_time(message=None, log=print):
+    elapsed_time = _timer() - _tld.time_stack.pop()
+    if message is None:
+        return
+    depth = len(_tld.time_stack)
+    log('| ' * depth + f'{message}: {elapsed_time:.3f} seconds')
+
+
+@contextmanager
+def timethis(message='', log=print):
+    _push_time()
+    try:
+        yield
+    finally:
+        _pop_time(message, log)
