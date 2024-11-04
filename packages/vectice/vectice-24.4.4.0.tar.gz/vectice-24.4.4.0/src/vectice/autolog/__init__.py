34f2f226125bbf3cd53b6c3e4b61e11f89cddc16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:03:51 +0000

    Add package vectice/24.4.4.0/vectice-24.4.4.0.tar.gz

---
+++ packages/vectice/vectice-24.4.4.0.tar.gz/vectice-24.4.4.0/src/vectice/autolog/__init__.py
@@ -0,0 +1,3 @@
+from vectice.autolog.autolog import cell, config, get_connection, iteration, notebook, phase_config
+
+__all__ = ["cell", "notebook", "config", "phase_config", "get_connection", "iteration"]
