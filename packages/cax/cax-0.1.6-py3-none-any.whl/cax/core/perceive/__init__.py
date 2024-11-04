ab8d82f93fa805ca9bcce974eb764bc48962219d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:11:26 +0000

    Add package cax/0.1.6/cax-0.1.6-py3-none-any.whl

---
+++ packages/cax/cax-0.1.6-py3-none-any.whl/cax/core/perceive/__init__.py
@@ -0,0 +1,11 @@
+"""Perception modules for Cellular Automata.
+
+This module contains various perception mechanisms used in Cellular Automata
+implementations. These perception modules are responsible for gathering
+information from the neighborhood of each cell, which is then used to
+determine the cell's next state.
+
+These modules are designed to work with the CAX library, providing flexible
+and efficient perception options for different types of cellular automata
+models.
+"""
