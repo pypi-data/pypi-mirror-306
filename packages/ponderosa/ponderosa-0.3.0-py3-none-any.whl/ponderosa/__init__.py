d1b8f26e203da19251a31d0dccfc7f62651cd8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:12:03 +0000

    Add package ponderosa/0.3.0/ponderosa-0.3.0-py3-none-any.whl

---
+++ packages/ponderosa/ponderosa-0.3.0-py3-none-any.whl/ponderosa/__init__.py
@@ -0,0 +1,16 @@
+from .ponderosa import (ArgAdderFunc,
+                     ArgGroup,
+                     ArgParser,
+                     CmdTree,
+                     NamespaceFunc,
+                     SubCmd,
+                     arggroup)
+
+__version__ = '0.3.0'
+__all__ = ['ArgAdderFunc',
+           'ArgGroup',
+           'ArgParser',
+           'CmdTree',
+           'NamespaceFunc',
+           'SubCmd',
+           'arggroup']
