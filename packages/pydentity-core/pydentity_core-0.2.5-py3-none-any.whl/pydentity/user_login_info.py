abfc0647bffbff04e2a335fcaf44ff35c6e68873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:11:22 +0000

    Add package pydentity-core/0.2.5/pydentity_core-0.2.5-py3-none-any.whl

---
+++ packages/pydentity-core/pydentity_core-0.2.5-py3-none-any.whl/pydentity/user_login_info.py
@@ -0,0 +1,22 @@
+class UserLoginInfo:
+    __slots__ = ('_login_provider', '_provider_key', '_display_name',)
+
+    def __init__(self, login_provider: str, provider_key: str, display_name: str | None = None) -> None:
+        self._login_provider = login_provider
+        self._provider_key = provider_key
+        self._display_name = display_name
+
+    @property
+    def login_provider(self) -> str:
+        return self._login_provider
+
+    @property
+    def provider_key(self) -> str:
+        return self._provider_key
+
+    @property
+    def display_name(self) -> str | None:
+        return self._display_name
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__}: {self.login_provider}:{self.provider_key}>'
