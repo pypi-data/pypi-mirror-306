7a42f9f0663bbf6db73193b0cafa3272ba455015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:22 +0000

    Add package pttavm/0.1.3/pttavm-0.1.3-py3-none-any.whl

---
+++ packages/pttavm/pttavm-0.1.3-py3-none-any.whl/pttavm/utils/version.py
@@ -0,0 +1,28 @@
+from typing import Dict
+
+def get_version() -> str:
+    """Return the current version of the package"""
+    return "0.1.3"
+
+class VersionController:
+    """Controller for checking PTT AVM API version"""
+    
+    def __init__(self, client):
+        self.client = client
+        self.service = client.get_service('version')
+    
+    def get_version(self) -> Dict[str, str]:
+        """
+        Get current API version
+        
+        Returns:
+            Dict containing version information
+        """
+        try:
+            response = self.service.get_version()
+            return {
+                'version': response.get('version', 'unknown'),
+                'release_date': response.get('releaseDate', 'unknown')
+            }
+        except Exception as e:
+            raise Exception(f"Failed to get version: {str(e)}")
