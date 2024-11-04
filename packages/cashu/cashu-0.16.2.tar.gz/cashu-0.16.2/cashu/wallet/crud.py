47f99c65121dd5c732eb14043b7f8ee5a10dba16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:03:56 +0000

    Add package cashu/0.16.2/cashu-0.16.2.tar.gz

---
+++ packages/cashu/cashu-0.16.2.tar.gz/cashu-0.16.2/cashu/wallet/crud.py
@@ -0,0 +1,464 @@
+import json
+import time
+from typing import Any, Dict, List, Optional, Tuple
+
+from ..core.base import Invoice, Proof, WalletKeyset
+from ..core.db import Connection, Database
+
+
+async def store_proof(
+    proof: Proof,
+    db: Database,
+    conn: Optional[Connection] = None,
+) -> None:
+    await (conn or db).execute(
+        """
+        INSERT INTO proofs
+          (id, amount, C, secret, time_created, derivation_path, dleq, mint_id, melt_id)
+        VALUES (:id, :amount, :C, :secret, :time_created, :derivation_path, :dleq, :mint_id, :melt_id)
+        """,
+        {
+            "id": proof.id,
+            "amount": proof.amount,
+            "C": str(proof.C),
+            "secret": str(proof.secret),
+            "time_created": int(time.time()),
+            "derivation_path": proof.derivation_path,
+            "dleq": json.dumps(proof.dleq.dict()) if proof.dleq else "",
+            "mint_id": proof.mint_id,
+            "melt_id": proof.melt_id,
+        },
+    )
+
+
+async def get_proofs(
+    *,
+    db: Database,
+    id: Optional[str] = "",
+    melt_id: str = "",
+    mint_id: str = "",
+    table: str = "proofs",
+    conn: Optional[Connection] = None,
+):
+    clauses = []
+    values: Dict[str, Any] = {}
+
+    if id:
+        clauses.append("id = :id")
+        values["id"] = id
+    if melt_id:
+        clauses.append("melt_id = :melt_id")
+        values["melt_id"] = melt_id
+    if mint_id:
+        clauses.append("mint_id = :mint_id")
+        values["mint_id"] = mint_id
+    where = ""
+    if clauses:
+        where = f"WHERE {' AND '.join(clauses)}"
+    rows = await (conn or db).fetchall(
+        f"""
+        SELECT * from {table}
+        {where}
+        """,
+        values,
+    )
+    return [Proof.from_dict(dict(r)) for r in rows] if rows else []
+
+
+async def get_reserved_proofs(
+    db: Database,
+    conn: Optional[Connection] = None,
+) -> List[Proof]:
+    rows = await (conn or db).fetchall(
+        """
+        SELECT * from proofs
+        WHERE reserved
+        """
+    )
+    return [Proof.from_dict(dict(r)) for r in rows]
+
+
+async def invalidate_proof(
+    proof: Proof,
+    db: Database,
+    conn: Optional[Connection] = None,
+) -> None:
+    await (conn or db).execute(
+        """
+        DELETE FROM proofs
+        WHERE secret = :secret
+        """,
+        {"secret": str(proof["secret"])},
+    )
+
+    await (conn or db).execute(
+        """
+        INSERT INTO proofs_used
+          (amount, C, secret, time_used, id, derivation_path, mint_id, melt_id)
+        VALUES (:amount, :C, :secret, :time_used, :id, :derivation_path, :mint_id, :melt_id)
+        """,
+        {
+            "amount": proof.amount,
+            "C": str(proof.C),
+            "secret": str(proof.secret),
+            "time_used": int(time.time()),
+            "id": proof.id,
+            "derivation_path": proof.derivation_path,
+            "mint_id": proof.mint_id,
+            "melt_id": proof.melt_id,
+        },
+    )
+
+
+async def update_proof(
+    proof: Proof,
+    *,
+    reserved: Optional[bool] = None,
+    send_id: Optional[str] = None,
+    mint_id: Optional[str] = None,
+    melt_id: Optional[str] = None,
+    db: Optional[Database] = None,
+    conn: Optional[Connection] = None,
+) -> None:
+    clauses = []
+    values: Dict[str, Any] = {}
+
+    if reserved is not None:
+        clauses.append("reserved = :reserved")
+        values["reserved"] = reserved
+        clauses.append("time_reserved = :time_reserved")
+        values["time_reserved"] = int(time.time())
+
+    if send_id is not None:
+        clauses.append("send_id = :send_id")
+        values["send_id"] = send_id
+
+    if mint_id is not None:
+        clauses.append("mint_id = :mint_id")
+        values["mint_id"] = mint_id
+
+    if melt_id is not None:
+        clauses.append("melt_id = :melt_id")
+        values["melt_id"] = melt_id
+
+    await (conn or db).execute(  # type: ignore
+        f"UPDATE proofs SET {', '.join(clauses)} WHERE secret = :secret",
+        {**values, "secret": str(proof.secret)},
+    )
+
+
+async def secret_used(
+    secret: str,
+    db: Database,
+    conn: Optional[Connection] = None,
+) -> bool:
+    rows = await (conn or db).fetchone(
+        """
+        SELECT * from proofs
+        WHERE secret = :secret
+        """,
+        {"secret": secret},
+    )
+    return rows is not None
+
+
+async def store_keyset(
+    keyset: WalletKeyset,
+    mint_url: str = "",
+    db: Optional[Database] = None,
+    conn: Optional[Connection] = None,
+) -> None:
+    await (conn or db).execute(  # type: ignore
+        """
+        INSERT INTO keysets
+          (id, mint_url, valid_from, valid_to, first_seen, active, public_keys, unit, input_fee_ppk)
+        VALUES (:id, :mint_url, :valid_from, :valid_to, :first_seen, :active, :public_keys, :unit, :input_fee_ppk)
+        """,
+        {
+            "id": keyset.id,
+            "mint_url": mint_url or keyset.mint_url,
+            "valid_from": keyset.valid_from or int(time.time()),
+            "valid_to": keyset.valid_to or int(time.time()),
+            "first_seen": keyset.first_seen or int(time.time()),
+            "active": keyset.active,
+            "public_keys": keyset.serialize(),
+            "unit": keyset.unit.name,
+            "input_fee_ppk": keyset.input_fee_ppk,
+        },
+    )
+
+
+async def get_keysets(
+    id: str = "",
+    mint_url: Optional[str] = None,
+    unit: Optional[str] = None,
+    db: Optional[Database] = None,
+    conn: Optional[Connection] = None,
+) -> List[WalletKeyset]:
+    clauses = []
+    values: Dict[str, Any] = {}
+    if id:
+        clauses.append("id = :id")
+        values["id"] = id
+    if mint_url:
+        clauses.append("mint_url = :mint_url")
+        values["mint_url"] = mint_url
+    if unit:
+        clauses.append("unit = :unit")
+        values["unit"] = unit
+    where = ""
+    if clauses:
+        where = f"WHERE {' AND '.join(clauses)}"
+
+    rows = await (conn or db).fetchall(  # type: ignore
+        f"""
+        SELECT * from keysets
+        {where}
+        """,
+        values,
+    )
+    return [WalletKeyset.from_row(r) for r in rows]
+
+
+async def update_keyset(
+    keyset: WalletKeyset,
+    db: Database,
+    conn: Optional[Connection] = None,
+) -> None:
+    await (conn or db).execute(
+        """
+        UPDATE keysets
+        SET active = :active
+        WHERE id = :id
+        """,
+        {
+            "active": keyset.active,
+            "id": keyset.id,
+        },
+    )
+
+
+async def store_lightning_invoice(
+    db: Database,
+    invoice: Invoice,
+    conn: Optional[Connection] = None,
+) -> None:
+    await (conn or db).execute(
+        """
+        INSERT INTO invoices
+          (amount, bolt11, id, payment_hash, preimage, paid, time_created, time_paid, out)
+        VALUES (:amount, :bolt11, :id, :payment_hash, :preimage, :paid, :time_created, :time_paid, :out)
+        """,
+        {
+            "amount": invoice.amount,
+            "bolt11": invoice.bolt11,
+            "id": invoice.id,
+            "payment_hash": invoice.payment_hash,
+            "preimage": invoice.preimage,
+            "paid": invoice.paid,
+            "time_created": invoice.time_created,
+            "time_paid": invoice.time_paid,
+            "out": invoice.out,
+        },
+    )
+
+
+async def get_lightning_invoice(
+    *,
+    db: Database,
+    id: str = "",
+    payment_hash: str = "",
+    out: Optional[bool] = None,
+    conn: Optional[Connection] = None,
+) -> Optional[Invoice]:
+    clauses = []
+    values: Dict[str, Any] = {}
+    if id:
+        clauses.append("id = :id")
+        values["id"] = id
+    if payment_hash:
+        clauses.append("payment_hash = :payment_hash")
+        values["payment_hash"] = payment_hash
+    if out is not None:
+        clauses.append("out = :out")
+        values["out"] = out
+
+    where = ""
+    if clauses:
+        where = f"WHERE {' AND '.join(clauses)}"
+    query = f"""
+        SELECT * from invoices
+        {where}
+        """
+    row = await (conn or db).fetchone(
+        query,
+        values,
+    )
+    return Invoice(**row) if row else None
+
+
+async def get_lightning_invoices(
+    db: Database,
+    paid: Optional[bool] = None,
+    pending: Optional[bool] = None,
+    conn: Optional[Connection] = None,
+) -> List[Invoice]:
+    clauses = []
+    values: Dict[str, Any] = {}
+
+    if paid is not None and not pending:
+        clauses.append("paid = :paid")
+        values["paid"] = paid
+
+    if pending:
+        clauses.append("paid = :paid")
+        values["paid"] = False
+        clauses.append("out = :out")
+        values["out"] = False
+
+    where = ""
+    if clauses:
+        where = f"WHERE {' AND '.join(clauses)}"
+
+    rows = await (conn or db).fetchall(
+        f"""
+        SELECT * from invoices
+        {where}
+        """,
+        values,
+    )
+    return [Invoice(**r) for r in rows]
+
+
+async def update_lightning_invoice(
+    db: Database,
+    id: str,
+    paid: bool,
+    time_paid: Optional[int] = None,
+    preimage: Optional[str] = None,
+    conn: Optional[Connection] = None,
+) -> None:
+    clauses = []
+    values: Dict[str, Any] = {}
+    clauses.append("paid = :paid")
+    values["paid"] = paid
+
+    if time_paid:
+        clauses.append("time_paid = :time_paid")
+        values["time_paid"] = time_paid
+    if preimage:
+        clauses.append("preimage = :preimage")
+        values["preimage"] = preimage
+
+    await (conn or db).execute(
+        f"UPDATE invoices SET {', '.join(clauses)} WHERE id = :id",
+        {**values, "id": id},
+    )
+
+
+async def bump_secret_derivation(
+    db: Database,
+    keyset_id: str,
+    by: int = 1,
+    skip: bool = False,
+    conn: Optional[Connection] = None,
+) -> int:
+    rows = await (conn or db).fetchone(
+        "SELECT counter from keysets WHERE id = :keyset_id", {"keyset_id": keyset_id}
+    )
+    # if no counter for this keyset, create one
+    if not rows:
+        await (conn or db).execute(
+            "UPDATE keysets SET counter = :counter WHERE id = :keyset_id",
+            {
+                "counter": 0,
+                "keyset_id": keyset_id,
+            },
+        )
+        counter = 0
+    else:
+        counter = int(rows["counter"])
+
+    if not skip:
+        await (conn or db).execute(
+            "UPDATE keysets SET counter = counter + :by WHERE id = :keyset_id",
+            {"by": by, "keyset_id": keyset_id},
+        )
+    return counter
+
+
+async def set_secret_derivation(
+    db: Database,
+    keyset_id: str,
+    counter: int,
+    conn: Optional[Connection] = None,
+) -> None:
+    await (conn or db).execute(
+        "UPDATE keysets SET counter = :counter WHERE id = :keyset_id",
+        {
+            "counter": counter,
+            "keyset_id": keyset_id,
+        },
+    )
+
+
+async def set_nostr_last_check_timestamp(
+    db: Database,
+    timestamp: int,
+    conn: Optional[Connection] = None,
+) -> None:
+    await (conn or db).execute(
+        "UPDATE nostr SET last = :last WHERE type = :type",
+        {"last": timestamp, "type": "dm"},
+    )
+
+
+async def get_nostr_last_check_timestamp(
+    db: Database,
+    conn: Optional[Connection] = None,
+) -> Optional[int]:
+    row = await (conn or db).fetchone(
+        """
+        SELECT last from nostr WHERE type = :type
+        """,
+        {"type": "dm"},
+    )
+    return row[0] if row else None
+
+
+async def get_seed_and_mnemonic(
+    db: Database,
+    conn: Optional[Connection] = None,
+) -> Optional[Tuple[str, str]]:
+    row = await (conn or db).fetchone(
+        """
+        SELECT seed, mnemonic from seed
+        """
+    )
+    return (
+        (
+            row["seed"],
+            row["mnemonic"],
+        )
+        if row
+        else None
+    )
+
+
+async def store_seed_and_mnemonic(
+    db: Database,
+    seed: str,
+    mnemonic: str,
+    conn: Optional[Connection] = None,
+) -> None:
+    await (conn or db).execute(
+        """
+        INSERT INTO seed
+          (seed, mnemonic)
+        VALUES (:seed, :mnemonic)
+        """,
+        {
+            "seed": seed,
+            "mnemonic": mnemonic,
+        },
+    )
---
+++ packages/cashu/cashu-0.16.2.tar.gz/cashu-0.16.2/cashu/wallet/lightning/lightning.py
@@ -0,0 +1,161 @@
+from typing import Optional
+
+import bolt11
+
+from ...core.base import Amount, Unit
+from ...core.helpers import sum_promises
+from ...core.settings import settings
+from ...lightning.base import (
+    InvoiceResponse,
+    PaymentResponse,
+    PaymentResult,
+    PaymentStatus,
+    StatusResponse,
+)
+from ...wallet.crud import get_lightning_invoice, get_proofs
+from ..wallet import Wallet
+
+
+class LightningWallet(Wallet):
+    """
+    Lightning wallet interface for Cashu
+    """
+
+    async def async_init(self, raise_connection_error: bool = True):
+        """Async init for lightning wallet"""
+        settings.tor = False
+        await self.load_proofs()
+        try:
+            await self.load_mint()
+        except Exception as e:
+            if raise_connection_error:
+                raise e
+
+    def __init__(self, *args, **kwargs):
+        if not args and not kwargs:
+            pass
+        super().__init__(*args, **kwargs)
+
+    async def create_invoice(
+        self, amount: int, memo: Optional[str] = None
+    ) -> InvoiceResponse:
+        """Create lightning invoice
+
+        Args:
+            amount (int): amount in satoshis
+            memo (str, optional): invoice memo. Defaults to None.
+        Returns:
+            str: invoice
+        """
+        invoice = await self.request_mint(amount, memo)
+        return InvoiceResponse(
+            ok=True, payment_request=invoice.bolt11, checking_id=invoice.payment_hash
+        )
+
+    async def pay_invoice(self, pr: str) -> PaymentResponse:
+        """Pay lightning invoice
+
+        Args:
+            pr (str): bolt11 payment request
+
+        Returns:
+            PaymentResponse: containing details of the operation
+        """
+        quote = await self.melt_quote(pr)
+        total_amount = quote.amount + quote.fee_reserve
+        assert total_amount > 0, "amount is not positive"
+        if self.available_balance < total_amount:
+            print("Error: Balance too low.")
+            return PaymentResponse(result=PaymentResult.FAILED)
+        _, send_proofs = await self.swap_to_send(self.proofs, total_amount)
+        try:
+            resp = await self.melt(send_proofs, pr, quote.fee_reserve, quote.quote)
+            if resp.change:
+                fees_paid_sat = quote.fee_reserve - sum_promises(resp.change)
+            else:
+                fees_paid_sat = quote.fee_reserve
+
+            invoice_obj = bolt11.decode(pr)
+            return PaymentResponse(
+                result=PaymentResult.SETTLED,
+                checking_id=invoice_obj.payment_hash,
+                preimage=resp.payment_preimage,
+                fee=Amount(Unit.msat, fees_paid_sat),
+            )
+        except Exception as e:
+            print("Exception:", e)
+            return PaymentResponse(result=PaymentResult.FAILED, error_message=str(e))
+
+    async def get_invoice_status(self, payment_hash: str) -> PaymentStatus:
+        """Get lightning invoice status (incoming)
+
+        Args:
+            invoice (str): lightning invoice
+
+        Returns:
+            str: status
+        """
+        invoice = await get_lightning_invoice(
+            db=self.db, payment_hash=payment_hash, out=False
+        )
+        if not invoice:
+            return PaymentStatus(result=PaymentResult.UNKNOWN)
+        if invoice.paid:
+            return PaymentStatus(result=PaymentResult.SETTLED)
+        try:
+            # to check the invoice state, we try minting tokens
+            await self.mint(invoice.amount, id=invoice.id)
+            return PaymentStatus(result=PaymentResult.SETTLED)
+        except Exception as e:
+            print(e)
+            return PaymentStatus(result=PaymentResult.FAILED)
+
+    async def get_payment_status(self, payment_hash: str) -> PaymentStatus:
+        """Get lightning payment status (outgoing)
+
+        Args:
+            payment_hash (str): lightning invoice payment_hash
+
+        Returns:
+            str: status
+        """
+
+        # NOTE: consider adding this in wallet.py and update invoice state to paid in DB
+
+        invoice = await get_lightning_invoice(
+            db=self.db, payment_hash=payment_hash, out=True
+        )
+
+        if not invoice:
+            return PaymentStatus(
+                result=PaymentResult.FAILED
+            )  # "invoice not found (in db)"
+        if invoice.paid:
+            return PaymentStatus(
+                result=PaymentResult.SETTLED, preimage=invoice.preimage
+            )  # "paid (in db)"
+        proofs = await get_proofs(db=self.db, melt_id=invoice.id)
+        if not proofs:
+            return PaymentStatus(
+                result=PaymentResult.FAILED
+            )  # "proofs not fount (in db)"
+        proofs_states = await self.check_proof_state(proofs)
+        if not proofs_states:
+            return PaymentStatus(result=PaymentResult.FAILED)  # "states not fount"
+
+        if all([p.state.pending for p in proofs_states.states]):
+            return PaymentStatus(result=PaymentResult.PENDING)  # "pending (with check)"
+        if any([p.state.spent for p in proofs_states.states]):
+            # NOTE: consider adding this check in wallet.py and mark the invoice as paid if all proofs are spent
+            return PaymentStatus(result=PaymentResult.SETTLED)  # "paid (with check)"
+        if all([p.state.unspent for p in proofs_states.states]):
+            return PaymentStatus(result=PaymentResult.FAILED)  # "failed (with check)"
+        return PaymentStatus(result=PaymentResult.UNKNOWN)  # "undefined state"
+
+    async def get_balance(self) -> StatusResponse:
+        """Get lightning balance
+
+        Returns:
+            int: balance in satoshis
+        """
+        return StatusResponse(error_message=None, balance=self.available_balance * 1000)
