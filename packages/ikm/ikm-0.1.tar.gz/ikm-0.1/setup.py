1dd486711afdb803d9bc133ba7e94fcf06989166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:04:37 +0000

    Add package ikm/0.1/ikm-0.1.tar.gz

---
+++ packages/ikm/ikm-0.1.tar.gz/ikm-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name='ikm',
+    version='0.1',
+    author='ahmed',
+    description='simple lib for ai',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+
+)
