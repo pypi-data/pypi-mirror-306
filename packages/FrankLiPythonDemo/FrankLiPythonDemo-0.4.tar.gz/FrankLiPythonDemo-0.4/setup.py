6ed18e21e910bc1ec72c09692782ba0f9204f917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:21 +0000

    Add package FrankLiPythonDemo/0.4/FrankLiPythonDemo-0.4.tar.gz

---
+++ packages/FrankLiPythonDemo/FrankLiPythonDemo-0.4.tar.gz/FrankLiPythonDemo-0.4/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="FrankLiPythonDemo",
+    version="0.4",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
+    description="李栋第一次尝试",
+    author="frankli",
+    author_email="frankli715@qq.com",
+    url="https://github.com/frankli/FrankLiPythonDemo",
+    long_description=open("README.md", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+)
