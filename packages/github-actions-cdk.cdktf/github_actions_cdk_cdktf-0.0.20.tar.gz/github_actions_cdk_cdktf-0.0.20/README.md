ebce3351f69e5d0fbb4c7e20eea00148c6da4128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:04:22 +0000

    Add package github-actions-cdk.cdktf/0.0.20/github_actions_cdk_cdktf-0.0.20.tar.gz

---
+++ packages/github-actions-cdk.cdktf/github_actions_cdk_cdktf-0.0.20.tar.gz/github_actions_cdk_cdktf-0.0.20/README.md
@@ -0,0 +1,9 @@
+# @github-actions-cdk/cdktf
+
+## Contributing
+
+Contributions are welcome! Please read the [CONTRIBUTING.md](../../CONTRIBUTING.md) for details on how to get involved.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](../../LICENCE) file for more information.
---
+++ packages/github-actions-cdk.cdktf/github_actions_cdk_cdktf-0.0.20.tar.gz/github_actions_cdk_cdktf-0.0.20/setup.py
@@ -0,0 +1,66 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "github-actions-cdk.cdktf",
+    "version": "0.0.20",
+    "description": "A TypeScript library for creating and managing GitHub Actions workflows using Constructs, enabling type-safe and modular CI/CD automation.",
+    "license": "MIT",
+    "url": "https://github.com/hupe1980/github-actions-cdk.git",
+    "long_description_content_type": "text/markdown",
+    "author": "hupe1980",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/hupe1980/github-actions-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "github_actions_cdk.cdktf",
+        "github_actions_cdk.cdktf._jsii"
+    ],
+    "package_data": {
+        "github_actions_cdk.cdktf._jsii": [
+            "cdktf@0.0.20.jsii.tgz"
+        ],
+        "github_actions_cdk.cdktf": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.8",
+    "install_requires": [
+        "cdktf>=0.20.9, <0.21.0",
+        "constructs>=10.4.2, <11.0.0",
+        "github-actions-cdk>=0.0.20, <0.0.21",
+        "jsii>=1.104.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard>=2.13.3,<4.3.0"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
