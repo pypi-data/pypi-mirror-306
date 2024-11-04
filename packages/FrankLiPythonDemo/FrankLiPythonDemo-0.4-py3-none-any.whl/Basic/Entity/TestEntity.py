1587a0de0f6d1c0f824ec063434befef26ba4668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:21 +0000

    Add package FrankLiPythonDemo/0.4/FrankLiPythonDemo-0.4-py3-none-any.whl

---
+++ packages/FrankLiPythonDemo/FrankLiPythonDemo-0.4-py3-none-any.whl/Basic/Entity/TestEntity.py
@@ -0,0 +1,24 @@
+class OldboyStudent:
+    school = 'oldboy'
+
+    def learn(self):
+        print('%s is learning' % self.school)
+
+    def eat(self):
+        print('%s is eating' % self.school)
+
+    def sleep(self):
+        print('%s is sleeping' % self.school)
+
+
+class NewboyStudent:
+    school = 'newboy'
+
+    def learn(self):
+        print('%s is learning' % self.school)
+
+    def eat(self):
+        print('%s is eating' % self.school)
+
+    def sleep(self):
+        print('%s is sleeping' % self.school)
