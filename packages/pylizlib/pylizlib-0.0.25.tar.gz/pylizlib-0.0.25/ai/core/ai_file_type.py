5eef3116919ace876160d2d014ac4d897861157b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:04:17 +0000

    Add package pylizlib/0.0.25/pylizlib-0.0.25.tar.gz

---
+++ packages/pylizlib/pylizlib-0.0.25.tar.gz/pylizlib-0.0.25/ai/core/ai_file_type.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+from network import netutils
+
+
+class AiFileType(Enum):
+    HG_MMPROJ = "mmproj"
+    HG_GGML = "ggml"
+
+
+class HgFile:
+    def __init__(
+            self,
+            file_name: str,
+            url: str,
+            file_type: AiFileType
+    ):
+        self.file_name = file_name
+        self.url = url
+        self.file_type = file_type
+
+    def get_file_size_byte(self) -> int:
+        return netutils.get_file_size_byte(self.url)
