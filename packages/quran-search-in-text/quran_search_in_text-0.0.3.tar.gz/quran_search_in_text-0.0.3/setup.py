e2f6a26812faf83dfd970b641032debd235a0f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:17 +0000

    Add package quran-search-in-text/0.0.3/quran_search_in_text-0.0.3.tar.gz

---
+++ packages/quran-search-in-text/quran_search_in_text-0.0.3.tar.gz/quran_search_in_text-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quran_search_in-text",
+    version="0.0.3",
+    packages=find_packages(),
+    include_package_data=True,
+    # سایر پارامترها
+)
