166742bdbd128a12dc6be65ad3351206dfd86623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 16:06:13 +0000

    Add package cornflow-client/2.0.0a5/cornflow-client-2.0.0a5.tar.gz

---
+++ packages/cornflow-client/cornflow-client-2.0.0a5.tar.gz/cornflow-client-2.0.0a5/cornflow_client/core/instance.py
@@ -0,0 +1,30 @@
+"""
+
+"""
+
+from abc import ABC, abstractmethod
+
+from .instance_solution import InstanceSolutionCore
+
+
+class InstanceCore(InstanceSolutionCore, ABC):
+    """
+    The instance template.
+    """
+
+    # TODO: make abstractmethod
+    def check(self, *args, **kwargs) -> dict:
+        """
+        Method that checks if there are inconsistencies in the data of the instance and if the problem is feasible
+
+        :return: An dictionary containing the inconsistencies found and indicating if the problem is infeasible
+        """
+        return dict()
+
+    @property
+    @abstractmethod
+    def schema_checks(self) -> dict:
+        """
+        A dictionary representation of the json-schema for the dictionary returned by the method Instance.check()
+        """
+        raise NotImplementedError()
