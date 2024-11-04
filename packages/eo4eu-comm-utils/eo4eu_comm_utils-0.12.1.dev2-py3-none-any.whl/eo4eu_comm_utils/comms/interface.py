27a6cb6c6ddebb2ca8a064f9df05d475f90d84fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 16:05:32 +0000

    Add package eo4eu-comm-utils/0.12.1.dev2/eo4eu_comm_utils-0.12.1.dev2-py3-none-any.whl

---
+++ packages/eo4eu-comm-utils/eo4eu_comm_utils-0.12.1.dev2-py3-none-any.whl/eo4eu_comm_utils/comms/interface.py
@@ -0,0 +1,24 @@
+import logging
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class LogLevel(Enum):
+    DEBUG = logging.DEBUG
+    INFO = logging.INFO
+    WARNING = logging.WARNING
+    ERROR = logging.ERROR
+    CRITICAL = logging.CRITICAL
+    SUCCESS = 1
+
+    def to_logging_level(self) -> int:
+        if self == LogLevel.SUCCESS:
+            return logging.INFO
+
+        return self.value
+
+
+class Comm(ABC):
+    @abstractmethod
+    def send(*args, **kwargs):
+        pass
