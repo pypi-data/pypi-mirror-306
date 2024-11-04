94e4bc6d82b2c6dcf607319d87f0a389b2b34b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:11 +0000

    Add package CheckmarxPythonSDK/1.0.5/checkmarxpythonsdk-1.0.5.tar.gz

---
+++ packages/CheckmarxPythonSDK/checkmarxpythonsdk-1.0.5.tar.gz/checkmarxpythonsdk-1.0.5/CheckmarxPythonSDK/CxOne/httpRequests.py
@@ -0,0 +1,138 @@
+# encoding: utf-8
+from .config import config
+from CheckmarxPythonSDK.utilities.httpRequests import build_request_funcs, check_response
+
+
+def get_data_from_config():
+    server_url = config.get("server")
+    token_url = config.get("access_control_url") + "/auth/realms/{TENANT_NAME}/protocol/openid-connect/token".format(
+        TENANT_NAME=config.get("tenant_name")
+    )
+    timeout = config.get("timeout")
+    verify_ssl_cert = config.get("verify")
+    cert = config.get("cert")
+    token_req_data = {
+        "grant_type": "client_credentials",
+        "username": config.get("username"),
+        "password": config.get("password"),
+        "client_id": config.get("client_id"),
+        "client_secret": config.get("client_secret"),
+    }
+    if config.get("grant_type") == "refresh_token":
+        token_req_data = {
+            "grant_type": "refresh_token",
+            "client_id": "ast-app",
+            "refresh_token": config.get("refresh_token"),
+        }
+    proxies = {
+        "http": config.get("proxy"),
+        "https": config.get("proxy"),
+    }
+    return server_url, token_url, timeout, verify_ssl_cert, cert, token_req_data, proxies
+
+
+get, post, put, patch, delete, head = build_request_funcs(get_data_from_config)
+
+headers = {
+    # "user-agent":
+    #     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    #     "Chrome/106.0.0.0 Safari/537.36"
+}
+
+
+def get_request(relative_url, is_iam=False):
+    """
+
+    Args:
+        relative_url (str):
+        is_iam (bool): True if the endpoint is for Identity And Management
+
+    Returns:
+
+    """
+
+    response = get(relative_url, is_iam=is_iam, headers=headers)
+    check_response(response)
+    return response
+
+
+def post_request(relative_url, data, files=None, is_iam=False):
+    """
+
+    Args:
+        relative_url (str):
+        data (str):
+        files:
+        is_iam (bool): True if the endpoint is for Identity And Management
+
+    Returns:
+
+    """
+    response = post(relative_url, data, files=files, is_iam=is_iam, headers=headers)
+    check_response(response)
+    return response
+
+
+def put_request(relative_url, data, files=None, is_iam=False):
+    """
+
+    Args:
+        relative_url (str):
+        data (str):
+        files:
+        is_iam (bool): True if the endpoint is for Identity And Management
+
+    Returns:
+
+    """
+    response = put(relative_url, data, files=files, is_iam=is_iam, headers=headers)
+    check_response(response)
+    return response
+
+
+def patch_request(relative_url, data, is_iam=False):
+    """
+
+    Args:
+        relative_url (str):
+        data (str):
+        is_iam (bool): True if the endpoint is for Identity And Management
+
+    Returns:
+
+    """
+    response = patch(relative_url, data, is_iam=is_iam, headers=headers)
+    check_response(response)
+    return response
+
+
+def delete_request(relative_url, data=None, is_iam=False):
+    """
+
+    Args:
+        relative_url (str):
+        data (str):
+        is_iam (bool): True if the endpoint is for Identity And Management
+
+    Returns:
+
+    """
+    response = delete(relative_url, data, is_iam=is_iam, headers=headers)
+    check_response(response)
+    return response
+
+
+def head_request(relative_url, is_iam=False):
+    """
+
+    Args:
+        relative_url (str):
+        is_iam (bool): True if the endpoint is for Identity And Management
+
+    Returns:
+
+    """
+
+    response = head(relative_url, is_iam=is_iam, headers=headers)
+    check_response(response)
+    return response
---
+++ packages/CheckmarxPythonSDK/checkmarxpythonsdk-1.0.5.tar.gz/checkmarxpythonsdk-1.0.5/CheckmarxPythonSDK/CxOne/scaAPI.py
@@ -0,0 +1,40 @@
+from ..CxScaApiSDK.api import (
+    Sca
+)
+from .httpRequests import (
+    get_request as get_re,
+    post_request as post_re,
+    put_request as put_re,
+    delete_request as delete_re,
+)
+
+url_prefix = "/api/sca"
+
+
+def get(relative_url):
+    return get_re(relative_url=url_prefix + relative_url)
+
+
+def post(relative_url, data, files=None):
+    return post_re(relative_url=url_prefix + relative_url, data=data, files=files)
+
+
+def put(relative_url, data, files=None):
+    return put_re(relative_url=url_prefix + relative_url, data=data, files=files)
+
+
+def delete(relative_url, data=None):
+    return delete_re(relative_url=url_prefix + relative_url, data=data)
+
+
+class ScaAPI(Sca):
+
+    def __init__(self, get_request=get, post_request=post, put_request=put, delete_request=delete):
+        Sca.__init__(self, get_request=get, post_request=post, put_request=put, delete_request=delete)
+        self.get_request = get_request
+        self.post_request = post_request
+        self.put_request = put_request
+        self.delete_request = delete_request
+
+    def __str__(self):
+        return """ScaAPI()"""
