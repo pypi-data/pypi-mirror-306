77bfec8e1d6fb60dbd8e504a21f6e47a4b0c17b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:13 +0000

    Add package diced/0.1.2/diced-0.1.2-cp310-none-win_amd64.whl

---
+++ packages/diced/diced-0.1.2-cp310-none-win_amd64.whl/diced/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.1"
+
+from . import lib
+from .lib import Scanner, Crispr, Region, Repeats, Repeat, Spacers, Spacer, scan
+
+__author__ = lib.__author__
+__license__ = "GPL-3.0-or-later"
+__doc__ = lib.__doc__
