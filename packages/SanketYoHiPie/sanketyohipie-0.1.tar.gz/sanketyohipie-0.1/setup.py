932791ae1601b33594ff739860354919cff4673d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 05:04:04 +0000

    Add package SanketYoHiPie/0.1/sanketyohipie-0.1.tar.gz

---
+++ packages/SanketYoHiPie/sanketyohipie-0.1.tar.gz/sanketyohipie-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='SanketYoHiPie',
+    version='0.1',
+    author='Sanket3yoprogrammer',
+    author_email='sanket@example.com',
+    description='A simple Python library for handling STT functionality',
+)
+packages = find_packages(),
+install_requirements = [
+    'selenium'
+    'webdriver_manager',
+]
