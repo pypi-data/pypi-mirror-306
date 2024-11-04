9745d8636659c244e14ea7fc7a7ef042aa7b2464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:00 +0000

    Add package treasurbox/1.0.3rc0/treasurbox-1.0.3rc0-py3-none-any.whl

---
+++ packages/treasurbox/treasurbox-1.0.3rc0-py3-none-any.whl/treasurbox/work.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env Python
+# _*_coding:utf-8 _*_
+# @Time: 2024-05-31 14:39:31
+# @Author: Alan
+# @File: work.py
+# @Describe: core code
+
+
+def add(x, y):
+    """加法函数"""
+    return x + y
+
+
+def multiply(x, y):
+    """乘法函数"""
+    return x * y
