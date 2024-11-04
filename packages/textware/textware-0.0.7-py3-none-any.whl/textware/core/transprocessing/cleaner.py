16b37bfd43a9d897460483d5279867d9d387d4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 16:05:33 +0000

    Add package textware/0.0.7/textware-0.0.7-py3-none-any.whl

---
+++ packages/textware/textware-0.0.7-py3-none-any.whl/textware/core/transprocessing/cleaner.py
@@ -0,0 +1,38 @@
+import re
+
+
+class Cleaner:
+    """Collection of text pruning and cleaning funcs
+    """
+
+    @staticmethod
+    def regularize(
+        s: str,
+        collapse_space: bool = False,
+        no_tabs: bool = False
+    ) -> str:
+        """
+        Cleans up the text.
+
+        - remove duplicate linebreaks
+        - (optionl) remove duplicated spaces
+        - (optionl) replace tabs with spaces
+
+        Parameters:
+            s (str): The input string to clean.
+            collapse_space(bool): If True, removes duplicate spaces within lines as well.
+            no_tabs (bool): If True, replaces tabs with single spaces.
+
+        Returns:
+            str: The cleaned string.
+        """
+
+        s = re.sub(r'\n{2,}', '\n', s)
+
+        if no_tabs:
+            s = s.replace('\t', ' ')
+
+        if collapse_space:
+            s = re.sub(r' {2,}', ' ', s)
+
+        return s
---
+++ packages/textware/textware-0.0.7-py3-none-any.whl/textware/pipe/__init__.py
@@ -0,0 +1,8 @@
+from textware.pipe.distr.association import distr_bay, distr_bay_quantized
+from textware.pipe.distr.markov import distr_unistate
+
+__all__ = [
+    'distr_bay_quantized',
+    'distr_bay',
+    'distr_unistate'
+]
