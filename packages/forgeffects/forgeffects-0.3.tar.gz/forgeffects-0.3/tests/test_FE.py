b516c105495b9ff17f9543db57a9b725f7cf62f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:03:51 +0000

    Add package forgeffects/0.3/forgeffects-0.3.tar.gz

---
+++ packages/forgeffects/forgeffects-0.3.tar.gz/forgeffects-0.3/tests/test_FE.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+import numpy as np
+from forgeffects.FE import FE
+
+# Obtener la ruta de la carpeta actual (donde se encuentra el script)
+current_dir = os.path.dirname(__file__)
+
+# Construir la ruta completa hacia los archivos .npy
+CC_path = os.path.join(current_dir, 'CC.npy')
+CE_path = os.path.join(current_dir, 'CE.npy')
+EE_path = os.path.join(current_dir, 'EE.npy')
+
+# Cargar los archivos .npy
+CC = np.load(CC_path)
+CE = np.load(CE_path)
+EE = np.load(EE_path)
+
+class TestFEFunction(unittest.TestCase):
+    
+    def test_CC_CE_EE_provided(self):
+        """Prueba para el caso en el que se proporcionan CC, CE y EE."""
+        result = FE(CC=CC, CE=CE, EE=EE, rep = 10000, THR = 0.5, maxorder = 5)
+        self.assertIsInstance(result, list)  # Verifica que el resultado es una lista
+        self.assertGreater(len(result), 0)  # Verifica que la lista no está vacía
+        #result[0].to_csv('archivo.csv', sep=';', index=False)
+        print(result)
+
+if __name__ == "__main__":
+    unittest.main()
