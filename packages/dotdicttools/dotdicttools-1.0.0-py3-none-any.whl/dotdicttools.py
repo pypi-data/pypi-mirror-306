6cc660d8c59b2dabf0990503ead9cff06e844cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:11:26 +0000

    Add package dotdicttools/1.0.0/dotdicttools-1.0.0-py3-none-any.whl

---
+++ packages/dotdicttools/dotdicttools-1.0.0-py3-none-any.whl/dotdicttools.py
@@ -0,0 +1,14 @@
+__version__ = "1.0.0"
+
+
+class DotDict(dict):
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __init__(self, d={}):
+        for k, v in d.items():
+            self[k] = v
+
+    def __setitem__(self, k, v):
+        return dict.__setitem__(self, k, DotDict(v) if isinstance(v, dict) else v)
