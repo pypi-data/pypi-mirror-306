ac17c691fbfe30215cba7be9e7b363f359b14bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:04:45 +0000

    Add package bohancompliment/0.1.0/bohancompliment-0.1.0-py3-none-any.whl

---
+++ packages/bohancompliment/bohancompliment-0.1.0-py3-none-any.whl/bohancompliment/__init__.py
@@ -0,0 +1,16 @@
+# src/pycompliment/__init__.py
+
+from .generator import (
+    compliment,
+    personalized_compliment,
+    multi_compliment,
+    compliment_in_language,
+)
+
+__all__ = [
+    'compliment',
+    'personalized_compliment',
+    'multi_compliment',
+    'compliment_in_language',
+]
+
