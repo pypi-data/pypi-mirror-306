9dd15696da82b7d6c91f858863b5a8259fb50f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:04:18 +0000

    Add package pancho/1.3.4/pancho-1.3.4-py3-none-any.whl

---
+++ packages/pancho/pancho-1.3.4-py3-none-any.whl/pancho/aux/wrappers.py
@@ -0,0 +1,36 @@
+import typing
+import functools
+import traceback
+
+from ..definition.contracts import Error
+
+
+def semantic(kind: typing.Literal['usecase', 'io', 'auditor', 'context', 'response', 'reader', 'writer']):
+    def decorator(func):
+        func.__dict__['__semantic__'] = kind
+
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            return await func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def skip(func):
+    func.__dict__['__semantic__'] = 'skip'
+
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        return await func(*args, **kwargs)
+
+    return wrapper
+
+
+def default_exception_wrapper(e: Exception, add_traceback: bool = False) -> Error:
+    return Error(
+        status_code=500,
+        message=str(e),
+        details=''.join(traceback.format_tb(e.__traceback__)) if add_traceback else None
+    )
