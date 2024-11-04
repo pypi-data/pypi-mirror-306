b8f3003e194390917898e9f4f2e3824d433d238a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:03:59 +0000

    Add package Aaron-Sam-calculator-5140/0.0.7/aaron_sam_calculator_5140-0.0.7.tar.gz

---
+++ packages/Aaron-Sam-calculator-5140/aaron_sam_calculator_5140-0.0.7.tar.gz/aaron_sam_calculator_5140-0.0.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Aaron_Sam_calculator_5140',
+    version='0.0.7',
+    author='Aaron Sam',
+    author_email='Aaron.sam@sjsu.edu',
+    description='A simple calculator package',
+    packages=['calculator', 'tests'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
