a64ebcf33c573e39c6cf46128eb8062e0bd12c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 16:05:33 +0000

    Add package electf4-basicML/0.1.6/electf4_basicML-0.1.6.tar.gz

---
+++ packages/electf4-basicML/electf4_basicML-0.1.6.tar.gz/electf4_basicML-0.1.6/README.md
@@ -0,0 +1,5 @@
+Example for Test package for make circle, then plot circle to display
+    from electf4 import tdd
+    tdd.tdd_make_circles()
+
+You can use this package work with the result predicted from Tensorflow, if you want to the see example you following: `Example <https://github.com/oatanurakch/electf4_BasicML/blob/master/example.ipynb>`_.
---
+++ packages/electf4-basicML/electf4_basicML-0.1.6.tar.gz/electf4_basicML-0.1.6/electf4/validators.py
@@ -0,0 +1,13 @@
+import numpy as np
+import numbers
+
+def check_random_state(seed):
+    if seed is None or seed is np.random:
+        return np.random.mtrand._rand
+    if isinstance(seed, numbers.Integral):
+        return np.random.RandomState(seed)
+    if isinstance(seed, np.random.RandomState):
+        return seed
+    raise ValueError(
+        "%r cannot be used to seed a numpy.random.RandomState instance" % seed
+    )
