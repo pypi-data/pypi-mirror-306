3cbe78899dc48ad899d600204e6a233f20f3c26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:04:35 +0000

    Add package maia2/0.6/maia2-0.6.tar.gz

---
+++ packages/maia2/maia2-0.6.tar.gz/maia2-0.6/maia2/dataset.py
@@ -0,0 +1,21 @@
+import gdown
+import os
+import pandas as pd
+
+def load_example_test_dataset(save_root = "./maia2_data"):
+    
+    url = "https://drive.google.com/uc?id=1fSu4Yp8uYj7xocbHAbjBP6DthsgiJy9X"
+    if os.path.exists(save_root) == False:
+        os.makedirs(save_root)
+    output_path = os.path.join(save_root, "example_test_dataset.csv")
+    
+    if os.path.exists(output_path):
+        print("Example test dataset already downloaded.")
+    else:
+        gdown.download(url, output_path, quiet=False)
+        
+    data = pd.read_csv(output_path)
+    data = data[data.move_ply > 10][['board', 'move', 'active_elo', 'opponent_elo']]
+    
+    return data
+    
