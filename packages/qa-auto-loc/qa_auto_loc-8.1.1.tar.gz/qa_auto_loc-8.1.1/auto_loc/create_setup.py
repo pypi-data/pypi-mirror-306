58b1aaa41626dbb3d32104043c8a9db14cf882d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:11:33 +0000

    Add package qa-auto-loc/8.1.1/qa_auto_loc-8.1.1.tar.gz

---
+++ packages/qa-auto-loc/qa_auto_loc-8.1.1.tar.gz/qa_auto_loc-8.1.1/auto_loc/create_setup.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+
+
+def create_setup(project_root, lib_root):
+    # Read text from the source .txt file
+    with open(os.path.join(lib_root, "run_text.txt"), "r") as txt_file:
+        content = txt_file.read()
+
+    # Path to the check.py file
+    check_py_path = os.path.join(project_root, "web_inspector.py")
+
+    # Check if the check.py file exists and is not empty
+    if not os.path.exists(check_py_path) or os.path.getsize(check_py_path) == 0:
+        # Create the .py file in the project root
+        with open(check_py_path, "w") as py_file:
+            py_file.write(content)
+
+        # Add check.py to .gitignore
+        gitignore_path = os.path.join(project_root, ".gitignore")
+        with open(gitignore_path, "a") as gitignore_file:
+            gitignore_file.write("\nweb_inspector.py\n")
+
+
+def main():
+    # Define the root directory of the project
+    project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(file))))))
+    lib_root = os.path.dirname(os.path.abspath(__file__))
+    parser = argparse.ArgumentParser(description="Run Web Inspector locally")
+    parser.add_argument('--run', action='store_true', help="run inspector")
+    args = parser.parse_args()
+    if args.run:
+        create_setup(project_root, lib_root)
+
+
+if __name__ == '__main__':
+    # e.g.: create-setup --run
+    main()
