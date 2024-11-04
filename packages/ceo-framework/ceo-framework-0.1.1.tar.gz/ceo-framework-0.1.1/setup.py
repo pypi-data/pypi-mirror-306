dc62e78201e8a830b5d620d61466b54eafe744ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:03:46 +0000

    Add package ceo-framework/0.1.1/ceo-framework-0.1.1.tar.gz

---
+++ packages/ceo-framework/ceo-framework-0.1.1.tar.gz/ceo-framework-0.1.1/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ceo-framework',
+    version='0.1.1',
+    description='Conditional Equality Operator Framework for conditional transformations in Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Eric D. McCullar',
+    author_email='MASAGDT@gmail.com',
+    url='https://github.com/MASAGDT/ceo_framework',
+    packages=find_packages(),
+    include_package_data=True,  # <-- Add this line
+    install_requires=[
+        'numpy',
+        'Pillow',
+        'joblib'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
