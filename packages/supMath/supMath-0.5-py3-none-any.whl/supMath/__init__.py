9e86720e16e9b95997fa407a8e3178b7ff66c488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:14 +0000

    Add package supMath/0.5/supMath-0.5-py3-none-any.whl

---
+++ packages/supMath/supMath-0.5-py3-none-any.whl/supMath/__init__.py
@@ -0,0 +1,18 @@
+"""
+Provides vast amount of unique mathematical functions and operators:
+ 
+    algebra
+    -------
+           various mathematical functions and operators which are frequently used in scientific formulas 
+
+    convert
+    -------
+           notation scale and measure-tomeasure converters
+
+    geometry
+    ---------
+           various geometrical formulas to work with shapes
+"""
+from .supConverters import *
+from .supGeometry import *
+from .supOperators import *
