6342b2e81c57456391951b19ca3d150c6676f74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:03:55 +0000

    Add package eevolve/0.0.6/eevolve-0.0.6.tar.gz

---
+++ packages/eevolve/eevolve-0.0.6.tar.gz/eevolve-0.0.6/eevolve/__init__.py
@@ -0,0 +1,6 @@
+from .brain import Brain
+from .agent import Agent
+from .board import Board
+from .task import Task, CollisionTask, BoardTask, AgentTask, FrameEndTask
+from .game import Game
+from .generator import PositionGenerator, AgentGenerator
