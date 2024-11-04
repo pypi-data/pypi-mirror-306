3a16958d4003f2414ffd7c74b4176260745e3201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 16:04:46 +0000

    Add package allied-vision-api/0.1.12/allied_vision_api-0.1.12.tar.gz

---
+++ packages/allied-vision-api/allied_vision_api-0.1.12.tar.gz/allied_vision_api-0.1.12/allied_vision_api/example/camera_api_example.py
@@ -0,0 +1,19 @@
+"""camera_api_example."""
+from allied_vision_api.camera_api import CameraApi
+
+
+if __name__ == '__main__':
+    camera_api = CameraApi()
+
+    """
+    拍一张照片:
+    
+        Args:
+            id_or_name: 支持相机的别名和相机id.
+            project_name: 所属项目, 不传默认项目名称为空字符串, 保存的图片没有项目名称.
+            timestamp: 时间戳, 默认是 00000000.00000000.
+            camera_close: 是否关闭相机, 默认不关闭, 传入True, 拍完照会关闭相机.
+            vimba_close: 是否关闭vimba, 默认不关闭, 传入True, 拍完照会关闭vimba驱动.
+            save_dir: 指定图片保存目录, 不传照片保存在执行入口同级目录下.
+    """
+    camera_api.acquire_one(id_or_name="相机的名称或id", project_name="项目名称", timestamp="时间戳", save_dir="照片保存到的目录")
