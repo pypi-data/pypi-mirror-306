09140479381420fd62c13a8365c59fc7ff0f8f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:46 +0000

    Add package azsaprfc/0.0.1/azsaprfc-0.0.1.tar.gz

---
+++ packages/azsaprfc/azsaprfc-0.0.1.tar.gz/azsaprfc-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'azsaprfc',
+    version = '0.0.1',
+    author = 'AndyZhu',
+    author_email = 'andyzhu@live.com', # 作者邮箱
+    description='SAP Tools for python', # 描述
+    # packages = ['azpysaprfc'],
+    packages=find_packages(),
+)
