eda931b8948bc18f1aaa46f12e4bd2c698a214a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:56 +0000

    Add package nathan-cohn-calculator-9641/0.0.2/nathan_cohn_calculator_9641-0.0.2.tar.gz

---
+++ packages/nathan-cohn-calculator-9641/nathan_cohn_calculator_9641-0.0.2.tar.gz/nathan_cohn_calculator_9641-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='nathan_cohn_calculator_9641',
+    version='0.0.2',
+    author='Nathan Cohn',
+    author_email='nathan.cohn@sjsu.edu',
+    description='A simple calculator package',
+    py_modules=['nathan_cohn_calculator_9641'],
+    install_requires=[
+        # List your package dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
