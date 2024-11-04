f54c6a72c94e018d9e1cea6d554f271e2723ecf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:11:55 +0000

    Add package rocket-sim-tayebk9/1.0.0/rocket_sim_tayebk9-1.0.0.tar.gz

---
+++ packages/rocket-sim-tayebk9/rocket_sim_tayebk9-1.0.0.tar.gz/rocket_sim_tayebk9-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='rocket-sim-tayebk9',
+   version='1.0.0',
+   author='Kahia Tayeb',
+   author_email='t.kahia@esi-sba.dz',
+   packages=find_packages(),
+   url='http://pypi.python.org/pypi/rocket/',
+   license='MIT',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=[]
+)
+
+
