60466f3e4fad5f1e8a5a462f7cc24ad0dd9cb693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:04:31 +0000

    Add package ceo-framework/0.1.7/ceo-framework-0.1.7.tar.gz

---
+++ packages/ceo-framework/ceo-framework-0.1.7.tar.gz/ceo-framework-0.1.7/ceo_framework/ceo_core.py
@@ -0,0 +1,27 @@
+from .transformations import Transformation  # Directly import from transformations
+from .conditions import Condition  # Directly import from conditions
+
+class CEO:
+    def __init__(self):
+        self.conditions = []
+        self.transformations = []
+
+    def add_condition(self, condition):
+        """Adds a Condition instance to the CEO."""
+        if not isinstance(condition, Condition):
+            raise TypeError("Condition must be an instance of Condition class.")
+        self.conditions.append(condition)
+
+    def add_transformation(self, transformation):
+        """Adds a Transformation instance to the CEO."""
+        if not isinstance(transformation, Transformation):
+            raise TypeError("Transformation must be an instance of Transformation class.")
+        self.transformations.append(transformation)
+
+    def process(self, data):
+        """Evaluates conditions and applies transformations conditionally."""
+        # If all conditions are met, apply all transformations
+        if all(condition.evaluate(data) for condition in self.conditions):
+            for transformation in self.transformations:
+                data = transformation.apply(data)
+        return data
