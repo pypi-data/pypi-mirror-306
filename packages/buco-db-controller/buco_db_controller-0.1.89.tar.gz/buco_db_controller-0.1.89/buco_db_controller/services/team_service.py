4adc9fb06aa6d3066926056e3da23631ce1308c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 05:04:20 +0000

    Add package buco-db-controller/0.1.89/buco_db_controller-0.1.89.tar.gz

---
+++ packages/buco-db-controller/buco_db_controller-0.1.89.tar.gz/buco_db_controller-0.1.89/buco_db_controller/services/team_service.py
@@ -0,0 +1,60 @@
+from typing import Union, List
+
+from buco_db_controller.models.team import Team
+from buco_db_controller.models.teams_stats import TeamStats
+from buco_db_controller.repositories.fixture_repository import FixtureRepository
+from buco_db_controller.repositories.team_repository import TeamRepository
+
+
+class TeamService:
+    def __init__(self):
+        self.team_repository = TeamRepository()
+        self.fixture_repository = FixtureRepository()
+
+    def upsert_many_teams(self, teams: List[dict]):
+        self.team_repository.upsert_many_teams(teams)
+
+    def get_teams(self, league_id: int, season: int) -> List[Team]:
+        response = self.team_repository.get_teams(league_id, season)
+
+        if not response.get('data', []):
+            raise ValueError(f'No teams found for league {league_id} and season {season}')
+
+        teams = [Team.from_dict(team) for team in response['data']]
+        return teams
+
+    def get_team_ids(self, league_id: int, seasons: Union[int, List[int]]) -> List[int]:
+        teams_over_seasons = self.team_repository.get_many_teams(league_id, seasons)
+
+        team_ids = []
+        for teams in teams_over_seasons:
+            team_ids.extend([team['team']['id'] for team in teams['data']])
+
+        team_ids = list(map(int, set(team_ids)))
+        team_ids.sort()
+        return team_ids
+
+    def count_teams_consecutive_years_in_league(self, ht_id: int, at_id: int, league_id: int, current_season: int) -> dict:
+        seasons_to_check = list(range(2014, current_season + 1))
+
+        def get_years_in_league(team_id):
+            _consecutive_years_in_league = 0
+
+            for season in seasons_to_check:
+                fixtures = self.fixture_repository.get_team_fixtures(team_id, league_id, season)
+
+                if fixtures and fixtures['data'][0]['league']['id'] == league_id:
+                    _consecutive_years_in_league += 1
+                else:
+                    _consecutive_years_in_league = 0
+
+            return _consecutive_years_in_league
+
+        ht_consecutive_years_in_league = get_years_in_league(ht_id)
+        at_consecutive_years_in_league = get_years_in_league(at_id)
+
+        years_in_league = {
+            'ht': ht_consecutive_years_in_league,
+            'at': at_consecutive_years_in_league
+        }
+        return years_in_league
