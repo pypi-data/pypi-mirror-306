c870e7d7ec639d16968d5a60fc69a5792fa8ed4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:56 +0000

    Add package pitrading/0.3.12/pitrading-0.3.12-py2.py3-none-any.whl

---
+++ packages/pitrading/pitrading-0.3.12-py2.py3-none-any.whl/pitrading/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for PiTrading."""
+
+__author__ = """saaltfiish"""
+__email__ = 'hujiazhen@piasset.net'
+__version__ = '0.3.12'
+
+from . import holidays
+from . import instrument
+from . import utils
+from .holidays import Holidays
+from .instrument import Instrument
