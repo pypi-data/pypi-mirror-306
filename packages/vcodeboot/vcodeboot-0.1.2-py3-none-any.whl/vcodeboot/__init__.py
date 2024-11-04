635a6dbee8a06d25b0ea0b76e644b5f85ed5ca42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 03:11:21 +0000

    Add package vcodeboot/0.1.2/vcodeboot-0.1.2-py3-none-any.whl

---
+++ packages/vcodeboot/vcodeboot-0.1.2-py3-none-any.whl/vcodeboot/__init__.py
@@ -0,0 +1,14 @@
+print("""
+========================================================
+                Welcome to Codeboot!
+               Created by Victorio N.
+            Thanks to Claude ai, ChatGPT
+========================================================
+
+IMPORTANT: To use vcodeboot turtle,
+      START YOUR CODE WITH turtlemode()
+
+========================================================
+""")
+from .globaux import *
+from .pixels import *
