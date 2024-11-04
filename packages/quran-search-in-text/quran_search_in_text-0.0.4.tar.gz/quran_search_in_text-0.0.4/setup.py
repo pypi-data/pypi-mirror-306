858777223744ee0791506eff480540a1d7bf069b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:19 +0000

    Add package quran-search-in-text/0.0.4/quran_search_in_text-0.0.4.tar.gz

---
+++ packages/quran-search-in-text/quran_search_in_text-0.0.4.tar.gz/quran_search_in_text-0.0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quran_search_in_text",
+    version="0.0.4",
+    packages=find_packages(),
+    include_package_data=True,
+    # سایر پارامترها
+)
