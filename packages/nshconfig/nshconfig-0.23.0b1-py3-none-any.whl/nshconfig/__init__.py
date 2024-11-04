5e77ce72043e417bca960ba0dd9ac50147e5901a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:23 +0000

    Add package nshconfig/0.23.0b1/nshconfig-0.23.0b1-py3-none-any.whl

---
+++ packages/nshconfig/nshconfig-0.23.0b1-py3-none-any.whl/nshconfig/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from pydantic import *  # type: ignore  # noqa: F403
+
+from ._config import Config as Config
+from ._config import ConfigDict as ConfigDict
+from ._export import Export as Export
+from ._missing import MISSING as MISSING
+from ._missing import AllowMissing as AllowMissing
+from ._missing import MissingField as MissingField
+from ._registry import Registry as Registry
