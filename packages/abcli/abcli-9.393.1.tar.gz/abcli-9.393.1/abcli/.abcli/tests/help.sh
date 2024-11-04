831efcea8e5b020a46f87e9000728790d0afd051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:31 +0000

    Add package abcli/9.393.1/abcli-9.393.1.tar.gz

---
+++ packages/abcli/abcli-9.393.1.tar.gz/abcli-9.393.1/abcli/.abcli/tests/help.sh
@@ -0,0 +1,51 @@
+#! /usr/bin/env bash
+
+function test_abcli_help() {
+    local options=$1
+
+    local module
+    for module in \
+        "@git" \
+        "@git browse" \
+        "@git checkout" \
+        "@git clone" \
+        "@git create_branch" \
+        "@git create_pull_request" \
+        "@git get_branch" \
+        "@git get_repo_name" \
+        "@git increment_version" \
+        "@git pull" \
+        "@git push" \
+        "@git recreate_ssh" \
+        "@git reset" \
+        "@git review" \
+        "@git status" \
+        "@git sync_fork" \
+        \
+        "@gpu status get" \
+        "@gpu status show" \
+        "@gpu validate" \
+        \
+        "@init" \
+        \
+        "@plugins get_module_name" \
+        "@plugins install" \
+        "@plugins list_of_external" \
+        "@plugins list_of_installed" \
+        "@plugins transform" \
+        \
+        "@terraform" \
+        "@terraform cat" \
+        "@terraform disable" \
+        "@terraform enable" \
+        \
+        "abcli"; do
+        abcli_eval ,$options \
+            abcli_help $module
+        [[ $? -ne 0 ]] && return 1
+
+        abcli_hr
+    done
+
+    return 0
+}
