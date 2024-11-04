69721b8b8d07bb8447775d9bd8e3e7e4b72733e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:49 +0000

    Add package scratchcon/1.1.1b0/scratchcon-1.1.1b0.tar.gz

---
+++ packages/scratchcon/scratchcon-1.1.1b0.tar.gz/scratchcon-1.1.1b0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scratchcon',
+    version='1.1.1-beta',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'scratchattach',
+    ],
+    author='G1ad0s',
+    author_email="on67703@gmail.com",
+    description='A Python library for interacting with the scratch.mit.edu API',
+    long_description=open('README.rst').read(),
+    url='https://github.com/G1ad0s/scratchcon',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
