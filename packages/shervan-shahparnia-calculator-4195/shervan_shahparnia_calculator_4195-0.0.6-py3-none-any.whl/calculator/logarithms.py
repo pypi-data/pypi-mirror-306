d18124e8aebb4d86e98b5ae3452fe59d0b125b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:29 +0000

    Add package shervan-shahparnia-calculator-4195/0.0.6/shervan_shahparnia_calculator_4195-0.0.6-py3-none-any.whl

---
+++ packages/shervan-shahparnia-calculator-4195/shervan_shahparnia_calculator_4195-0.0.6-py3-none-any.whl/calculator/logarithms.py
@@ -0,0 +1,10 @@
+import math
+
+def natural_log(x):
+    return math.log(x)
+
+def log_base_10(x):
+    return math.log10(x)
+
+def exponential(x):
+    return math.exp(x)
---
+++ packages/shervan-shahparnia-calculator-4195/shervan_shahparnia_calculator_4195-0.0.6-py3-none-any.whl/calculator/trigonometry.py
@@ -0,0 +1,10 @@
+import math
+
+def sine(angle):
+    return math.sin(math.radians(angle))
+
+def cosine(angle):
+    return math.cos(math.radians(angle))
+
+def tangent(angle):
+    return math.tan(math.radians(angle))
