bcca3264938448d582f43baf5c7b725ff42bf19b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 04:04:51 +0000

    Add package eda-visualizer/0.2.4.1/eda_visualizer-0.2.4.1.tar.gz

---
+++ packages/eda-visualizer/eda_visualizer-0.2.4.1.tar.gz/eda_visualizer-0.2.4.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eda_visualizer',
+    version='0.2.4.1',
+    description='A Python library for automatic univariate analysis using Pandas and Matplotlib',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Rohan',
+    author_email='rohanpatil4600@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'seaborn'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
