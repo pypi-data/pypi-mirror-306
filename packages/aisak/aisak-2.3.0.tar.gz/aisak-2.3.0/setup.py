82b1e4f6c8f5caf426de4945246e0b4e95fc9198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 15:04:17 +0000

    Add package aisak/2.3.0/aisak-2.3.0.tar.gz

---
+++ packages/aisak/aisak-2.3.0.tar.gz/aisak-2.3.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aisak",
+    version="2.3.0",
+    author="Mandela Logan",
+    author_email="mandelakorilogan@gmail.com",
+    description="AISAK, short for Artificially Intelligent Swiss Army Knife, is a general-purpose AI system comprising various models designed for different tasks. Developed by the AISAK team, one of the models within AISAK is a state-of-the-art large multimodal model designed for text generation tasks. This package leverages usage of the model, named AISAK-O, which is fine-tuned on extensive datasets to excel in understanding and interpreting various queries in natural language text.",
+    packages=find_packages(),
+    install_requires=[
+        "accelerate",
+        "torch",
+		  "torchaudio",
+		  "torchvision",
+		  "pillow",
+        "transformers==4.45.1",
+		  "edge-tts",
+		  "nest_asyncio",
+		  "asyncio",
+		  "wavio",
+		  "requests",
+		  "sounddevice",
+		  "qwen-vl-utils",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
