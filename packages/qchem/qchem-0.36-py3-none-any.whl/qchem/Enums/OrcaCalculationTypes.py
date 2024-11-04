84dffb46282b97410b8c16eacab240268585b820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-04 14:04:44 +0000

    Add package qchem/0.36/qchem-0.36-py3-none-any.whl

---
+++ packages/qchem/qchem-0.36-py3-none-any.whl/qchem/Enums/OrcaCalculationTypes.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class OrcaCalculationType(Enum):
+    OPTIMIZATION = "OPT"
+    INFRARED_SPECTRUM = "FREQ"
+    HARTREE_FOCK = "HF"
---
+++ packages/qchem/qchem-0.36-py3-none-any.whl/qchem/Enums/OrcaDensityFunctional.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class OrcaDensityFunctional(Enum):
+    B3LYP = "B3LYP"
