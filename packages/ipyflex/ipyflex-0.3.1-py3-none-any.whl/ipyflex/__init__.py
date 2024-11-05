d047bd980ca76f8650abe96c292bae5b9518a1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 17:05:00 +0000

    Add package ipyflex/0.3.1/ipyflex-0.3.1-py3-none-any.whl

---
+++ packages/ipyflex/ipyflex-0.3.1-py3-none-any.whl/ipyflex/__init__.py
@@ -0,0 +1,20 @@
+from .flex_layout import FlexLayout
+from ._version import __version__
+
+__all__ = ['FlexLayout', '__version__']
+
+
+try:
+    from ._version import __version__
+except ImportError:
+    # Fallback when using the package in dev mode without installing
+    # in editable mode with pip. It is highly recommended to install
+    # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
+    import warnings
+
+    warnings.warn("Importing 'ipyflex' outside a proper installation.")
+    __version__ = "dev"
+
+
+def _jupyter_labextension_paths():
+    return [{"src": "labextension", "dest": "ipyflex"}]
