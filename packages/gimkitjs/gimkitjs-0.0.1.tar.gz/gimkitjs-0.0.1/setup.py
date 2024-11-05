f079c21ea8bf5cd1db9621ad7e902d00c0c27f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 04:04:19 +0000

    Add package gimkitjs/0.0.1/gimkitjs-0.0.1.tar.gz

---
+++ packages/gimkitjs/gimkitjs-0.0.1.tar.gz/gimkitjs-0.0.1/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name="gimkitjs",              # Thay "my_module" bằng tên bạn muốn giữ
+    version="0.0.1",
+    packages=["gimkitjs"],
+    description="Reserved package name",
+    long_description="This package name is reserved.",
+    author="bingcube",
+    author_email="blurskydev@gmail.com",
+)
