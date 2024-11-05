82a106add89d1166271fb29718c37fcb42a7059e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:30 +0000

    Add package eym/0.1.0/eym-0.1.0.tar.gz

---
+++ packages/eym/eym-0.1.0.tar.gz/eym-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eym",
+    version="0.1.0",
+    description="eym is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'eym = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
