d86e2aad549c63146cc6dd66c35f03b4469c7ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 07:04:32 +0000

    Add package cqg/0.1.0/cqg-0.1.0.tar.gz

---
+++ packages/cqg/cqg-0.1.0.tar.gz/cqg-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cqg",
+    version="0.1.0",
+    description="cqg is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'cqg = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
