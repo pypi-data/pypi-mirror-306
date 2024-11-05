8781897f5cd7c551568aa4e9863a381b918c5b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:04:39 +0000

    Add package aws_administrator/2.1.0/aws_administrator-2.1.0-py3-none-any.whl

---
+++ packages/aws_administrator/aws_administrator-2.1.0-py3-none-any.whl/aws_administrator/scripts/aws_sso_mappings.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: latin-1 -*-
+"""Update AWS SSO mappings data with new principals."""
+
+from ..helpers import helper_aws_sso
+from ..helpers import helper_common
+from ..helpers.helper_parameters import *
+
+# Read CSV file with new identity mappings.
+data = helper_common.read_csv(
+    sso_mappings_map_file
+)
+
+# Process JSON data to update to new identities.
+mappings = helper_aws_sso.process_mappings(
+    data,
+    sso_mappings_input_file,
+    sso_mappings_old_user_heading,
+    sso_mappings_new_user_heading,
+    sso_mappings_old_group_pattern,
+    sso_mappings_new_group_pattern
+)
+
+# Write updated principals JSON file.
+helper_common.write_json_str(
+    sso_mappings_output_file,
+    mappings
+)
