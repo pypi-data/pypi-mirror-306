761f89f16d0507808e2440d644ed5e4135277c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:17 +0000

    Add package AuroraSign/0.0.40/aurorasign-0.0.40.tar.gz

---
+++ packages/AuroraSign/aurorasign-0.0.40.tar.gz/aurorasign-0.0.40/AuroraSign/aurora_signature/sign/validation/__init__.py
@@ -0,0 +1,16 @@
+
+from .pdf_embedded import (
+    read_certification_data,
+)
+from .dss import DocumentSecurityStore
+
+from .ltv import (
+    get_timestamp_chain,
+)
+
+__all__ = [
+    'read_certification_data',
+    'get_timestamp_chain',
+    'DocumentSecurityStore'
+]
+
