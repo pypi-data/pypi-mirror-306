93ade45e22519d7ba7001b415430e5275c7687d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 07:04:37 +0000

    Add package omdev/0.0.0.dev103/omdev-0.0.0.dev103.tar.gz

---
+++ packages/omdev/omdev-0.0.0.dev103.tar.gz/omdev-0.0.0.dev103/omdev/scripts/execrss.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# @omlish-script
+import resource
+import sys
+
+
+def _get_rss() -> int:
+    return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+
+# @omlish-manifest
+_CLI_MODULE = {'$omdev.cli.types.CliModule': {
+    'cmd_name': 'py/execrss',
+    'mod_name': __name__,
+}}
+
+
+def _main() -> None:
+    [src] = sys.argv[1:]
+    start = _get_rss()
+    exec(src)
+    end = _get_rss()
+    print(end - start)
+
+
+if __name__ == '__main__':
+    _main()
