725a27e125cf7bdccf93c9a984d7c8073d1f18fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:04:57 +0000

    Add package xtj/0.1.0/xtj-0.1.0.tar.gz

---
+++ packages/xtj/xtj-0.1.0.tar.gz/xtj-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xtj",
+    version="0.1.0",
+    description="xtj is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'xtj = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
