f1ec2481cf2f2ef3fb0abb616618698f49ff5e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:12 +0000

    Add package lin-reg87/1.0.0/lin_reg87-1.0.0.tar.gz

---
+++ packages/lin-reg87/lin_reg87-1.0.0.tar.gz/lin_reg87-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='lin_reg87',
+   version='1.0.0',
+   author='<DAIT DEHANE YACINE>',
+   author_email='<y.daitdehane@esi-sba.dz>',
+   packages=['lin_reg87'],
+   url='http://pypi.python.org/pypi/<lin_reg87>/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['lin_reg87']
+)
