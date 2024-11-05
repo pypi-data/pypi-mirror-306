fb9b71d602fc7a27d2d4ad9834cdd9117c498a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:19 +0000

    Add package cdlake/0.1.2/cdlake-0.1.2.tar.gz

---
+++ packages/cdlake/cdlake-0.1.2.tar.gz/cdlake-0.1.2/cdlake/lake.py
@@ -0,0 +1,16 @@
+from ._internal import (Cdl as _CdlImpl, DatasetCatalog)
+from .filesystem import CdlFS
+
+
+class Cdl:
+    def __init__(
+        self,
+        catalog: DatasetCatalog | None = None,
+    ) -> None:
+        self._impl = _CdlImpl(catalog)
+
+    def open(self, url: str) -> CdlFS:
+        return CdlFS(self._impl.open(url))
+
+    def __repr__(self) -> str:
+        return 'Connected Data Lake'
