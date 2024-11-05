6f705158ebef3b4d5ee664a5edf8cdac62658c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:04:59 +0000

    Add package MethodicConfigurator/0.9.9/methodicconfigurator-0.9.9.tar.gz

---
+++ packages/MethodicConfigurator/methodicconfigurator-0.9.9.tar.gz/methodicconfigurator-0.9.9/README.md
@@ -0,0 +1,89 @@
+# Everyone should be able to configure ArduPilot for their vehicles
+
+<!--
+SPDX-FileCopyrightText: 2024 Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
+
+SPDX-License-Identifier: GPL-3.0-or-later
+-->
+
+| Lint | Quality | Test | Deploy | Maintain |
+| ---- | ------- | ---- | ------ | -------- |
+| [![Pylint](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/pylint.yml/badge.svg)](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/pylint.yml) | [![Codacy Badge](https://app.codacy.com/project/badge/Grade/720794ed54014c58b9eaf7a097a4e98e)](https://app.codacy.com/gh/amilcarlucas/MethodicConfigurator/dashboard?utm_source=gh&utm_medium=referral&utm_content=&utm_campaign=Badge_grade) | [![Python unit-tests](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/unit-tests.yml/badge.svg)](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/unit-tests.yml) | [![pages-build-deployment](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/pages/pages-build-deployment/badge.svg)](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/pages/pages-build-deployment) | [![Average time to resolve an issue](http://isitmaintained.com/badge/resolution/ArduPilot/MethodicConfigurator.svg)](http://isitmaintained.com/project/ArduPilot/MethodicConfigurator) |
+| [![test Python cleanliness](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/ruff.yml/badge.svg)](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/ruff.yml) | [![OpenSSF Best Practices](https://www.bestpractices.dev/projects/9101/badge)](https://www.bestpractices.dev/projects/9101) | [![Pytest unittests](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/unittests.yml/badge.svg)](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/unittests.yml) | [![Upload MethodicConfigurator Package](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/python-publish.yml/badge.svg)](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/python-publish.yml) | [![Percentage of issues still open](http://isitmaintained.com/badge/open/ArduPilot/MethodicConfigurator.svg)](http://isitmaintained.com/project/ArduPilot/MethodicConfigurator) |
+| | [![Known Vulnerabilities](https://snyk.io/test/github/amilcarlucas/MethodicConfigurator/badge.svg)](https://snyk.io/test/github/amilcarlucas/MethodicConfigurator) | [![codecov](https://codecov.io/github/amilcarlucas/MethodicConfigurator/graph/badge.svg?token=76P928EOL2)](https://codecov.io/github/amilcarlucas/MethodicConfigurator) | [![Windows Build](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/windows_build.yml/badge.svg)](https://github.com/ArduPilot/MethodicConfigurator/actions/workflows/windows_build.yml) | |
+| | [![Code Climate](https://codeclimate.com/github/amilcarlucas/MethodicConfigurator.png)](https://codeclimate.com/github/amilcarlucas/MethodicConfigurator) | [![Coverity Scan Build Status](https://scan.coverity.com/projects/30346/badge.svg)](https://scan.coverity.com/projects/ardupilot-methodic-configurator) | | |
+
+Amilcar Lucas's ArduPilot Methodic Configurator is a Python tool that implements a [clear and proven configuration sequence of ArduPilot of drones](https://ardupilot.github.io/MethodicConfigurator/TUNING_GUIDE_ArduCopter).
+
+![When to use ArduPilot Methodic Configurator](images/when_to_use_amc.png)
+
+It provides a graphical user interface (GUI) for managing and visualizing ArduPilot parameters, parameter files and documentation.
+
+![Application Screenshot](images/App_screenshot1.png)
+
+## Usage
+
+There is a [Quick-start guide](QUICKSTART.md) and a more detailed [Usermanual](USERMANUAL.md)
+
+## MS Windows Installation
+
+Download the [latest MethodicConfiguratorSetup-x.x.x.exe installer file](https://github.com/ArduPilot/MethodicConfigurator/releases/tag/latest).
+
+## Linux Installation
+
+### Older distros without venv
+
+Install [python pip](https://pypi.org/project/pip/). Then do:
+
+```bash
+pip install -U MethodicConfigurator
+```
+
+To run it do:
+
+```bash
+ardupilot_methodic_configurator
+```
+
+### Newer distros with venv
+
+```bash
+python -m venv .ardupilot_methodic_configurator_venv
+source .ardupilot_methodic_configurator_venv/bin/activate
+python -m pip install --upgrade pip
+pip install MethodicConfigurator
+```
+
+To run it do:
+
+```bash
+source .ardupilot_methodic_configurator_venv/bin/activate
+ardupilot_methodic_configurator
+```
+
+## MacOS Installation
+
+Install [git](https://git-scm.com/) and [python](https://www.python.org/downloads/). Then do:
+
+```bash
+git clone https://github.com/ArduPilot/MethodicConfigurator.git
+cd MethodicConfigurator
+./install_macos.sh
+```
+
+## Support and Contributing
+
+Please feel free to submit [issues](https://github.com/ArduPilot/MethodicConfigurator/issues) or [pull requests](https://github.com/ArduPilot/MethodicConfigurator/pulls). More information is available on the [contributing and support](CONTRIBUTING.md) page.
+
+## Software architecture
+
+To meet the [Software requirements](ARCHITECTURE.md#software-requirements) a [software architecture](ARCHITECTURE.md#the-software-architecture) was designed and implemented.
+
+## Code of conduct
+
+To use and develop this software you must obey the [ArduPilot Methodic Configurator Code of Conduct](CODE_OF_CONDUCT.md).
+
+## License
+
+This project is licensed under the [GNU General Public License v3.0](LICENSE.md).
+It builds upon other [open-source software packages](credits/CREDITS.md)
---
+++ packages/MethodicConfigurator/methodicconfigurator-0.9.9.tar.gz/methodicconfigurator-0.9.9/setup.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+
+'''
+This script creates the MethodicConfigurator pip python package
+
+This file is part of Ardupilot methodic configurator. https://github.com/ArduPilot/MethodicConfigurator
+
+SPDX-FileCopyrightText: 2024 Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
+
+SPDX-License-Identifier: GPL-3.0-or-later
+'''
+
+import shutil
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+from MethodicConfigurator.version import VERSION
+
+dev_requirements = [
+    'ruff',
+    'pre-commit',
+    'pytest',
+    'pytest-cov',
+    'coverage',
+    'mock',
+    # Add any other development requirements here
+]
+
+extra_scripts = [
+    'MethodicConfigurator/annotate_params.py',
+    'MethodicConfigurator/extract_param_defaults.py',
+    'MethodicConfigurator/param_pid_adjustment_update.py'
+]
+
+PRJ_URL = "https://github.com/ArduPilot/MethodicConfigurator"
+
+for file in extra_scripts:
+    os.chmod(file, 0o755)
+
+os.chmod("MethodicConfigurator/ardupilot_methodic_configurator.py", 0o755)
+
+# Read the long description from the README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+    # Use Absolute links so that the pyPI page renders correctly
+    long_description = long_description.replace("(USERMANUAL.md", f"({PRJ_URL}/blob/master/USERMANUAL.md")
+    long_description = long_description.replace("(QUICKSTART.md", f"({PRJ_URL}/blob/master/QUICKSTART.md")
+    long_description = long_description.replace("(CONTRIBUTING.md", f"({PRJ_URL}/blob/master/CONTRIBUTING.md")
+    long_description = long_description.replace("(ARCHITECTURE.md", f"({PRJ_URL}/blob/master/ARCHITECTURE.md")
+    long_description = long_description.replace("(CODE_OF_CONDUCT.md", f"({PRJ_URL}/blob/master/CODE_OF_CONDUCT.md")
+    long_description = long_description.replace("(LICENSE.md", f"({PRJ_URL}/blob/master/LICENSE.md")
+    long_description = long_description.replace("(USECASES.md", f"({PRJ_URL}/blob/master/USECASES.md")
+    long_description = long_description.replace("(credits/CREDITS.md", f"({PRJ_URL}/blob/master/credits/CREDITS.md")
+    long_description = long_description.replace("images/App_screenshot1.png",
+                                                f"{PRJ_URL}/raw/master/images/App_screenshot1.png")
+
+# So that the vehicle_templates directory contents get correctly read by the MANIFEST.in file
+TEMPLATES_SRC_DIR = 'vehicle_templates'
+TEMPLATES_DST_DIR = 'MethodicConfigurator/vehicle_templates'
+
+if os.path.exists(TEMPLATES_DST_DIR):
+    shutil.rmtree(TEMPLATES_DST_DIR)
+
+try:
+    shutil.copytree(TEMPLATES_SRC_DIR, TEMPLATES_DST_DIR)
+    print("Directory tree copied successfully.")
+except FileNotFoundError:
+    print(f"Error: Source directory '{TEMPLATES_SRC_DIR}' does not exist.")
+except FileExistsError:
+    print(f"The destination directory '{TEMPLATES_DST_DIR}' already exists and cannot be overwritten.")
+except PermissionError:
+    print(f"Permission denied when trying to copy '{TEMPLATES_SRC_DIR}' to '{TEMPLATES_DST_DIR}'. " \
+          "Please check your permissions.")
+except Exception as e:  # pylint: disable=broad-except
+    print(f"An unexpected error occurred while copying the directory tree: {e}")
+
+setup(
+    name='MethodicConfigurator',
+    version=VERSION,
+    zip_safe=True,
+    description='A clear configuration sequence for ArduPilot vehicles',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=PRJ_URL,
+    author='Amilcar do Carmo Lucas',
+    author_email='amilcar.lucas@iav.de',
+    packages=find_packages(),
+    install_requires=[
+        'defusedxml',
+        'matplotlib',
+        'numpy',
+        'platformdirs',
+        'pymavlink',
+        'pyserial',
+        'pillow',
+        'setuptools',
+        'requests',
+    ],
+    extras_require={
+        'dev': dev_requirements,
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Scientific/Engineering',
+    ],
+    # Add the license
+    license='GPLv3',
+    python_requires='>=3.6',
+    keywords=['ArduPilot', 'Configuration', 'SCM', 'Methodic', 'ArduCopter', 'ArduPlane', 'ArduRover', 'ArduSub'],
+    #package_dir={"": "MethodicConfigurator"}, this unfortunatly breaks the build
+    include_package_data=True,
+    scripts=extra_scripts,
+    # Specify entry points for command-line scripts
+    entry_points={
+        'console_scripts': [
+            'ardupilot_methodic_configurator=MethodicConfigurator.ardupilot_methodic_configurator:main',
+            'extract_param_defaults=MethodicConfigurator.extract_param_defaults:main',
+            'annotate_params=MethodicConfigurator.annotate_params:main',
+            'param_pid_adjustment_update=MethodicConfigurator.param_pid_adjustment_update:main',
+        ],
+    },
+    project_urls={
+        'Homepage': PRJ_URL,
+        'Documentation': f'{PRJ_URL}/blob/master/USERMANUAL.md',
+        'Bug Tracker': f'{PRJ_URL}/issues',
+        'Source Code': PRJ_URL,
+        'Forum': 'https://discuss.ardupilot.org/t/new-ardupilot-methodic-configurator-gui/115038/',
+        'Chat': 'https://discord.com/invite/ArduPilot',
+        'Download': f'{PRJ_URL}/releases',
+    },
+)
+
+# Remove the symbolic link now that the setup is done
+if os.path.exists(TEMPLATES_DST_DIR):
+    shutil.rmtree(TEMPLATES_DST_DIR)
