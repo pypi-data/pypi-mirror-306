57646f00ce0327c2038e1f77a43491ad0ed341bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 07:04:18 +0000

    Add package KeyisBLogging/1.0.6/KeyisBLogging-1.0.6-py3-none-any.whl

---
+++ packages/KeyisBLogging/KeyisBLogging-1.0.6-py3-none-any.whl/KeyisBLogging/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="KeyisBLogging",
+    version="1.0.6",
+    author="KeyisB",
+    author_email="keyisb.pip@gmail.com",
+    description="KeyisBLogging",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/KeyisB/libs/tree/main/KeyisBLogging",
+    packages=["KeyisBLogging"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    license="MMB License v1.0"
+)
