d4c6844bd59710311b6a65f0dac4a698079d4c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 04:04:24 +0000

    Add package framex/0.5.0/framex-0.5.0.tar.gz

---
+++ packages/framex/framex-0.5.0.tar.gz/framex-0.5.0/framex/cli/_cli.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING, Callable, Literal
+
+from polars import read_ipc, read_parquet
+
+from framex._dicts import _REMOTE_DATASETS
+from framex._dicts._constants import _EXTENSION
+
+# from framex._dicts._constants import _EXTENSION, _LOCAL_DIR
+
+if TYPE_CHECKING:
+    from polars import DataFrame
+
+
+def _save(
+    frame: DataFrame,
+    path: Path,
+    format: str | Literal["feather", "parquet", "csv", "json", "ipc"] = _EXTENSION,
+) -> None:
+    """
+    Saves a LazyFrame to the desired format.
+
+    Parameters
+    ----------
+    frame : polars.LazyFrame
+        The frame to save.
+    name : str
+        The name of the dataset.
+    path : Path[str]
+        The path to save the dataset to.
+    format : str or Literal["feather", "parquet", "csv", "json", "ipc"], optional
+        The format to save the dataset in.
+        Default is "feather"
+    """
+    if format in ("feather", "ipc"):
+        frame.write_ipc(path, compression="zstd")
+    elif format == "parquet":
+        frame.write_parquet(path, compression="zstd")
+    elif format == "csv":
+        frame.write_csv(path)
+    elif format == "json":
+        frame.write_ndjson(path)
+
+
+def get(
+    name: str,
+    *,
+    dir: str | Path | None = None,
+    overwrite: bool = False,
+    format: str | Literal["feather", "parquet", "csv", "json", "ipc"] = _EXTENSION,
+) -> None:
+    """
+    Loads dataset by with the given name if available.
+
+    Parameters
+    ----------
+    name : str
+        Name of the dataset to load.
+
+    dir : str, optional
+        Directory to save the dataset to.
+
+    overwrite : bool, optional
+        Whether to overwrite the dataset if it already exists.
+        Default is True
+
+    format : str or Literal["feather", "parquet", "csv", "json", "ipc"], optional
+        The format to save the dataset in.
+        Default is "feather"
+
+    Returns
+    -------
+    None
+    """
+    if dir is None:
+        dir = Path().resolve()
+    else:
+        dir = Path(dir).resolve()
+    # select the function to load the dataset
+    if _EXTENSION == "parquet":
+        loader: Callable[..., DataFrame] = read_parquet
+    elif _EXTENSION == "feather":
+        loader: Callable[..., DataFrame] = read_ipc
+
+    # check if the dataset is available
+    if name not in _REMOTE_DATASETS:
+        msg = f"Dataset {name} not found."
+        raise ValueError(msg)
+    else:
+        frame = loader(_REMOTE_DATASETS.get(name))
+
+    # list of all files in the directory
+    dir = Path(dir).resolve()
+    cached = list(dir.glob(f"*{format}"))
+    path = dir / f"{name}.{format}"
+
+    if path in cached:
+        if not overwrite:
+            msg = f"File {path} already exists.\nUse `--overwrite` to overwrite."
+            print(msg)
+            return
+        else:
+            print(f"Overwriting: {path}")
+            _save(frame=frame, path=path, format=format)
+    elif path not in cached:
+        _save(frame=frame, path=path, format=format)
+        print(f"Saving: {path}")
+
+    return
---
+++ packages/framex/framex-0.5.0.tar.gz/framex-0.5.0/framex/datasets/get_url.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from framex._dicts import _DATASETS
+from framex._dicts._constants import _EXTENSION
+
+
+def get_url(
+    name: str, format: Literal["csv", "parquet", "feather"] = _EXTENSION
+) -> str:
+    """
+    Returns the URL of the dataset.
+
+    Parameters
+    ----------
+    name : str
+        Name of the dataset.
+
+    format : Literal["csv", "parquet", "feather"], optional
+        Format of the dataset.
+        Default is "parquet"
+
+    Returns
+    -------
+    str
+        URL of the dataset.
+    """
+    if name not in _DATASETS:
+        msg = f"Dataset {name} not found."
+        raise ValueError(msg)
+
+    url = _DATASETS.get(name)
+    url = url.replace(_EXTENSION, format)
+
+    return url
