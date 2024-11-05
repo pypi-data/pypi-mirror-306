6ef8889ce2f5895133979a14833548013dc80a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:11 +0000

    Add package jou/0.1.0/jou-0.1.0.tar.gz

---
+++ packages/jou/jou-0.1.0.tar.gz/jou-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jou",
+    version="0.1.0",
+    description="jou is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'jou = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
