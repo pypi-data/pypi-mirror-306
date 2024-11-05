a5335f20e460300f44406ad71a0c0b4d0d973284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 07:04:01 +0000

    Add package eevolve/0.0.8/eevolve-0.0.8-py3-none-any.whl

---
+++ packages/eevolve/eevolve-0.0.8-py3-none-any.whl/eevolve/__init__.py
@@ -0,0 +1,8 @@
+from .brain import Brain
+from .agent import Agent
+from .board import Board
+from .task import Task, CollisionTask, BoardTask, AgentTask, FrameEndTask, PairTask
+from .game import Game
+from .generator import PositionGenerator, AgentGenerator, ColorGenerator
+from .eemath import Math
+from .loader import Loader
