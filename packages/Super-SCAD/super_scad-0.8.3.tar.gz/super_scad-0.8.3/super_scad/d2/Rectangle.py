97fa8b99d157de737663a25e693e11c0fb37cd58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 03:07:47 +0000

    Add package Super-SCAD/0.8.3/super_scad-0.8.3.tar.gz

---
+++ packages/Super-SCAD/super_scad-0.8.3.tar.gz/super_scad-0.8.3/super_scad/d2/Rectangle.py
@@ -0,0 +1,105 @@
+from super_scad.d2.private.PrivateSquare import PrivateSquare
+from super_scad.scad.ArgumentAdmission import ArgumentAdmission
+from super_scad.scad.Context import Context
+from super_scad.scad.ScadWidget import ScadWidget
+from super_scad.transformation.Translate2D import Translate2D
+from super_scad.type.Vector2 import Vector2
+
+
+class Rectangle(ScadWidget):
+    """
+    Widget for creating rectangles.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def __init__(self,
+                 *,
+                 size: Vector2 | None = None,
+                 width: float | None = None,
+                 depth: float | None = None,
+                 center: bool = False,
+                 position: Vector2 | None = None):
+        """
+        Object constructor.
+
+        :param size: The size of the rectangle.
+        :param width: The width (the size along the x-axis) of the rectangle.
+        :param depth: The depth (the size along the y-axis) of the rectangle.
+        :param center: Whether the rectangle is centered at its position.
+        :param position: The position of the square. The default value is the origin.
+        """
+        ScadWidget.__init__(self, args=locals())
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def _validate_arguments(self) -> None:
+        """
+        Validates the arguments supplied to the constructor of this SuperSCAD widget.
+        """
+        admission = ArgumentAdmission(self._args)
+        admission.validate_exclusive({'size'}, {'width', 'depth'})
+        admission.validate_required({'width', 'size'},
+                                    {'depth', 'size'},
+                                    {'center'})
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @property
+    def size(self) -> Vector2:
+        """
+        Returns the size of this rectangle.
+        """
+        return Vector2(x=self.width, y=self.depth)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @property
+    def width(self) -> float:
+        """
+        Returns the width (the size along the x-axis) of this rectangle.
+        """
+        if 'size' in self._args:
+            return self.uc(self._args['size'].x)
+
+        return self.uc(self._args['width'])
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @property
+    def depth(self) -> float:
+        """
+        Returns the depth (the size along the y-axis) of this rectangle.
+        """
+        if 'size' in self._args:
+            return self.uc(self._args['size'].y)
+
+        return self.uc(self._args['depth'])
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @property
+    def center(self) -> bool:
+        """
+        Returns whether the rectangle is centered at this origin.
+        """
+        return self._args['center']
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @property
+    def position(self) -> Vector2:
+        """
+        Returns position this rectangle.
+        """
+        return self.uc(self._args.get('position', Vector2.origin))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def build(self, context: Context) -> ScadWidget:
+        """
+        Builds a SuperSCAD widget.
+
+        :param context: The build context.
+        """
+        rectangle = PrivateSquare(size=self.size, center=self.center)
+
+        position = self.position
+        if position.is_not_origin:
+            rectangle = Translate2D(vector=position, child=rectangle)
+
+        return rectangle
+
+# ----------------------------------------------------------------------------------------------------------------------
