c7ce9816733cbedf085a009bedeaddc6766d2dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:04:59 +0000

    Add package fgv/0.1.0/fgv-0.1.0.tar.gz

---
+++ packages/fgv/fgv-0.1.0.tar.gz/fgv-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fgv",
+    version="0.1.0",
+    description="fgv is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'fgv = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
