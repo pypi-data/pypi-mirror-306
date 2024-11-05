e60d4c8231a65b0d9ac882c220cf2ac01b84acc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 07:04:35 +0000

    Add package ctv/0.1.0/ctv-0.1.0.tar.gz

---
+++ packages/ctv/ctv-0.1.0.tar.gz/ctv-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ctv",
+    version="0.1.0",
+    description="ctv is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'ctv = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
