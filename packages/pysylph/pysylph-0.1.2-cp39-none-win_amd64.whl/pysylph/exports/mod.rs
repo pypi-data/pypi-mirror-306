53d7d37ec9601f9b174372574e5bb42c59d538b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:14 +0000

    Add package pysylph/0.1.2/pysylph-0.1.2-cp39-none-win_amd64.whl

---
+++ packages/pysylph/pysylph-0.1.2-cp39-none-win_amd64.whl/pysylph/exports/mod.rs
@@ -0,0 +1,7 @@
+//! This module contains code taken verbatim from the `sylph` source.
+//!
+//! The code here is re-used and redistributed under the terms of the MIT
+//! license. All credits and copyright to Jim Shaw and Yun William Yu.
+
+pub mod contain;
+pub mod sketch;
