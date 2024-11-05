0916dc6905ae555fea0538741a9b1c8bbe7014cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 03:07:21 +0000

    Add package letta-nightly/0.5.1.dev20241104104148/letta_nightly-0.5.1.dev20241104104148.tar.gz

---
+++ packages/letta-nightly/letta_nightly-0.5.1.dev20241104104148.tar.gz/letta_nightly-0.5.1.dev20241104104148/letta/schemas/organization.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import Field
+
+from letta.schemas.letta_base import LettaBase
+from letta.utils import get_utc_time
+
+
+class OrganizationBase(LettaBase):
+    __id_prefix__ = "organization"
+
+
+class Organization(OrganizationBase):
+    id: str = Field(..., description="The id of the organization.")
+    name: str = Field(..., description="The name of the organization.")
+    created_at: Optional[datetime] = Field(default_factory=get_utc_time, description="The creation date of the organization.")
+
+
+class OrganizationCreate(OrganizationBase):
+    name: Optional[str] = Field(None, description="The name of the organization.")
