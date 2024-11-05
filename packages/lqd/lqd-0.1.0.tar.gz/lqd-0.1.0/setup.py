4e8e411ff47f2b7863bff487cf15d73f06b8d43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:26 +0000

    Add package lqd/0.1.0/lqd-0.1.0.tar.gz

---
+++ packages/lqd/lqd-0.1.0.tar.gz/lqd-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lqd",
+    version="0.1.0",
+    description="lqd is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'lqd = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
