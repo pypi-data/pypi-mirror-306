f0cb6d7a4a0be93eecce572141d2c07856bd5456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 06:04:08 +0000

    Add package a5-client/0.1/a5_client-0.1.tar.gz

---
+++ packages/a5-client/a5_client-0.1.tar.gz/a5_client-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='a5_client',
+    version='0.1',
+    packages=find_packages(),
+    description='a5 API client',
+    author='Juan F. Bianchi',
+    author_email='jbianchi@ina.gob.ar',
+    url='https://github.com/jbianchi81/a5_client',  # Optional
+)
