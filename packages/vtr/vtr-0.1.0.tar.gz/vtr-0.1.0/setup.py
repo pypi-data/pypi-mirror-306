80e43823a96618b6a8e11e78ef36234d379a7f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:06:02 +0000

    Add package vtr/0.1.0/vtr-0.1.0.tar.gz

---
+++ packages/vtr/vtr-0.1.0.tar.gz/vtr-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vtr",
+    version="0.1.0",
+    description="vtr is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'vtr = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
