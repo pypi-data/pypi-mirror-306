f72f7b6eafd51e8fe64381e33aecbc1ffdb51b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 06:04:20 +0000

    Add package deeploupe/0.0.0/deeploupe-0.0.0.tar.gz

---
+++ packages/deeploupe/deeploupe-0.0.0.tar.gz/deeploupe-0.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="deeploupe",
+    version="0.0.0",
+    author="Heinrich Tremblay",
+    author_email="heinrich.tremblay@outlook.com",
+    description="Coming soon",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
