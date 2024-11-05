1f502539ec78a2404224635625f4f87f7500a785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:05:11 +0000

    Add package aurora-signature/1.6/aurora_signature-1.6-py3-none-any.whl

---
+++ packages/aurora-signature/aurora_signature-1.6-py3-none-any.whl/aurora_signature/__init__.py
@@ -0,0 +1,32 @@
+import click
+from .main import sign
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def cli():
+    pass
+
+@cli.command()
+@click.option('-i', '--input_file', type=str, help='File input', required=True)
+@click.option('-o', '--output_file', type=str, help='File output', required=True)
+@click.option('-tk', '--api_token', type=str, help='API Token', required=True)
+@click.option('-t', '--tenant_id', type=int, help='TenantID', required=True)
+@click.option('-pki', '--pki', type=int, help='Certificate Type', required=True, default=0)
+
+def sign(input_file, output_file, api_token, tenant_id, pki):
+    """Sign document"""
+    logo = """
+    +------------------------------+
+    | Sign document by Paperlogic  |
+    +------------------------------+
+    """
+    click.echo(f"Start Signing")
+    print('input_file', input_file)
+    print('output_file', output_file)
+    print('api_token', api_token)
+    print('tenant_id', tenant_id)
+    print('pki', pki)
+    click.echo(f"Complete Signing")
+    
+    click.echo(logo)
