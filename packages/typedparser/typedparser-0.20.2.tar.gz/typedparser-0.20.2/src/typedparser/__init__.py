e67773672d6cb448fd33ec8a0ea5418e044304a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 05:04:40 +0000

    Add package typedparser/0.20.2/typedparser-0.20.2.tar.gz

---
+++ packages/typedparser/typedparser-0.20.2.tar.gz/typedparser-0.20.2/src/typedparser/__init__.py
@@ -0,0 +1,17 @@
+from ._typedattr import definenumpy, attrs_from_dict, NamedTupleMixin
+from ._typedparser import add_argument, TypedParser, VerboseQuietArgs
+from .custom_format import CustomArgparseFmt
+from .objects import get_attr_names
+
+__all__ = [
+    "definenumpy",
+    "attrs_from_dict",
+    "NamedTupleMixin",
+    "add_argument",
+    "TypedParser",
+    "VerboseQuietArgs",
+    "CustomArgparseFmt",
+    "get_attr_names",
+]
+
+__version__ = "0.20.2"
