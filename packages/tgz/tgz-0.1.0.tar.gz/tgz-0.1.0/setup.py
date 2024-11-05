c8bc07a943bf072a08e8e95f66595bc4d65e3da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:37 +0000

    Add package tgz/0.1.0/tgz-0.1.0.tar.gz

---
+++ packages/tgz/tgz-0.1.0.tar.gz/tgz-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tgz",
+    version="0.1.0",
+    description="tgz is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'tgz = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
