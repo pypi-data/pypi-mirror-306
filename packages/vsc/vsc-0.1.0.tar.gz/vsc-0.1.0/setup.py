3e75023a17fc6d8fdffb623407f6d7173ca5510d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:58 +0000

    Add package vsc/0.1.0/vsc-0.1.0.tar.gz

---
+++ packages/vsc/vsc-0.1.0.tar.gz/vsc-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vsc",
+    version="0.1.0",
+    description="vsc is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'vsc = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
