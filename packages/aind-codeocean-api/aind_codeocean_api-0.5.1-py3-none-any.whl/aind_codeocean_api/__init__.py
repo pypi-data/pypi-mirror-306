1d950956892265b115fae9f8941abeb113050a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:13 +0000

    Add package aind-codeocean-api/0.5.1/aind_codeocean_api-0.5.1-py3-none-any.whl

---
+++ packages/aind-codeocean-api/aind_codeocean_api-0.5.1-py3-none-any.whl/aind_codeocean_api/__init__.py
@@ -0,0 +1,18 @@
+"""
+Python wrapper of CodeOcean's REST API.
+
+DEPRECATION WARNING. Code Ocean has published their own SDK:
+
+https://github.com/codeocean/codeocean-sdk-python
+
+We will be dropping support of this package in favor of the official SDK.
+"""
+import warnings
+
+__version__ = "0.5.1"
+
+warnings.warn(
+    "This package is deprecated and will be removed in a future version.",
+    DeprecationWarning,
+    stacklevel=2,
+)
