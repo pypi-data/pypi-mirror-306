376a5b31e58e14d060b8d0062a9549821e2dbc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 05:04:19 +0000

    Add package neural-condense/0.0.2/neural_condense-0.0.2.tar.gz

---
+++ packages/neural-condense/neural_condense-0.0.2.tar.gz/neural_condense-0.0.2/neural_condense/datatypes.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+import torch
+
+
+class CondenseHeader(BaseModel):
+    CONDENSE_API_KEY: str
+
+
+class CondensePayload(BaseModel):
+    context: str
+    tier: str
+    target_model: str
+    miner_uid: int = -1
+    top_incentive: float = 0.9
+
+
+class ClientResponse:
+    def __init__(
+        self,
+        condensed_tokens: torch.Tensor,
+        prompt_tokens: torch.Tensor = None,
+        inputs_embeds: torch.Tensor = None,
+    ):
+        self.condensed_tokens = condensed_tokens
+        self.prompt_tokens = prompt_tokens
+        self.inputs_embeds = inputs_embeds
