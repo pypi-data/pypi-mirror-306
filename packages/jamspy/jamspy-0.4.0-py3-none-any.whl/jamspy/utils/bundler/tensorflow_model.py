bcf8be1d59c823d0aa49dbaf396b899332950538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:24 +0000

    Add package jamspy/0.4.0/jamspy-0.4.0-py3-none-any.whl

---
+++ packages/jamspy/jamspy-0.4.0-py3-none-any.whl/jamspy/utils/bundler/tensorflow_model.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+import tensorflow as tf  # type: ignore
+
+from jamspy.utils.bundler.common import ARTEFACTS_DIR, Bundle, create_tar_gz
+
+
+class TensorflowBundler(Bundle):
+    def __init__(self, model_obj: Union[tf.keras.Model, tf.keras.Sequential]):
+        super().__init__()
+        self.model = model_obj
+
+    def bundle(self, model_name: str) -> None:
+        framework = 'tensorflow'
+        model_save_path = f'{ARTEFACTS_DIR}/{framework}-{model_name}'
+        tar_gz_path = f'{ARTEFACTS_DIR}/{framework}-{model_name}.tar.gz'
+
+        self.model.save(model_save_path)
+        create_tar_gz(model_save_path, tar_gz_path)
