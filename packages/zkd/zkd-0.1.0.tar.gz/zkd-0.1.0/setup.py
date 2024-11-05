e38f7c7d5286b5923fee6c0e89ec5219df99df56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:05:03 +0000

    Add package zkd/0.1.0/zkd-0.1.0.tar.gz

---
+++ packages/zkd/zkd-0.1.0.tar.gz/zkd-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zkd",
+    version="0.1.0",
+    description="zkd is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'zkd = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
