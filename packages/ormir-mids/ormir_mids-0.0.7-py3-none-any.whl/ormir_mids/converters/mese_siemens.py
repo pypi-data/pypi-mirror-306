31cb3e89c7fba79705616b8243c65d511b78ee4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 03:07:41 +0000

    Add package ormir-mids/0.0.7/ormir_mids-0.0.7-py3-none-any.whl

---
+++ packages/ormir-mids/ormir_mids-0.0.7-py3-none-any.whl/ormir_mids/converters/mese_siemens.py
@@ -0,0 +1,50 @@
+import os
+
+from .abstract_converter import Converter
+from voxel import MedicalVolume
+from ..utils.headers import get_raw_tag_value, group, get_manufacturer
+
+
+class MeSeConverterSiemensMagnitude(Converter):
+
+    @classmethod
+    def get_name(cls):
+        return 'MESE_Siemens_Magnitude'
+
+    @classmethod
+    def get_directory(cls):
+        return os.path.join('mr-anat')
+
+    @classmethod
+    def get_file_name(cls, subject_id: str):
+        return os.path.join(f'{subject_id}_mese')
+
+    @classmethod
+    def is_dataset_compatible(cls, med_volume: MedicalVolume):
+        if 'SIEMENS' not in get_manufacturer(med_volume):
+            return False
+
+        # check if magnitude
+        if 'M' not in get_raw_tag_value(med_volume, '00080008'):
+            return False
+
+
+        scanning_sequence = get_raw_tag_value(med_volume, '00180020')[0]
+        echo_train_length = get_raw_tag_value(med_volume, '00180091')[0]
+        # echo_times = get_raw_tag_value(med_volume, 'EchoTime')
+
+        if scanning_sequence == 'SE' and echo_train_length > 1: #maybe scanning_sequence is Siemens-specific?
+            return True
+
+        return False
+
+    @classmethod
+    def convert_dataset(cls, med_volume: MedicalVolume):
+        med_volume_out = group(med_volume, 'EchoTime')
+
+        # rename flip angle. Maybe Siemens-specific again?
+        med_volume_out.omids_header['RefocusingFlipAngle'] = med_volume_out.omids_header.pop('FlipAngle')
+        med_volume_out.omids_header['PulseSequenceType'] = 'Multi-echo Spin Echo'
+
+        return med_volume_out
+
---
+++ packages/ormir-mids/ormir_mids-0.0.7-py3-none-any.whl/ormir_mids/dcm2omids.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys
+from .utils.headers import concatenate_volumes_3d, group
+from .utils.io import load_dicom, save_bids, load_dicom_with_subfolders
+from .converters import converter_list
+import pathlib
+
+import argparse
+
+
+def convert_dicom_to_ormirmids(input_folder, output_folder, anonymize='anon', recursive=True):
+    """
+    Convert DICOM to ORMIR-MIDS format.
+    
+    Parameters:
+    - input_folder (str): Path to the input folder with DICOM files.
+    - output_folder (str): Path to the output folder where results will be saved.
+    - anonymize (str): Pseudonym for patient name (default: 'anon').
+    - recursive (bool): Whether to recurse into subfolders (default: True).
+    """
+    
+    inputDir = input_folder
+    outputDir = output_folder
+    ANON_NAME = anonymize
+    RECURSIVE = recursive
+    series_config = None
+    siemens_megre_flag = False
+    concat_flag = False
+    concat_list = []
+
+    if RECURSIVE:
+        med_volume_list = load_dicom_with_subfolders(inputDir)
+    else:
+        med_volume_list = [load_dicom(inputDir)]
+
+    if os.path.exists(os.path.join(inputDir + 'series_config.json')):
+        siemens_megre_flag = True
+        with open(inputDir + 'series_config.json') as json_file:
+            series_config = json.load(json_file)
+
+    megre_count = 0
+    for med_volume in med_volume_list:
+        if siemens_megre_flag and med_volume[0:3] in series_config['series_nos']:
+            concat_flag = True
+            megre_count += 1
+        for converter_class in converter_list:
+            if converter_class.is_dataset_compatible(med_volume):
+                print('Volume compatible with', converter_class.get_name())
+                output_path = pathlib.Path(outputDir) / converter_class.get_directory()
+                output_path.mkdir(parents=True, exist_ok=True)
+                converted_volume = converter_class.convert_dataset(med_volume)
+                if ANON_NAME:
+                    patient_name = ANON_NAME
+                else:
+                    patient_name = med_volume.patient_header['PatientName']
+                if concat_flag:
+                    concat_list.append(converted_volume)
+                    if megre_count == len(series_config['series_nos']):
+                        concat_volume_4d = concatenate_volumes_3d(concat_list)
+                        converted_megre_volume = group(concat_volume_4d, 'EchoTime')
+                        save_bids(str(output_path / converter_class.get_file_name(patient_name)) + '.nii.gz',
+                                  converted_megre_volume)
+                        print('Volume saved')
+                        continue
+                    continue
+                save_bids(str(output_path / converter_class.get_file_name(patient_name)) + '.nii.gz', converted_volume)
+                print('Volume saved')
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert DICOM to ORMIR-MIDS format')
+    parser.add_argument('input_folder', type=str, help='Input folder')
+    parser.add_argument('output_folder', type=str, help='Output folder')
+    parser.add_argument('--anonymize', '-a', const='anon', metavar='pseudo_name', dest='anonymize', type=str, nargs = '?', help='Use the pseudo_name (default: anon) as patient name')
+    parser.add_argument('--recursive', '-r', action='store_true', help='Recurse into subfolders')
+
+    args = parser.parse_args()
+
+    inputDir = args.input_folder
+    outputDir = args.output_folder
+    ANON_NAME = args.anonymize
+    RECURSIVE = args.recursive
+    series_config = None
+    siemens_megre_flag = False
+    concat_flag = False
+    concat_list = []
+
+    if RECURSIVE:
+        med_volume_list = load_dicom_with_subfolders(inputDir)
+    else:
+        med_volume_list = [load_dicom(inputDir)]
+
+    if os.path.exists(os.path.join(inputDir + 'series_config.json')):
+        siemens_megre_flag = True
+        with open(inputDir + 'series_config.json') as json_file:
+            series_config = json.load(json_file)
+
+    megre_count = 0
+    for med_volume in med_volume_list:
+        if siemens_megre_flag and med_volume[0:3] in series_config['series_nos']:
+            concat_flag = True
+            megre_count += 1
+        for converter_class in converter_list:
+            if converter_class.is_dataset_compatible(med_volume):
+                print('Volume compatible with', converter_class.get_name())
+                output_path = pathlib.Path(outputDir) / converter_class.get_directory()
+                output_path.mkdir(parents=True, exist_ok=True)
+                converted_volume = converter_class.convert_dataset(med_volume)
+                if ANON_NAME:
+                    patient_name = ANON_NAME
+                else:
+                    patient_name = med_volume.patient_header['PatientName']
+                if concat_flag:
+                    concat_list.append(converted_volume)
+                    if megre_count == len(series_config['series_nos']):
+                        concat_volume_4d = concatenate_volumes_3d(concat_list)
+                        converted_megre_volume = group(concat_volume_4d, 'EchoTime')
+                        save_bids(str(output_path / converter_class.get_file_name(patient_name)) + '.nii.gz',
+                                  converted_megre_volume)
+                        print('Volume saved')
+                        continue
+                    continue
+                save_bids(str(output_path / converter_class.get_file_name(patient_name)) + '.nii.gz', converted_volume)
+                print('Volume saved')
+
+
+# if __name__ == "__main__":
+#     main()
