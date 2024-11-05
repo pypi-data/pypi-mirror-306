bc7998ae9f2866bbeef8cd36550670c6a75010b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:05:02 +0000

    Add package zjm/0.1.0/zjm-0.1.0.tar.gz

---
+++ packages/zjm/zjm-0.1.0.tar.gz/zjm-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zjm",
+    version="0.1.0",
+    description="zjm is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'zjm = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
