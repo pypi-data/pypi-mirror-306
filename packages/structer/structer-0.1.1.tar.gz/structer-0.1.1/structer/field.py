1ce5775c583036112484d952d3e7ec9ace7cf5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:06:04 +0000

    Add package structer/0.1.1/structer-0.1.1.tar.gz

---
+++ packages/structer/structer-0.1.1.tar.gz/structer-0.1.1/structer/field.py
@@ -0,0 +1,36 @@
+import re
+
+from .fieldtype import FieldTypeABC
+
+
+class Field:
+    def __init__(self, field_name: str, field_type: FieldTypeABC):
+        if not isinstance(field_name, str):
+            raise TypeError('`field_name` parameter must be a string')
+        if field_name.startswith('_'):
+            raise ValueError(
+                '`field_name` parameter must not starts by underscores (_)'
+            )
+        if not re.search(r'^[A-za-z][A-za-z0-9_]*$', field_name):
+            raise ValueError(
+                '`field_name` parameter is invalid, must be alphanumeric'
+            )
+
+        if not issubclass(type(field_type), FieldTypeABC):
+            raise TypeError(
+                '`field_type` parameter must be a valid field type.'
+            )
+
+        self._name = field_name
+        self._type = field_type
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def type(self):
+        return self._type
+
+    def __repr__(self):
+        return 'Field({!r}, {!r})'.format(self._name, self._type)
