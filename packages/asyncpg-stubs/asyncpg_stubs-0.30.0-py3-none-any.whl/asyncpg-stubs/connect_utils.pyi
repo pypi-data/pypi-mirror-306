f22e1830b56673483d4a4bbaa69d6afcbedba3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 05:04:43 +0000

    Add package asyncpg-stubs/0.30.0/asyncpg_stubs-0.30.0-py3-none-any.whl

---
+++ packages/asyncpg-stubs/asyncpg_stubs-0.30.0-py3-none-any.whl/asyncpg-stubs/connect_utils.pyi
@@ -0,0 +1,76 @@
+from asyncio import AbstractEventLoop, Future, Protocol
+from collections.abc import Awaitable, Callable
+from enum import Enum, IntEnum
+from ssl import SSLContext
+from typing import Any, Final, Literal, NamedTuple
+from typing_extensions import Self, TypeAlias
+
+from asyncpg import compat
+
+from . import connection
+
+_ParsedSSLType: TypeAlias = SSLContext | Literal[False]
+_PasswordType: TypeAlias = str | Callable[[], str] | Callable[[], Awaitable[str]]
+
+PGPASSFILE: Final[str]
+
+class SSLMode(IntEnum):
+    disable: int
+    allow: int
+    prefer: int
+    require: int
+    verify_ca: int
+    verify_full: int
+    @classmethod
+    def parse(cls, sslmode: str | Self) -> Self: ...
+
+class SSLNegotiation(compat.StrEnum):
+    postgres: str
+    direct: str
+
+class _ConnectionParameters(NamedTuple):
+    user: str
+    password: _PasswordType | None
+    database: str
+    ssl: _ParsedSSLType | None
+    sslmode: SSLMode | None
+    ssl_negotiation: SSLNegotiation
+    server_settings: dict[str, str] | None
+    target_session_attrs: SessionAttribute
+    krbsrvname: str | None
+    gsslib: connection._GSSLibType | None
+
+class _ClientConfiguration(NamedTuple):
+    command_timeout: float | None
+    statement_cache_size: int
+    max_cached_statement_lifetime: int
+    max_cacheable_statement_size: int
+
+class TLSUpgradeProto(Protocol):
+    on_data: Future[bool]
+    host: str
+    port: int
+    ssl_context: SSLContext
+    ssl_is_advisory: bool | None
+    def __init__(
+        self,
+        loop: AbstractEventLoop | None,
+        host: str,
+        port: int,
+        ssl_context: SSLContext,
+        ssl_is_advisory: bool | None,
+    ) -> None: ...
+    def data_received(self, data: bytes) -> None: ...
+    def connection_lost(self, exc: Exception | None) -> None: ...
+
+class SessionAttribute(str, Enum):
+    any: str
+    primary: str
+    standby: str
+    prefer_standby: str
+    read_write: str
+    read_only: str
+
+target_attrs_check: Final[
+    dict[SessionAttribute, Callable[[connection.Connection[Any]], Any]]
+]
