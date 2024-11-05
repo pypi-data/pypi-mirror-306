efa07934ea0e8ce050b0ca6452560dc37988f21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 04:06:07 +0000

    Add package thinice/1.0.21/thinice-1.0.21-py3-none-any.whl

---
+++ packages/thinice/thinice-1.0.21-py3-none-any.whl/thinice/cli/debug.py
@@ -0,0 +1,47 @@
+import typer
+import os
+from rich.pretty import pprint
+from . import app
+from typing_extensions import Annotated
+panel_debug = 'Debugging'
+
+debug_app = typer.Typer()
+@debug_app.command(rich_help_panel=panel_debug,
+             help='Dump local inventory',
+            epilog="""~~~shell\n
+thinice debug dump\n
+~~~
+"""
+)
+def dump():
+    app.vault.inventory.dump()
+
+@debug_app.command(rich_help_panel=panel_debug,
+             help='Ignore hash (job)',
+            epilog="""~~~shell\n
+thinice debug ignore JnNqXl\n
+~~~
+"""
+)
+def ignore(
+    hash: Annotated[str, typer.Argument(help='Hash prefix (or full hash)')],
+):
+    # direct access here!
+    app.vault.inventory.inventory['_debug']['ignore'].append(hash)
+    app.vault.inventory.save()
+
+@debug_app.command(rich_help_panel=panel_debug,
+             help='Ignore hash (job)',
+            epilog="""~~~shell\n
+thinice debug ignore JnNqXl\n
+~~~
+"""
+)
+def clear():
+    # direct access here!
+    app.vault.inventory.inventory['_debug']['ignore'] = list()
+    app.vault.inventory.save()
+
+    if os.getenv('THINICE_IGN'):
+        print('THINICE_IGN = ', os.getenv('THINICE_IGN'))
+
