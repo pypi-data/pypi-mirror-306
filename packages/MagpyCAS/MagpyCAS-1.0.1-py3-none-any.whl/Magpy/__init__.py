8b947d97cd97f333fbaa8b6483721b28c1a01a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 07:04:03 +0000

    Add package MagpyCAS/1.0.1/MagpyCAS-1.0.1-py3-none-any.whl

---
+++ packages/MagpyCAS/MagpyCAS-1.0.1-py3-none-any.whl/Magpy/__init__.py
@@ -0,0 +1,12 @@
+# import Magmas 
+# import RandSampMags 
+from Magmas import *
+from RandSampMags import *
+import MagmaPropertiesChecker
+
+__all__ = [
+    "Magmas",
+    "RandSampMags",
+    "MagmaPropertiesChecker"
+]
+
