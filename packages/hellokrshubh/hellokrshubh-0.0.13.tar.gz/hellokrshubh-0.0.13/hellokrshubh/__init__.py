5397351a15a08207cac74179c9ed264a0dab0dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 03:07:41 +0000

    Add package hellokrshubh/0.0.13/hellokrshubh-0.0.13.tar.gz

---
+++ packages/hellokrshubh/hellokrshubh-0.0.13.tar.gz/hellokrshubh-0.0.13/hellokrshubh/__init__.py
@@ -0,0 +1,9 @@
+from . import libcalculatorpython
+
+libcalculatorpython.libcalculatorpython_cython_submodule()
+
+from .add import add
+from .subtract import subtract
+from .multiply import multiply
+from .divide import divide
+from .square import square
