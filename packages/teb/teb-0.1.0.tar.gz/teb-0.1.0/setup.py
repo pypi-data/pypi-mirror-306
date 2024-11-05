0695d24ab8aa6f0491b28738913d57e80311b31a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:35 +0000

    Add package teb/0.1.0/teb-0.1.0.tar.gz

---
+++ packages/teb/teb-0.1.0.tar.gz/teb-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="teb",
+    version="0.1.0",
+    description="teb is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'teb = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
