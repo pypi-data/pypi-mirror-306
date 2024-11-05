7447b1634100cf1a79ac602883fb40f455dc3f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:06:09 +0000

    Add package tnf/0.1.0/tnf-0.1.0.tar.gz

---
+++ packages/tnf/tnf-0.1.0.tar.gz/tnf-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tnf",
+    version="0.1.0",
+    description="tnf is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'tnf = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
