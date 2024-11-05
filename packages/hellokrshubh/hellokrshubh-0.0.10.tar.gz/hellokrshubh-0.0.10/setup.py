30e7b9bed8fded5fcb79ff803ae94e9913064616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 03:07:17 +0000

    Add package hellokrshubh/0.0.10/hellokrshubh-0.0.10.tar.gz

---
+++ packages/hellokrshubh/hellokrshubh-0.0.10.tar.gz/hellokrshubh-0.0.10/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.10'
+DESCRIPTION = 'calculator with sum, subtract, multiply, divide and square'
+LONG_DESCRIPTION = 'hello world package'
+
+# Setting up
+setup(
+    name="hellokrshubh",
+    version=VERSION,
+    author="Shubham Kumar",
+    author_email="shubham2021996@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=['hellokrshubh'],
+    package_data={'' : ['*.so', '*.dylib', '*.pyd']},
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
