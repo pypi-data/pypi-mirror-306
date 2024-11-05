7aa0c1f1aa5d94a67e9dbe55225629448ff4e995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:35 +0000

    Add package zeeland/0.2.0/zeeland-0.2.0.tar.gz

---
+++ packages/zeeland/zeeland-0.2.0.tar.gz/zeeland-0.2.0/zeeland/__init__.py
@@ -0,0 +1,4 @@
+from zeeland.core import convert_backslashes, get_default_storage_path
+from zeeland.singleton import Singleton, singleton
+
+__all__ = ["get_default_storage_path", "convert_backslashes", "Singleton", "singleton"]
