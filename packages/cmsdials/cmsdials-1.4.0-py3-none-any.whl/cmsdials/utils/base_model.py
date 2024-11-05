fcd2b72c1307908944b518d1583e636437b635bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 04:05:56 +0000

    Add package cmsdials/1.4.0/cmsdials-1.4.0-py3-none-any.whl

---
+++ packages/cmsdials/cmsdials-1.4.0-py3-none-any.whl/cmsdials/utils/base_model.py
@@ -0,0 +1,29 @@
+from importlib import util as importlib_util
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+if importlib_util.find_spec("pandas"):
+    import pandas as pd
+
+    PANDAS_NOT_INSTALLED = False
+else:
+    PANDAS_NOT_INSTALLED = True
+
+
+class OBaseModel(BaseModel):
+    def cleandict(self):
+        return {key: value for key, value in self.dict().items() if value is not None}
+
+
+class PaginatedBaseModel(BaseModel):
+    exc_type: Optional[str] = None
+    exc_formatted: Optional[str] = None
+
+    def to_pandas(self):
+        if PANDAS_NOT_INSTALLED:
+            raise RuntimeError(
+                "The 'pandas' package is not installed, you can re-install cmsdials specifying the pandas extra: pip install cmsdials[pandas]"
+            )
+        return pd.DataFrame([res.__dict__ for res in self.results])
