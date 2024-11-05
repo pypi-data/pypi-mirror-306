a1094bd40cd16f0a23c18b5228ac975b144b1fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:06:14 +0000

    Add package MambuPy/2.0.0b48/MambuPy-2.0.0b48-py3-none-any.whl

---
+++ packages/MambuPy/MambuPy-2.0.0b48-py3-none-any.whl/mambupy/api/__init__.py
@@ -0,0 +1,22 @@
+""" MambuPy's Mambu REST API v2 package.
+
+Objects using `Mambu REST API <https://api.mambu.com/?python>`_
+
+.. autosummary::
+   :toctree: _autosummary
+
+    MambuPy.api.classes
+    MambuPy.api.entities
+    MambuPy.api.interfaces
+    MambuPy.api.vos
+    MambuPy.api.mambustruct
+    MambuPy.api.mambuloan
+    MambuPy.api.mambugroup
+    MambuPy.api.mambuclient
+    MambuPy.api.mambubranch
+    MambuPy.api.mambucentre
+    MambuPy.api.mambuuser
+    MambuPy.api.mambuproduct
+
+Entities use the Mixin pattern to acquire functionality.
+"""
