ad87802ee4fa3ba547e720367a0df6a46e193428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 06:04:23 +0000

    Add package geospace/0.3.7/geospace-0.3.7-py3-none-any.whl

---
+++ packages/geospace/geospace-0.3.7-py3-none-any.whl/geospace/__init__.py
@@ -0,0 +1,21 @@
+from geospace._const import *
+from geospace.boundary import *
+from geospace.ras_to_shp import *
+from geospace.raster import *
+from geospace.shape import *
+from geospace.shp_to_ras import *
+from geospace.zonal_stats import *
+from geospace.projection import *
+from geospace.utils import *
+from geospace.spatial_calc import *
+from geospace.map_calc import map_calc
+
+try:
+    from geospace.gee_export import *
+except Exception:
+    pass
+
+try:
+    from geospace.mask import *
+except Exception:
+    pass
