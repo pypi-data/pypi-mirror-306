b7c3b3c8c0f881bee83b49c17bdc8554d03ffb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 07:04:30 +0000

    Add package KeyisBFiles/1.0.7/KeyisBFiles-1.0.7-py3-none-any.whl

---
+++ packages/KeyisBFiles/KeyisBFiles-1.0.7-py3-none-any.whl/KeyisBFiles/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+name = "KeyisBFiles"
+
+
+setup(
+    name=name,
+    version="1.0.6",
+    author="KeyisB",
+    author_email="keyisb.pip@gmail.com",
+    description=name,
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/KeyisB/libs/tree/main/KeyisBFiles",
+    packages=[name],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    license="MMB License v1.0"
+)
