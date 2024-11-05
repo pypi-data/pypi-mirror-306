9f9dfa4375bc5354190c916e7053049bd89e09aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:11 +0000

    Add package marfyl-jwt-manager/0.6.0/marfyl_jwt_manager-0.6.0.tar.gz

---
+++ packages/marfyl-jwt-manager/marfyl_jwt_manager-0.6.0.tar.gz/marfyl_jwt_manager-0.6.0/jwt_manager/application/config.py
@@ -0,0 +1,4 @@
+import os
+
+#SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
+SECRET_KEY = "your-default-secret-key"
---
+++ packages/marfyl-jwt-manager/marfyl_jwt_manager-0.6.0.tar.gz/marfyl_jwt_manager-0.6.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='marfyl_jwt_manager',
+    version='0.6.0',
+    description='A service to process jwt from headers',
+    author='Eduardo Ponce',
+    author_email='poncejones@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'PyJWT',
+        'cryptography',
+        'fastapi',
+        'starlette'
+    ],
+    python_requires='>=3.9',
+)
