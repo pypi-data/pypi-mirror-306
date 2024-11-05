31007b80f9355de245dbada834d05eb0926353a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:05:07 +0000

    Add package zpg/0.1.0/zpg-0.1.0.tar.gz

---
+++ packages/zpg/zpg-0.1.0.tar.gz/zpg-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zpg",
+    version="0.1.0",
+    description="zpg is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'zpg = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
