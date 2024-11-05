66990e00c725d8be2470bc4dc8cc69165036d351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 03:08:24 +0000

    Add package python-injection/0.11.0/python_injection-0.11.0.tar.gz

---
+++ packages/python-injection/python_injection-0.11.0.tar.gz/python_injection-0.11.0/injection/integrations/fastapi.py
@@ -0,0 +1,60 @@
+from collections.abc import Callable
+from types import GenericAlias
+from typing import Any, TypeAliasType
+
+from injection import Module, mod
+from injection.exceptions import InjectionError
+from injection.integrations import _is_installed
+
+__all__ = ("Inject",)
+
+if _is_installed("fastapi", __name__):
+    from fastapi import Depends
+
+
+def Inject[T](  # noqa: N802
+    cls: type[T] | TypeAliasType | GenericAlias,
+    /,
+    module: Module | None = None,
+    *,
+    scoped: bool = True,
+) -> Any:
+    """
+    Declare a FastAPI dependency with `python-injection`.
+    """
+
+    dependency: InjectionDependency[T] = InjectionDependency(cls, module or mod())
+    return Depends(dependency, use_cache=scoped)
+
+
+class InjectionDependency[T]:
+    __slots__ = ("__call__", "__class", "__module")
+
+    __call__: Callable[[], T]
+    __class: type[T] | TypeAliasType | GenericAlias
+    __module: Module
+
+    def __init__(self, cls: type[T] | TypeAliasType | GenericAlias, module: Module):
+        lazy_instance = module.get_lazy_instance(cls, default=NotImplemented)
+        self.__call__ = lambda: self.__ensure(~lazy_instance)
+        self.__class = cls
+        self.__module = module
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, type(self)):
+            return self.__key == other.__key
+
+        return NotImplemented
+
+    def __hash__(self) -> int:
+        return hash(self.__key)
+
+    @property
+    def __key(self) -> tuple[type[T] | TypeAliasType | GenericAlias, Module]:
+        return self.__class, self.__module
+
+    def __ensure(self, instance: T) -> T:
+        if instance is NotImplemented:
+            raise InjectionError(f"`{self.__class}` is an unknown dependency.")
+
+        return instance
