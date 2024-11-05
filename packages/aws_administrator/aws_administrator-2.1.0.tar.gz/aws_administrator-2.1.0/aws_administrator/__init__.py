c2b6f14e461a627a3ed3866ae70aa2392f7d0c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:04:39 +0000

    Add package aws_administrator/2.1.0/aws_administrator-2.1.0.tar.gz

---
+++ packages/aws_administrator/aws_administrator-2.1.0.tar.gz/aws_administrator-2.1.0/aws_administrator/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: latin-1 -*-
+
+"""
+Run AWS administrative scripts.
+
+Available scripts can be found in the
+'scripts' directory of this package.
+
+Prerequisite steps:
+1. Copy the parameters/parameters.ini file to your current working directory.
+2. Update the file with the necessary values.
+
+Example usage:
+from aws_administrator.scripts import aws_sso_get
+"""
+
+
+__version__ = '2.1.0'
+__author__ = 'Ahmad Ferdaus Abd Razak'
+__status__ = 'Production'
