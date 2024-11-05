9863cb045968c6d904b3a18dcb1dfd14167946da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 06:03:52 +0000

    Add package mpcforces-extractor/0.1.5a0/mpcforces_extractor-0.1.5a0-py3-none-any.whl

---
+++ packages/mpcforces-extractor/mpcforces_extractor-0.1.5a0-py3-none-any.whl/mpcforces_extractor/api/config.py
@@ -0,0 +1,7 @@
+ITEMS_PER_PAGE = 100
+UPLOAD_FOLDER = "data/uploads"
+OUTPUT_FOLDER = "data/output"
+STATIC_DIR = "mpcforces_extractor/frontend/static"  # Path to the static directory
+TEMPLATES_DIR = (
+    "mpcforces_extractor/frontend/templates"  # Path to the templates directory
+)
