91792186e9a22ae39d461ddc8e5440a8b9029fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 04:04:24 +0000

    Add package gimkitpy/0.0.1/gimkitpy-0.0.1.tar.gz

---
+++ packages/gimkitpy/gimkitpy-0.0.1.tar.gz/gimkitpy-0.0.1/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name="gimkitpy",              
+    version="0.0.1",
+    packages=["gimkitpy"],
+    description="Reserved package name",
+    long_description="This package name is reserved.",
+    author="bingcube",
+    author_email="blurskydev@gmail.com",
+)
