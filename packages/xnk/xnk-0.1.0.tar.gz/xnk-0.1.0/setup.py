3d657011b8e4ecd27f473c9ccd6d71f663472e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:04:56 +0000

    Add package xnk/0.1.0/xnk-0.1.0.tar.gz

---
+++ packages/xnk/xnk-0.1.0.tar.gz/xnk-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xnk",
+    version="0.1.0",
+    description="xnk is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'xnk = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
