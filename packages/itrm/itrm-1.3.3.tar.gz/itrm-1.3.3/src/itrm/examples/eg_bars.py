bfd66c3e16cabcb43bcf11613d357361877eda20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:04:52 +0000

    Add package itrm/1.3.3/itrm-1.3.3.tar.gz

---
+++ packages/itrm/itrm-1.3.3.tar.gz/itrm-1.3.3/src/itrm/examples/eg_bars.py
@@ -0,0 +1,12 @@
+"""
+This example shows how a horizontal bar chart can be generated, first with ASCII
+characters only, and then again with Unicode characters.
+"""
+
+import itrm
+
+x = [2.3, 11.0, 13.3, 5.2, 7.5]
+labels = ['apples', 'oranges', 'bananas', 'pears', 'grapes']
+itrm.bars(x, labels, uni=False)
+print()
+itrm.bars(x, labels, uni=True)
