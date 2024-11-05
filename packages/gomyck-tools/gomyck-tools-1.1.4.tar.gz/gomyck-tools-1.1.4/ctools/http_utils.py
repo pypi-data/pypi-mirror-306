09a8f381d8152f01856f336c6a060c9059dbdae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:43 +0000

    Add package gomyck-tools/1.1.4/gomyck-tools-1.1.4.tar.gz

---
+++ packages/gomyck-tools/gomyck-tools-1.1.4.tar.gz/gomyck-tools-1.1.4/ctools/http_utils.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def get(url, params=None, headers=None):
+  result = ""
+  try:
+    response = requests.get(url, params=params, headers=headers, timeout=60, verify=False)
+    response.raise_for_status()
+    if response.status_code == 200:
+      result = response.content
+  except Exception as e:
+    print("GET请求异常:", e)
+  return result.decode('utf-8')
+
+
+def post(url, data=None, json=None, headers=None, files=None):
+  result = ""
+  response = requests.post(url, data=data, json=json, files=files, headers=headers, timeout=60, verify=False)
+  response.raise_for_status()
+  if response.status_code == 200:
+    result = response.content
+  return result.decode('utf-8')
