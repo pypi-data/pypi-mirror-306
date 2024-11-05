4961e926560544117e8d0e92f974a55a4f4ee9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:11 +0000

    Add package jok/0.1.0/jok-0.1.0.tar.gz

---
+++ packages/jok/jok-0.1.0.tar.gz/jok-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jok",
+    version="0.1.0",
+    description="jok is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'jok = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
