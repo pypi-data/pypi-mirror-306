12da07804f31f50cae5e69b6e46bc99f68f5707f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 03:07:43 +0000

    Add package opencapif-sdk/0.1/opencapif_sdk-0.1.tar.gz

---
+++ packages/opencapif-sdk/opencapif_sdk-0.1.tar.gz/opencapif_sdk-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opecapif-sdk',
+    version='0.0.1',
+    packages=find_packages(include=['sdk']),
+    # Add other arguments as needed
+)
