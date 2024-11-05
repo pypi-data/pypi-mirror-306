78c3290b05405947954eae770f219d9a6555b5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:04:57 +0000

    Add package xtv/0.1.0/xtv-0.1.0.tar.gz

---
+++ packages/xtv/xtv-0.1.0.tar.gz/xtv-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xtv",
+    version="0.1.0",
+    description="xtv is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'xtv = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
