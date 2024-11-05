58bc54517911ff7855b55480a047760001a19c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 06:04:00 +0000

    Add package Drkcrw/0.1/drkcrw-0.1.tar.gz

---
+++ packages/Drkcrw/drkcrw-0.1.tar.gz/drkcrw-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+    name='Drkcrw',
+    version='0.1',
+    author='rrzex',
+    author_email='alexcrow221@gmail.com',
+    zip_safe=False,
+    description='simple lib for ai',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+
+)
