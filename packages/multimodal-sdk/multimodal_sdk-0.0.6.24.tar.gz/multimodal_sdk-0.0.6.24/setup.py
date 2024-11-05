46d000c1d0c9c34bdb6b7368467862da5d4553e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:06:06 +0000

    Add package multimodal-sdk/0.0.6.24/multimodal_sdk-0.0.6.24.tar.gz

---
+++ packages/multimodal-sdk/multimodal_sdk-0.0.6.24.tar.gz/multimodal_sdk-0.0.6.24/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multimodal_sdk',
+    version='0.0.6.24',
+    packages=find_packages(),
+    install_requires=[
+        'tenacity',
+    ],
+)
