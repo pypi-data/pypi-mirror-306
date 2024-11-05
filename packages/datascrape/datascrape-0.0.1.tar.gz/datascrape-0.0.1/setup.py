99e575eeef8bfdc92dab50ad494d5e90de084e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:21 +0000

    Add package datascrape/0.0.1/datascrape-0.0.1.tar.gz

---
+++ packages/datascrape/datascrape-0.0.1.tar.gz/datascrape-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="datascrape",
+    version="0.0.1",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A brief description of your package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/your_package_name",
+    packages=find_packages(),
+    install_requires=[
+        # Add any package dependencies here, e.g., "requests>=2.20.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
