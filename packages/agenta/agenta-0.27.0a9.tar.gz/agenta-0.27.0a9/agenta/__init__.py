835e797a42f60e2800fee5f14e0084360b9a14d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 04:05:01 +0000

    Add package agenta/0.27.0a9/agenta-0.27.0a9.tar.gz

---
+++ packages/agenta/agenta-0.27.0a9.tar.gz/agenta-0.27.0a9/agenta/__init__.py
@@ -0,0 +1,31 @@
+from .sdk.utils.preinit import PreInitObject
+from .sdk.types import (
+    DictInput,
+    MultipleChoice,
+    FloatParam,
+    InFile,
+    IntParam,
+    MultipleChoiceParam,
+    GroupedMultipleChoiceParam,
+    MessagesInput,
+    TextParam,
+    FileInputURL,
+    BinaryParam,
+)
+
+from .sdk.utils.logging import log as logging
+from .sdk.tracing import Tracing
+from .sdk.decorators.tracing import instrument
+from .sdk.tracing.conventions import Reference
+from .sdk.decorators.routing import entrypoint, app, route
+from .sdk.agenta_init import Config, AgentaSingleton, init
+from .sdk.utils.costs import calculate_token_usage
+from .sdk.client import Agenta
+from .sdk.litellm import litellm as callbacks
+from .sdk.config_manager import ConfigManager
+from .sdk import assets as assets
+from .sdk import tracer
+
+config = PreInitObject("agenta.config", Config)
+DEFAULT_AGENTA_SINGLETON_INSTANCE = AgentaSingleton()
+tracing = DEFAULT_AGENTA_SINGLETON_INSTANCE.tracing  # type: ignore
---
+++ packages/agenta/agenta-0.27.0a9.tar.gz/agenta-0.27.0a9/agenta/sdk/__init__.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+from .utils.preinit import PreInitObject  # always the first import!
+from .types import (
+    DictInput,
+    MultipleChoice,
+    FloatParam,
+    InFile,
+    IntParam,
+    MultipleChoiceParam,
+    GroupedMultipleChoiceParam,
+    TextParam,
+    MessagesInput,
+    FileInputURL,
+    BinaryParam,
+)
+
+from .tracing import Tracing, get_tracer
+from .decorators.tracing import instrument
+from .tracing.conventions import Reference
+from .decorators.routing import entrypoint, app, route
+from .agenta_init import Config, AgentaSingleton, init as _init
+from .utils.costs import calculate_token_usage
+from .config_manager import ConfigManager
+
+config = PreInitObject("agenta.config", Config)
+DEFAULT_AGENTA_SINGLETON_INSTANCE = AgentaSingleton()
+tracing = DEFAULT_AGENTA_SINGLETON_INSTANCE.tracing  # type: ignore
+
+tracer = get_tracer(tracing)
+
+
+def init(
+    host: Optional[str] = None,
+    app_id: Optional[str] = None,
+    api_key: Optional[str] = None,
+    config_fname: Optional[str] = None,
+):
+    global tracing
+    global tracer
+
+    _init(
+        host=host,
+        app_id=app_id,
+        api_key=api_key,
+        config_fname=config_fname,
+    )
+
+    tracer = get_tracer(tracing)
