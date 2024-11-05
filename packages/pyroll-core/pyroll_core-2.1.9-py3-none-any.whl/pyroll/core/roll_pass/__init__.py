d1d3ac52545b1bb24f9ffc835a6b605b1ee2a25f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 17:04:40 +0000

    Add package pyroll-core/2.1.9/pyroll_core-2.1.9-py3-none-any.whl

---
+++ packages/pyroll-core/pyroll_core-2.1.9-py3-none-any.whl/pyroll/core/roll_pass/__init__.py
@@ -0,0 +1,9 @@
+from .base import BaseRollPass
+from .roll_pass import RollPass
+from .three_roll_pass import ThreeRollPass
+from .deformation_unit import DeformationUnit
+
+from . import hookimpls
+
+
+
