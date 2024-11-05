d7af831a556bd78c6fedbf1eeb211d5163a1c3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:38 +0000

    Add package hexss/0.8.5/hexss-0.8.5.tar.gz

---
+++ packages/hexss/hexss-0.8.5.tar.gz/hexss-0.8.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hexss',
+    version='0.8.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    url='https://github.com/hexs/hexss',
+    entry_points={
+        'console_scripts': [
+            'hexss_camera_server = hexss.server.camera_server:run',
+            'hexss_file_manager_server = hexss.server.file_manager_server:run',
+        ]
+    }
+)
