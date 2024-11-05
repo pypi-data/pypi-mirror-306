3b8db5b5fb60bcc198480b4964b8652496fb2bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 07:04:30 +0000

    Add package visivo/1.0.43/visivo-1.0.43.tar.gz

---
+++ packages/visivo/visivo-1.0.43.tar.gz/visivo-1.0.43/visivo/commands/test_phase.py
@@ -0,0 +1,28 @@
+import sys
+import click
+from visivo.logging.logger import Logger
+from visivo.testing.runner import Runner
+from visivo.commands.compile_phase import compile_phase
+
+
+def test_phase(
+    output_dir: str, default_source: str, working_dir: str, alert_names: str
+):
+    project = compile_phase(
+        default_source=default_source, working_dir=working_dir, output_dir=output_dir
+    )
+    Logger.instance().debug("Testing project")
+    alerts = list(map(lambda an: project.find_alert(name=an), alert_names))
+    alerts = list(filter(None, alerts))
+    test_runner = Runner(
+        traces=project.trace_objs,
+        project=project,
+        output_dir=output_dir,
+        alerts=alerts,
+        default_source=default_source,
+    )
+    if not test_runner.run().success:
+        sys.exit(1)
+
+
+test_phase.__test__ = False
