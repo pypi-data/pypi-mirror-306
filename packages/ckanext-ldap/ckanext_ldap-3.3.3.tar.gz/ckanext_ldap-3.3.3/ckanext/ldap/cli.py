f2599800db11e1b8ba4403379473faee1b24ba9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 03:08:15 +0000

    Add package ckanext-ldap/3.3.3/ckanext_ldap-3.3.3.tar.gz

---
+++ packages/ckanext-ldap/ckanext_ldap-3.3.3.tar.gz/ckanext_ldap-3.3.3/ckanext/ldap/cli.py
@@ -0,0 +1,54 @@
+import click
+from ckan.plugins import toolkit
+
+from ckanext.ldap.lib.utils import init_tables
+
+
+def get_commands():
+    return [ldap]
+
+
+@click.group()
+def ldap():
+    """
+    The LDAP CLI.
+    """
+    pass
+
+
+@ldap.command(name='initdb')
+def init_db():
+    """
+    Ensures the database tables we need exist in the database and creates them if they
+    don't.
+    """
+    if init_tables():
+        click.secho(f'Initialised tables', fg='green')
+    else:
+        click.secho(f'Tables already existed', fg='green')
+
+
+@ldap.command(name='setup-org')
+def setup_org():
+    """
+    Sets up the default organisation which all ldap users will be automatically made
+    members of.
+    """
+    # get the organisation all users will be added to
+    organization_id = toolkit.config['ckanext.ldap.organization.id']
+
+    # set up context
+    user = toolkit.get_action('get_site_user')({'ignore_auth': True}, {})
+    context = {'user': user['name']}
+
+    try:
+        toolkit.get_action('organization_show')(context, {'id': organization_id})
+        click.secho('Organisation already exists, doing nothing', fg='green')
+    except toolkit.ObjectNotFound:
+        # see the following commit to understand why this line is here
+        # http://github.com/ckan/ckanext-harvest/commit/f315f41c86cbde4a49ef869b6993598f8cb11e2d
+        context.pop('__auth_audit', None)
+        toolkit.get_action('organization_create')(
+            context, {'id': organization_id, 'name': organization_id}
+        )
+        click.secho('New organisation created', fg='green')
