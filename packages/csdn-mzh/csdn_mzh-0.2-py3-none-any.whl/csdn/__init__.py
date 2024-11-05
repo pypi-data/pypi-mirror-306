2aa6bd54bb90da22b345a47d48170e4fe6e36cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:04:54 +0000

    Add package csdn-mzh/0.2/csdn_mzh-0.2-py3-none-any.whl

---
+++ packages/csdn-mzh/csdn_mzh-0.2-py3-none-any.whl/csdn/__init__.py
@@ -0,0 +1,19 @@
+"""
+这个包的说明文档。
+
+这里可以描述包的功能和用法。
+
+示例用法：
+    # 导入包
+    import your_package_name
+
+    # 使用包中的功能
+    result = your_package_name.some_function()
+
+注意事项：
+    - 确保安装依赖。
+    - 根据需要配置环境变量。
+"""
+__version__ = "0.1.0"
+__author__ = "你的名字"
+__description__ = "这个包的描述"
