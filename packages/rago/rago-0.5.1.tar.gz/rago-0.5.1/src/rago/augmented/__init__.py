c428a220c5bb56c690208129adab96d12ce67055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 06:04:11 +0000

    Add package rago/0.5.1/rago-0.5.1.tar.gz

---
+++ packages/rago/rago-0.5.1.tar.gz/rago-0.5.1/src/rago/augmented/__init__.py
@@ -0,0 +1,11 @@
+"""Augmented package."""
+
+from __future__ import annotations
+
+from rago.augmented.base import AugmentedBase
+from rago.augmented.hugging_face import HuggingFaceAug
+
+__all__ = [
+    'AugmentedBase',
+    'HuggingFaceAug',
+]
