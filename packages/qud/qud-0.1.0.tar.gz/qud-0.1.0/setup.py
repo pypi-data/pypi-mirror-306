60647530c145e03a82e24a563f948e3a8054c451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:54 +0000

    Add package qud/0.1.0/qud-0.1.0.tar.gz

---
+++ packages/qud/qud-0.1.0.tar.gz/qud-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qud",
+    version="0.1.0",
+    description="qud is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'qud = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
