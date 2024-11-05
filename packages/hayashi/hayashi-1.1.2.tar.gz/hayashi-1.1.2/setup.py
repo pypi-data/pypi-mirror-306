d8cca72cffbf6387e18724e257c81a811d43e5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 03:08:07 +0000

    Add package hayashi/1.1.2/hayashi-1.1.2.tar.gz

---
+++ packages/hayashi/hayashi-1.1.2.tar.gz/hayashi-1.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='hayashi',
+    packages=find_packages(),
+    version='1.1.2',
+    description='Python library for computing the number of absorption features of the 21 cm forest in a semianalytic formalism.',
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+    author='Pablo Villanueva-Domingo',
+    author_email='pablo.villanueva.domingo@gmail.com',
+    install_requires=["numpy",
+                      "scipy",
+                      "tqdm",
+                      "colossus"],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
