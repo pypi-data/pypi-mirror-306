e160c092f5af1b9e320f34c700567d3decc15e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:11 +0000

    Add package ubigeoaii/0.2/ubigeoaii-0.2.tar.gz

---
+++ packages/ubigeoaii/ubigeoaii-0.2.tar.gz/ubigeoaii-0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='ubigeoaii',
+    version='0.2',
+    packages=['ubigeoai'],
+    description='Libreria que automatiza la ubicacion espacial de los Bounding Boxes',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='GEOINFORMATICA UACJ',
+    author_email='brayanmurillogutierrez988@outlook.es',
+    url='https://github.com/brayancarichi/UbiGeoAI.git',
+    install_requires=[
+        'fastai',
+'geopandas',
+'imutils',
+'ipython',
+'matplotlib',
+'numpy',
+'opencv_python',
+'opencv_python_headless',
+'pandas',
+'Pillow',
+'rasterio',
+'scikit_learn',
+'seaborn',
+'Shapely',
+'skimage',
+'supervision',
+'tqdm',
+'ultralytics',
+    ],
+)
