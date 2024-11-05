7da2b7b1621f078169d26772007108a86a2f7354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 07:04:36 +0000

    Add package MMBP/1.4.1.0.2/mmbp-1.4.1.0.2.tar.gz

---
+++ packages/MMBP/mmbp-1.4.1.0.2.tar.gz/mmbp-1.4.1.0.2/.setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+name = 'MMBP'
+
+setup(
+    name=name,
+    version='1.4.1.0.2',
+    author="KeyisB",
+    author_email="keyisb.pip@gmail.com",
+    description=name,
+    long_description='',
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/KeyisB/libs/tree/main/{name}",
+    packages=[name],
+    package_dir={'': f'{name}'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    license="MMB License v1.0",
+    install_requires= [],
+)
