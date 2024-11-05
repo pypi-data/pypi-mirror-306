01175614be59b5ba4e33f68a322f8128bb78e162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:10 +0000

    Add package ofh/0.1.0/ofh-0.1.0.tar.gz

---
+++ packages/ofh/ofh-0.1.0.tar.gz/ofh-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ofh",
+    version="0.1.0",
+    description="ofh is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'ofh = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
