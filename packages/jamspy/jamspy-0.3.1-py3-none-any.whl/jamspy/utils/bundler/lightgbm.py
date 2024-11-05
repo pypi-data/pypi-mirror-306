1f951c0b362cf85ffbdd4620a0cb72795ba629a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:04:57 +0000

    Add package jamspy/0.3.1/jamspy-0.3.1-py3-none-any.whl

---
+++ packages/jamspy/jamspy-0.3.1-py3-none-any.whl/jamspy/utils/bundler/lightgbm.py
@@ -0,0 +1,18 @@
+from lightgbm import Booster
+
+from jamspy.utils.bundler.common import ARTEFACTS_DIR, Bundle, create_tar_gz
+
+
+class LGBMBundler(Bundle):
+
+    def __init__(self, model_obj: Booster):
+        super().__init__()
+        self.model = model_obj
+
+    def bundle(self, model_name: str) -> None:
+        framework = 'lightgbm'
+        model_save_path = f'{ARTEFACTS_DIR}/{framework}-{model_name}.txt'
+        tar_gz_path = f'{ARTEFACTS_DIR}/{framework}-{model_name}.tar.gz'
+
+        self.model.save_model(model_save_path)
+        create_tar_gz(model_save_path, tar_gz_path)
