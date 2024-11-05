15c717fd1959d7cb6bdbb37d8b93d9229cdc0d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:04:41 +0000

    Add package brickllm/1.0.1/brickllm-1.0.1.tar.gz

---
+++ packages/brickllm/brickllm-1.0.1.tar.gz/brickllm-1.0.1/brickllm/helpers/__init__.py
@@ -0,0 +1,19 @@
+from .llm_models import _get_model
+from .prompts import (
+    get_elem_children_instructions,
+    get_elem_instructions,
+    get_relationships_instructions,
+    prompt_template_local,
+    schema_to_ttl_instructions,
+    ttl_example,
+)
+
+__all__ = [
+    "_get_model",
+    "get_elem_instructions",
+    "get_elem_children_instructions",
+    "get_relationships_instructions",
+    "schema_to_ttl_instructions",
+    "ttl_example",
+    "prompt_template_local",
+]
