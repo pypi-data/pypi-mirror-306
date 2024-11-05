2406da3d3e6490cb136bd955be6c0d3cba012e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:24 +0000

    Add package emlog/0.4/emlog-0.4-py3-none-any.whl

---
+++ packages/emlog/emlog-0.4-py3-none-any.whl/code/config/EmLogCfgReader.py
@@ -0,0 +1,41 @@
+import xml.etree.ElementTree as ET
+
+class EmLogCfgReader:
+    config = None
+
+    @staticmethod
+    def loadXml():
+        EmLogCfgReader.deserialize()
+
+    @staticmethod
+    def deserialize():
+        tree = ET.parse("EmLog.xml")
+        root = tree.getroot()
+
+        enabled = bool(root.get("enabled", default="false"))
+        debug = int(root.get("debug", default="0"))
+        discardSize = int(root.get("discardSize", default="1048576"))
+        discardMemoryRate = float(root.get("discardMemoryRate", default="1"))
+        formatConcurrency = int(root.get("formatConcurrency", default="1"))
+        ioBufferSize = int(root.get("ioBufferSize", default="8192"))
+        ioConcurrency = int(root.get("ioConcurrency", default="4"))
+        ioInterval = int(root.get("ioInterval", default="100"))
+        clrTimeout = int(root.get("clrTimeout", default="180000"))
+        tcpRetryInterval = int(root.get("tcpRetryInterval", default="5000"))
+        reportInterval = int(root.get("reportInterval", default="60000"))
+        peakMode = EmLogCfgReader.getPeakModeCfg(root.get("peakMode", default=None))
+        # deployConfig =
+        # variablesConfig =
+        # layoutsConfig =
+        # targetsConfig =
+        # rulesConfig =
+
+    @staticmethod
+    def getPeakModeCfg(peakModeStr):
+
+        try:
+            if not peakModeStr:
+                return None
+
+        except Exception as e:
+            print(e)
