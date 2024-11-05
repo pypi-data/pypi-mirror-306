c36b093b043be7d8c852f2d68cbf884b83b8e57b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:52 +0000

    Add package pykolofinance/2.1.1/pykolofinance-2.1.1-py3-none-any.whl

---
+++ packages/pykolofinance/pykolofinance-2.1.1-py3-none-any.whl/pykolofinance/audtilog/tasks.py
@@ -0,0 +1,28 @@
+from bson import json_util
+from django.conf import settings
+from elasticsearch import Elasticsearch
+
+from core.celery import APP
+
+from pykolofinance.audtilog.mongo_connector import get_database
+
+dbname = get_database()
+log_items = settings.APP_NAME.lower()
+collection_name = dbname[log_items]
+
+
+@APP.task()
+def send_logs_to_logger(data):
+    try:
+        collection_name.insert_one(data)
+        json_util
+        # elastic_search = Elasticsearch(hosts=settings.MONGODB_LOGGER_URL)
+        # result = elastic_search.index(index=settings.APP_NAME.lower(), body=data)
+        return data
+    except Exception as e:
+        print(f"Failed to Log Data: {e}")
+
+
+@APP.task()
+def send_log_to_auditlogger(data):
+    pass
