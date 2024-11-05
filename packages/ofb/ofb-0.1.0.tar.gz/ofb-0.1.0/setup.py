80b70c9d3e5df98c98eb8da4b865854c8951b284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:10 +0000

    Add package ofb/0.1.0/ofb-0.1.0.tar.gz

---
+++ packages/ofb/ofb-0.1.0.tar.gz/ofb-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ofb",
+    version="0.1.0",
+    description="ofb is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'ofb = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
