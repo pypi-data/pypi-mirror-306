535f80be7d46aa2b79296c25b835519026e18543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:06:15 +0000

    Add package dag-dataset/0.1.1/dag_dataset-0.1.1-py2.py3-none-any.whl

---
+++ packages/dag-dataset/dag_dataset-0.1.1-py2.py3-none-any.whl/dag_dataset/constants.py
@@ -0,0 +1,10 @@
+REQUIRED_ATTR_NAMES_LIST = [
+    'data_folder_path',
+    'data_objects_hierarchy',
+    'data_objects_file_paths',
+    'data_objects_table_path',
+    'num_header_rows',    
+]
+OPTIONAL_ATTR_NAMES_LIST = [
+    'other_columns',
+]
