9c66460b031ff80818d6ae489714176703aea5da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:21 +0000

    Add package VexHubUtils/0.1.1/vexhubutils-0.1.1.tar.gz

---
+++ packages/VexHubUtils/vexhubutils-0.1.1.tar.gz/vexhubutils-0.1.1/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="VexHubUtils",
+    version="0.1.1",
+    author="COMPLEX",
+    author_email="complex@vexhub.dev",
+    description="A package for Vex Hub utilities",
+    packages=find_packages(),
+    install_requires=[],
+)
