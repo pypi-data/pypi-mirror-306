753e3ded2b5e92b23d1813e96ababb45b2513a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:15 +0000

    Add package tdp/0.1.0/tdp-0.1.0.tar.gz

---
+++ packages/tdp/tdp-0.1.0.tar.gz/tdp-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="tdp",
+    version="0.1.0",
+    license="MIT",
+    author="taekop",
+    author_email="taekop@naver.com",
+    description="Hello",
+    long_description="Hello",
+    long_description_content_type="text/markdown",
+    url="https://github.com/taekop/tdp",
+    packages=setuptools.find_packages(),
+    keywords="tdp",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    python_requires=">=3.9,<4.0",
+)
