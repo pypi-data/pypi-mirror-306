6876950f2433ab2a4c93a24ce739b85f5a677d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 06:04:08 +0000

    Add package Ascii-Art-TNH/1.1.3/ascii_art_tnh-1.1.3-py3-none-any.whl

---
+++ packages/Ascii-Art-TNH/ascii_art_tnh-1.1.3-py3-none-any.whl/ascii_art_TNH/__main__.py
@@ -0,0 +1,10 @@
+from .ascii_art import *
+def main():
+    while(True):
+      user_input = input("Enter an animal or 'exit': ")
+      if user_input == "exit":
+         break
+      ascii_art(user_input)
+
+if __name__ == "__main__":
+    main()
