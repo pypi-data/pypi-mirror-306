8f089610b2578d4dc1d4a1381f3eacc1abd2694a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 17:04:33 +0000

    Add package gimkit-api/0.0.1/gimkit_api-0.0.1.tar.gz

---
+++ packages/gimkit-api/gimkit_api-0.0.1.tar.gz/gimkit_api-0.0.1/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name="gimkit-api",              
+    version="0.0.1",
+    packages=["gimkit-api"],
+    description="Reserved package name",
+    long_description="This package name is reserved.",
+    author="bingcube",
+    author_email="blurskydev@gmail.com",
+)
