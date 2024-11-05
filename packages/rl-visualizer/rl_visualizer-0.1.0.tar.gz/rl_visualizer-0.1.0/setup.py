063b3f711f633dc62e965afbaa189ab7809b87f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:04:51 +0000

    Add package rl-visualizer/0.1.0/rl_visualizer-0.1.0.tar.gz

---
+++ packages/rl-visualizer/rl_visualizer-0.1.0.tar.gz/rl_visualizer-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rl_visualizer",
+    version="0.1.0",
+    author="Bedirhan Sen",
+    author_email="bdrhnsen@gmail.com",
+    description="A package for visualizing action probabilities, entropy, and rewards in reinforcement learning models.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bdrhnsen/rl_visualizer", 
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "gymnasium",
+        "gym",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
