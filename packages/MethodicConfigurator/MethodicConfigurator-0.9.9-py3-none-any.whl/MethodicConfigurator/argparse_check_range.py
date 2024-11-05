907af767c70a88e3f4b7681d61a4099b207f532e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:04:59 +0000

    Add package MethodicConfigurator/0.9.9/MethodicConfigurator-0.9.9-py3-none-any.whl

---
+++ packages/MethodicConfigurator/MethodicConfigurator-0.9.9-py3-none-any.whl/MethodicConfigurator/argparse_check_range.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+'''
+This file is part of Ardupilot methodic configurator. https://github.com/ArduPilot/MethodicConfigurator
+
+SPDX-FileCopyrightText: 2024 Dmitriy Kovalev
+
+SPDX-License-Identifier: Apache-2.0
+
+https://gist.github.com/dmitriykovalev/2ab1aa33a8099ef2d514925d84aa89e7
+'''
+
+from argparse import Action
+from argparse import ArgumentError
+from operator import gt
+from operator import ge
+from operator import lt
+from operator import le
+from MethodicConfigurator.internationalization import _
+
+class CheckRange(Action):
+    '''
+    Check if the Argparse argument value is within the specified range
+    '''
+    ops = {"inf": gt,
+           "min": ge,
+           "sup": lt,
+           "max": le}
+
+    def __init__(self, *args, **kwargs):
+        if "min" in kwargs and "inf" in kwargs:
+            raise ValueError(_("either min or inf, but not both"))
+        if "max" in kwargs and "sup" in kwargs:
+            raise ValueError(_("either max or sup, but not both"))
+
+        for name in self.ops:
+            if name in kwargs:
+                setattr(self, name, kwargs.pop(name))
+
+        super().__init__(*args, **kwargs)
+
+    def interval(self):
+        if hasattr(self, "min"):
+            _lo = f"[{self.min}"
+        elif hasattr(self, "inf"):
+            _lo = f"({self.inf}"
+        else:
+            _lo = "(-infinity"
+
+        if hasattr(self, "max"):
+            _up = f"{self.max}]"
+        elif hasattr(self, "sup"):
+            _up = f"{self.sup})"
+        else:
+            _up = "+infinity)"
+
+        msg = _("valid range: {_lo}, {_up}")
+        return msg.format(**locals())
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        for name, op in self.ops.items():
+            if hasattr(self, name) and not op(values, getattr(self, name)):
+                raise ArgumentError(self, self.interval())
+        setattr(namespace, self.dest, values)
---
+++ packages/MethodicConfigurator/MethodicConfigurator-0.9.9-py3-none-any.whl/MethodicConfigurator/middleware_template_overview.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+'''
+This file is part of Ardupilot methodic configurator. https://github.com/ArduPilot/MethodicConfigurator
+
+SPDX-FileCopyrightText: 2024 Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
+
+SPDX-License-Identifier: GPL-3.0-or-later
+'''
+
+class TemplateOverview:  # pylint: disable=too-many-instance-attributes
+    """
+    Represents a single vehicle template configuration within the ArduPilot Methodic Configurator.
+
+    This class encapsulates the data and attributes associated with a specific vehicle template configuration.
+    It is designed to hold information about various components of a drone, such as the flight controller, telemetry system,
+    ESCs, propellers, and GNSS Receiver, along with their specifications. The class facilitates easy access to these
+    attributes, enabling the GUI to display and select the templates in a structured format.
+    """
+    def __init__(self, components_data: dict):
+        # The declaration order of these parameters determines the column order in the GUI
+        self.fc_manufacturer = components_data.get('Flight Controller', {}).get('Product', {}).get('Manufacturer', '')
+        self.fc_model = components_data.get('Flight Controller', {}).get('Product', {}).get('Model', '')
+        self.tow_max_kg = components_data.get('Frame', {}).get('Specifications', {}).get('TOW max Kg', '')
+        self.prop_diameter_inches = components_data.get('Propellers', {}).get('Specifications', {}).get('Diameter_inches', '')
+        self.rc_protocol = components_data.get('RC Receiver', {}).get('FC Connection', {}).get('Protocol', '')
+        self.telemetry_model = components_data.get('Telemetry', {}).get('Product', {}).get('Model', '')
+        self.esc_protocol = components_data.get('ESC', {}).get('FC Connection', {}).get('Protocol', '')
+        self.gnss_model = components_data.get('GNSS Receiver', {}).get('Product', {}).get('Model', '')
+        self.gnss_connection = components_data.get('GNSS Receiver', {}).get('FC Connection', {}).get('Type', '')
+
+    @staticmethod
+    def columns():
+        # Must match the order in the __init__() function above
+        return ("Template path",
+                "FC\nManufacturer",
+                "FC\nModel",
+                "TOW Max\n[KG]",
+                "Prop Diameter\n[inches]",
+                "RC\nProtocol",
+                "Telemetry\nModel",
+                "ESC\nProtocol",
+                "GNSS\nModel",
+                "GNSS\nConnection",
+                )
+
+    def attributes(self):
+        return self.__dict__.keys()
