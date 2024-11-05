ee4ea5ab942486a2e2230502dcffdb037b43a484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:05:29 +0000

    Add package lsj/0.1.0/lsj-0.1.0.tar.gz

---
+++ packages/lsj/lsj-0.1.0.tar.gz/lsj-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lsj",
+    version="0.1.0",
+    description="lsj is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'lsj = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
