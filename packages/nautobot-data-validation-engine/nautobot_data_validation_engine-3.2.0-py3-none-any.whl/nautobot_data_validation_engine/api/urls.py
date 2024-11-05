bed0a0a1b935635f3c1ca0f9bfa0843fbbe0de0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 14:04:51 +0000

    Add package nautobot-data-validation-engine/3.2.0/nautobot_data_validation_engine-3.2.0-py3-none-any.whl

---
+++ packages/nautobot-data-validation-engine/nautobot_data_validation_engine-3.2.0-py3-none-any.whl/nautobot_data_validation_engine/api/urls.py
@@ -0,0 +1,25 @@
+"""API routes."""
+
+from nautobot.core.api.routers import OrderedDefaultRouter
+
+from nautobot_data_validation_engine.api import views
+
+router = OrderedDefaultRouter(view_name="Data Validation Engine")
+
+# Regular expression rules
+router.register("regex-rules", views.RegularExpressionValidationRuleViewSet)
+
+# Min/max rules
+router.register("min-max-rules", views.MinMaxValidationRuleViewSet)
+
+# Required rules
+router.register("required-rules", views.RequiredValidationRuleViewSet)
+
+# Unique rules
+router.register("unique-rules", views.UniqueValidationRuleViewSet)
+
+# Data Compliance
+router.register("data-compliance", views.DataComplianceAPIView)
+
+
+urlpatterns = router.urls
