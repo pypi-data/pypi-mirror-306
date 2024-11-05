979876f24f45f286b3a0c3f591ebe1b5039499bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 17:04:55 +0000

    Add package ConstellationDAQ/0.1/constellationdaq-0.1-py3-none-any.whl

---
+++ packages/ConstellationDAQ/constellationdaq-0.1-py3-none-any.whl/constellation/satellites/H5DataWriter/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+SPDX-FileCopyrightText: 2024 DESY and the Constellation authors
+SPDX-License-Identifier: CC-BY-4.0
+
+This is the entry point for the caenhv satellite.
+"""
+
+from .H5DataWriter import main
+
+main()
