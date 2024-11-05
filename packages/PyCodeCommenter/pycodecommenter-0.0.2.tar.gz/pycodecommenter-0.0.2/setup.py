6c9752fd7377861806b314fee01e69102aa2c232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 04:05:19 +0000

    Add package PyCodeCommenter/0.0.2/pycodecommenter-0.0.2.tar.gz

---
+++ packages/PyCodeCommenter/pycodecommenter-0.0.2.tar.gz/pycodecommenter-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyCodeCommenter',  
+    version='0.0.2', 
+    packages=find_packages(),
+    install_requires=[  
+        
+    ],
+    author='NABASA AMOS',
+    author_email='amosnabasa256@gmail.com',
+    description='A Python library for generating Google-style docstrings.',
+    long_description=open('C:\\Users\\DSN\\Desktop\\Library\\code\\PyCodeCommenter\\README.md').read(),  # Path to your README file
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/yourproject',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
