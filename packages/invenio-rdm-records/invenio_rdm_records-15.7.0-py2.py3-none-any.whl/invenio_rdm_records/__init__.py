b6c5a3f55df55823bcc369e5d8d92dc61434af89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 03:07:17 +0000

    Add package invenio-rdm-records/15.7.0/invenio_rdm_records-15.7.0-py2.py3-none-any.whl

---
+++ packages/invenio-rdm-records/invenio_rdm_records-15.7.0-py2.py3-none-any.whl/invenio_rdm_records/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019-2024 CERN.
+# Copyright (C) 2019-2024 Northwestern University.
+# Copyright (C) 2024      KTH Royal Institute of Technology.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""DataCite-based data model for Invenio."""
+
+from .ext import InvenioRDMRecords
+
+__version__ = "15.7.0"
+
+__all__ = ("__version__", "InvenioRDMRecords")
