c8a3d16786b59ba0548037a45de94d50e8c73a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 06:04:05 +0000

    Add package patrick-personnal-lib/0.0.4/patrick_personnal_lib-0.0.4.tar.gz

---
+++ packages/patrick-personnal-lib/patrick_personnal_lib-0.0.4.tar.gz/patrick_personnal_lib-0.0.4/patrick_personnal_lib/__init__.py
@@ -0,0 +1,3 @@
+from .ask_path_save_load import select_path_to_save, select_file, creer_dossier, ask_yes_no_question
+from .DB_manipulation import build_list_for_one_colonne_to_create_DB, initialize_database, insert_in_DB_from_excel
+from .excel_utils import save_to_excel, lire_cellule_excel, convert_dict_to_list, construire_dict_cellule_colonne_for_DB, lire_une_cellule, formater_valeur
