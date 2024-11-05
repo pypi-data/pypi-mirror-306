84217e4561e82619e19b434ca6c3790d3ee6b750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:05:03 +0000

    Add package zkn/0.1.0/zkn-0.1.0.tar.gz

---
+++ packages/zkn/zkn-0.1.0.tar.gz/zkn-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zkn",
+    version="0.1.0",
+    description="zkn is a collection of libraries for building AI applications",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'zkn = name_register:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
