4c8983ee8c951b9d1c838113b24e084eedca2609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 05:04:12 +0000

    Add package django-rest-extensions/1.0.0/django_rest_extensions-1.0.0.tar.gz

---
+++ packages/django-rest-extensions/django_rest_extensions-1.0.0.tar.gz/django_rest_extensions-1.0.0/rest_extensions/permissions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Xiang Wang @ 2019-10-10 01:13:52
+
+
+from rest_framework.permissions import BasePermission
+
+
+class NoDeletePermission(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method == "DELETE":
+            return False
+        return True
