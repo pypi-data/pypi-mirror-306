1913293682dc8559512659846554dc2394cc1a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 03:07:35 +0000

    Add package construction-utils/0.2.1/construction_utils-0.2.1.tar.gz

---
+++ packages/construction-utils/construction_utils-0.2.1.tar.gz/construction_utils-0.2.1/tests/test_construction_readme_generator.py
@@ -0,0 +1,65 @@
+# Copyright (C) 2024 twyleg
+import shutil
+
+import pytest
+
+import logging
+from pathlib import Path
+
+from construction_utils.readme_generator import Construction, Workspace, ConstructionReadmeGenerator, WorkspaceReadmeGenerator
+
+#
+# General naming convention for unit tests:
+#               test_INITIALSTATE_ACTION_EXPECTATION
+#
+
+
+FILE_DIR = Path(__file__).parent
+
+
+@pytest.fixture(autouse=True)
+def print_tmp_path(tmp_path):
+    logging.info("tmp_path: %s", tmp_path)
+    return None
+
+
+@pytest.fixture
+def workspace(tmp_path, monkeypatch):
+    workspace_template_filepath = FILE_DIR / "resources/workspaces/example_construction_workspace"
+    dst_workspace_filepath = tmp_path / "construction_workspace"
+    shutil.copytree(workspace_template_filepath, dst_workspace_filepath)
+    monkeypatch.chdir(dst_workspace_filepath)
+    return dst_workspace_filepath
+
+
+class TestConstructionReader:
+    def test_ValidConstruction_ReadConstruction_ConstructionReadSuccessfully(self, caplog, workspace):
+        construction = Construction(workspace / "construction_a")
+
+        assert construction.things_data["name"] == "Construction A"
+        assert construction.things_data["tags"][0] == "construction"
+        assert construction.things_data["tags"][1] == "a"
+        assert construction.things_data["thingiverse_id"] == 0
+        assert construction.things_data["thingiverse_creator"] == "creator"
+
+        assert len(construction.filepaths_source) == 3
+        assert len(construction.filepaths_img) == 3
+        assert len(construction.filepaths_3d) == 3
+        assert len(construction.filepaths_gcode) == 1
+
+
+class TestConstructionReadmeGenerator:
+    def test_ValidConstructionWorkspace_GenerateConstructionReadme_ReadmeGenerated(self, caplog, workspace):
+        construction_dir = workspace / "construction_a"
+        construction = Construction(construction_dir)
+        construction_readme_generator = ConstructionReadmeGenerator(construction)
+        construction_readme_generator.generate()
+
+
+class TestWorkspaceReadmeGenerator:
+    def test_ValidConstructionWorkspace_GenerateWorkspaceReadme_ReadmeGenerated(self, caplog, workspace):
+        construction_workspace = Workspace(workspace)
+        construction_workspace_readme_generator = WorkspaceReadmeGenerator(construction_workspace)
+        construction_workspace_readme_generator.generate()
+        for construction in construction_workspace.constructions:
+            print(construction.construction_dir_path)
---
+++ packages/construction-utils/construction_utils-0.2.1.tar.gz/construction_utils-0.2.1/tests/test_freecad_exporter.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2024 twyleg
+import shutil
+
+import pytest
+
+import logging
+from pathlib import Path
+
+from construction_utils.freecad_exporter import FreecadExporter
+
+
+FILE_DIR = Path(__file__).parent
+
+
+@pytest.fixture
+def workspace(tmp_path):
+    source_dir_filepath = FILE_DIR / "resources/src"
+    dst_workspace_filepath = tmp_path / "src/"
+    shutil.copytree(source_dir_filepath, dst_workspace_filepath)
+    return tmp_path
+
+
+class TestConstructionReadmeGenerator:
+
+    def file_exists(self, file_path: Path) -> bool:
+        return file_path.exists() and file_path.is_file()
+
+    def test_ValidSourceFiles_AddExportJobsInMultipleFormats_ImagesExported(self, caplog, workspace):
+        freecad_exporter = FreecadExporter()
+        freecad_exporter.add_export_job(workspace / "src/example_part_a.FCStd")
+        freecad_exporter.add_export_job(workspace / "src/example_part_b.FCStd", workspace / "output_b/")
+        freecad_exporter.add_export_job(workspace / "src/example_part_c.FCStd", workspace / "output_c/example_output_part_c.png")
+
+        freecad_exporter.export()
+
+        assert self.file_exists(workspace / "src/example_part_a.png")
+        assert self.file_exists(workspace / "output_b/example_part_b.png")
+        assert self.file_exists(workspace / "output_c/example_output_part_c.png")
