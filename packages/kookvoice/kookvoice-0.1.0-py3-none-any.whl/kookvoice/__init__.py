7049809263631ad2878e1c336adc95621ba7d53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 04:04:38 +0000

    Add package kookvoice/0.1.0/kookvoice-0.1.0-py3-none-any.whl

---
+++ packages/kookvoice/kookvoice-0.1.0-py3-none-any.whl/kookvoice/__init__.py
@@ -0,0 +1,17 @@
+from .kookvoice import Player, start, run,PlayInfo,on_event, set_ffmpeg
+from .kookvoice import run_async, configure_logging, Status
+
+
+__all__ = [
+    "Player",
+    "set_ffmpeg",
+    "on_event",
+    "run_async",
+    "start",
+    "run",
+    "PlayInfo",
+    "configure_logging",
+    "Status"
+]
+
+
