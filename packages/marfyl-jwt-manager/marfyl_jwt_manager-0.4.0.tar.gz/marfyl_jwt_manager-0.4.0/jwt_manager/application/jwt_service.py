1701fd7f09d2e60306a2f800c3510184d291f4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-05 15:05:35 +0000

    Add package marfyl-jwt-manager/0.4.0/marfyl_jwt_manager-0.4.0.tar.gz

---
+++ packages/marfyl-jwt-manager/marfyl_jwt_manager-0.4.0.tar.gz/marfyl_jwt_manager-0.4.0/jwt_manager/application/jwt_service.py
@@ -0,0 +1,23 @@
+# Standard Import
+
+# Other Import
+from fastapi import Request, HTTPException
+
+# Local Import
+from jwt_manager.domain.services import JWTManager
+from jwt_manager.infrastructure.header_parser import extract_token_from_header
+
+jwt_manager = JWTManager()
+
+def get_current_user(request: Request):
+    """
+    Main Function to get current user
+    :param request: info user
+    :return: jwt_manager.decode_token(token)
+    """
+    token = extract_token_from_header(request)
+    try:
+        user_data = jwt_manager.decode_token(token)
+        return user_data
+    except Exception as e:
+        raise HTTPException(status_code=401, detail=str(e))
