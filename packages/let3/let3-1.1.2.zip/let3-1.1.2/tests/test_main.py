5e002de6f0f9b90a164b47e2c2609454e685d740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 07:04:59 +0000

    Add package let3/1.1.2/let3-1.1.2.zip

---
+++ packages/let3/let3-1.1.2.zip/let3-1.1.2/tests/test_main.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2016 Adam Karpierz
+# SPDX-License-Identifier: MIT
+
+import unittest
+
+import let
+from let import let
+
+
+class MainTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_main(self):
+        pass
