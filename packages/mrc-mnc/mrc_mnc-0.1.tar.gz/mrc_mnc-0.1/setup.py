f29c4095ea44d53ad290a7b972a6e06c22cd7572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:05:01 +0000

    Add package mrc-mnc/0.1/mrc_mnc-0.1.tar.gz

---
+++ packages/mrc-mnc/mrc_mnc-0.1.tar.gz/mrc_mnc-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mrc_mnc",
+    version="0.1",
+    author="yingkehuang",
+    author_email="ykhuang@ihep.ac.cn",
+    description="A brief description of your package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
