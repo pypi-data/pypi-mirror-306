3f94f1a33bda948251d458a1519113abda495014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 06:05:17 +0000

    Add package bfqcircuits/0.2.1/bfqcircuits-0.2.1-py3-none-any.whl

---
+++ packages/bfqcircuits/bfqcircuits-0.2.1-py3-none-any.whl/bfqcircuits/__init__.py
@@ -0,0 +1,7 @@
+#  Copyright (c) 2024. Martin Spiecker. All rights reserved.
+
+"""Main package of Black Forest Quantum Circuits."""
+
+__author__ = """Martin Spiecker"""
+__email__ = "47812395+MartinSpiecker@users.noreply.github.com"
+__version__ = "v0.2.1"
