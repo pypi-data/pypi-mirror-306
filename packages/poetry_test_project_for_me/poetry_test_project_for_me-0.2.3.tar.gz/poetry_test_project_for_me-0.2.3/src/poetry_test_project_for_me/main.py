40b8c8e32482f6211f7c6bc0160d72bd389a431b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 16:05:27 +0000

    Add package poetry_test_project_for_me/0.2.3/poetry_test_project_for_me-0.2.3.tar.gz

---
+++ packages/poetry_test_project_for_me/poetry_test_project_for_me-0.2.3.tar.gz/poetry_test_project_for_me-0.2.3/src/poetry_test_project_for_me/main.py
@@ -0,0 +1,20 @@
+import requests
+
+def main():
+    """Download a random image from the internet.
+    """
+    url = 'https://picsum.photos/600/900'  # This gets a random 600*900 image
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        with open('random_image.jpg', 'wb') as file:
+            file.write(response.content)
+        print("Image downloaded successfully.")
+    else:
+        print("Failed to download image.")
+
+    print("Done.")
+
+if __name__ == "__main__":
+    main()
