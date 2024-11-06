fafda9b67e57b869199a35766bef82b70737784f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 15:05:03 +0000

    Add package tinyscibio/0.3.0/tinyscibio-0.3.0.tar.gz

---
+++ packages/tinyscibio/tinyscibio-0.3.0.tar.gz/tinyscibio-0.3.0/src/tinyscibio/__init__.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from ._intervals import (
+    NoMatchingChr,
+    NotInt64StartEnd,
+    NotSatisfyMinColReq,
+    bed_to_df,
+    find_overlaps,
+)
+from ._io import _PathLike, get_parent_dir, make_dir, parse_path
+from ._version import version as __version__
+from .api_server import request_api_server
+from .bam import (
+    BAMetadata,
+    count_indel_bases,
+    count_indel_events,
+    count_mismatch_events,
+    count_soft_clip_bases,
+    count_unaligned_events,
+    parse_cigar,
+    parse_md,
+)
+
+__all__ = [
+    "_PathLike",
+    "BAMetadata",
+    "make_dir",
+    "parse_path",
+    "get_parent_dir",
+    "request_api_server",
+    "parse_md",
+    "parse_cigar",
+    "count_unaligned_events",
+    "count_indel_events",
+    "count_mismatch_events",
+    "count_soft_clip_bases",
+    "count_indel_bases",
+    "bed_to_df",
+    "find_overlaps",
+    "NotSatisfyMinColReq",
+    "NotInt64StartEnd",
+    "NoMatchingChr",
+    "__version__",
+]
