d0076e73202155b1f2ac1fc223e8200a8696af14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:57 +0000

    Add package sea-query/0.2.5/sea_query-0.2.5-cp39-none-win_amd64.whl

---
+++ packages/sea-query/sea_query-0.2.5-cp39-none-win_amd64.whl/sea_query/foreign_key.py
@@ -0,0 +1,13 @@
+from ._internal import (
+    ForeignKey,
+    ForeignKeyAction,
+    ForeignKeyCreateStatement,
+    ForeignKeyDropStatement,
+)
+
+__all__ = [
+    "ForeignKey",
+    "ForeignKeyAction",
+    "ForeignKeyCreateStatement",
+    "ForeignKeyDropStatement",
+]
