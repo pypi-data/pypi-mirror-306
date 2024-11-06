493c3ff50a375457245d8d927cd87abc18d84f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:57 +0000

    Add package sea-query/0.2.5/sea_query-0.2.5-cp312-none-win32.whl

---
+++ packages/sea-query/sea_query-0.2.5-cp312-none-win32.whl/sea_query/query.py
@@ -0,0 +1,27 @@
+from ._internal import (
+    DeleteStatement,
+    InsertStatement,
+    LockBehavior,
+    LockType,
+    NullsOrder,
+    OnConflict,
+    OrderBy,
+    Query,
+    SelectStatement,
+    UnionType,
+    UpdateStatement,
+)
+
+__all__ = [
+    "DeleteStatement",
+    "InsertStatement",
+    "LockBehavior",
+    "LockType",
+    "NullsOrder",
+    "OnConflict",
+    "OrderBy",
+    "Query",
+    "SelectStatement",
+    "UnionType",
+    "UpdateStatement",
+]
