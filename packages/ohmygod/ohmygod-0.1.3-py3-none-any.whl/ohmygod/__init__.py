9ea36c906c9e8bdfa3a8322d544792a6f6efcccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 15:05:13 +0000

    Add package ohmygod/0.1.3/ohmygod-0.1.3-py3-none-any.whl

---
+++ packages/ohmygod/ohmygod-0.1.3-py3-none-any.whl/ohmygod/__init__.py
@@ -0,0 +1,10 @@
+"""OH MY GOD package built upon rich console interface"""
+
+__version__ = "0.1.3"
+
+
+from .main import OhMyGod
+from .message import Message as HolyMessage
+from .utils import Color as HolyColor
+
+__all__ = ["OhMyGod", "HolyMessage", "HolyColor"]
