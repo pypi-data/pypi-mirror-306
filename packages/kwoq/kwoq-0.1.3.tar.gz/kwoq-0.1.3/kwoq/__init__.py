72e68cb380e93d69047486e9fb90501d456853e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 03:09:09 +0000

    Add package kwoq/0.1.3/kwoq-0.1.3.tar.gz

---
+++ packages/kwoq/kwoq-0.1.3.tar.gz/kwoq-0.1.3/kwoq/__init__.py
@@ -0,0 +1,4 @@
+from .analysis import EconometricAnalysis
+from .api import EconometricAI
+
+__all__ = ['EconometricAnalysis', 'EconometricAI']
---
+++ packages/kwoq/kwoq-0.1.3.tar.gz/kwoq-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kwoq",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'scipy',
+        'statsmodels',
+        'seaborn',
+        'scikit-learn',
+        'requests',
+    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple data processing and visualization library",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
