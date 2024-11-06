21641a5da482a962f3f78b97386941cc7bf824e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:52 +0000

    Add package pyjacket/0.2.4/pyjacket-0.2.4-py3-none-any.whl

---
+++ packages/pyjacket/pyjacket-0.2.4-py3-none-any.whl/pyjacket/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.2.4'
+
+
+from pyjacket.core import *
+
+from pyjacket import (
+    arrtools, 
+    # core,
+    # cvtools, 
+    # dna,
+    filetools, 
+    # graphs, 
+    # ntheory,
+    # stheory,
+    )
