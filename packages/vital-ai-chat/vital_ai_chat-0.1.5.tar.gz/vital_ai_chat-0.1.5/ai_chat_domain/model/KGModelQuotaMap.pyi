952662b25439c30b7cefd279fe80cf4630b45314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 06:04:39 +0000

    Add package vital-ai-chat/0.1.5/vital_ai_chat-0.1.5.tar.gz

---
+++ packages/vital-ai-chat/vital_ai_chat-0.1.5.tar.gz/vital_ai_chat-0.1.5/ai_chat_domain/model/KGModelQuotaMap.pyi
@@ -0,0 +1,10 @@
+
+import datetime
+from vital_ai_vitalsigns.model.VITAL_GraphContainerObject import VITAL_GraphContainerObject
+
+
+class KGModelQuotaMap(VITAL_GraphContainerObject):
+        haleyChatMessageURI: str
+
+str
+
---
+++ packages/vital-ai-chat/vital_ai_chat-0.1.5.tar.gz/vital_ai_chat-0.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vital-ai-chat',
+    version='0.1.5',
+    author='Marc Hadfield',
+    author_email='marc@vital.ai',
+    description='VitalSigns chat domain',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/vital-ai/vitalhome-chat',
+    packages=find_packages(),
+    package_data={
+         '': ['*.pyi'],
+    },
+    license='Apache License 2.0',
+    install_requires=[
+            'vital-ai-vitalsigns>=0.1.21',
+            'vital-ai-domain>=0.1.7',
+            'vital-ai-haley-kg>=0.1.5',
+            'vital-ai-haley-ml>=0.1.5',
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
