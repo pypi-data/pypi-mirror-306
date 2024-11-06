6fdb6e180eb7800e9dac434bcc8a9c1c507e25fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:51 +0000

    Add package ichier/0.1.5/ichier-0.1.5.tar.gz

---
+++ packages/ichier/ichier-0.1.5.tar.gz/ichier-0.1.5/setup.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+import sys
+
+sys.path.insert(0, ".")
+
+import ichier
+
+VERSION = ichier.__version__
+DESCRIPTION = "Integrated Circuit Hierarchy"
+PROJECT = "ichier"
+AUTHOR = ichier.__author__
+EMAIL = ichier.__email__
+URL = ichier.__url__
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=Path("README.md").read_text("utf-8"),
+    long_description_content_type="text/markdown",
+    url=URL,
+    packages=find_packages(exclude=["tests"]),
+    python_requires=">=3.8",
+    install_requires=Path("requirements.txt").read_text("utf-8").split(),
+    keywords=[
+        PROJECT,
+        "verilog",
+        "spice",
+        "cdl",
+        "ic",
+        "hierarchy",
+        "linux",
+    ],
+    entry_points={
+        "console_scripts": [
+            "ichier=ichier._main:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: Unix",
+    ],
+)
