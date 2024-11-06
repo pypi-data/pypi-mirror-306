74f05950439e8c4aa4b66cb8a9b343bab61ba8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 14:05:37 +0000

    Add package video-reader-rs/0.1.10/video_reader_rs-0.1.10.tar.gz

---
+++ packages/video-reader-rs/video_reader_rs-0.1.10.tar.gz/video_reader_rs-0.1.10/README.md
@@ -0,0 +1,178 @@
+<h1 align="center">
+  <code>video_reader-rs</code>
+</h1>
+<p align="center">A python module to decode videos based on rust ffmpeg-next, with a focus on ML use cases.</p>
+
+## :bulb: Why yet another library based on ffmpeg ?
+
+When training ML models on videos, it is usefull to load small sub-clips of videos. So decoding the
+entire video is not necessary.
+
+The great [decord](https://github.com/dmlc/decord) library seems to be unmaintained, while having
+a few issues. The main one (for us) is bad memory management, which makes it crash on large videos.
+Indeed it allocates memory for the whole video when instantiating a VideoReader object. While in fact
+you might want to only get a few frames from this video.
+
+So we took great inspiration from this library to rewrite the `get_batch` function using ffmpeg-next
+rust bindings. We also added the `decode` function which is usefull for decoding the entire video or
+for temporally reducing it using a `compression_factor`. Option to resize the video while decoding is also
+added.
+
+NOTE: other functionalities of `decord` are not implemented (yet?).
+
+Benchmark indicates that `video_reader-rs` is performing equally or better than `decord`, while using less memory.
+At least on the intended ML uses cases where video resolution remains reasonable, eg not 4K videos.
+
+## :hammer_and_wrench: Installation
+### Install via pip
+```bash
+pip install video-reader-rs
+```
+Should work with python >= 3.8 on recent linux x86_64, macos and windows.
+
+### Manual installation
+You need to have ffmpeg installed on your system.
+Install maturin:
+```bash
+pip install maturin
+```
+
+Activate a virtual-env where you want to use the video_reader library and build the library as follows:
+```bash
+maturin develop --release
+```
+`maturin develop` builds the crate and installs it as a python module directly in the current virtualenv.
+the `--release` flag ensures the Rust part of the code is compiled in release mode, which enables compiler optimizations.
+
+:warning: If you are using a version of **ffmpeg >= 6.0** you need to enable the `ffmpeg_6_0` feature:
+```bash
+maturin develop --release --features ffmpeg_6_0
+```
+
+## :computer: Usage
+Decoding a video is as simple as:
+```python
+import video_reader as vr
+frames = vr.decode(filename, resize, compression_factor, threads, start_frame, end_frame)
+```
+* **filename**: path to the video file to decode
+* **resize**: optional resizing for the video.
+* **compression_factor**: temporal sampling, eg if 0.25, take 25% of the frames, evenly spaced.
+* **threads**: number of CPU cores to use for ffmpeg decoding, 0 means auto (let ffmpeg pick the optimal number).
+* **start_frame** - Start decoding from this frame index
+* **end_frame** - Stop decoding at this frame index
+
+Returns a numpy array of shape (N, H, W, C).
+
+We can do the same thing if we want grayscale frames, and it will retun an array of shape (N, H, W).
+```python
+frames = vr.decode_gray(filename, resize, compression_factor, threads, start_frame, end_frame)
+```
+
+If we only need a sub-clip of the video we can use the `get_batch` function:
+```python
+frames = vr.get_batch(filename, indices, threads=0, resize_shorter_side=None, with_fallback=False)
+```
+* **filename**: path to the video file to decode
+* **indices**: list of indices of the frames to get
+* **threads**: number of CPU cores to use for ffmpeg decoding, currently has no effect as `get_batch` does not support multithreading. (NOTE: it is still as fast as decord from our benchmarking)
+* **resize_shorter_side**: optional resizing for the video.
+* **with_fallback**: False by default, if True will fallback to iterating over all packets of the video and only decoding the frames that match in `indices`. It is safer to use when the video contains B-frames and you really need to get the frames exactly corresponding to the given indices. It can also be faster in some use cases if you have many cpu cores available.
+
+We can also get the shape of the raw video
+```python
+(n, h, w) = vr.get_shape(filename)
+```
+
+Or get a dict with information about the video, returned as Dict[str, str]
+```python
+info_dict = vr.get_info(filename)
+print(info_dict["fps"])
+```
+
+We can encode the video with h264 codec
+```python
+vr.save_video(frames, "video.mp4", fps=15, codec="h264")
+```
+NOTE: currently only work if the frames shape is a multiple of 32.
+
+### :warning: Dealing with High Res videos
+ If you are dealing with High Resolution videos such as HD, UHD etc. We recommend using `vr.decode_fast()` which has the same arguments as `vr.decode()` but will return a list of frames. It uses async conversion from yuv420p to RGB to speed things up.
+
+If you have some memory limitations that wont let you decode the entire video at once, you can decode by chunk like so:
+```python
+import  video_reader as vr
+
+videoname = "/path/to/your/video.mp4"
+chunk_size = 800 # adjust to fit within your memory limit
+video_length = int(vr.get_shape(videoname)[0])
+
+for i in range(0, video_length, chunk_size):
+    end = min(i + chunk_size, video_length)
+    frames = vr.decode_fast(
+        videoname,
+        start_frame=i,
+        end_frame=end,
+        threads=8 # adjust to whatever works best for your use case
+        # you can also set threads=0 to let ffmpeg auto detect
+    )
+    # do something with this chunk of 800 `frames`
+```
+
+
+## :rocket: Performance comparison
+Decoding a video with shape (2004, 1472, 1472, 3). Tested on a laptop (12 cores Intel i7-9750H CPU @ 2.60GHz), 15Gb of RAM with Ubuntu 22.04.
+
+Options: 
+- f: compression factor
+- r: resize shorter side
+- g: grayscale
+
+| Options | OpenCV | decord* | vr.decode | vr.decode_fast |
+|:---:|:---:|:---:|:---:|:---:|
+| f 1.0 | 65s | 18s | 9.3s | **6.2s** | 
+| f 0.5 | 33.96s | 14.6s | 5.5s | **4.2s** | 
+|f 0.25 | 7.16s | 14.03s | 4.2s | **3.8s** |
+|f 0.25, r 512| 6.5s | 13.3s | 3.92s | **3.5s** |
+| f 0.25, g | 20.2s | 25.7s | **6.6s** | N/A |
+
+\* decord was tested on a machine with more RAM and CPU cores because it was crashing on the laptop with only 15Gb. See below.
+
+## :boom: Crash test
+Tested on a laptop with 15Gb of RAM, with ubuntu 22.04 and python 3.10.
+Run this script:
+```python
+import video_reader as vr
+from time import time
+
+def bench_video_decode(filename, compress_factor, resize):
+    start =  time()
+    vid = vr.decode(filename, resize_shorter_side=resize, compression_factor=compress_factor, threads=0)
+    duration = time() - start
+    print(f"Duration {duration:.2f}sec")
+    return vid
+
+vid = bench_video_decode("sample.mp4", 0.25)
+print("video shape:", vid.shape)
+
+# Terminal output:
+# Duration 4.81sec
+# video shape: (501, 1472, 1472, 3)
+```
+
+And then run this script:
+```python
+from decord import VideoReader
+
+vr = VideoReader("sample.mp4")
+
+# Terminal output:
+# terminate called after throwing an instance of 'std::bad_alloc'
+#  what():  std::bad_alloc
+# [1]    9636 IOT instruction (core dumped)
+```
+
+## :stars: Credits
+- [decord](https://github.com/dmlc/decord) for showing how to `get_batch` efficiently.
+- [ffmpeg-next](https://github.com/zmwangx/rust-ffmpeg) for the Rust bindings to ffmpeg.
+- [video-rs](https://github.com/oddity-ai/video-rs) for the nice high level api which makes it easy to encode videos and for the code snippet to convert ffmpeg frames to ndarray ;-)
---
+++ packages/video-reader-rs/video_reader_rs-0.1.10.tar.gz/video_reader_rs-0.1.10/src/lib.rs
@@ -0,0 +1,373 @@
+use numpy::ndarray::{Array3, Array4, Dim};
+use numpy::{IntoPyArray, PyArray, PyReadonlyArray4};
+mod video_io;
+use ffmpeg::format::Pixel as AvPixel;
+use ffmpeg_next as ffmpeg;
+use log::debug;
+use pyo3::{
+    exceptions::PyRuntimeError,
+    pymodule,
+    types::{IntoPyDict, PyDict, PyModule},
+    Bound, PyResult, Python,
+};
+use std::collections::HashMap;
+use video_io::{rgb2gray, save_video, DecoderConfig, VideoReader};
+
+use once_cell::sync::Lazy;
+use tokio::runtime::{self, Runtime};
+
+static RUNTIME: Lazy<Runtime> = Lazy::new(|| {
+    runtime::Builder::new_multi_thread()
+        .enable_io()
+        .build()
+        .unwrap()
+});
+
+#[pymodule]
+fn video_reader<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
+    env_logger::init();
+    /// Get shape of the video: number of frames, height and width
+    fn get_shape(filename: &String) -> Result<(usize, usize, usize), ffmpeg::Error> {
+        let vr = VideoReader::new(filename.to_owned(), DecoderConfig::default())?;
+        let width = vr.decoder().video().width() as usize;
+        let height = vr.decoder().video().height() as usize;
+        let num_frames = vr.stream_info().frame_count();
+        Ok((*num_frames, height, width))
+    }
+
+    /// Get video information: width, height, number of frames and fps
+    fn get_info(filename: &str) -> Result<HashMap<&str, String>, ffmpeg::Error> {
+        let vr = VideoReader::new(filename.to_owned(), DecoderConfig::default())?;
+        let mut info_dict = vr.decoder().video_info().clone();
+        info_dict.insert("frame_count", vr.stream_info().frame_count().to_string());
+        Ok(info_dict)
+    }
+
+    /// Decode video and return ndarray representing RGB frames
+    fn decode_video(
+        filename: &str,
+        decoder_config: DecoderConfig,
+    ) -> Result<Array4<u8>, ffmpeg::Error> {
+        let vr = VideoReader::new(filename.to_owned(), decoder_config)?;
+        vr.decode_video()
+    }
+
+    /// Decode video as yuv420p and ansynchronously convert to RGB frames.
+    /// Returns a Vec<FrameArray>
+    fn decode_video_fast(
+        filename: &str,
+        decoder_config: DecoderConfig,
+    ) -> Result<Vec<Array3<u8>>, ffmpeg::Error> {
+        let vr = VideoReader::new(
+            filename.to_owned(),
+            decoder_config,
+            // compression_factor,
+            // resize_shorter_side,
+            // threads,
+            // true,
+            // start_frame.as_ref(),
+            // end_frame.as_ref(),
+            // Some(AvPixel::YUV420P),
+        )?;
+        Ok(RUNTIME.block_on(async { vr.decode_video_fast().await.unwrap() }))
+    }
+
+    /// Decode video and return ndarray representing grayscale frames
+    fn decode_video_gray(
+        filename: &str,
+        decoder_config: DecoderConfig,
+    ) -> Result<Array3<u8>, ffmpeg::Error> {
+        let vr = VideoReader::new(
+            filename.to_owned(),
+            decoder_config,
+            // compression_factor,
+            // resize_shorter_side,
+            // threads,
+            // true,
+            // start_frame,
+            // end_frame,
+            // None,
+        )?;
+        let vid = vr.decode_video()?;
+        let gray_vid = rgb2gray(vid);
+        Ok(gray_vid)
+    }
+
+    fn get_batch(
+        filename: &String,
+        indices: Vec<usize>,
+        resize_shorter_side: Option<f64>,
+        threads: Option<usize>,
+        with_fallback: bool,
+    ) -> Result<Array4<u8>, ffmpeg::Error> {
+        let video: Array4<u8>;
+        // video reader that will use Seeking, only works in single thread mode for now
+        let decoder_config =
+            DecoderConfig::new(1, resize_shorter_side, None, false, None, None, None);
+        let mut vr = VideoReader::new(filename.to_owned(), decoder_config)?;
+        let start_time: i32 = vr
+            .decoder()
+            .video_info()
+            .get("start_time")
+            .unwrap()
+            .as_str()
+            .parse::<i32>()
+            .unwrap_or(0);
+        let num_zero_pts = vr
+            .stream_info()
+            .frame_times()
+            .iter()
+            .filter(|(_, v)| v.pts() <= &0)
+            .collect::<Vec<_>>()
+            .len();
+        let first_key_idx = vr.stream_info().key_frames()[0];
+        let (_, first_key) = vr
+            .stream_info()
+            .frame_times()
+            .iter()
+            .nth(first_key_idx)
+            .unwrap();
+        // Try to detect weird cases and if so switch to decoding without seeking
+        // NOTE: start_time > 0 means we have B-frames. Currently `get_batch` does not guarantee
+        // that we get the exact frame we want in this case, so by setting with_fallback to True
+        // we can enable a more accurate method, namely `get_batch_safe`.
+        if with_fallback
+            && ((num_zero_pts > 1)
+                || (first_key.dur() <= &0)
+                || (first_key.dts() < &0)
+                || start_time > 0)
+        {
+            debug!("Decoding using `get_batch_safe`");
+            // switch to video reader that will iterate on all frames, we can use threading here
+            let min_idx = indices.iter().min();
+            let max_idx = indices.iter().max();
+            let decoder_config = DecoderConfig::new(
+                threads.unwrap_or(0),
+                resize_shorter_side,
+                None,
+                true,
+                min_idx.copied(),
+                max_idx.copied(),
+                None,
+            );
+            vr = VideoReader::new(filename.to_owned(), decoder_config)?;
+            video = vr.get_batch_safe(indices)?;
+        } else {
+            video = vr.get_batch(indices)?;
+        }
+        Ok(video)
+    }
+
+    // wrapper of `decode_video`
+    /// Decode video and return a 4D ndarray representing RGB frames with the shape (N, H, W, C)
+    /// * `filename` - Path to the video file
+    /// * `resize_shorter_side` - Resize the shorter side of the video to this value.
+    /// * `compression_factor` - Factor for temporal compression. If None, then no compression.
+    /// * `threads` - Number of threads to use for decoding. If None, let ffmpeg decide the optimal
+    /// * `start_frame` - Start decoding from this frame index
+    /// * `end_frame` - Stop decoding at this frame index
+    /// number.
+    /// * Returns a 4D ndarray with shape (N, H, W, C)
+    #[pyfn(m)]
+    #[pyo3(name = "decode")]
+    #[pyo3(signature = (filename, resize_shorter_side=None, compression_factor=None, threads=None, start_frame=None, end_frame=None))]
+    fn decode_py<'py>(
+        py: Python<'py>,
+        filename: &str,
+        resize_shorter_side: Option<f64>,
+        compression_factor: Option<f64>,
+        threads: Option<usize>,
+        start_frame: Option<usize>,
+        end_frame: Option<usize>,
+    ) -> PyResult<Bound<'py, PyArray<u8, Dim<[usize; 4]>>>> {
+        let decoder_config = DecoderConfig::new(
+            threads.unwrap_or(0),
+            resize_shorter_side,
+            compression_factor,
+            true,
+            start_frame,
+            end_frame,
+            None,
+        );
+        let res_decode = decode_video(filename, decoder_config);
+        match res_decode {
+            Ok(vid) => Ok(vid.into_pyarray_bound(py)),
+            Err(e) => Err(PyRuntimeError::new_err(format!("Error: {}", e))),
+        }
+    }
+
+    // wrapper of `decode_video_fast`
+    /// Decode video and return a Vec of RGB frames with the shape (H, W, C)
+    /// * `filename` - Path to the video file
+    /// * `resize_shorter_side` - Resize the shorter side of the video to this value.
+    /// * `compression_factor` - Factor for temporal compression. If None, then no compression.
+    /// * `threads` - Number of threads to use for decoding. If None, let ffmpeg decide the optimal
+    /// * `start_frame` - Start decoding from this frame index
+    /// * `end_frame` - Stop decoding at this frame index
+    /// number.
+    /// * Returns a Vec of 3D ndarray with shape (H, W, C)
+    #[pyfn(m)]
+    #[pyo3(name = "decode_fast")]
+    #[pyo3(signature = (filename, resize_shorter_side=None, compression_factor=None, threads=None, start_frame=None, end_frame=None))]
+    #[allow(clippy::type_complexity)]
+    fn decode_fast_py<'py>(
+        py: Python<'py>,
+        filename: &str,
+        resize_shorter_side: Option<f64>,
+        compression_factor: Option<f64>,
+        threads: Option<usize>,
+        start_frame: Option<usize>,
+        end_frame: Option<usize>,
+    ) -> PyResult<Vec<Bound<'py, PyArray<u8, Dim<[usize; 3]>>>>> {
+        let decoder_config = DecoderConfig::new(
+            threads.unwrap_or(0),
+            resize_shorter_side,
+            compression_factor,
+            true,
+            start_frame,
+            end_frame,
+            Some(AvPixel::YUV420P),
+        );
+        let res_decode = decode_video_fast(filename, decoder_config);
+        match res_decode {
+            Ok(vid) => Ok(vid
+                .into_iter()
+                .map(|x| x.into_pyarray_bound(py))
+                .collect::<Vec<_>>()),
+            Err(e) => Err(PyRuntimeError::new_err(format!("Error: {}", e))),
+        }
+    }
+
+    // wrapper of `decode_video_gray`
+    /// Decode video and return a 3D ndarray representing gray frames with the shape (N, H, W)
+    /// * `filename` - Path to the video file
+    /// * `resize_shorter_side` - Resize the shorter side of the video to this value.
+    /// * `compression_factor` - Factor for temporal compression. If None, then no compression.
+    /// * `threads` - Number of threads to use for decoding. If None, let ffmpeg decide the optimal
+    /// * `start_frame` - Start decoding from this frame index
+    /// * `end_frame` - Stop decoding at this frame index
+    /// number.
+    /// * Returns a 3D ndarray with shape (N, H, W)
+    #[pyfn(m)]
+    #[pyo3(name = "decode_gray")]
+    #[pyo3(signature = (filename, resize_shorter_side=None, compression_factor=None, threads=None, start_frame=None, end_frame=None))]
+    fn decode_gray_py<'py>(
+        py: Python<'py>,
+        filename: &str,
+        resize_shorter_side: Option<f64>,
+        compression_factor: Option<f64>,
+        threads: Option<usize>,
+        start_frame: Option<usize>,
+        end_frame: Option<usize>,
+    ) -> PyResult<Bound<'py, PyArray<u8, Dim<[usize; 3]>>>> {
+        let decoder_config = DecoderConfig::new(
+            threads.unwrap_or(0),
+            resize_shorter_side,
+            compression_factor,
+            true,
+            start_frame,
+            end_frame,
+            None,
+        );
+        let res_decode = decode_video_gray(filename, decoder_config);
+        match res_decode {
+            Ok(vid) => Ok(vid.into_pyarray_bound(py)),
+            Err(e) => Err(PyRuntimeError::new_err(format!("Error: {}", e))),
+        }
+    }
+
+    /// Get a batch of frames from a video file, trying to use Seek when possible, and falling back
+    /// to iterating over all frames when bad metadata is detected (eg. several frames with pts=0,
+    /// frame with dts < 0, etc).
+    /// * `filename` - Path to the video file
+    /// * `indices` - Indices of frames to retrieve
+    /// * `threads` - Number of threads to use (only usefull when falling back to safe frame
+    /// iterating).
+    /// * `resize_shorter_side` - Resize the shorter side of the video to this value.
+    /// * `threads` - Number of threads to use for decoding. If None, let ffmpeg decide the optimal
+    /// * `with_fallback` - If True, will try to detect wrong metadata in video and if so fallback
+    /// to decoding without seeking. If False (or None), will always use seeking. Default is None.
+    /// number.
+    /// * Returns a 4D ndarray with shape (N, H, W, C)
+    #[pyfn(m)]
+    #[pyo3(name = "get_batch")]
+    #[pyo3(signature = (filename, indices, threads=None, resize_shorter_side=None, with_fallback=None))]
+    fn get_batch_py<'py>(
+        py: Python<'py>,
+        filename: &str,
+        indices: Vec<usize>,
+        threads: Option<usize>,
+        resize_shorter_side: Option<f64>,
+        with_fallback: Option<bool>,
+    ) -> PyResult<Bound<'py, PyArray<u8, Dim<[usize; 4]>>>> {
+        let vid = get_batch(
+            &filename.to_string(),
+            indices,
+            resize_shorter_side,
+            threads,
+            with_fallback.unwrap_or(false),
+        );
+        match vid {
+            Ok(vid) => Ok(vid.into_pyarray_bound(py)),
+            Err(e) => Err(PyRuntimeError::new_err(format!("Error: {}", e))),
+        }
+    }
+
+    // wrapper of `get_shape`
+    /// Get shape of the video: number of frames, height and width
+    /// * `filename` - Path to the video file
+    #[pyfn(m)]
+    #[pyo3(name = "get_shape")]
+    fn get_shape_py<'py>(
+        py: Python<'py>,
+        filename: &str,
+    ) -> PyResult<Bound<'py, PyArray<usize, Dim<[usize; 1]>>>> {
+        let res = get_shape(&filename.to_string());
+        match res {
+            Ok((num_frames, height, width)) => {
+                let shape = vec![num_frames, height, width];
+                Ok(shape.into_pyarray_bound(py))
+            }
+            Err(e) => Err(PyRuntimeError::new_err(format!("Error: {}", e))),
+        }
+    }
+
+    // wrapper of `get_info`
+    /// Get metadata info of the video: number of frames, height, width and fps
+    /// * `filename` - Path to the video file
+    /// * Returns a dictionary with the metadata
+    #[pyfn(m)]
+    #[pyo3(name = "get_info")]
+    fn get_info_py<'py>(py: Python<'py>, filename: &str) -> PyResult<Bound<'py, PyDict>> {
+        let binding = filename.to_string();
+        let res = get_info(&binding);
+        match res {
+            Ok(info_map) => Ok(info_map.into_py_dict_bound(py)),
+            Err(e) => Err(PyRuntimeError::new_err(format!("Error: {}", e))),
+        }
+    }
+
+    // wrapper of `save_video`
+    /// Save 4D np.ndarray of frames to video file
+    /// * `ndarray` - np.ndarray of shape (N, H, W, C)
+    /// * `output_filename` - Path to the output video file
+    /// * `fps` - Frames per second of the output video
+    /// * `codec` - Codec to use for the output video, eg "h264"
+    /// * Returns None
+    #[pyfn(m)]
+    #[pyo3(name = "save_video")]
+    fn save_video_py(
+        ndarray: PyReadonlyArray4<u8>,
+        output_filename: &str,
+        fps: usize,
+        codec: &str,
+    ) -> PyResult<()> {
+        let ndarray = ndarray.as_array().to_owned();
+        let res = save_video(ndarray, output_filename, fps, codec);
+        match res {
+            Ok(_) => Ok(()),
+            Err(e) => Err(PyRuntimeError::new_err(format!("Error: {}", e))),
+        }
+    }
+    Ok(())
+}
