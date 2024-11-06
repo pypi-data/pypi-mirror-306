ec534d8a7fec008fbe92b382361278acea5408c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:05:21 +0000

    Add package pineapple-wdb/0.1.0/pineapple_wdb-0.1.0.tar.gz

---
+++ packages/pineapple-wdb/pineapple_wdb-0.1.0.tar.gz/pineapple_wdb-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pineapple_wdb',
+    version='0.1.0',
+    author='Ciber Cleaner',
+    author_email='tuyishimireemmanuel24@gmail.com',
+    description='A SQLite database browser and management tool.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/millbox5/pineapple.git',  # Update with your repo URL
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+        'Flask-Cors',
+        'Peewee',
+        'Werkzeug',
+        'pygments',
+        # Add other dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
