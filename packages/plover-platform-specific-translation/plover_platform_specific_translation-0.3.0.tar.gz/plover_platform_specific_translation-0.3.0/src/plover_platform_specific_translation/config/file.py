769ea6645141aeed0dbb2a16820f0b21a7c4126b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 15:05:17 +0000

    Add package plover-platform-specific-translation/0.3.0/plover_platform_specific_translation-0.3.0.tar.gz

---
+++ packages/plover-platform-specific-translation/plover_platform_specific_translation-0.3.0.tar.gz/plover_platform_specific_translation-0.3.0/src/plover_platform_specific_translation/config/file.py
@@ -0,0 +1,34 @@
+"""
+Module to handle reading and writing to JSON
+"""
+
+import json
+from pathlib import Path
+from typing import Any
+
+
+def load(filepath: Path) -> dict[str, Any]:
+    """
+    Reads in data from a JSON file
+
+    Raises an error if the specified config file is not JSON format.
+    """
+    data: dict[str, Any]
+    try:
+        with filepath.open(encoding="utf-8") as file:
+            data = json.load(file)
+            file.close()
+    except FileNotFoundError:
+        data = {}
+    except json.JSONDecodeError as exc:
+        raise ValueError("Unable to decode file contents as JSON") from exc
+
+    return data
+
+def save(filepath: Path, data: dict[str, Any]) -> None:
+    """
+    Saves a dictionary to a JSON file.
+    """
+    with filepath.open("w", encoding="utf-8") as file:
+        json.dump(data, file, indent=2)
+        file.close()
