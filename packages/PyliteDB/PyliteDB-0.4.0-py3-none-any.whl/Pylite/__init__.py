cc478b4030ade2214aa7bfedeeb45625322e9f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 16:05:36 +0000

    Add package PyliteDB/0.4.0/PyliteDB-0.4.0-py3-none-any.whl

---
+++ packages/PyliteDB/PyliteDB-0.4.0-py3-none-any.whl/Pylite/__init__.py
@@ -0,0 +1,20 @@
+# __init__.py
+from .TypeHelpers import *
+from .Table import Table
+from .Database import Database
+
+# from .DepColumn import Column
+# from .Types import PyliteTypes
+
+# Optional: Metadata
+__version__ = "0.3.0"
+__author__ = "Ilify"
+__email__ = "iliesmraihia@gmail.com"
+
+# Provide a simple package-level description
+"""
+PyLite: A lightweight database library using Pandas DataFrames.
+"""
+
+# Expose the Database class when the package is imported
+__all__ = ["Database", "Table","email","password","color","phone","username","url","creditCard","date","currency","postalCode","uuid","latitude","longitude","location","ipv4","mac","path","domain"]
