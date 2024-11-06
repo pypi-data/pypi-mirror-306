0499ecaddf9e58bc9cec3164dcf57782e9274633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 06:04:56 +0000

    Add package scipe/1.2.0/scipe-1.2.0.tar.gz

---
+++ packages/scipe/scipe-1.2.0.tar.gz/scipe-1.2.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup # type: ignore
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="scipe",
+    version="1.2.0",
+    package_dir={"scipe": "src"},
+    packages=["scipe"],
+    author="Ankush Garg",
+    author_email="ankush-garg@berkeley.edu",
+    description="Systematic Chain Improvement and Problem Evaluation",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="",
+    python_requires=">=3.9",
+    install_requires=[
+        "langchain-core==0.3.12",
+        "langgraph==0.1.19",
+        "ipykernel==5.5.6",
+        "pandas==2.2.2",
+        "python-dotenv==1.0.1",
+        "openpyxl==3.1.5",
+        "litellm==1.46.5"
+    ],
+    extras_requires={
+        "dev": ["twine==5.1.1"]
+    },
+    include_package_data=True
+)
