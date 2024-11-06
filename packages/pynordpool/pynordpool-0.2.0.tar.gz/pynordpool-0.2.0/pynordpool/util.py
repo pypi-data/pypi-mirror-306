051dbac213f1f84669f44900504d23afa1033610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 16:05:35 +0000

    Add package pynordpool/0.2.0/pynordpool-0.2.0.tar.gz

---
+++ packages/pynordpool/pynordpool-0.2.0.tar.gz/pynordpool-0.2.0/pynordpool/util.py
@@ -0,0 +1,15 @@
+"""Utils for Nordpool."""
+
+from __future__ import annotations
+
+import datetime as dt
+
+CET = "Europe/Stockholm"
+
+
+async def parse_datetime(dt_str: str) -> dt.datetime:
+    """Parse a string and return a datetime.datetime.
+
+    input: 2024-11-04T23:00:00Z or 2024-11-04T12:15:03.8832404Z
+    """
+    return dt.datetime.fromisoformat(dt_str)
