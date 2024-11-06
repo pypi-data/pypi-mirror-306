6941a576499c1f0bfc97ed193f0c6f2a52302846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 05:05:33 +0000

    Add package neoapi-sdk/0.1.3/neoapi-sdk-0.1.3.tar.gz

---
+++ packages/neoapi-sdk/neoapi-sdk-0.1.3.tar.gz/neoapi-sdk-0.1.3/neoapi/models.py
@@ -0,0 +1,23 @@
+from typing import Any, Dict, Optional
+from pydantic import BaseModel, Field
+
+class LLMOutput(BaseModel):
+    model: str = "unknown"
+    prompt_tokens: int = 0
+    completion_tokens: int = 0
+    total_tokens: int = 0
+    cost: float = 0.0
+    response: Any = None
+    text: str
+    timestamp: float
+    project: str = "default_project"
+    group: str = "default_group"
+    analysis_slug: Optional[str] = None
+    metadata: Optional[Dict[str, Any]] = None
+    need_analysis_response: bool = False
+    format_json_output: bool = False
+    save_text: bool = True
+
+    model_config = {
+        "extra": "allow"
+    }
