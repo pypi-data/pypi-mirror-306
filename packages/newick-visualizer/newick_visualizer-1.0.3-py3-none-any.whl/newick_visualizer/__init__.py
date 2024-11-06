ebf442c470e7a191a9d301b68089282ab8fd6ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 16:05:27 +0000

    Add package newick-visualizer/1.0.3/newick_visualizer-1.0.3-py3-none-any.whl

---
+++ packages/newick-visualizer/newick_visualizer-1.0.3-py3-none-any.whl/newick_visualizer/__init__.py
@@ -0,0 +1,7 @@
+"""Newick Tree Visualizer Package"""
+
+from ._version import __version__
+from .core.tree_generator import create_tree_html
+from .core.template_manager import TemplateManager
+
+__all__ = ['create_tree_html', 'TemplateManager', '__version__']
