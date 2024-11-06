2b45919ef272b139fa26a3e430ae4529eada9fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 06:05:06 +0000

    Add package multiscale_spatial_image/2.0.0/multiscale_spatial_image-2.0.0.tar.gz

---
+++ packages/multiscale_spatial_image/multiscale_spatial_image-2.0.0.tar.gz/multiscale_spatial_image-2.0.0/multiscale_spatial_image/__init__.py
@@ -0,0 +1,15 @@
+"""multiscale-spatial-image
+
+Generate a multiscale spatial image."""
+
+__all__ = [
+    "MultiscaleSpatialImage",
+    "Methods",
+    "to_multiscale",
+    "itk_image_to_multiscale",
+    "__version__",
+]
+
+from .__about__ import __version__
+from .multiscale_spatial_image import MultiscaleSpatialImage
+from .to_multiscale import Methods, to_multiscale, itk_image_to_multiscale
