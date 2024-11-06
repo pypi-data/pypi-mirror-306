8b643f5e60d0804fbfcf22a9640eb38ef6000a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:05:21 +0000

    Add package mkdocs-gherkin-plugin/1.0/mkdocs_gherkin_plugin-1.0.tar.gz

---
+++ packages/mkdocs-gherkin-plugin/mkdocs_gherkin_plugin-1.0.tar.gz/mkdocs_gherkin_plugin-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkdocs-gherkin-plugin',
+    version="1.0",
+    description="",
+    author="Aleksandar KIRILOV",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.plugins': [
+            'gherkin-plugin=mkdocs_gherkin_plugin:GherkinPlugin'
+        ]
+    }
+)
