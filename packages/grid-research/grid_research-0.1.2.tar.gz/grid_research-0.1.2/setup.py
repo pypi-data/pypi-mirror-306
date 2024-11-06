b96212d27ecd11a2a2a473450a871de25fa4faab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 07:04:59 +0000

    Add package grid-research/0.1.2/grid_research-0.1.2.tar.gz

---
+++ packages/grid-research/grid_research-0.1.2.tar.gz/grid_research-0.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grid-research",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "openai==1.52.2",
+        "html2text",
+        "python-dotenv",
+        "aiohttp",
+        "beautifulsoup4",
+        "lxml",
+        "fake-useragent",
+        "pandas",
+        "pydantic>=2.0.0",
+    ],
+    author="Your Name",
+    author_email="minh@everythingcompany.co",
+    description="A package for automated research data collection using LLMs and Web Search API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bobcoi03/grid",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8",
+)
