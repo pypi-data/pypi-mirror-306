e7a89a890e7e4716a97b286c240f1db35388d138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:51 +0000

    Add package vaibhav7021-my-math-package/0.9/vaibhav7021_my_math_package-0.9.tar.gz

---
+++ packages/vaibhav7021-my-math-package/vaibhav7021_my_math_package-0.9.tar.gz/vaibhav7021_my_math_package-0.9/README.md
@@ -0,0 +1,10 @@
+# my_math_package
+
+A simple math package with basic operations: add, subtract, multiply, and divide.
+
+## Installation
+
+You can install this package by running:
+
+```bash
+pip install my_math_package
---
+++ packages/vaibhav7021-my-math-package/vaibhav7021_my_math_package-0.9.tar.gz/vaibhav7021_my_math_package-0.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vaibhav7021_my_math_package",
+    version="0.9",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    description="A simple math operations package",
+    python_requires='>=3.6',
+)
