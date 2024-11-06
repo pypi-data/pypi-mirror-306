1c762fcc5bfd0e1e190a5aae7861d3661331b3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 05:05:10 +0000

    Add package datarobot-mlops/10.0.12/datarobot_mlops-10.0.12-py3-none-any.whl

---
+++ packages/datarobot-mlops/datarobot_mlops-10.0.12-py3-none-any.whl/datarobot/mlops/__init__.py
@@ -0,0 +1,14 @@
+#  ---------------------------------------------------------------------------------
+#  Copyright (c) 2019 DataRobot, Inc. and its affiliates. All rights reserved.
+#  Last updated 2024.
+#
+#  DataRobot, Inc. Confidential.
+#  This is proprietary source code of DataRobot, Inc. and its affiliates.
+#
+#  This file and its contents are subject to DataRobot Tool and Utility Agreement.
+#  For details, see
+#  https://www.datarobot.com/wp-content/uploads/2021/07/DataRobot-Tool-and-Utility-Agreement.pdf.
+#  ---------------------------------------------------------------------------------
+raise ImportError(
+    "This module has moved; please update your imports from `datarobot.mlops.*` to `datarobot_mlops.*`."
+)
