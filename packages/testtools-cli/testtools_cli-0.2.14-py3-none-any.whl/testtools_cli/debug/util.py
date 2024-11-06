53d0bf746c12c28fb3528f7fd3995fb1bce7b242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:36 +0000

    Add package testtools-cli/0.2.14/testtools_cli-0.2.14-py3-none-any.whl

---
+++ packages/testtools-cli/testtools_cli-0.2.14-py3-none-any.whl/testtools_cli/debug/util.py
@@ -0,0 +1,26 @@
+import logging
+import subprocess
+import os
+
+log = logging.getLogger("rich")
+
+
+def execute_command_in_directory(
+    command: list[str], directory: str, parameter: list[str]
+) -> int:
+    """
+    Execute a command in a specified directory with a given parameter.
+
+    :param command: The command to execute (as a list of arguments).
+    :param directory: The directory in which to execute the command.
+    :param parameter: The parameter to pass to the command.
+    """
+    if not os.path.isdir(directory):
+        raise NotADirectoryError(f"The specified directory does not exist: {directory}")
+    command.extend(parameter)
+    log.info(f"⏳ Executing command: [{' '.join(command)}]...")
+    result = subprocess.run(
+        command,
+        cwd=directory,
+    )
+    return result.returncode
