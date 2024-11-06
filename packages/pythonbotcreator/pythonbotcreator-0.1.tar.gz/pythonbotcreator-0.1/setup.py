09b0af3ac7362aa598b9c341539a4ee17ac71cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 16:04:55 +0000

    Add package pythonbotcreator/0.1/pythonbotcreator-0.1.tar.gz

---
+++ packages/pythonbotcreator/pythonbotcreator-0.1.tar.gz/pythonbotcreator-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pythonbotcreator',
+      version='0.1',
+      description='Telegram Bots and Automation of tasks.',
+      packages=['pythonbotcreator'],
+      author_email='contact@pythonbotcreator.in',
+      zip_safe=False)
