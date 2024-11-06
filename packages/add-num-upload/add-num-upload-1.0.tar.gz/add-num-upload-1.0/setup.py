f21953381c87358a6329ae46902bd41a7a034c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 15:05:05 +0000

    Add package add-num-upload/1.0/add-num-upload-1.0.tar.gz

---
+++ packages/add-num-upload/add-num-upload-1.0.tar.gz/add-num-upload-1.0/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import  setup
+
+packages = ['add_num_upload']
+
+setup(name='add-num-upload',version='1.0',author='zjf', packages=packages, package_dir={'requests': 'requests'},)
