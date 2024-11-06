ea22272000b7837d257819bff00d7dc7597a1d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 03:08:39 +0000

    Add package corerec/0.2.5/corerec-0.2.5.tar.gz

---
+++ packages/corerec/corerec-0.2.5.tar.gz/corerec-0.2.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='corerec',
+    version='0.2.5',
+    description='A Framework to built custom recommendation envornments.',
+    author='Vishesh Yadav',
+    author_email='sciencely98@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'networkx',
+        'scikit_learn',
+        # 'torch',
+        'tqdm',
+        'memory_profiler',
+        'pandas',
+        'torch_geometric',
+        'dask',
+        'hypothesis',
+        'mkl',
+        'cmake',
+        'ninja',
+        'pyyaml',
+        'adamax',
+        'adadelta',
+        'adagrad',
+        'asgd',
+        'lbfgs',
+        'rmsprop',
+        'sgd',
+        'sparse_adam',
+
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
