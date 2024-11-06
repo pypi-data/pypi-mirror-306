a82d761e946328d2f93867b139471598d2a43463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:51 +0000

    Add package vaibhav7021-my-math-package/0.9/vaibhav7021_my_math_package-0.9-py3-none-any.whl

---
+++ packages/vaibhav7021-my-math-package/vaibhav7021_my_math_package-0.9-py3-none-any.whl/vaibhav7021_my_math_package/__init__.py
@@ -0,0 +1,5 @@
+# Import functions directly from main.py
+from .main import add, subtract, multiply, divide
+
+# Optional: Make these available at the package level
+__all__ = ['add', 'subtract', 'multiply', 'divide']
