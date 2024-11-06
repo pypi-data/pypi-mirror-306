a702f8a57d7b5f099ec2d0cbcad22121830aba0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 15:04:50 +0000

    Add package ipfs-accelerate-py/0.0.11/ipfs_accelerate_py-0.0.11.tar.gz

---
+++ packages/ipfs-accelerate-py/ipfs_accelerate_py-0.0.11.tar.gz/ipfs_accelerate_py-0.0.11/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+	name='ipfs_accelerate_py',
+	version='0.0.11',
+	packages=[
+        'ipfs_accelerate_py',
+	],
+	install_requires=[
+        'transformers',
+        'ipfs_transformers_py',
+        'ipfs_model_manager_py',
+		'torch',
+        'torchvision',
+        'numpy',
+        'torchtext',
+		'urllib3',
+		'requests',
+		'boto3',
+		'trio',
+	]
+)
