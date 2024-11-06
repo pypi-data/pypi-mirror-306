5dc4d32b24c41093b7e73ed8c393628ace5e55be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 15:04:50 +0000

    Add package ipfs-accelerate-py/0.0.11/ipfs_accelerate_py-0.0.11-py3-none-any.whl

---
+++ packages/ipfs-accelerate-py/ipfs_accelerate_py-0.0.11-py3-none-any.whl/ipfs_accelerate_py/__init__.py
@@ -0,0 +1,23 @@
+import backends
+from .backends import backends
+import config
+from .config import config
+import endpoints
+from .endpoints import endpoints_py
+import install_depends
+from .install_depends import install_depends
+import test_ipfs_accelerate
+from .test_ipfs_accelerate import test_ipfs_accelerate
+import ipfs_accelerate
+from .ipfs_accelerate import ipfs_accelerate_py
+import test_backend
+from .test_backend import test_backend_py
+export = {
+    "backends": backends,
+    "config": config,
+    "test_ipfs_accelerate": test_ipfs_accelerate,
+    "test_backend": test_backend_py,
+    "install_depends": install_depends,
+    "endpoints": endpoints,
+    "ipfs_accelerate_py": ipfs_accelerate_py
+}
