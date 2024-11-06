c439c62b75c399ac4f91e0023e89667137af8479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 06:04:59 +0000

    Add package tail-test/0.1.0/tail_test-0.1.0.tar.gz

---
+++ packages/tail-test/tail_test-0.1.0.tar.gz/tail_test-0.1.0/README.md
@@ -0,0 +1,55 @@
+
+
+<p align="center">
+<img src="img/logo.png" alt="img" width="40%">
+<h4 align="center">
+Automatic, Easy and Realistic tool for LLM Evaluation
+</h4>
+</p>
+
+<p align="center">
+| <a href="https://yale-nlp.github.io/TAIL/"><b>Documentation</b></a> | <a href="https://arxiv.org/abs/2309.06180"><b>Paper</b></a> | 
+</p>
+
+<p align="center">
+<img src="img/outline.png" alt="img" width="65%">
+</p>
+
+## Introduction
+TAIL is an automatic toolkit for creating realistic evaluation
+benchmarks and assessing the performance of long-context LLMs. 
+With TAIL, users
+can customize the building of a long-context,
+document-grounded QA benchmark and obtain
+visualized performance metrics of evaluated
+models.
+## Quickstart 
+1. install the package from PyPi:
+    ```
+    # (Recommended) Create a new conda environment.
+    conda create -n tail python=3.10 -y
+    conda activate tail
+
+    # Install tailtest
+    pip install tailtest
+    ```
+    set yout OPENAI_API_KEY:
+    ```
+    export OPENAI_API_KEY="..."
+    ```
+2. Prepare a source document you want to use to generate benchmark and organize in the format of json.
+    `[{"text": "Content of your document"}]`
+
+3. Benchmark Generation:
+
+    ```
+    tail-cli.build --raw_document_path "/Users/frank/Desktop/code/TAIL/data/law_10.json" --QA_save_path "/Users/frank/Desktop/code/TAIL/data/QA_law.json" --document_length 8000 16000 32000 48000 64000 80000 96000 112000 128000 --depth_list 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95
+    ```
+
+4. Model Evaluation & Testing:
+
+    ```
+    tail-cli.eval --QA_save_path "/data/QA.json" --test_model_name "gpt-4o" --test_depth_list 25 75 --test_doc_length 8000 32000 --test_result_save_dir "/data/result/"
+    ```
+
+## Citation
---
+++ packages/tail-test/tail_test-0.1.0.tar.gz/tail_test-0.1.0/tail_test/test_llm_performance.py
@@ -0,0 +1,101 @@
+import json 
+from openai import OpenAI  
+import os
+import termcolor
+import time
+import argparse
+import matplotlib.pyplot as plt
+from vllm import LLM, SamplingParams
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from tail_test.benchmark_generation import gen_benchmark
+from tail_test.visualize import visualize
+
+
+def vllm_complete(truth,client,model,tokenizer,messages,question,options_str):
+    sampling_params = SamplingParams(temperature=0.0, max_tokens=512)
+
+    inputs = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    outputs = model.generate(prompts=[inputs], sampling_params=sampling_params)
+    answers = outputs[0].output.outputs[0].text
+
+    completion = client.chat.completions.create(  
+        model="gpt-4o",  
+        messages=[  
+            {"role": "user", "content": f"Please response only in a single character in A,B,C,D,E,F: Here is a question: {question} options: {options_str}. Hers's is the candidate's response:{answers}.What is the candidate's choice? "},  
+        ]  
+    )  
+        
+    remarks = completion.choices[0].message.content.strip()
+    return remarks[0]==truth[0],answers,remarks
+
+def api_answer(client,ground_truth,messages,args,question,options_str):
+    completions = client.chat.completions.create(
+                model=args.test_model_name,
+                messages=messages,
+                max_tokens=256, temperature=0.0)
+    answers = completions.choices[0].message.content.strip()
+    completion = client.chat.completions.create(  
+        model="gpt-4o",  
+        messages=[  
+            {"role": "user", "content": f"Please response only in a single character in A,B,C,D,E,F: Here is a question: {question} options: {options_str}. Hers's is the candidate's response:{answers}.What is the candidate's choice? "},  
+        ]  
+    )  
+    remark = completion.choices[0].message.content
+    return remark[0] == ground_truth[0],answers,remark
+
+def test_llm_performance(args,client):
+    model_name = args.test_model_name.split('/')[-1] if '/' in args.test_model_name else args.test_model_name
+    with open(args.QA_save_path, 'r', encoding='utf-8') as file:  
+        data = json.load(file) 
+    
+    result = []
+    
+    is_api_based = True
+    if args.test_model_name not in ["gpt-4o","gemini-1.5-flash","gemini-1.5-pro","glm-4-flash"]:
+        is_api_based = False
+
+        tokenizer = AutoTokenizer.from_pretrained(args.test_model_name, trust_remote_code=True)
+    
+        model = LLM(
+            model = args.test_model_name, 
+            dtype="auto",   
+            trust_remote_code=True,  
+            seed=2024,
+            tensor_parallel_size=1,
+            enforce_eager=False)
+        
+    matching_entries = []
+    for depth in args.test_depth_list:
+        for length in args.test_doc_length:
+            matching_items = [item for item in data if item['depth'] == depth and item['document_length'] == length]
+            if not matching_items:
+                raise ValueError(f"No QA found for depth {depth} and token_length {length}. Please generate QA first.")
+            matching_entries.extend(matching_items)
+
+    num = 0
+    start_time = time.time()
+    print(f"[TAIL] Start testing {model_name}. Total test sample numbers: {len(matching_entries)}")
+    for item in matching_entries:   
+        num += 1
+        token_lengths = item["document_length"]
+        depth = item['depth']  
+        question = item["QA"]['question']  
+        options = item["QA"]['options']  
+        document = "".join(item["whole_document"])
+        doc_index = item["doc_index"]
+        options_str = ' '.join([f"{key}: {value}" for key, value in options.items()])  
+        ground_truth = item["QA"]["answer"]
+        messages = [{"role": "user", "content": "I will give you a multiple choice question and a corresponding document. Please think step by step to answer and provide your chain of thoughts." + f"Questions: {question} Options: {options_str}"+"Please answer the above question refer to this document only: " + document}] 
+
+        if is_api_based:
+            acc,answer,extract_answer = api_answer(client,ground_truth,messages,args,question,options_str)
+        else:
+            acc,answer,extract_answer = vllm_complete(ground_truth,model,tokenizer,messages,question,options_str)
+        result.append({"doc_index":doc_index,"depth":depth,"token_lengths":token_lengths,'result': acc, "question":question, "options":options,'LLM_original_answer': answer,"LLM_extracted_answer":extract_answer ,"ground_truth":ground_truth})  
+        point_time = time.time()
+        print(f"{num}/{len(matching_entries)},lengths:{token_lengths},depth:{depth},Answer: {acc*100}% "+f"Time: {round((point_time - start_time)/60, 1)} min Used")
+    
+    with open(os.path.join(args.test_result_save_dir, f"result_{model_name}.json"), 'w') as json_file:  
+        json.dump(result, json_file)  
+
+    print(f"[TAIL] Sucessfully finished testing {model_name} on {args.QA_save_path}.")
