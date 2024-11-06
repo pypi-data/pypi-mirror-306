220d13bab4db9ec2a6d91b296f0ebc291584728c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 03:09:28 +0000

    Add package modern-di-fastapi/0.4.1/modern_di_fastapi-0.4.1.tar.gz

---
+++ packages/modern-di-fastapi/modern_di_fastapi-0.4.1.tar.gz/modern_di_fastapi-0.4.1/modern_di_fastapi/__init__.py
@@ -0,0 +1,9 @@
+from modern_di_fastapi.main import Provide, build_di_container, fetch_di_container, setup_di
+
+
+__all__ = [
+    "Provide",
+    "build_di_container",
+    "fetch_di_container",
+    "setup_di",
+]
