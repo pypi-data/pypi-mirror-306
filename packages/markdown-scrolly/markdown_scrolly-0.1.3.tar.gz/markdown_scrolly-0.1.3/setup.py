e15de876f1b3dfe3bd6ad71b8639cd9c90bad2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 05:05:46 +0000

    Add package markdown-scrolly/0.1.3/markdown_scrolly-0.1.3.tar.gz

---
+++ packages/markdown-scrolly/markdown_scrolly-0.1.3.tar.gz/markdown_scrolly-0.1.3/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fhand:
+    long_description = fhand.read()
+
+setuptools.setup(
+    name="markdown-scrolly",
+    version="0.1.3",
+    author="Willy Pregliasco",
+    author_email="willy.pregliasco@gmail.com",
+    description=("MD renderer for control aestetics using CSS "
+                " and to include simple scroll animations."),
+    license_files = ('LICENSE',),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    project_urls={
+        "Gitlab Page": "https://gitlab.com/wpregliasco/markdown-scrolly",
+        "Bug Tracker": "https://gitlab.com/wpregliasco/markdown-scrolly/-/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["markdown"],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={
+        "scrolly": ["resources/*"],  # Include all files in 'resources' folder
+    },
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "scrolly = scrolly.cli:main",
+            "html_package = scrolly.cli_pkg:main"
+        ]
+    }
+)
