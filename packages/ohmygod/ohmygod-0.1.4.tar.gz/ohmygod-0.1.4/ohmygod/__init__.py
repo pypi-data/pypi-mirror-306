6ace588eb05b691f3264f7958e7681be4714a75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 16:05:57 +0000

    Add package ohmygod/0.1.4/ohmygod-0.1.4.tar.gz

---
+++ packages/ohmygod/ohmygod-0.1.4.tar.gz/ohmygod-0.1.4/ohmygod/__init__.py
@@ -0,0 +1,10 @@
+"""OH MY GOD package built upon rich console interface"""
+
+__version__ = "0.1.4"
+
+
+from .main import OhMyGod
+from .message import Message as HolyMessage
+from .utils import Color as HolyColor
+
+__all__ = ["OhMyGod", "HolyMessage", "HolyColor"]
