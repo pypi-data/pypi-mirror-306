78479807b3680ef1777815e74c5b5958af93bd00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 14:05:25 +0000

    Add package psik/1.2.2/psik-1.2.2.tar.gz

---
+++ packages/psik/psik-1.2.2.tar.gz/psik-1.2.2/psik/models.py
@@ -0,0 +1,69 @@
+from typing import Optional, Dict, Any
+from enum import Enum
+from pathlib import Path
+
+from pydantic import BaseModel, Field, SecretStr
+
+class JobState(str, Enum):
+    new = "new"
+    queued = "queued"
+    active = "active"
+    completed = "completed"
+    failed = "failed"
+    canceled = "canceled"
+
+    def is_final(self) -> bool:
+        return self.value in ["completed", "failed", "canceled"]
+
+class ResourceSpec(BaseModel):
+    duration              : int = Field(default=10, title="Max walltime of the job in minutes")  
+    node_count            : Optional[int] = Field(default=None, title="Number of compute nodes allocated to job.")
+    process_count         : Optional[int] = Field(default=None, title="Instruct the backend to start this many process instances. Mutually exclusive with node+processes_per_node. Default if neither is 1 process.")
+    processes_per_node    : Optional[int] = None
+    cpu_cores_per_process : int = Field(default=1, title="CPU cores for each process instance.") 
+    gpu_cores_per_process : int = 0
+    exclusive_node_use    : bool = True
+
+# Was JobAttributes.  However, these are actually
+# backend configuration options.
+#
+# Hence `custom_attributes` is renamed to
+# `attributes`, since it applies to this specific backend.
+#
+# Nevertheless, these can be specified/overriden as part of a JobSpec,
+# so they should not include API-unsafe values (e.g. psik path,
+# server hostname, etc.)
+class BackendConfig(BaseModel):
+    type              : str = "local"
+    queue_name        : Optional[str] = None
+    project_name      : Optional[str] = None
+    reservation_id    : Optional[str] = None
+    attributes        : Dict[str,str] = {} # backend config. options
+
+class JobSpec(BaseModel):
+    name        : Optional[str] = Field(default=None, title="Job name")
+    directory   : Optional[str] = Field(default=None, title="Job run directory")
+    script      : str = Field(..., title="Shell / shebang script to execute")
+
+    environment : Dict[str,str] = Field(default={},title="custom environment variables")
+    inherit_environment : bool  = Field(default=True, title="If this flag is set to False, the job starts with an empty environment.")
+
+    resources   : ResourceSpec  = Field(default=ResourceSpec(), title="Job resource requirements")
+    backend     : BackendConfig = Field(default=BackendConfig(), title="Backend configuration values.")
+    # deps       : List[str]     = Field(default=[], title="Dependencies required before starting this job.")
+    callback    : Optional[str] = Field(default=None, title="URL to send event notifications.")
+    cb_secret   : Optional[SecretStr] = Field(default=None, title="hmac256 secret to sign updates sent to 'callback'")
+    client_secret : Optional[SecretStr] = Field(default=None, title="hmac256 secret to validate callback updates from clients")
+
+#j = JobSpec(script="mpirun hostname")
+#print(j.json())
+
+# Data models specific to status routes:
+class Callback(BaseModel):
+    jobndx  : int = Field(..., title="Sequential job index.")
+    state   : JobState = Field(..., title="State reached by job.")
+    info    : int = Field(default=0, title="Status code.")
+
+def load_jobspec(fname):
+    return JobSpec.model_validate_json(
+                Path(fname).read_text(encoding='utf-8'))
---
+++ packages/psik/psik-1.2.2.tar.gz/psik-1.2.2/psik/psik.py
@@ -0,0 +1,169 @@
+import asyncio
+from typing import Optional, List
+from pathlib import Path
+from typing_extensions import Annotated
+import sys
+import os
+import shutil
+import logging
+_logger = logging.getLogger(__name__)
+
+import typer
+
+from .config import load_config
+from .job import Job
+from .manager import JobManager
+from .models import (
+        JobState,
+        JobSpec,
+        load_jobspec
+)
+
+def run_async(f):
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(f)
+
+def setup_logging(v, vv):
+    if vv:
+        logging.basicConfig(level=logging.DEBUG)
+    elif v:
+        logging.basicConfig(level=logging.INFO)
+
+app = typer.Typer()
+
+V1 = Annotated[bool, typer.Option("-v", help="show info-level logs")]
+V2 = Annotated[bool, typer.Option("-vv", help="show debug-level logs")]
+CfgArg = Annotated[Optional[Path], typer.Option("--config",
+                   envvar="PSIK_CONFIG",
+                   help="Config file path [default ~/.config/psik.json].")]
+
+@app.command()
+def status(stamp : str = typer.Argument(..., help="Job's timestamp / handle."),
+           v : V1 = False, vv : V2 = False, cfg : CfgArg = None):
+    """
+    Read job status information and history.
+    """
+    setup_logging(v, vv)
+    config = load_config(cfg)
+    base = config.prefix
+
+    async def stat():
+        job = await Job(base / stamp)
+        #print(job.spec.dump_model_json(indent=4))
+        print(job.spec.name)
+        print("    base: %s"%str(base / stamp))
+        print("    work: %s"%str(job.spec.directory))
+        print()
+        print("    time ndx state info")
+        for line in job.history:
+            print("    %.3f %3d %10s %8d" % line)
+    run_async(stat())
+
+@app.command()
+def rm(stamps : List[str] = typer.Argument(...,
+                                           help="Job's timestamp / handle."),
+       cfg : CfgArg = None):
+    """
+    Remove job tracking directories for the given jobstamps.
+    """
+    config = load_config(cfg)
+    base = config.prefix
+    # TODO: Check with the backend and prevent deletion if
+    # job is still there.
+
+    err = 0
+    for stamp in stamps:
+        jobdir = base / str(stamp)
+        if not jobdir.is_dir():
+            _logger.error("%s not found", jobdir)
+            err += 1
+            continue
+        if not os.access(jobdir, os.W_OK):
+            _logger.error("%s no write permissions", jobdir)
+            err += 1
+            continue
+        shutil.rmtree(jobdir)
+
+    raise typer.Exit(code=err)
+
+@app.command()
+def start(stamp : str = typer.Argument(..., help="Job's timestamp / handle."),
+           v : V1 = False, vv : V2 = False, cfg : CfgArg = None):
+    """
+    (re)start a job.
+    """
+    setup_logging(v, vv)
+    config = load_config(cfg)
+    base = config.prefix
+
+    job = Job(base / str(stamp))
+    run_async( job.submit() )
+
+@app.command()
+def cancel(stamp : str = typer.Argument(..., help="Job's timestamp / handle."),
+           v : V1 = False, vv : V2 = False, cfg : CfgArg = None):
+    """
+    Cancel a job.
+    """
+    setup_logging(v, vv)
+    config = load_config(cfg)
+    base = config.prefix
+
+    job = Job(base / str(stamp))
+    run_async( job.cancel() )
+
+@app.command()
+def reached(base : str = typer.Argument(..., help="Job's base directory."),
+            jobndx : int = typer.Argument(..., help="Sequential job index."),
+            state : JobState = typer.Argument(..., help="State reached by job."),
+            info  : int = typer.Argument(default=0, help="Status code.")):
+    """
+    Record that a job has entered the given state.
+    This script is typically not called by a user, but
+    instead called during a job's (pre-filled) callbacks.
+    """
+    job = Job(base)
+    ok = run_async( job.reached(jobndx, state, info) )
+    if not ok:
+        raise typer.Exit(code=1)
+
+@app.command()
+def ls(v : V1 = False, vv : V2 = False, cfg : CfgArg = None):
+    """
+    List jobs.
+    """
+    setup_logging(v, vv)
+    config = load_config(cfg)
+    mgr = JobManager(config)
+    async def show():
+        async for job in mgr.ls():
+            t, ndx, state, info = job.history[-1]
+            #print(f"{job.base} {job.spec.name} {t} {ndx} {state} {info}")
+            print(f"{job.base} '{job.spec.name}' {state}/{ndx}")
+    run_async(show())
+
+@app.command()
+def run(jobspec : str = typer.Argument(..., help="jobspec.json file to run"),
+        submit  : Annotated[bool, typer.Option(help="Submit job to queue")] = True,
+        v : V1 = False, vv : V2 = False, cfg : CfgArg = None):
+    """
+    Create a job directory from a jobspec.json file.
+
+    If submit is True (default), the job will also be submitted
+    to the backend's job queue.  Use --no-submit to prevent this.
+    """
+    setup_logging(v, vv)
+    config = load_config(cfg)
+    mgr = JobManager(config)
+    try:
+        spec = load_jobspec(jobspec)
+    except Exception as e:
+        _logger.exception("Error parsing JobSpec from file %s", jobspec)
+        raise typer.Exit(code=1)
+
+    async def create_submit(spec, submit):
+        job = await mgr.create(spec)
+        if submit:
+            await job.submit()
+        
+    run_async( create_submit(spec, submit) )
