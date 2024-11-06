9623d9765e645c7185c917787d2029438f435246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 15:05:15 +0000

    Add package test-deal/1.0.0/test_deal-1.0.0-py3-none-any.whl

---
+++ packages/test-deal/test_deal-1.0.0-py3-none-any.whl/test/dlt.py
@@ -0,0 +1,16 @@
+
+RPC_NACOS_NAME ={
+"layout_detection":"text-mmu-rpc-layoutlmv3_detection",
+"formula_detection":"text-mmu-rpc-yolov8_recognition",
+"formula_recogintion":"text-mmu-rpc-unimernet_inference",
+"ocr_recognition":"text-mmu-rpc-mineru_recognition",
+"table_recognition":"text-mmu-rpc-pptable_recognition"
+}
+
+RPC_CONFIG_IPPORT_ENV ={
+"layout_detection":"RPC_LAYOUT_DETECTION_IPPORT",
+"formula_detection":"RPC_FORMULA_DETECTION_IPPORT",
+"formula_recogintion":"RPC_FORMULA_RECOGNITION_IPPORT",
+"ocr_recognition":"RPC_OCR_RECOGNITION_IPPORT",
+"table_recognition":"RPC_TABLE_RECOGNITION_IPPORT",
+}
