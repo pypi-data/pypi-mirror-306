02580db2d0f15abd9a65360abff931523c8fdb2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 15:05:07 +0000

    Add package hoomy/1.6.25/hoomy-1.6.25.tar.gz

---
+++ packages/hoomy/hoomy-1.6.25.tar.gz/hoomy-1.6.25/hoomy/help.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: qicongsheng
+def get_version():
+    return '1.6.25'
+
+
+def print_version():
+    print('''treasure %s
+Development tools for python.''' % get_version())
