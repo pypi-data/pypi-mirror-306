945e9188b326eec67e162a20e35dea9c7924a197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 14:05:37 +0000

    Add package quantics/0.1.0/quantics-0.1.0.tar.gz

---
+++ packages/quantics/quantics-0.1.0.tar.gz/quantics-0.1.0/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="quantics",
+    version="0.1.0",
+    author="Daniel Igbokwe",
+    author_email="igbodani14@gmail.com",
+    long_description=long_description,
+    packages=find_packages(),
+    python_requires=">=3.6, <3.13",
+    install_requires=[
+        "umap-learn",
+        "numpy",
+        "pandas",
+        "scikit-learn"
+    ],
+
+)
