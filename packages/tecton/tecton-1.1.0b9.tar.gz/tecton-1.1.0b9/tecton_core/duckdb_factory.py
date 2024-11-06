74dc3cde93366f0d1065e7a47f1659be94d69639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 14:05:09 +0000

    Add package tecton/1.1.0b9/tecton-1.1.0b9.tar.gz

---
+++ packages/tecton/tecton-1.1.0b9.tar.gz/tecton-1.1.0b9/tecton_core/duckdb_factory.py
@@ -0,0 +1,111 @@
+import logging
+import tempfile
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+from typing import Optional
+
+from tecton_core import _gen_version
+from tecton_core import conf
+
+
+if TYPE_CHECKING:
+    from duckdb import DuckDBPyConnection
+
+"""
+Factory code for creating DuckDB connections.
+"""
+
+logger = logging.getLogger(__name__)
+
+
+def get_ext_version():
+    return "latest" if _gen_version.VERSION == "99.99.99" else _gen_version.VERSION
+
+
+_home_dir_override: Optional[str] = None
+
+
+def set_home_dir_override(value: Optional[str]) -> None:
+    global _home_dir_override
+    _home_dir_override = value
+
+
+@dataclass
+# Example: DuckDBConfig(memory_limit='1GB', num_threads=4)
+class DuckDBConfig:
+    memory_limit: Optional[str] = None  # Use string to specify memory size, e.g., '1GB'
+    num_threads: Optional[int] = None  # Number of threads for DuckDB
+
+
+def create_connection(duckdb_config: DuckDBConfig = DuckDBConfig()) -> "DuckDBPyConnection":
+    """
+    Create a new instance of DuckDBPyConnection.
+    """
+    try:
+        import duckdb
+    except ImportError:
+        msg = (
+            "Couldn't initialize DuckDB connection. "
+            "Please install DuckDB dependencies by executing `pip install tecton[rift]`."
+        )
+        raise RuntimeError(msg)
+
+    conn_config = {}
+    if conf.get_or_none("DUCKDB_EXTENSION_REPO"):
+        conn_config["allow_unsigned_extensions"] = "true"
+
+    if conf.get_bool("DUCKDB_PERSIST_DB"):
+        connection = duckdb.connect("duckdb.db", config=conn_config)
+    else:
+        connection = duckdb.connect(config=conn_config)
+
+    # Initialize the DuckDB connection
+    if _home_dir_override:
+        connection.sql(f"SET home_directory='{_home_dir_override}'")
+    connection.sql("INSTALL httpfs;")
+    connection.sql("LOAD httpfs;")
+    connection.sql(f"SET http_retries='{conf.get_or_raise('DUCKDB_HTTP_RETRIES')}'")
+
+    if conf.get_bool("DUCKDB_DISK_SPILLING_ENABLED"):
+        # The directory will be deleted when the TemporaryDirectory object is destroyed even if we don't call
+        # __enter__. This means as long as we store the object somewhere the directory will live as the context and
+        # will be cleaned up at interpreter exit.
+        temporary_directory = tempfile.TemporaryDirectory(suffix=".tecton_duckdb")
+        connection.sql(f"SET temp_directory = '{temporary_directory.name}'")
+
+    duckdb_memory_limit = (
+        duckdb_config.memory_limit
+        if duckdb_config.memory_limit is not None
+        else conf.get_or_none("DUCKDB_MEMORY_LIMIT")
+    )
+    if duckdb_memory_limit:
+        if conf.get_bool("DUCKDB_DEBUG"):
+            print(f"Setting duckdb memory limit to {duckdb_memory_limit}")
+
+        connection.sql(f"SET memory_limit='{duckdb_memory_limit}'")
+
+    num_duckdb_threads = (
+        duckdb_config.num_threads if duckdb_config.num_threads is not None else conf.get_or_none("DUCKDB_NTHREADS")
+    )
+    if num_duckdb_threads:
+        connection.sql(f"SET threads TO {num_duckdb_threads};")
+        if conf.get_bool("DUCKDB_DEBUG"):
+            print(f"Setting duckdb threads to {num_duckdb_threads}")
+
+    # Workaround for pypika not supporting the // operator
+    connection.sql("CREATE OR REPLACE MACRO _tecton_int_div(a, b) AS a // b")
+    extension_repo = conf.get_or_none("DUCKDB_EXTENSION_REPO")
+    if extension_repo:
+        versioned_extension_repo = extension_repo.format(version=get_ext_version())
+        connection.sql(f"SET custom_extension_repository='{versioned_extension_repo}'")
+        if conf.get_bool("DUCKDB_ALLOW_CACHE_EXTENSION"):
+            # Allow using local cached version of extension
+            connection.sql("INSTALL tecton")
+        else:
+            # Always download the latest version of the duckdb extension
+            connection.sql("FORCE INSTALL tecton")
+        connection.sql("LOAD tecton")
+
+    connection.sql("SET TimeZone='UTC'")
+
+    return connection
---
+++ packages/tecton/tecton-1.1.0b9.tar.gz/tecton-1.1.0b9/tecton_core/specs/calculation_node_spec.py
@@ -0,0 +1,121 @@
+from abc import ABC
+from abc import abstractmethod
+from typing import List
+from typing import Optional
+from typing import Union
+
+import pypika.functions as fn
+from pypika import NULL
+from pypika.terms import Term
+from pypika.terms import ValueWrapper
+
+from tecton_core import data_types
+from tecton_core.data_types import DataType
+from tecton_core.data_types import data_type_from_proto
+from tecton_core.errors import TectonInternalError
+from tecton_core.specs import utils
+from tecton_proto.common import calculation_node__client_pb2 as calculation_node_pb2
+
+
+@utils.frozen_strict
+class AbstractSyntaxTreeNodeSpec(ABC):
+    dtype: Optional[DataType]
+
+    @classmethod
+    def from_proto(cls, node: calculation_node_pb2.AbstractSyntaxTreeNode) -> "AbstractSyntaxTreeNodeSpec":
+        value_type = node.WhichOneof("value")
+        if value_type == "literal_value":
+            return LiteralValueNodeSpec.from_proto(node)
+        elif value_type == "column_reference":
+            return ColumnReferenceNodeSpec.from_proto(node)
+        elif value_type == "operation":
+            return OperationNodeSpec.from_proto(node)
+        elif value_type == "date_part":
+            return DatePartNodeSpec.from_proto(node)
+        else:
+            msg = "Unknown AbstractSyntaxTreeNode Type"
+            raise TectonInternalError(msg)
+
+    @abstractmethod
+    def to_query_term(self) -> Term:
+        raise NotImplementedError
+
+
+@utils.frozen_strict
+class OperationNodeSpec(AbstractSyntaxTreeNodeSpec):
+    dtype: DataType
+    operation: calculation_node_pb2.OperationType
+    operands: List[AbstractSyntaxTreeNodeSpec]
+
+    @classmethod
+    def from_proto(cls, proto: calculation_node_pb2.AbstractSyntaxTreeNode) -> "OperationNodeSpec":
+        operation_proto = proto.operation
+        return cls(
+            dtype=data_type_from_proto(proto.dtype),
+            operation=operation_proto.operation,
+            operands=[AbstractSyntaxTreeNodeSpec.from_proto(operand) for operand in operation_proto.operands],
+        )
+
+    def to_query_term(self) -> Term:
+        if self.operation == calculation_node_pb2.OperationType.COALESCE:
+            return self._build_coalesce_query()
+        else:
+            msg = f"In Calculation sql generation, calculation operation {self.operation.name} not supported."
+            raise TectonInternalError(msg)
+
+    def _build_coalesce_query(self) -> Term:
+        if len(self.operands) < 1:
+            msg = "Calculation function Coalesce must have at least 1 operand."
+            raise TectonInternalError(msg)
+        operand_sqls = [operand.to_query_term() for operand in self.operands]
+        return fn.Coalesce(*operand_sqls)
+
+
+@utils.frozen_strict
+class LiteralValueNodeSpec(AbstractSyntaxTreeNodeSpec):
+    dtype: Optional[DataType]
+    value: Optional[Union[float, int, bool, str]]
+
+    @classmethod
+    def from_proto(cls, proto: calculation_node_pb2.AbstractSyntaxTreeNode) -> "LiteralValueNodeSpec":
+        literal_value_proto = proto.literal_value
+        value_type = literal_value_proto.WhichOneof("value")
+        if value_type == "null_value":
+            return cls(dtype=None, value=None)
+        else:
+            return cls(dtype=data_type_from_proto(proto.dtype), value=getattr(literal_value_proto, value_type))
+
+    def to_query_term(self) -> Term:
+        if self.dtype is None or self.value is None:
+            return NULL
+        sql = ValueWrapper(self.value)
+        if isinstance(self.dtype, data_types.Int64Type):
+            sql = fn.Cast(sql, "BIGINT")
+        elif isinstance(self.dtype, data_types.Float64Type):
+            sql = fn.Cast(sql, "DOUBLE")
+        return sql
+
+
+@utils.frozen_strict
+class ColumnReferenceNodeSpec(AbstractSyntaxTreeNodeSpec):
+    dtype: DataType
+    value: str
+
+    @classmethod
+    def from_proto(cls, proto: calculation_node_pb2.AbstractSyntaxTreeNode) -> "ColumnReferenceNodeSpec":
+        return cls(dtype=data_type_from_proto(proto.dtype), value=proto.column_reference)
+
+    def to_query_term(self) -> Term:
+        raise NotImplementedError
+
+
+@utils.frozen_strict
+class DatePartNodeSpec(AbstractSyntaxTreeNodeSpec):
+    value: calculation_node_pb2.DatePart.ValueType
+
+    @classmethod
+    def from_proto(cls, proto: calculation_node_pb2.AbstractSyntaxTreeNode) -> "DatePartNodeSpec":
+        return cls(value=proto.date_part, dtype=None)
+
+    def to_query_term(self) -> Term:
+        raise NotImplementedError
