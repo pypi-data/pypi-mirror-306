6365b1a19c6d94d8fc41a9b4870f32a6b2deaeaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:49 +0000

    Add package hatch-meson/0.0.1/hatch_meson-0.0.1.tar.gz

---
+++ packages/hatch-meson/hatch_meson-0.0.1.tar.gz/hatch_meson-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="hatch-meson",
+    version="0.0.1",
+    description="TODO",
+    author="Dustin Spicuzza",
+    author_email="dustin@virtualroadside.com",
+    license="MIT",
+    packages=["todo"],
+)
