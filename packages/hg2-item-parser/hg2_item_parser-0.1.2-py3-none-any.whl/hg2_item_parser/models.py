f21f7e1fdd09dbff27cc8d9555ba1c57ea297dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 06:04:48 +0000

    Add package hg2-item-parser/0.1.2/hg2_item_parser-0.1.2-py3-none-any.whl

---
+++ packages/hg2-item-parser/hg2_item_parser-0.1.2-py3-none-any.whl/hg2_item_parser/models.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+from typing import Literal
+
+
+class BaseItemModel:
+    def __str__(self) -> str:
+        return "\n".join(f"{name}: {value}" for name, value in self.__dict__.items())
+
+
+@dataclass
+class ItemInfo(BaseItemModel):
+    id: int
+    title_id: int
+    title: str
+    image_id: int
+    image_url: str
+    damage_type: str | None
+    rarity: int
+
+
+@dataclass
+class ItemProperty(BaseItemModel):
+    max_lvl: int
+
+
+@dataclass
+class WeaponProperty(ItemProperty):
+    cost: int
+    max_lvl_hp: int | float
+    type: str | None
+    max_lvl_damage: int | float
+    max_lvl_ammo: int | float | Literal["∞"]
+    max_lvl_atk_speed: float
+    deploy_limit: int
+    duration: float
+    crit_rate: str
+
+
+@dataclass
+class CostumeProperty(ItemProperty):
+    cost: int
+    max_lvl_hp: int | float
+
+
+@dataclass
+class BadgeProperty(ItemProperty):
+    cost: int
+
+
+@dataclass
+class PetProperty(ItemProperty):
+    max_lvl_damage: int | float
+    crit_rate: str
+    base_sync: int | float
+    max_sync: int | float
+
+
+@dataclass
+class ItemSkill(BaseItemModel):
+    id: int
+    damage_type: str | None
+    title_id: int
+    title: str
+    description_template_id: int
+    description_template: str
+    description: str
+
+
+@dataclass
+class Item:
+    info: ItemInfo
+    property: ItemProperty
+    skills: list[ItemSkill]
+
+    def __str__(self) -> str:
+        info_str = f"INFO\n{self.info}"
+        property_str = f"PROPERTY\n{self.property}"
+        border = f"\n{"-"*15}\n"
+        skills_str = f"SKILLS\n{border.join(str(skill) for skill in self.skills)}"
+
+        return f"{info_str}\n\n{property_str}\n\n{skills_str}"
