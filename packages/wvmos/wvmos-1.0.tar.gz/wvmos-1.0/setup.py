3df29c9bacc5d8e2afd13c9e1750a4e37ac01f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 03:09:16 +0000

    Add package wvmos/1.0/wvmos-1.0.tar.gz

---
+++ packages/wvmos/wvmos-1.0.tar.gz/wvmos-1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+# Package meta-data.
+NAME = 'wvmos'
+DESCRIPTION = 'This package is written for MOS score prediction based on fine-tuned wav2vec2.0 model'
+URL = 'https://github.com/AndreevP/wvmos'
+EMAIL = 'andreev.pk@phystech.edu'
+AUTHOR = 'Pavel Andreev'
+REQUIRES_PYTHON = '>=3.9.0'
+VERSION = '1.0'
+
+setup(
+    name="wvmos",
+    version="1.0",
+    packages=find_packages(),
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'numpy',
+        'scipy',
+        'tqdm',
+        'librosa',
+        'torch>=1.7.0',
+        'transformers>=4.12.5',
+        'pytorch-lightning>=1.5.5'
+    ],
+    include_package_data=True
+)
