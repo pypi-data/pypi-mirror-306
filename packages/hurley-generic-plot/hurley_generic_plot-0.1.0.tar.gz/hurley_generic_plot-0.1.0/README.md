74a459a89c09eb286b6dfe00bd0d88a1ddf54a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 06:05:40 +0000

    Add package hurley-generic-plot/0.1.0/hurley_generic_plot-0.1.0.tar.gz

---
+++ packages/hurley-generic-plot/hurley_generic_plot-0.1.0.tar.gz/hurley_generic_plot-0.1.0/README.md
@@ -0,0 +1,1 @@
+# Hurley-generic-plot
---
+++ packages/hurley-generic-plot/hurley_generic_plot-0.1.0.tar.gz/hurley_generic_plot-0.1.0/hurley_generic_plot/__init__.py
@@ -0,0 +1,9 @@
+"""
+hurley-generic-plot: Just some generic functions to make plots
+"""
+
+__version__ = "0.1.0"
+
+from .test import test
+
+__all__ = ['test'] 
