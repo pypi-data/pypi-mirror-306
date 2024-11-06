53800e8695973fc18bd38ed5553b5db65e3bee1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:47 +0000

    Add package valeriepieris/0.1.10/valeriepieris-0.1.10.tar.gz

---
+++ packages/valeriepieris/valeriepieris-0.1.10.tar.gz/valeriepieris-0.1.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import Extension, find_packages, setup
+from Cython.Build import cythonize
+import numpy
+
+extensions = [
+    Extension("valeriepieris", ["src/valeriepieris.pyx"], define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],include_dirs=[numpy.get_include()]),
+]
+setup(
+   packages=find_packages(where="src"), 
+   package_dir={"": "src"},
+   ext_modules = cythonize(extensions, annotate=True, 
+   compiler_directives={'language_level' : "3", "cdivision":True, "boundscheck":False, "wraparound":False, "nonecheck":False}
+   ),
+   python_requires=">=3.8",
+   install_requires=["numpy>=2.0.0"],
+   setup_requires=["numpy>=2.0.0", "Cython>=0.29.33" ],
+)
