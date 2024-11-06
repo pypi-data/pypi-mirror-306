8ad7c04d34594f2138cf048d081293d8db2abddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 16:05:30 +0000

    Add package kvrocks-dao/0.0.7/kvrocks_dao-0.0.7.tar.gz

---
+++ packages/kvrocks-dao/kvrocks_dao-0.0.7.tar.gz/kvrocks_dao-0.0.7/kvrocks_dao/__init__.py
@@ -0,0 +1,8 @@
+from kvrocks_dao.common.entity import BaseEntity
+from kvrocks_dao.common.dao import BaseDAO
+
+# Export only the items you want to make public
+__all__ = [
+    BaseEntity,
+    BaseDAO,
+]
