e55bd88de75360a502583e12d50b815ae03cdd89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 14:05:40 +0000

    Add package soundscapy/0.7.5/soundscapy-0.7.5-py3-none-any.whl

---
+++ packages/soundscapy/soundscapy-0.7.5-py3-none-any.whl/soundscapy/__init__.py
@@ -0,0 +1,44 @@
+"""
+Soundscapy is a Python library for soundscape analysis and visualisation.
+"""
+
+# ruff: noqa: E402
+from loguru import logger
+
+# https://loguru.readthedocs.io/en/latest/resources/recipes.html#configuring-loguru-to-be-used-by-a-library-or-an-application
+logger.disable("soundscapy")
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("soundscapy")
+
+from soundscapy import surveys
+from soundscapy import databases
+from soundscapy import plotting
+
+from soundscapy.logging import setup_logging
+from soundscapy.databases import araus, isd, satp
+from soundscapy.surveys import processing
+from soundscapy.plotting import scatter_plot, density_plot
+
+__all__ = [
+    "surveys",
+    "databases",
+    "plotting",
+    "araus",
+    "isd",
+    "satp",
+    "processing",
+    "scatter_plot",
+    "density_plot",
+    "setup_logging",
+]
+
+try:
+    from soundscapy import audio
+
+    __all__.extend(["audio"])
+    __all__.extend(audio.__all__)
+
+except ImportError:
+    logger.debug("Audio module not available")
