8f070ed5afd39c58a13ed4a738525d0ede69f59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:05:19 +0000

    Add package locast/0.1.5/locast-0.1.5.tar.gz

---
+++ packages/locast/locast-0.1.5.tar.gz/locast-0.1.5/locast/candle_storage/candle_storage.py
@@ -0,0 +1,31 @@
+from typing import List, Protocol
+
+from locast.candle.candle import Candle
+from locast.candle.exchange import Exchange
+from locast.candle.exchange_resolution import ResolutionDetail
+from locast.candle_storage.cluster_info import ClusterInfo
+
+
+class CandleStorage(Protocol):
+    async def store_candles(self, candles: List[Candle]) -> None: ...
+
+    async def retrieve_cluster(
+        self,
+        exchange: Exchange,
+        market: str,
+        resolution: ResolutionDetail,
+    ) -> List[Candle]: ...
+
+    async def delete_cluster(
+        self,
+        exchange: Exchange,
+        market: str,
+        resolution: ResolutionDetail,
+    ) -> None: ...
+
+    async def get_cluster_info(
+        self,
+        exchange: Exchange,
+        market: str,
+        resolution: ResolutionDetail,
+    ) -> ClusterInfo: ...
