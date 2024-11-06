56808a0db5747a755cf33467b052216a33df45c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:43 +0000

    Add package rocket-a/1.0.0/rocket_a-1.0.0.tar.gz

---
+++ packages/rocket-a/rocket_a-1.0.0.tar.gz/rocket_a-1.0.0/rocket_a/rocket.py
@@ -0,0 +1,53 @@
+# Save as rocket.py
+from math import sqrt
+
+class Rocket():
+    # Rocket simulates a rocket ship for a game,
+    #  or a physics simulation.
+    
+    def __init__(self, x=0, y=0):
+        # Each rocket has an (x,y) position.
+        self.x = x
+        self.y = y
+        
+    def move_rocket(self, x_increment=0, y_increment=1):
+        # Move the rocket according to the paremeters given.
+        #  Default behavior is to move the rocket up one unit.
+        self.x += x_increment
+        self.y += y_increment
+        
+     def __eq__(self, other):
+        if isinstance(other, Rocket):
+            return self.x == other.x and self.y == other.y
+        return False
+
+    def get_distance(self, other_rocket):
+        # Calculates the distance from this rocket to another rocket,
+        #  and returns that value.
+        distance = sqrt((self.x-other_rocket.x)**2+(self.y-other_rocket.y)**2)
+        return distance
+    
+    def __str__(self):
+        return f"A Rocket positioned at ({self.x},{self.y})"
+
+    def __repr__(self):
+        return f"Rocket({self.x},{self.y})"
+    
+class Shuttle(Rocket): 
+    def __init__(self, x=0, y=0, flights_completed=0):
+        super().__init__(x, y)
+        self.flights_completed = flights_completed
+
+class CircleRocket(Rocket):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
+
+    def get_area(self):
+        return math.pi * self.radius**2
+
+    def get_circumference(self):
+        return 2 * math.pi * self.radius
+
+    def __str__(self):
+        return f"A CircleRocket positioned at ({self.x}, {self.y}) with radius {self.radius}"
