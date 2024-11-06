5957875335ad7d7fccd73ed0c6d079391790bd9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:41 +0000

    Add package arcade/3.0.0.dev38/arcade-3.0.0.dev38-py3-none-any.whl

---
+++ packages/arcade/arcade-3.0.0.dev38-py3-none-any.whl/arcade/__pyinstaller/hook-arcade.py
@@ -0,0 +1,53 @@
+"""
+"Hook" file for pyinstaller - enables Arcade to be easily packaged with PyInstaller.
+
+Docs: https://pyinstaller.readthedocs.io/en/stable/hooks.html
+Official Sample: https://github.com/pyinstaller/hooksample
+
+Note: See setup.cfg for entry points that enable this hook file to be found
+
+For a walk through of building an Arcade application with pyinstaller see:
+https://api.arcade.academy/en/latest/tutorials/bundling_with_pyinstaller/index.html
+"""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import pymunk
+from PyInstaller.compat import is_darwin, is_unix, is_win
+
+import arcade
+
+pymunk_path = Path(pymunk.__file__).parent
+arcade_path = Path(arcade.__file__).parent
+
+datas = [
+    (
+        arcade_path / "resources" / "system",
+        "./arcade/resources/system",
+    ),
+    (
+        arcade_path / "VERSION",
+        "./arcade/VERSION",
+    ),
+]
+
+if is_win:
+    binaries = [
+        (pymunk_path / "_chipmunk.pyd", "."),
+    ]
+elif is_darwin:
+    binaries = [
+        (pymunk_path / "_chipmunk.abi3.so", "."),
+        (arcade_path / "lib", "./arcade/lib"),
+    ]
+elif is_unix:
+    binaries = [
+        (pymunk_path / "_chipmunk.abi3.so", "."),
+    ]
+else:
+    raise NotImplementedError(
+        "You are running on an unsupported operating system. "
+        "Only Linux, Mac, and Windows are supported."
+    )
