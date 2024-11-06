98db0ec083848cfaee9e7e4f37e71a940f4d2467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:57 +0000

    Add package sea-query/0.2.5/sea_query-0.2.5-cp38-none-win_amd64.whl

---
+++ packages/sea-query/sea_query-0.2.5-cp38-none-win_amd64.whl/sea_query/table.py
@@ -0,0 +1,21 @@
+from ._internal import (
+    Column,
+    ColumnType,
+    Table,
+    TableAlterStatement,
+    TableCreateStatement,
+    TableDropStatement,
+    TableRenameStatement,
+    TableTruncateStatement,
+)
+
+__all__ = [
+    "Column",
+    "ColumnType",
+    "Table",
+    "TableAlterStatement",
+    "TableCreateStatement",
+    "TableDropStatement",
+    "TableRenameStatement",
+    "TableTruncateStatement",
+]
