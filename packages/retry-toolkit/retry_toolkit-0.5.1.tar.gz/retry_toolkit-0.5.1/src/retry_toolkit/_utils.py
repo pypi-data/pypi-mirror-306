6fd106892cf93d4132df152ddbf766bbb0ebbc14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 05:04:47 +0000

    Add package retry-toolkit/0.5.1/retry_toolkit-0.5.1.tar.gz

---
+++ packages/retry-toolkit/retry_toolkit-0.5.1.tar.gz/retry_toolkit-0.5.1/src/retry_toolkit/_utils.py
@@ -0,0 +1,30 @@
+#┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅#
+# SPDX-FileCopyrightText: © 2024 David E. James
+# SPDX-License-Identifier: MIT
+# SPDX-FileType: SOURCE
+#┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈#
+
+import inspect
+
+
+#-------------------------------------------------------------------------------
+# Private Utilities
+#-------------------------------------------------------------------------------
+
+def _ensure_callable(var, default):
+    if callable(var):
+        return var
+
+    if var is not None:
+        return lambda *args, **kwargs: var
+
+    if callable(default) and not inspect.isclass(default):
+        return default
+
+    return lambda *args, **kwargs: default
+
+
+async def _acall_f(func, *args, **kwargs):
+    if inspect.iscoroutinefunction(func):
+        return await func(*args, **kwargs)
+    return func(*args, **kwargs)
