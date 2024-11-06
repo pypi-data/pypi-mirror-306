00b446476dbcb8e571ef31acc055804078daf013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 03:09:48 +0000

    Add package ykh-gui-mamba-1105/0.1/ykh_gui_mamba_1105-0.1.tar.gz

---
+++ packages/ykh-gui-mamba-1105/ykh_gui_mamba_1105-0.1.tar.gz/ykh_gui_mamba_1105-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ykh_gui_mamba_1105",
+    version="0.1",
+    author="YingkeHuang",
+    author_email="ykhaung@ihep.ac.cn",
+    description="gui4dim",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
