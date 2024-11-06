52e132774c1f606500b3d07ac87b0b5a6d8e3713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:51 +0000

    Add package chainit/0.4.0/chainit-0.4.0-py3-none-any.whl

---
+++ packages/chainit/chainit-0.4.0-py3-none-any.whl/chainit/__init__.py
@@ -0,0 +1,12 @@
+"""
+.. include:: ../README.md
+
+# API Documentation
+"""
+
+__docformat__ = "restructuredtext"
+
+from .decorator import chainit
+from .klass import ChainIt
+
+__all__ = ("ChainIt", "chainit")
