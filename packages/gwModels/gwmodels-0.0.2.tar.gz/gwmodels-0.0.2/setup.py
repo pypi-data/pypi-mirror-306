e1f2e4ad5055ccaea9ac9d270cc58827abf13aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 06:04:35 +0000

    Add package gwModels/0.0.2/gwmodels-0.0.2.tar.gz

---
+++ packages/gwModels/gwmodels-0.0.2.tar.gz/gwmodels-0.0.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages, setup
+
+long_description = open('README.md').read()
+
+# Load requirements from requirements.txt
+# with open('requirements.txt') as f:
+#     requirements = f.read().splitlines()
+# Define dependencies in a list
+requirements = [
+    'python==3.10', 'gsl', 'numpy', 'scipy', 'swig', 'h5py', 'pkg-config', 
+    'matplotlib', 'python-lal', 'python-lalsimulation', 'astropy', 'gwpy', 
+    'pandas', 'ipykernel', 'ipywidgets', 'notebook', 'pyarrow', 'numba', 
+    'python-ldas-tools-framecpp', 'multiprocess', 'jupyterlab', 'nodejs', 
+    'libconfig', 'python-dotenv', 'gwtools', 'gwsurrogate', 'wolframclient'
+]
+
+setup(name='gwModels',
+      version='0.0.2',
+      author='Tousif Islam',
+      author_email='tousifislam24@gmail.com',
+      license='MIT',
+      include_package_data=True,
+      contributors=['Tousif Islam'],
+      description='Python package to provide gravitational waveform models',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      install_requires=requirements,
+      # will start new downloads if these are installed in a non-standard location
+      # install_requires=["numpy","matplotlib","scipy"],
+      classifiers=[
+                'Intended Audience :: Other Audience',
+                'Intended Audience :: Science/Research',
+                'Natural Language :: English',
+                'License :: OSI Approved :: MIT License',
+                'Programming Language :: Python',
+                'Topic :: Scientific/Engineering',
+                'Topic :: Scientific/Engineering :: Mathematics',
+                'Topic :: Scientific/Engineering :: Physics',
+      ],
+)
