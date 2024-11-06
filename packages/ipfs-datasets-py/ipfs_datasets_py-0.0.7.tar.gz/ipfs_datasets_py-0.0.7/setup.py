3eb087c84253bdae1c1723019dd3b0956c49612d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 15:04:53 +0000

    Add package ipfs-datasets-py/0.0.7/ipfs_datasets_py-0.0.7.tar.gz

---
+++ packages/ipfs-datasets-py/ipfs_datasets_py-0.0.7.tar.gz/ipfs_datasets_py-0.0.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="ipfs_datasets_py",
+	version='0.0.7',
+	packages=[
+		'ipfs_datasets_py',
+	],
+	install_requires=[
+        'orbitdb_kit_py',
+        'ipfs_kit_py',
+        'ipfs_model_manager_py',
+        'ipfs_faiss_py',
+        'transformers',
+        'numpy',
+        'urllib3',
+        'requests',
+        'boto3',
+	]
+)
