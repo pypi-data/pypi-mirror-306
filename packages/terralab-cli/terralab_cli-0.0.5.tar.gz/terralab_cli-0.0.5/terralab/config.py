13522183c9a774d14556a4cb486aed8613b2459b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 07:05:07 +0000

    Add package terralab-cli/0.0.5/terralab_cli-0.0.5.tar.gz

---
+++ packages/terralab-cli/terralab_cli-0.0.5.tar.gz/terralab_cli-0.0.5/terralab/config.py
@@ -0,0 +1,29 @@
+# config.py
+
+import logging
+from pathlib import Path
+from oauth2_cli_auth import OAuth2ClientInfo
+from dotenv import dotenv_values
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class CliConfig:
+    """A class to hold configuration information for the CLI"""
+
+    def __init__(self, config_file="terralab/.terralab-cli-config"):
+        self.config = dotenv_values(config_file)
+        LOGGER.debug(f"Found config with values: {self.config}")
+
+        self.client_info = OAuth2ClientInfo.from_oidc_endpoint(
+            self.config["OAUTH_OPENID_CONFIGURATION_URI"],
+            client_id=self.config["OAUTH_CLIENT_ID"],
+            scopes=[f"openid+email+profile+{self.config['OAUTH_CLIENT_ID']}"],
+        )
+
+        self.server_port = int(self.config["SERVER_PORT"])
+
+        self.token_file = (
+            f'{Path.home()}/{self.config["LOCAL_STORAGE_PATH"]}/access_token'
+        )
