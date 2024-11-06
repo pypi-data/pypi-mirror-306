4d540f931e8cdb20534f9fa163be667d304639bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:44 +0000

    Add package terralab-cli/0.0.3/terralab_cli-0.0.3.tar.gz

---
+++ packages/terralab-cli/terralab_cli-0.0.3.tar.gz/terralab_cli-0.0.3/terralab/commands/auth_commands.py
@@ -0,0 +1,22 @@
+# commands/auth_commands.py
+
+import click
+
+from logic import auth_logic
+
+
+@click.group()
+def auth():
+    """Commands for authenticating to Terralab"""
+
+
+@auth.command()
+def login():
+    """Authenticate with Terralab via browser login to Terra b2c"""
+    auth_logic.check_local_token_and_fetch_if_needed()
+
+
+@auth.command()
+def logout():
+    """Clear the local authentication token"""
+    auth_logic.clear_local_token()
