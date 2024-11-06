5458cc16296a51a7b2c4b874e50e1184fe4ade55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 06:05:17 +0000

    Add package superutilities/0.2.3/superutilities-0.2.3-py3-none-any.whl

---
+++ packages/superutilities/superutilities-0.2.3-py3-none-any.whl/superutilities/__init__.py
@@ -0,0 +1,9 @@
+from .main import ConflictingInputsDetected
+from .main import SanitizeString
+from .main import SInput
+from .main import PermutationCalculator
+from .main import getMedian
+from .main import saveJsonToFile
+from .main import getJsonFromFile
+import re
+import json
