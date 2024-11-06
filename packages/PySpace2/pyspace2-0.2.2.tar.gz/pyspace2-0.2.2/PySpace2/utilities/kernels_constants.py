16a58ec2fcbd5db0a594bf81cb81de8dd016eed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 07:05:20 +0000

    Add package PySpace2/0.2.2/pyspace2-0.2.2.tar.gz

---
+++ packages/PySpace2/pyspace2-0.2.2.tar.gz/pyspace2-0.2.2/PySpace2/utilities/kernels_constants.py
@@ -0,0 +1,6 @@
+KERNELS_URLS = {
+    "lsk/naif0012.tls": "generic_kernels/lsk/naif0012.tls",
+    "pck/gm_de431.tpc": "generic_kernels/pck/gm_de431.tpc",
+    "pck/pck00010.tpc": "generic_kernels/pck/pck00010.tpc",
+    "spk/de432s.bsp": "generic_kernels/spk/planets/de432s.bsp",
+}
