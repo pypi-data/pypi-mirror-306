e7fae3ec88e5e28c473572ce7909f7a0b27855fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 05:06:37 +0000

    Add package schemon-python-logger/0.0.3/schemon-python-logger-0.0.3.tar.gz

---
+++ packages/schemon-python-logger/schemon-python-logger-0.0.3.tar.gz/schemon-python-logger-0.0.3/README.md
@@ -0,0 +1,8 @@
+![Build Status](https://github.com/Schemon-Inc/schemon-python-logger/actions/workflows/publish_pypi.yml/badge.svg)
+![PyPI](https://img.shields.io/pypi/v/schemon-python-logger)
+![Downloads](https://img.shields.io/pypi/dm/schemon-python-logger)
+![License](https://img.shields.io/github/license/Schemon-Inc/schemon-python-logger)
+![Python Version](https://img.shields.io/pypi/pyversions/schemon-python-logger)
+![GitHub Stars](https://img.shields.io/github/stars/Schemon-Inc/schemon-python-logger?style=social)
+
+# schemon-python-logger
---
+++ packages/schemon-python-logger/schemon-python-logger-0.0.3.tar.gz/schemon-python-logger-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+import subprocess
+
+VERSION = "0.0.3"
+
+
+def create_git_tag(version):
+    try:
+        subprocess.check_call(["git", "tag", f"v{version}"])
+        subprocess.check_call(["git", "push", "origin", f"v{version}"])
+    except subprocess.CalledProcessError as e:
+        print(f"Error creating or pushing tag: {e}")
+
+
+create_git_tag(VERSION)
+
+setup(
+    name="schemon-python-logger",
+    version=VERSION,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    license="Apache License 2.0",
+    license_files=["LICENSE"],  # Specify the license file
+    install_requires=[],
+    entry_points={},
+    python_requires=">=3.8",
+    include_package_data=True,  # Include package data specified in MANIFEST.in
+    package_data={},
+    exclude_package_data={},
+)
