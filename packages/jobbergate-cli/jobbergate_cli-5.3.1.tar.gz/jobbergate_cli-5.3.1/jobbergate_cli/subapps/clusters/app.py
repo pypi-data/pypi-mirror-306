2742ffacf8e66b797797e117f711feaae430ddbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 06:04:33 +0000

    Add package jobbergate-cli/5.3.1/jobbergate_cli-5.3.1.tar.gz

---
+++ packages/jobbergate-cli/jobbergate_cli-5.3.1.tar.gz/jobbergate_cli-5.3.1/jobbergate_cli/subapps/clusters/app.py
@@ -0,0 +1,27 @@
+"""
+Provide a ``typer`` app that can interact with Cluster data in a cruddy manner.
+"""
+
+import typer
+
+from jobbergate_cli.render import terminal_message
+from jobbergate_cli.schemas import JobbergateContext
+from jobbergate_cli.subapps.clusters.tools import get_client_ids
+
+
+app = typer.Typer(help="Commands to interact with clusters")
+
+
+@app.command("list")
+def list_all(
+    ctx: typer.Context,
+):
+    """
+    Show available clusters
+    """
+    jg_ctx: JobbergateContext = ctx.obj
+
+    # Make static type checkers happy
+    assert jg_ctx is not None
+
+    terminal_message("\n".join(get_client_ids(jg_ctx)), subject="Cluster Names", color="yellow", indent=True)
