5496bea9ad5b5db65fb4bc45fdfcd2fcf7868103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 15:05:10 +0000

    Add package aigdb/0.1.0/aigdb-0.1.0.tar.gz

---
+++ packages/aigdb/aigdb-0.1.0.tar.gz/aigdb-0.1.0/debugger.py
@@ -0,0 +1,10 @@
+# aigdb/debugger.py
+
+class AIDebugger:
+    def __init__(self, model):
+        self.model = model
+
+    def debug(self):
+        # Implement your debugging logic here
+        pass
+
