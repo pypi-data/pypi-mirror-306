c9fe0f035c974cc9989129914494a3f8879b3c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:05:05 +0000

    Add package the-datagarden-models/1.4.5/the_datagarden_models-1.4.5-py3-none-any.whl

---
+++ packages/the-datagarden-models/the_datagarden_models-1.4.5-py3-none-any.whl/datagarden_models/models/economics/__init__.py
@@ -0,0 +1,28 @@
+from pydantic import Field
+
+from ..base import DataGardenModel, DataGardenModelLegends
+from .base_economics import EconomicBaseKeys
+from .gdp import GDP, GDPV1Keys
+from .infllation import Inflation, InflationV1Keys
+
+
+class EconomicsV1Keys(GDPV1Keys, EconomicBaseKeys, InflationV1Keys):
+	GDP = "gdp"
+	DATAGARDEN_MODEL_NAME = "Economics"
+
+
+class EconomicsV1Legends(DataGardenModelLegends):
+	GDP = "Gross Domestic Product"
+	INFLATION = "Inflation"
+
+
+L = EconomicsV1Legends
+
+
+class EconomicsV1(DataGardenModel):
+	MODEL_LEGEND: str = "Economic data for a region. "
+	datagarden_model_version: str = Field(
+		"v1.0", frozen=True, description=L.DATAGARDEN_MODEL_VERSION
+	)
+	gdp: GDP = Field(default_factory=GDP, description=L.GDP)
+	inflation: Inflation = Field(default_factory=Inflation, description=L.INFLATION)
