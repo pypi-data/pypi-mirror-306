83897c543674c968397eacfe472582d751bfd04c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 03:09:46 +0000

    Add package weather-retriever/0.1.13/weather_retriever-0.1.13-py3-none-any.whl

---
+++ packages/weather-retriever/weather_retriever-0.1.13-py3-none-any.whl/weather_retriever/weather_codes.py
@@ -0,0 +1,223 @@
+# https://openweathermap.org/weather-conditions
+weather_codes = {
+    200: {
+        "def": "thunderstorm with light rain",
+        "translation": "가벼운 비를 동반한 뇌우",
+    },
+    201: {
+        "def": "thunderstorm with rain",
+        "translation": "비를 동반한 뇌우",
+    },
+    202: {
+        "def": "thunderstorm with heavy rain",
+        "translation": "강한 비를 동반한 뇌우",
+    },
+    210: {
+        "def": "light thunderstorm",
+        "translation": "약한 뇌우",
+    },
+    211: {
+        "def": "thunderstorm",
+        "translation": "뇌우",
+    },
+    212: {
+        "def": "heavy thunderstorm",
+        "translation": "강한 뇌우",
+    },
+    221: {
+        "def": "ragged thunderstorm",
+        "translation": "거친 뇌우"
+    },
+    230: {
+        "def": "thunderstorm with light drizzle",
+        "translation": "가벼운 이슬비를 동반한 뇌우",
+    },
+    231: {
+        "def": "thunderstorm with drizzle",
+        "translation": "이슬비를 동반한 뇌우",
+    },
+    232: {
+        "def": "thunderstorm with heavy drizzle",
+        "translation": "강한 이슬비를 동반한 뇌우",
+    },
+    300: {
+        "def": "light intensity drizzle",
+        "translation": "약한 이슬비",
+    },
+    301: {
+        "def": "drizzle",
+        "translation": "이슬비",
+    },
+    302: {
+        "def": "heavy intensity drizzle",
+        "translation": "강한 이슬비",
+    },
+    310: {
+        "def": "light intensity drizzle rain",
+        "translation": "약한 이슬비",
+    },
+    311: {
+        "def": "drizzle rain",
+        "translation": "이슬비",
+    },
+    312: {
+        "def": "heavy intensity drizzle rain",
+        "translation": "강한 이슬비",
+    },
+    313: {
+        "def": "shower rain and drizzle",
+        "translation": "소나기 및 이슬비",
+    },
+    314: {
+        "def": "heavy shower rain and drizzle",
+        "translation": "강한 소나기 및 이슬비",
+    },
+    321: {
+        "def": "shower drizzle",
+        "translation": "소나기 이슬비",
+        },
+    500: {
+        "def": "light rain",
+        "translation": "약한 비",
+    },
+    501: {
+        "def": "moderate rain",
+        "translation": "비",
+    },
+    502: {
+        "def": "heavy intensity rain",
+        "translation": "강한 비",
+    },
+    503: {
+        "def": "very heavy rain",
+        "translation": "매우 강한 비",
+    },
+    504: {
+        "def": "extreme rain",
+        "translation": "극심한 비",
+    },
+    511: {
+        "def": "freezing rain",
+        "translation": "어는 비",
+    },
+    520: {
+        "def": "light intensity shower rain",
+        "translation": "약한 소나기",
+    },
+    521: {
+        "def": "shower rain",
+        "translation": "소나기",
+    },
+    522: {
+        "def": "heavy intensity shower rain",
+        "translation": "강한 소나기",
+    },
+    531: {
+        "def": "ragged shower rain",
+        "translation": "거친 소나기",
+    },
+    600: {
+        "def": "light snow",
+        "translation": "가벼운 눈",
+    },
+    601: {
+        "def": "snow",
+        "translation": "눈",
+    },
+    602: {
+        "def": "heavy snow",
+        "translation": "강한 눈",
+    },
+    611: {
+        "def": "sleet",
+        "translation": "진눈깨비",
+    },
+    612: {
+        "def": "light shower sleet",
+        "translation": "가벼운 소나기 진눈깨비",
+    },
+    613: {
+        "def": "shower sleet",
+        "translation": "소나기 진눈깨비",
+    },
+    615: {
+        "def": "light rain and snow",
+        "translation": "약한 비와 눈",
+    },
+    616: {
+        "def": "rain and snow",
+        "translation": "비와 눈",
+    },
+    620: {
+        "def": "light shower snow",
+        "translation": "가벼운 소나기 눈",
+    },
+    621: {
+        "def": "shower snow",
+        "translation": "소나기 눈",
+    },
+    622: {
+        "def": "heavy shower snow",
+        "translation": "강한 소나기 눈",
+    },
+    701: {
+        "def": "mist",
+        "translation": "옅은 안개",
+    },
+    711: {
+        "def": "smoke",
+        "translation": "연기가 자욱한 하늘",
+    },
+    721: {
+        "def": "haze",
+        "translation": "연무",
+    },
+    731: {
+        "def": "sand/dust whirls",
+        "translation": "모래 또는 먼지 회오리",
+    },
+    741: {
+        "def": "fog",
+        "translation": "짙은 안개",
+    },
+    751: {
+        "def": "sand",
+        "translation": "모래바람",
+    },
+    761: {
+        "def": "dust",
+        "translation": "먼지가 많은 하늘",
+    },
+    762: {
+        "def": "volcanic ash",
+        "translation": "화산재가 많은 하늘",
+    },
+    771: {
+        "def": "squalls",
+        "translation": "돌풍",
+    },
+    781: {
+        "def": "tornado",
+        "translation": "토네이도",
+    },
+    800: {
+        "def": "clear sky",
+        "translation": "맑은 하늘",
+    },
+    801: {
+        "def": "few clouds: 11-25%",
+        "translation": "약간의 구름 낀 하늘",
+    },
+    802: {
+        "def": "scattered clouds: 25-50%",
+        "translation": "구름 낀 하늘",
+    },
+    803: {
+        "def": "broken clouds: 51-84%",
+        "translation": "다소 구름이 낀 흐린 하늘",
+    },
+    804: {
+        "def": "overcast clouds: 85-100%",
+        "translation": "구름이 많은 매우 흐린 하늘",
+    },
+}
