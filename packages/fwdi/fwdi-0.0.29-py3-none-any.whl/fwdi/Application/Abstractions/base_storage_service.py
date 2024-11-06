4394c64565568330fb20a085df4902fb007e16ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:50 +0000

    Add package fwdi/0.0.29/fwdi-0.0.29-py3-none-any.whl

---
+++ packages/fwdi/fwdi-0.0.29-py3-none-any.whl/fwdi/Application/Abstractions/base_storage_service.py
@@ -0,0 +1,20 @@
+#        _       __        __    __________    _____     __   __     __
+#   	/ \	    |  |	  |__|  |__________|  |  _  \   |__|  \ \   / /
+#      / _ \	|  |	   __       |  |      | |_)  |   __    \ \_/ /   Alitrix - Modern NLP
+#     / /_\ \	|  |	  |  |      |  |      |  _  /   |  |    } _ {    Languages: Python, C#
+#    / _____ \	|  |____  |  |      |  |      | | \ \   |  |   / / \ \   http://github.com/Alitrix
+#   /_/     \_\	|_______| |__|	    |__|      |_|  \_\  |__|  /_/   \_\
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020-2021 The Alitrix Authors <http://github.com/Alitrix>
+
+from abc import ABCMeta, abstractmethod
+
+from .base_service import BaseServiceFWDI
+
+class BaseStorageServiceFWDI(BaseServiceFWDI, metaclass=ABCMeta):
+    
+    @abstractmethod
+    def write(self, message:str):
+        ...
