d73e35d05dbfbae0428a44455522d23238a24e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 07:04:59 +0000

    Add package red-wine-model-matthew9655/0.0.2/red_wine_model_matthew9655-0.0.2.tar.gz

---
+++ packages/red-wine-model-matthew9655/red_wine_model_matthew9655-0.0.2.tar.gz/red_wine_model_matthew9655-0.0.2/src/red_wine_model/predict.py
@@ -0,0 +1,34 @@
+import typing as t
+
+import pandas as pd
+
+from red_wine_model import __version__ as _version
+from red_wine_model.config.core import config
+from red_wine_model.processing.data_manager import load_pipeline
+from red_wine_model.processing.validation import validate_inputs
+
+pipeline_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
+red_wine_pipe = load_pipeline(file_name=pipeline_file_name)
+
+
+def make_prediction(
+    *,
+    input_data: t.Union[pd.DataFrame, dict],
+) -> dict:
+    """Make a prediction using a saved model pipeline."""
+
+    data = pd.DataFrame(input_data)
+    validated_data, errors = validate_inputs(input_data=data)
+    results = {"predictions": None, "version": _version, "errors": errors}
+
+    print(results)
+
+    if not errors:
+        predictions = red_wine_pipe.predict(X=validated_data[config.m_config.features])
+        results = {
+            "predictions": predictions,
+            "version": _version,
+            "errors": errors,
+        }
+
+    return results
---
+++ packages/red-wine-model-matthew9655/red_wine_model_matthew9655-0.0.2.tar.gz/red_wine_model_matthew9655-0.0.2/tests/test_prediction.py
@@ -0,0 +1,28 @@
+"""
+Note: These tests will fail if you have not first trained the model.
+"""
+
+import numpy as np
+from sklearn.metrics import accuracy_score
+
+from red_wine_model.predict import make_prediction
+
+
+def test_make_prediction(sample_input_data):
+    x_test, y_test = sample_input_data
+    # Given
+    expected_no_predictions = 159
+
+    # When
+    result = make_prediction(input_data=x_test)
+
+    # Then
+    predictions = result.get("predictions")
+    assert isinstance(predictions, np.ndarray)
+    assert isinstance(predictions[0], np.int64)
+    assert result.get("errors") is None
+    assert len(predictions) == expected_no_predictions
+    _predictions = list(predictions)
+    accuracy = accuracy_score(_predictions, y_test)
+    print(f"accuracy score: {accuracy}")
+    assert accuracy > 0.90
