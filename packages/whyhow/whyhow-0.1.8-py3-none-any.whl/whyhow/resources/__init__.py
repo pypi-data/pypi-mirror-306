e01762453a73768de43c15274744581e464464b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 07:05:23 +0000

    Add package whyhow/0.1.8/whyhow-0.1.8-py3-none-any.whl

---
+++ packages/whyhow/whyhow-0.1.8-py3-none-any.whl/whyhow/resources/__init__.py
@@ -0,0 +1,29 @@
+"""Collection of route specific resources."""
+
+from whyhow.resources.chunks import AsyncChunksResource, ChunksResource
+from whyhow.resources.documents import (
+    AsyncDocumentsResource,
+    DocumentsResource,
+)
+from whyhow.resources.graphs import AsyncGraphsResource, GraphsResource
+from whyhow.resources.nodes import AsyncNodesResource, NodesResource
+from whyhow.resources.schemas import AsyncSchemasResource, SchemasResource
+from whyhow.resources.workspaces import (
+    AsyncWorkspacesResource,
+    WorkspacesResource,
+)
+
+__all__ = [
+    "AsyncChunksResource",
+    "AsyncDocumentsResource",
+    "AsyncGraphsResource",
+    "AsyncNodesResource",
+    "AsyncSchemasResource",
+    "AsyncWorkspacesResource",
+    "ChunksResource",
+    "DocumentsResource",
+    "GraphsResource",
+    "NodesResource",
+    "SchemasResource",
+    "WorkspacesResource",
+]
