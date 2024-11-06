b1507fd558de00dc572abf747983aed766836d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 03:08:58 +0000

    Add package simp-sql/0.1.3/simp_sql-0.1.3.tar.gz

---
+++ packages/simp-sql/simp_sql-0.1.3.tar.gz/simp_sql-0.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simp_sql",
+    version="0.1.3",
+    author="Nikhil Kumawat",
+    description="A package that generates and optimizes SQL queries.",
+    long_description=open("readme.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "mysql-connector-python>=8.0.0"  # List your dependencies here
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
