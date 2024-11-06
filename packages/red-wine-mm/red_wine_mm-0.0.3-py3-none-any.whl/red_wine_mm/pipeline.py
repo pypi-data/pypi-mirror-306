60f55c2105509ad02dfa7420fb13c4a6102692b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 15:05:17 +0000

    Add package red-wine-mm/0.0.3/red_wine_mm-0.0.3-py3-none-any.whl

---
+++ packages/red-wine-mm/red_wine_mm-0.0.3-py3-none-any.whl/red_wine_mm/pipeline.py
@@ -0,0 +1,20 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+from red_wine_mm.config.core import config
+
+red_wine_pipe = Pipeline(
+    [
+        ("scaler", StandardScaler()),
+        (
+            "random_forest_classifier",
+            GridSearchCV(
+                RandomForestClassifier(random_state=config.m_config.random_state),
+                param_grid={"n_estimators": [50, 100, 200]},
+                cv=5,
+            ),
+        ),
+    ]
+)
