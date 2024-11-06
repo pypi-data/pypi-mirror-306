8a742fd67576469af3901a64efe3a11b2340a6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 06:05:33 +0000

    Add package scyllapy/1.3.3/scyllapy-1.3.3-cp38-abi3-macosx_10_12_x86_64.whl

---
+++ packages/scyllapy/scyllapy-1.3.3-cp38-abi3-macosx_10_12_x86_64.whl/scyllapy/exceptions.py
@@ -0,0 +1,17 @@
+from ._internal.exceptions import (
+    ScyllaPyBaseError,
+    ScyllaPyBindingError,
+    ScyllaPyDBError,
+    ScyllaPyMappingError,
+    ScyllaPyQueryBuiderError,
+    ScyllaPySessionError,
+)
+
+__all__ = (
+    "ScyllaPyBaseError",
+    "ScyllaPyDBError",
+    "ScyllaPySessionError",
+    "ScyllaPyMappingError",
+    "ScyllaPyQueryBuiderError",
+    "ScyllaPyBindingError",
+)
