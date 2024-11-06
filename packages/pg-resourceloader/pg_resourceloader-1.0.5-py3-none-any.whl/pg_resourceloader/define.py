a80379b03dc71813a57a634277be9e678504036f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 03:08:38 +0000

    Add package pg-resourceloader/1.0.5/pg_resourceloader-1.0.5-py3-none-any.whl

---
+++ packages/pg-resourceloader/pg_resourceloader-1.0.5-py3-none-any.whl/pg_resourceloader/define.py
@@ -0,0 +1,13 @@
+KEY_RESOURCE_LOADER = "resource_loader"
+KEY_RESOURCE_LOADER_BIN_DIR = "bin_dir"
+KEY_RESOURCE_LOADER_BIN_LOADER_DIR = "bin_loader_dir"
+"""
+resource loader format
+====
+{
+  "resource_loader": {
+    "bin_dir": "cfg_bin",
+    "bin_loader_dir": "loaders"
+  }
+}
+"""
