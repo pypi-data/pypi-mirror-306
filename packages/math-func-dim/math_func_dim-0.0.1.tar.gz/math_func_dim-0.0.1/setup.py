8a936f600bd95bb08212e350a3f1698064123d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:05:05 +0000

    Add package math-func-dim/0.0.1/math_func_dim-0.0.1.tar.gz

---
+++ packages/math-func-dim/math_func_dim-0.0.1.tar.gz/math_func_dim-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="math_func_dim",
+    version="0.0.1",
+    author="yingkehuang",
+    author_email="ykhuang@ihep.ac.cn",
+    description="A brief description of your package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
