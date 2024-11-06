c2c52784096e8e2bc6ccb000e074aae4a26b84be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 03:09:20 +0000

    Add package regression-djaber/0.1/regression_djaber-0.1.tar.gz

---
+++ packages/regression-djaber/regression_djaber-0.1.tar.gz/regression_djaber-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='regression_djaber',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple linear regression model implemented from scratch.',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/regression_pkg',  # Replace with your GitHub repo
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
