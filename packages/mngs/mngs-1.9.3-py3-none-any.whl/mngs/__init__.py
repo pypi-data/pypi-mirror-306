d6b4bb0a2a58ceb36161fbdd385787dba79b3d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 07:05:07 +0000

    Add package mngs/1.9.3/mngs-1.9.3-py3-none-any.whl

---
+++ packages/mngs/mngs-1.9.3-py3-none-any.whl/mngs/__init__.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: "2024-11-06 09:25:00 (ywatanabe)"
+# File: ./mngs_repo/src/mngs/__init__.py
+
+########################################
+# Warnings
+########################################
+import warnings
+warnings.filterwarnings("ignore", message="There is no current event loop", category=DeprecationWarning)
+
+########################################
+# Try import
+########################################
+# import os
+# from .gen._suppress_output import suppress_output
+# _do_suppress = os.getenv("MNGS_SUPPRESS_IMPORTING_MESSAGES", "").lower() == "true"
+# with suppress_output(suppress=__do_suppress):
+
+
+########################################
+# Core Modules
+########################################
+from ._sh import sh
+from . import io
+from . import path
+from . import dict
+from . import gen
+from . import decorators
+from . import ai
+from . import dsp
+from . import gists
+from . import linalg
+from . import nn
+from . import os
+from . import plt
+from . import stats
+from . import torch
+from . import tex
+from . import types
+from . import resource
+from . import web
+from . import db
+from . import pd
+from . import str
+
+# ########################################
+# # Modules (python -m mngs print_config)
+# ########################################
+# from .gen._print_config import print_config
+# # Usage: python -m mngs print_config
+
+__copyright__ = "Copyright (C) 2024 Yusuke Watanabe"
+__version__ = "1.9.3"
+__license__ = "MIT"
+__author__ = "ywatanabe1989"
+__author_email__ = "ywatanabe@alumni.u-tokyo.ac.jp"
+__url__ = "https://github.com/ywatanabe1989/mngs"
+
+
+# EOF
