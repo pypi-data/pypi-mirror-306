507fd8f11308182803764bf98126fe211f44fbb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-06 04:04:50 +0000

    Add package cwo-analysis/4.0.0/cwo_analysis-4.0.0.tar.gz

---
+++ packages/cwo-analysis/cwo_analysis-4.0.0.tar.gz/cwo_analysis-4.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cwo_analysis',
+    version='4.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'numpy'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cwo_analysis=data_analysis_cwo.main:main',
+        ],
+    },
+    description='A data-analysis tool for cwo project',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Shashikant Pathak',
+    author_email='s.pathak@ugent.be',
+    url='https://github.com/shashikantpathakk3/data-analysis',
+)
+
+
