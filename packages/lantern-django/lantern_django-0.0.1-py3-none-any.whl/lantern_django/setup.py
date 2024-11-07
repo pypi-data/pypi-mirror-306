2fdf19a74178a942ed70676bea80b06d361cad25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:09:29 +0000

    Add package lantern-django/0.0.1/lantern_django-0.0.1-py3-none-any.whl

---
+++ packages/lantern-django/lantern_django-0.0.1-py3-none-any.whl/lantern_django/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lantern-django",
+    version="0.0.1",
+    description="Django client for Lantern",
+    url="https://github.com/lanterndata/lantern-python",
+    author="Di Qi",
+    author_email="di@lantern.dev",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/lanterndata/lantern-python/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["lantern_django"],
+    package_dir={"lantern_django": "."},
+    python_requires=">=3.8",
+    install_requires=["django", "numpy"],
+)
