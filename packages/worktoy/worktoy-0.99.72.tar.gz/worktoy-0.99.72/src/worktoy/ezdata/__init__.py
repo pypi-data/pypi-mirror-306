ce14bb3d091ab46e202d685f4eebee2f4f3ad39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:09:50 +0000

    Add package worktoy/0.99.72/worktoy-0.99.72.tar.gz

---
+++ packages/worktoy/worktoy-0.99.72.tar.gz/worktoy-0.99.72/src/worktoy/ezdata/__init__.py
@@ -0,0 +1,6 @@
+"""The 'worktoy.ezdata' module provides the 'EZData' class. This dataclass
+automatically generates the '__init__' method allowing class creation
+inline. """
+#  AGPL-3.0 license
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
