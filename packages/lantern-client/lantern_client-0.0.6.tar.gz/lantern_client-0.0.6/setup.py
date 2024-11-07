f241ebf9d6d089abff2bf83e7822bedc11acdd78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:09:29 +0000

    Add package lantern-client/0.0.6/lantern_client-0.0.6.tar.gz

---
+++ packages/lantern-client/lantern_client-0.0.6.tar.gz/lantern_client-0.0.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lantern-client",
+    version="0.0.6",
+    description="Python client for Lantern",
+    url="https://github.com/lanterndata/lantern-python",
+    author="Varik Matevosyan",
+    author_email="varik@lantern.dev",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/lanterndata/lantern-python/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["lantern"],
+    package_dir={"lantern": "."},
+    python_requires=">=3.8",
+    install_requires=["psycopg2-binary ==2.9.10", "numpy"],
+)
