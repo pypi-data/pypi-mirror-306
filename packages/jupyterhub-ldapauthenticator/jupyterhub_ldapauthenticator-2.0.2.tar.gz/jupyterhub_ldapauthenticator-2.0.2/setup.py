8d8d9c55c01bcad5eaac172a213b6d908a3f63b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 04:05:30 +0000

    Add package jupyterhub-ldapauthenticator/2.0.2/jupyterhub_ldapauthenticator-2.0.2.tar.gz

---
+++ packages/jupyterhub-ldapauthenticator/jupyterhub_ldapauthenticator-2.0.2.tar.gz/jupyterhub_ldapauthenticator-2.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name="jupyterhub-ldapauthenticator",
+    version="2.0.2",
+    description="LDAP Authenticator for JupyterHub",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jupyterhub/ldapauthenticator",
+    author="Yuvi Panda",
+    author_email="yuvipanda@riseup.net",
+    license="3 Clause BSD",
+    packages=["ldapauthenticator"],
+    python_requires=">=3.9",
+    install_requires=[
+        "jupyterhub>=4.1.6",
+        "ldap3>=2.9.1",
+        "traitlets",
+    ],
+    extras_require={
+        "test": [
+            "pytest",
+            "pytest-asyncio",
+            "pytest-cov",
+        ],
+    },
+    entry_points={
+        "jupyterhub.authenticators": [
+            "ldap = ldapauthenticator:LDAPAuthenticator",
+            "ldapauthenticator = ldapauthenticator:LDAPAuthenticator",
+        ],
+    },
+)
