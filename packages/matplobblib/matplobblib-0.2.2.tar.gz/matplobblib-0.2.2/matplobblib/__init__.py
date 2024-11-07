3d5f545c678fab8151c8e93f7e1d89f9affbf7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 05:06:17 +0000

    Add package matplobblib/0.2.2/matplobblib-0.2.2.tar.gz

---
+++ packages/matplobblib/matplobblib-0.2.2.tar.gz/matplobblib-0.2.2/matplobblib/__init__.py
@@ -0,0 +1,14 @@
+import importlib as _importlib
+
+
+submodules = [
+    'tvims',
+    'aisd',
+    'econometrics'
+]
+def __dir__():
+    return submodules
+
+def __getattr__(name):
+    if name in submodules:
+        return _importlib.import_module(f'matplobblib.{name}')
