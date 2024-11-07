c23692585b85449e696a1e6f0eed6f8672aca734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 05:05:57 +0000

    Add package nerdron/0.3.1/nerdron-0.3.1.tar.gz

---
+++ packages/nerdron/nerdron-0.3.1.tar.gz/nerdron-0.3.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nerdron',
+    version='0.3.1',  # Version of your package
+    description='A simple and flexible neural network package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Muhammad Ramzy',
+    author_email='mhdramzy777@gmail.com',
+    url='https://github.com/MuhammadRamzy/nerdron',
+    packages=find_packages(),  # Automatically finds all packages in the nerdron directory
+    install_requires=[          # External dependencies
+        'numpy>=1.19.2',        # Make sure to specify your dependencies
+    ],
+    test_suite='tests',         # Test suite location
+    classifiers=[               # Classification metadata
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',    # Specify the required Python version
+)
