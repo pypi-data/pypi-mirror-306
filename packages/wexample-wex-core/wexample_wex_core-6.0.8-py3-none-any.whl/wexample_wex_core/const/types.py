92c288c5ff23e0c34c210a160c3c6c526bb6d665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:09:59 +0000

    Add package wexample-wex-core/6.0.8/wexample_wex_core-6.0.8-py3-none-any.whl

---
+++ packages/wexample-wex-core/wexample_wex_core-6.0.8-py3-none-any.whl/wexample_wex_core/const/types.py
@@ -0,0 +1,6 @@
+from typing import Mapping
+
+from wexample_helpers.const.types import StringsList, BasicInlineValue
+
+CoreCommandArgsDict = Mapping[str, BasicInlineValue]
+CoreCommandArgsList = StringsList
