2c1be272a5386f4fcdf2292a8a9975ccaff627b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:09:49 +0000

    Add package tq42-grpc-client/1.0.234/tq42_grpc_client-1.0.234-py3-none-any.whl

---
+++ packages/tq42-grpc-client/tq42_grpc_client-1.0.234-py3-none-any.whl/com/terraquantum/license/v1alpha1/license_service_pb2.pyi
@@ -0,0 +1,5 @@
+from com.terraquantum.license.v1alpha1 import list_licenses_pb2 as _list_licenses_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
