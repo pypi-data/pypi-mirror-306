250de2aba7609a0e531b3d213aea0d5380bfb9dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:09:48 +0000

    Add package bconsole/0.0.1/bconsole-0.0.1-py3-none-any.whl

---
+++ packages/bconsole/bconsole-0.0.1-py3-none-any.whl/bconsole/__init__.py
@@ -0,0 +1,8 @@
+"""A simple module to make it a little less painful to use console applications."""
+
+from .console import Background, Console, Foreground, Modifier
+
+__author__ = "BetaKors"
+__version__ = "0.0.1"
+__license__ = "MIT"
+__all__ = ["Background", "Console", "Foreground", "Modifier"]
