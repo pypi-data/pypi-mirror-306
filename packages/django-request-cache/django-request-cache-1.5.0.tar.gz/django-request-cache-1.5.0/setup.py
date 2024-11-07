3a51350fae0c6f222c3846372d54507f417091c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 04:06:01 +0000

    Add package django-request-cache/1.5.0/django-request-cache-1.5.0.tar.gz

---
+++ packages/django-request-cache/django-request-cache-1.5.0.tar.gz/django-request-cache-1.5.0/setup.py
@@ -0,0 +1,44 @@
+import io
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    README = '\n' + f.read()
+
+setup(
+    name='django-request-cache',
+    version=os.getenv('PACKAGE_VERSION', '0.0.0').replace('refs/tags/', ''),
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='A simple Django app that provides a per-request cache.',
+    long_description=README,
+    url='https://github.com/anexia/django-request-cache',
+    author='Harald Nezbeda',
+    author_email='hnezbeda@anexia-it.com',
+    install_requires=[
+        "django_userforeignkey"
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
