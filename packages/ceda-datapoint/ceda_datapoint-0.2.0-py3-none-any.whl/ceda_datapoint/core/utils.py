7138b48c1ed0c47c875162632289d063cfb8bbfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 04:06:25 +0000

    Add package ceda-datapoint/0.2.0/ceda_datapoint-0.2.0-py3-none-any.whl

---
+++ packages/ceda-datapoint/ceda_datapoint-0.2.0-py3-none-any.whl/ceda_datapoint/core/utils.py
@@ -0,0 +1,31 @@
+__author__    = "Daniel Westwood"
+__contact__   = "daniel.westwood@stfc.ac.uk"
+__copyright__ = "Copyright 2024 United Kingdom Research and Innovation"
+
+# URLs for specific organisations stac catalogs
+# NOTE: In future this may be extended to intake catalogs depending on demand and similarity.
+urls = {
+    'CEDA':'https://api.stac.ceda.ac.uk'
+}
+
+# Will at some point become deprecated but is currently needed for CMIP6/CCI records.
+method_format = {
+    'reference_file': 'kerchunk',
+    'reference_file_2': 'CFA',
+}
+
+def generate_id():
+    import random
+    chars = [*'0123456789abcdefghijklmnopqrstuvwxyz']
+    id = ''
+    for i in range(6):
+        j = random.randint(0,len(chars)-1)
+        id += chars[j]
+
+    return f'DP:{id}'
+
+def hash_id(hash_token):
+    hash_val=0
+    for ch in hash_token:
+        hash_val = ( hash_val*281  ^ ord(ch)*997) & 0xFFFFFFFF
+    return str(hash_val)[:6]
