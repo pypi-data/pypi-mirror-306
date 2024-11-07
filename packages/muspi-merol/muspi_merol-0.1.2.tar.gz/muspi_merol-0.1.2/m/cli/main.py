aaa91f430dba26b7854b2d537b7be4c4fedd70a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:09:46 +0000

    Add package muspi-merol/0.1.2/muspi_merol-0.1.2.tar.gz

---
+++ packages/muspi-merol/muspi_merol-0.1.2.tar.gz/muspi_merol-0.1.2/m/cli/main.py
@@ -0,0 +1,36 @@
+from sys import argv
+
+from typer import Typer
+
+from ..config.load import load_config
+from ..utils.console import console
+from ..utils.register import get_commands
+
+app = Typer(help="CLI utilities for personal use", no_args_is_help=True, add_completion=False)
+
+
+# load other commands
+
+for sub_app in get_commands():
+    if sub_app.info.name:
+        app.add_typer(sub_app)
+    else:
+        app.registered_commands.extend(sub_app.registered_commands)
+
+
+# load aliases
+
+config = load_config()
+
+for alias, command in config["aliases"].items():
+
+    @app.command(name=alias, help=f"alias of {command!r}", context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
+    def _(cmd=command):
+        from shlex import split
+        from subprocess import Popen
+
+        console.print("\n [d]>>", *split(cmd), end=" ", style="violet")
+        console.print(" ".join(argv[2:]), style="yellow", end="\n\n")
+
+        code = Popen(split(cmd) + argv[2:]).wait()
+        exit(code)
