565f6bd6f609381080ab4ccbaa0d82af14f0d118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:09:48 +0000

    Add package dir-content-diff/1.11.0/dir_content_diff-1.11.0.tar.gz

---
+++ packages/dir-content-diff/dir_content_diff-1.11.0.tar.gz/dir_content_diff-1.11.0/tests/test_version.py
@@ -0,0 +1,11 @@
+"""Test the version of the ``dir-content-diff`` package."""
+
+from importlib.metadata import version
+
+import dir_content_diff
+
+
+def test_version():
+    """Test the version of the dir-content-diff package."""
+    pkg_version = version("dir-content-diff")
+    assert dir_content_diff.__version__ == pkg_version
