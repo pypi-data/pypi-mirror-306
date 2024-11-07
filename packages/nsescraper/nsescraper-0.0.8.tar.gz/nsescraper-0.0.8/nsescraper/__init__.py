5502006c265177508d16f518a94b4510df86076d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:09:25 +0000

    Add package nsescraper/0.0.8/nsescraper-0.0.8.tar.gz

---
+++ packages/nsescraper/nsescraper-0.0.8.tar.gz/nsescraper-0.0.8/nsescraper/__init__.py
@@ -0,0 +1,7 @@
+"""__init__"""
+from .nsescraper import *
+
+__version__ = '0.0.8'
+__maintainer__ = 'Ujjwal Chowdhury'
+
+__all__ = ['intraday_index','intraday_stock','historical_index','historical_stock','Stock']
