5f33ec18ec644353bbed14c751a8cca16b5b1b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 05:05:51 +0000

    Add package 3dgs-reconstruction/1.0/3dgs_reconstruction-1.0.tar.gz

---
+++ packages/3dgs-reconstruction/3dgs_reconstruction-1.0.tar.gz/3dgs_reconstruction-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import  setup
+import setuptools
+packages = ['3dgs_reconstruction']
+setup(name='3dgs_reconstruction',
+	version='1.0',
+	author='BerryChen',
+    packages=packages,
+    package_dir={'requests': 'requests'},)
