85a81c8f02a7c4a01586f6f1a06d39eaf5ad084c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 05:05:23 +0000

    Add package khalid-dsnd-probability/1.0/khalid_dsnd_probability-1.0.tar.gz

---
+++ packages/khalid-dsnd-probability/khalid_dsnd_probability-1.0.tar.gz/khalid_dsnd_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='khalid_dsnd_probability',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['khalid_dsnd_probability'],
+      author = 'Khalid Saaydh',
+      author_email = 'Khalidw501@gmail.com',
+      zip_safe=False)
