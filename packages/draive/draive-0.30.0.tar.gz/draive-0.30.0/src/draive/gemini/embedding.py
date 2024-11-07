9dbf46cd94f5aaf46847301e5d8a014081151b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 04:05:37 +0000

    Add package draive/0.30.0/draive-0.30.0.tar.gz

---
+++ packages/draive/draive-0.30.0.tar.gz/draive-0.30.0/src/draive/gemini/embedding.py
@@ -0,0 +1,42 @@
+from collections.abc import Sequence
+from typing import Any
+
+from haiway import ctx
+
+from draive.embedding import Embedded, TextEmbedding
+from draive.gemini.client import SHARED, GeminiClient
+from draive.gemini.config import GeminiEmbeddingConfig
+
+__all__ = [
+    "gemini_text_embedding",
+]
+
+
+def gemini_text_embedding(
+    client: GeminiClient = SHARED,
+    /,
+) -> TextEmbedding:
+    async def gemini_embed_text(
+        values: Sequence[str],
+        **extra: Any,
+    ) -> list[Embedded[str]]:
+        config: GeminiEmbeddingConfig = ctx.state(GeminiEmbeddingConfig).updated(**extra)
+        with ctx.scope("embed_text"):
+            results: list[list[float]] = await client.embedding(
+                config=config,
+                inputs=values,
+            )
+
+            return [
+                Embedded(
+                    value=embedded[0],
+                    vector=embedded[1],
+                )
+                for embedded in zip(
+                    values,
+                    results,
+                    strict=True,
+                )
+            ]
+
+    return TextEmbedding(embed=gemini_embed_text)
---
+++ packages/draive/draive-0.30.0.tar.gz/draive-0.30.0/src/draive/openai/embedding.py
@@ -0,0 +1,42 @@
+from collections.abc import Sequence
+from typing import Any
+
+from haiway import ctx
+
+from draive.embedding import Embedded, TextEmbedding
+from draive.openai.client import SHARED, OpenAIClient
+from draive.openai.config import OpenAIEmbeddingConfig
+
+__all__ = [
+    "openai_text_embedding",
+]
+
+
+def openai_text_embedding(
+    client: OpenAIClient = SHARED,
+    /,
+) -> TextEmbedding:
+    async def openai_embed_text(
+        values: Sequence[str],
+        **extra: Any,
+    ) -> list[Embedded[str]]:
+        with ctx.scope("embed_text"):
+            config: OpenAIEmbeddingConfig = ctx.state(OpenAIEmbeddingConfig).updated(**extra)
+            results: list[list[float]] = await client.embedding(  # pyright: ignore[reportDeprecated]
+                config=config,
+                inputs=values,
+            )
+
+            return [
+                Embedded(
+                    value=embedded[0],
+                    vector=embedded[1],
+                )
+                for embedded in zip(
+                    values,
+                    results,
+                    strict=True,
+                )
+            ]
+
+    return TextEmbedding(embed=openai_embed_text)
