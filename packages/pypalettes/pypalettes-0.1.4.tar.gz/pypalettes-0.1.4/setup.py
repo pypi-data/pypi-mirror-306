cb79b20acd5d6e1659c12dc0f024d8780b421aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 05:05:37 +0000

    Add package pypalettes/0.1.4/pypalettes-0.1.4.tar.gz

---
+++ packages/pypalettes/pypalettes-0.1.4.tar.gz/pypalettes-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pypalettes",
+    version="0.1.4",
+    packages=find_packages(),
+    description="A large (+2500) collection of color maps for Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Joseph Barbier",
+    author_email="joseph.barbierdarnal@gmail.com",
+    url="https://github.com/JosephBARBIERDARNAL/pypalettes/blob/main/README.md",
+    include_package_data=True,
+    package_data={
+        "pypalettes": ["palettes.csv"],
+    },
+    install_requires=["matplotlib"],
+)
