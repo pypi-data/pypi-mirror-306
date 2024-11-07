9faef2ddcc650b532744c27004641cc607b166de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 04:06:23 +0000

    Add package arize-phoenix/5.7.0/arize_phoenix-5.7.0-py3-none-any.whl

---
+++ packages/arize-phoenix/arize_phoenix-5.7.0-py3-none-any.whl/phoenix/server/api/types/ChatCompletionSubscriptionPayload.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+import strawberry
+from strawberry.relay import GlobalID
+
+from .Experiment import Experiment
+from .Span import Span
+
+
+@strawberry.interface
+class ChatCompletionSubscriptionPayload:
+    dataset_example_id: Optional[GlobalID] = None
+
+
+@strawberry.type
+class TextChunk(ChatCompletionSubscriptionPayload):
+    content: str
+
+
+@strawberry.type
+class FunctionCallChunk(ChatCompletionSubscriptionPayload):
+    name: str
+    arguments: str
+
+
+@strawberry.type
+class ToolCallChunk(ChatCompletionSubscriptionPayload):
+    id: str
+    function: FunctionCallChunk
+
+
+@strawberry.type
+class FinishedChatCompletion(ChatCompletionSubscriptionPayload):
+    span: Span
+
+
+@strawberry.type
+class ChatCompletionSubscriptionError(ChatCompletionSubscriptionPayload):
+    message: str
+
+
+@strawberry.type
+class ChatCompletionOverDatasetSubscriptionResult(ChatCompletionSubscriptionPayload):
+    experiment: Experiment
