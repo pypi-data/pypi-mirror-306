0634e62e99c18fb40326900bec8fdf2629f47b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 05:06:15 +0000

    Add package pysidocast/1.3.1/pysidocast-1.3.1.tar.gz

---
+++ packages/pysidocast/pysidocast-1.3.1.tar.gz/pysidocast-1.3.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, Extension
+from distutils import ccompiler
+
+compiler = ccompiler.get_default_compiler()
+# print(compiler)
+
+
+args = []
+if compiler == 'msvc':
+    args = ["/O2", "/GS-", "/fp:fast", "/std:c++latest", "/Zc:strictStrings-", "/Ob3"]  # , "/Wall"
+
+
+def main():
+    setup(
+        name="pysidocast",
+        version="1.3.1",
+        description="Python Ray Caster for pygame",
+        author="Yvant2000",
+        author_email="yvant2000@gmail.com",
+        ext_modules=[
+            Extension(
+                "pysidocast",
+                ["src/casting.cpp"],
+                include_dirs=["src/"],
+                language="c++",
+                extra_compile_args=args
+            )
+        ]
+    )
+
+
+if __name__ == "__main__":
+    main()
