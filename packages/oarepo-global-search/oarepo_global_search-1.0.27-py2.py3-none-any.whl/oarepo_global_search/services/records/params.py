662bb1bced8f0440a7152626ce42b5e9c424857d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:09:58 +0000

    Add package oarepo-global-search/1.0.27/oarepo_global_search-1.0.27-py2.py3-none-any.whl

---
+++ packages/oarepo-global-search/oarepo_global_search-1.0.27-py2.py3-none-any.whl/oarepo_global_search/services/records/params.py
@@ -0,0 +1,19 @@
+from invenio_records_resources.services.records.params import ParamInterpreter
+
+
+class GlobalSearchStrParam(ParamInterpreter):
+    """Evaluate the 'q' or 'suggest' parameter."""
+
+    def apply(self, identity, search, params):
+        """Evaluate the query str on the search."""
+        if "json" in params:
+            query = params["json"]["query"]
+            aggs = params["json"]["aggs"]
+            post_filter = params["json"]["post_filter"]
+            for agg in aggs:
+                search.aggs.bucket(agg, aggs[agg])
+            search = search.query(query)
+            if post_filter != {}:
+                search = search.post_filter(post_filter)
+
+        return search
