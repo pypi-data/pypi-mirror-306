7382d8346e5b8bdffef6bb2aa9d84cb4df915c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 04:06:01 +0000

    Add package pyroll-cli/3.0.0/pyroll_cli-3.0.0-py3-none-any.whl

---
+++ packages/pyroll-cli/pyroll_cli-3.0.0-py3-none-any.whl/pyroll/cli/res/input.py
@@ -0,0 +1,47 @@
+import pyroll.core as pr
+
+# initial profile
+in_profile = pr.Profile.round(
+    diameter=30e-3,
+    temperature=1200 + 273.15,
+    strain=0,
+    material=["C45", "steel"],
+    flow_stress=40e6,
+    density=7.7e3,
+    specific_heat_capacity=465,
+    thermal_conductivity=23,
+)
+
+# pass sequence
+sequence = pr.PassSequence([
+    pr.RollPass(
+        label="Oval I",
+        roll=pr.Roll(
+            groove=pr.CircularOvalGroove(
+                r1=6e-3,
+                r2=40e-3,
+                depth=8e-3,
+            ),
+            nominal_radius=160e-3,
+            rotational_frequency=1
+        ),
+        gap=2e-3,
+    ),
+    pr.Transport(
+        label="I => II",
+        duration=1
+    ),
+    pr.RollPass(
+        label="Round II",
+        roll=pr.Roll(
+            groove=pr.RoundGroove(
+                r1=1e-3,
+                r2=12.5e-3,
+                depth=11.5e-3
+            ),
+            nominal_radius=160e-3,
+            rotational_frequency=1
+        ),
+        gap=2e-3,
+    ),
+])
