d6d676c3281a9851cbc3f01d440995b008cd904e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 04:06:09 +0000

    Add package whisperx/3.1.6/whisperx-3.1.6.tar.gz

---
+++ packages/whisperx/whisperx-3.1.6.tar.gz/whisperx-3.1.6/setup.py
@@ -0,0 +1,37 @@
+import os
+import platform
+
+import pkg_resources
+from setuptools import find_packages, setup
+
+# Чтение содержимого README файла.
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name="whisperx",
+    py_modules=["whisperx"],
+    version="3.1.6",
+    description="Time-Accurate Automatic Speech Recognition using Whisper.",
+    readme="README.md",
+    python_requires=">=3.8",
+    author="Max Bain",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/m-bain/whisperx",
+    license="MIT",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ]
+    + [f"pyannote.audio==3.1.1"],
+    entry_points={
+        "console_scripts": ["whisperx=whisperx.transcribe:cli"],
+    },
+    include_package_data=True,
+    extras_require={"dev": ["pytest"]},
+)
